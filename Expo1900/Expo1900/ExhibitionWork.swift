94c8bd23ad044a06989e6d4abd3ce41466b5f2dd
iluxsm <be9.smile@gmail.com>
2020-12-22 00:59:37 +0900

    feat: ExhibitionWork 타입 구현

---
+++ Expo1900/Expo1900/ExhibitionWork.swift
@@ -0,0 +1,8 @@
+//
+//  ExhibitionWork.swift
+//  Expo1900
+//
+//  Created by iluxsm on 2020/12/22.
+//
+
+import Foundation
--- Expo1900/Expo1900/ParsingData.swift
+++
@@ -1,8 +0,0 @@
-//
-//  ParsingData.swift
-//  Expo1900
-//
-//  Created by iluxsm on 2020/12/21.
-//
-
-import Foundation
