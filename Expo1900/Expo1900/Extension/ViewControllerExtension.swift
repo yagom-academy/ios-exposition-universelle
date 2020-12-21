250cfbbe40c64a4055aff75aaf865db61655dd10
wonhee <wonhee010@gmail.com>
2020-12-22 02:35:18 +0900

    feat: add VC Extension

---
+++ Expo1900/Expo1900/Extension/ViewControllerExtension.swift
@@ -0,0 +1,14 @@
+//
+//  ViewControllerExtension.swift
+//  Expo1900
+//
+//  Created by Wonhee on 2020/12/22.
+//
+
+import UIKit
+
+extension UIViewController {
+    func getErrorAlertController(error: Error, handler: ()?) -> UIAlertController {
+        let alertController = UIAlertController(title: "오류", message: <#T##String?#>, preferredStyle: <#T##UIAlertController.Style#>)
+    }
+}
