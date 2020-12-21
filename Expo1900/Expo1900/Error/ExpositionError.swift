784a7ab4ad2deb0f81d916a90190d1b42f48eaca
wonhee <wonhee010@gmail.com>
2020-12-22 03:54:05 +0900

    feat: add error case

--- Expo1900/Expo1900/Error/ExpositionError.swift
+++ Expo1900/Expo1900/Error/ExpositionError.swift
@@ -9,6 +9,7 @@ import Foundation
 
 enum ExpositionError: Error {
     case getExpositionData
+    case getKoreaItemsData
     case convertNumber
     case unknown
 }
@@ -18,6 +19,8 @@ extension ExpositionError: LocalizedError {
         switch self {
         case .getExpositionData:
             return "박람회 정보를 가져오는데 실패했습니다.\n잠시 후 다시 시도해주세요."
+        case .getKoreaItemsData:
+            return "한국의 출품작을 가져오는데 실피했습니다.\n잠시 후 다시 시도해주세요."
         case .convertNumber:
             return "숫자를 변환하는데 문제가 있습니다.\n잠시 후 다시 시도해주세요."
         case .unknown:
