00e97c2ca56a85a2dbdb1ceaa3bd50e1c082ff56
lina0322 <llim@student.42seoul.kr>
2020-12-21 19:52:31 +0900

    Add : TableViewController and DetailViewController

---
+++ Expo1900/Expo1900/DetailViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DetailViewController.swift
+//  Expo1900
+//
+//  Created by 임리나 on 2020/12/21.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
