999a8d587e1e9b1ef26f34a8f34da985ece7e1c6
zziro95 <opers7878@gmail.com>
2020-12-21 17:31:49 +0900

    refactor: 프로퍼티 네이밍 변경 및 UIImage 변수 생성

--- Expo1900/Expo1900/ExpoModel/KoreanEntries.swift
+++ Expo1900/Expo1900/ExpoModel/KoreanEntries.swift
@@ -1,10 +1,15 @@
 import Foundation
+import UIKit
 
 struct KoreanEntries: Codable {
     let name: String
     let imageName: String
     let shortDescriptions: String
     let descriptions: String
+    
+    var image: UIImage {
+        return UIImage(named: "imageName")!
+    }
     
     enum CodingKeys: String, CodingKey {
         case name
