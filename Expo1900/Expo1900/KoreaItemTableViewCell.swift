4cc93f9a806578bf5de941777fd26de119b9160f
wonhee <wonhee010@gmail.com>
2020-12-22 03:53:36 +0900

    feat: add korea item cell ui

--- Expo1900/Expo1900/KoreaItemTableViewCell.swift
+++ Expo1900/Expo1900/KoreaItemTableViewCell.swift
@@ -8,16 +8,7 @@
 import UIKit
 
 class KoreaItemTableViewCell: UITableViewCell {
-
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-    
+    @IBOutlet weak var itemImage: UIImageView!
+    @IBOutlet weak var itemTitle: UILabel!
+    @IBOutlet weak var itemShortDescription: UILabel!
 }
