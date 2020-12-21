d0f6503100d5e1ef41a89f8ae280a25ecb2c37fe
Hojoon Kim <elddy0948@hotmail.co.kr>
2020-12-22 04:05:08 +0900

    feat : ExpositionPosterScrollView의 view와 init 구현
    StackView와 init구현

--- Expo1900/Expo1900/Views/ExpositionPosterScrollView.swift
+++ Expo1900/Expo1900/Views/ExpositionPosterScrollView.swift
@@ -8,5 +8,63 @@
 import UIKit
 
 class ExpositionPosterScrollView: UIScrollView {
-
+    //MARK: - Views
+    private let expositionPosterStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.alignment = .center
+        stackView.spacing = 8
+        stackView.distribution = .fill
+        return stackView
+    }()
+    
+    private let expositionTitleTextView: UITextView = {
+        let textView = UITextView()
+        textView.isEditable = false
+        textView.textAlignment = .center
+        textView.textColor = .label
+        textView.font = .boldSystemFont(ofSize: 30)
+        return textView
+    }()
+    
+    private let posterImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFit
+        imageView.clipsToBounds = true
+        return imageView
+    }()
+    
+    private let expositionInformationsTextView: UITextView = {
+        let textView = UITextView()
+        textView.isEditable = false
+        textView.textAlignment = .center
+        textView.textColor = .label
+        return textView
+    }()
+    
+    private let descriptionTextView: UITextView = {
+        let textView = UITextView()
+        textView.textColor = .label
+        textView.isScrollEnabled = false
+        textView.isEditable = false
+        textView.font = .boldSystemFont(ofSize: 20)
+        return textView
+    }()
+    
+    //MARK: - init
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.translatesAutoresizingMaskIntoConstraints = false
+        expositionPosterStackView.addArrangedSubview(expositionTitleTextView)
+        expositionPosterStackView.addArrangedSubview(posterImageView)
+        expositionPosterStackView.addArrangedSubview(expositionInformationsTextView)
+        expositionPosterStackView.addArrangedSubview(descriptionTextView)
+        self.addSubview(expositionPosterStackView)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
 }
