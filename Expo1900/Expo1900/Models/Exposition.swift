e48ca971419901cb1a82ea958fc345bc624be341
Hojoon Kim <elddy0948@hotmail.co.kr>
2020-12-21 19:16:51 +0900

    add : Exposition 타입 정의
    Exposition 타입 정의

--- Expo1900/Expo1900/Models/Exposition.swift
+++ Expo1900/Expo1900/Models/Exposition.swift
@@ -5,4 +5,16 @@
 //  Created by 김호준 on 2020/12/21.
 //
 
-import Foundation
+import UIKit
+
+struct Exposition: Codable {
+    let title: String
+    let visitors: Int
+    let location: String
+    let duration: String
+    let description: String
+    
+    var posterImage: UIImage {
+        UIImage(named: "poster") ?? UIImage()
+    }
+}
