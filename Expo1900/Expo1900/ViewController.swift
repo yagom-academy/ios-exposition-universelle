095dfcc260e5e3ce6e977158a76bcf5b3070c025
Hojoon Kim <elddy0948@hotmail.co.kr>
2020-12-22 04:22:46 +0900

    feat : View 확인을 위한 ScrollView Constraints메서드 구현 및 ScrollView 구현

--- Expo1900/Expo1900/ViewController.swift
+++ Expo1900/Expo1900/ViewController.swift
@@ -1,9 +1,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var posterInformation: Exposition?
+    var expositionPosterScrollView: ExpositionPosterScrollView?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        expositionPosterScrollView = ExpositionPosterScrollView()
+        guard let expositionPosterScrollView = expositionPosterScrollView else {
+            return
+        }
+        view.addSubview(expositionPosterScrollView)
     }
 }
 
