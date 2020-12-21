6b9ce67a68aa71ef39b65f0c1e90e8bd7138c929
wonhee <wonhee010@gmail.com>
2020-12-22 02:36:15 +0900

    feat: add init data func

--- Expo1900/Expo1900/ViewController.swift
+++ Expo1900/Expo1900/ViewController.swift
@@ -7,10 +7,34 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var posterImage: UIImageView!
+    @IBOutlet weak var visitorsLabel: UILabel!
+    @IBOutlet weak var visitSpotLabel: UILabel!
+    @IBOutlet weak var periodLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
 
+    // MARK: - data property
+    private var expositionData: Exposition? = nil
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        initExpositionData()
     }
-
+    
+    // MARK: - init data
+    private func initExpositionData() {
+        let jsonDecoder = JSONDecoder()
+        guard let expositionJsonData: NSDataAsset = NSDataAsset(name: "exposition_universelle_1900") else {
+            return
+        }
+        do {
+            self.expositionData = try jsonDecoder.decode(Exposition.self, from: expositionJsonData.data)
+        } catch {
+            print(error)
+        }
+    }
 }
 
