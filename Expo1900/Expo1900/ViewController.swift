c3060da5ea16bd6c35e59d3e2034981611eb102d
Hojoon Kim <elddy0948@hotmail.co.kr>
2020-12-22 04:25:19 +0900

    feat : JSON 데이터를 받아오는 getPosterInfo()메서드를 구현하고, 메서드 호출

--- Expo1900/Expo1900/ViewController.swift
+++ Expo1900/Expo1900/ViewController.swift
@@ -12,6 +12,31 @@ class ViewController: UIViewController {
             return
         }
         view.addSubview(expositionPosterScrollView)
+        getPosterInfo()
+        expositionPosterScrollView.configurePoster(with: posterInformation)
+        setupConstraints(scrollView: expositionPosterScrollView)
+    }
+    
+    //MARK: - Private
+    private func setupConstraints(scrollView: ExpositionPosterScrollView) {
+        var constraints = [NSLayoutConstraint]()
+        constraints.append(scrollView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor))
+        constraints.append(scrollView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor))
+        constraints.append(scrollView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor))
+        constraints.append(scrollView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor))
+        NSLayoutConstraint.activate(constraints)
+    }
+    
+    private func getPosterInfo() {
+        if let data = NSDataAsset(name: "exposition_universelle_1900")?.data {
+            let decoder = JSONDecoder()
+            do {
+                let taskData = try decoder.decode(Exposition.self, from: data)
+                posterInformation = taskData
+            } catch let error {
+                print(error)
+            }
+        }
     }
 }
 
