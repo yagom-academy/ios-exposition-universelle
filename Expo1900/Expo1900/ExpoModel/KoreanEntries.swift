7c9e0ef1547a05d3a2c8321f4bb7be51c359cd11
zziro95 <opers7878@gmail.com>
2020-12-21 16:30:50 +0900

    style: KoreanEntries 구조체의 프로퍼티 네이밍을 변경

--- Expo1900/Expo1900/ExpoModel/KoreanEntries.swift
+++ Expo1900/Expo1900/ExpoModel/KoreanEntries.swift
@@ -3,12 +3,13 @@ import Foundation
 struct KoreanEntries: Codable {
     let name: String
     let imageName: String
-    let shortDesc: String
-    let desc: String
+    let shortDescriptions: String
+    let descriptions: String
     
     enum CodingKeys: String, CodingKey {
-        case name, desc
+        case name
         case imageName = "image_name"
-        case shortDesc = "short_desc"
+        case shortDescriptions = "short_desc"
+        case descriptions = "desc"
     }
 }
