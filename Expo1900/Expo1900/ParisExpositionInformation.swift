d0348b560683032eda22e0f5edd4430b6a407cd0
yeon <jryoun1@naver.com>
2020-12-21 20:40:22 +0900

    feat: KoreaExpositionItem 구조체 타입 추가

---
+++ Expo1900/Expo1900/ParisExpositionInformation.swift
@@ -0,0 +1,15 @@
+//
+//  ExpositionInformation.swift
+//  Expo1900
+//
+//  Created by Yeon on 2020/12/21.
+//
+
+import Foundation
+struct ParisExpositionInformation: Codable {
+    let title: String
+    let visitors: Int
+    let location: String
+    let duration: String
+    let description: String
+}
