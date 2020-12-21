86f5f3ed5d48237d68736dde9dcfef07c7ca2ab3
iluxsm <be9.smile@gmail.com>
2020-12-22 01:01:37 +0900

    feat: ExhibitionWork 타입 구현

--- Expo1900/Expo1900/ExhibitionWork.swift
+++ Expo1900/Expo1900/ExhibitionWork.swift
@@ -5,4 +5,24 @@
 //  Created by iluxsm on 2020/12/22.
 //
 
-import Foundation
+import UIKit
+
+struct ExhibitionWork: Decodable {
+    private let name: String?
+    private let imageName: String?
+    private let shortDescription: String?
+    private let description: String?
+    private var image: UIImage? {
+        if let imageName = self.imageName {
+            return UIImage(named: imageName)
+        }
+        return nil
+    }
+    
+    private enum CodingKeys: String, CodingKey {
+        case name
+        case imageName = "image_name"
+        case shortDescription = "short_desc"
+        case description = "desc"
+    }
+}
