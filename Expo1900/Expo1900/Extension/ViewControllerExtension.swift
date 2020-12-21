38c2b48917c7c381c1990f613bbf6bdfbabd30e5
wonhee <wonhee010@gmail.com>
2020-12-22 03:06:11 +0900

    feat: add get error alert func

--- Expo1900/Expo1900/Extension/ViewControllerExtension.swift
+++ Expo1900/Expo1900/Extension/ViewControllerExtension.swift
@@ -8,7 +8,18 @@
 import UIKit
 
 extension UIViewController {
-    func getErrorAlertController(error: Error, handler: ()?) -> UIAlertController {
-        let alertController = UIAlertController(title: "오류", message: <#T##String?#>, preferredStyle: <#T##UIAlertController.Style#>)
+    func errorAlert(error: Error, handler: ((UIAlertAction) -> Void)?) -> UIAlertController {
+        var errorMessage: String?
+        if let expositionError = error as? ExpositionError {
+            errorMessage = expositionError.localizedDescription
+        }
+        else {
+            errorMessage = ExpositionError.unknown.localizedDescription
+        }
+        
+        let alert = UIAlertController(title: "오류", message: errorMessage, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "확인", style: .default, handler: handler)
+        alert.addAction(okAction)
+        return alert
     }
 }
