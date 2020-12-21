1a9465f71973ce6842bba694e7cf81b955157591
zziro95 <opers7878@gmail.com>
2020-12-21 11:45:01 +0900

    feat: 첫 화면 이미지 추가 및 Exposition 타입 구현

---
+++ Expo1900/Expo1900/ExpoModel/Exposition.swift
@@ -0,0 +1,17 @@
+//
+//  Exposition.swift
+//  Expo1900
+//
+//  Created by Jinho Choi on 2020/12/21.
+//
+
+import Foundation
+
+struct Exposition: Codable {
+    let title: String
+    let titleImage: String
+    let visitors: UInt
+    let location: String
+    let duration: String
+    let descriptions: String
+}
