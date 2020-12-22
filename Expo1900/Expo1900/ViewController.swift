a25a215a607c123305d83846df94a199fac32a67
zdodev <iver0945@gmail.com>
2020-12-22 18:14:45 +0900

    feat: KoreaExposition 설정

--- Expo1900/Expo1900/ViewController.swift
+++ Expo1900/Expo1900/ViewController.swift
@@ -21,12 +21,27 @@ class ViewController: UIViewController {
             return
         }
         
-        navigationController.
-        
+        setPostText(data)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.navigationBar.isHidden = true
+    }
+    
+    fileprivate func setPostText(_ data: Exposition) {
         titleLabel.text = data.title
         visitorsLabel.text = "방문객 : \(String(data.visitors)) 명"
         locationLabel.text = "개최지 : \(data.location)"
         durationLabel.text = "개최 기간 : \(data.duration)"
         descriptionLabel.text = data.description
+    }
+    
+    @IBAction func pressedButton(_ sender: UIButton) {
+        guard let koreaExpositionViewController = storyboard?.instantiateViewController(identifier: "KoreaExposition") else {
+            return
+        }
+        navigationController?.navigationBar.isHidden = false
+        navigationController?.pushViewController(koreaExpositionViewController, animated: true)
     }
 }
