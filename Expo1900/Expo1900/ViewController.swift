fc17a17611f3aa87b45e6565c4949e642af229cf
innieminnie <rkddlsl1001@gmail.com>
2020-12-22 13:15:41 +0900

    feat: UIComponents IBOutlet연결 및 expositionDescription editable 설정

--- Expo1900/Expo1900/ViewController.swift
+++ Expo1900/Expo1900/ViewController.swift
@@ -8,10 +8,19 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var expositionName: UILabel!
+    @IBOutlet weak var expositionPoster: UIImageView!
+    
+    @IBOutlet weak var numberOfVisitor: UILabel!
+    @IBOutlet weak var expositionPlace: UILabel!
+    @IBOutlet weak var expositionDuration: UILabel!
+    @IBOutlet weak var expositionDescription: UITextView!
+    
     @IBOutlet weak var nextButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationController?.isNavigationBarHidden = true
+        expositionDescription.isEditable = false
     }
 }
 
