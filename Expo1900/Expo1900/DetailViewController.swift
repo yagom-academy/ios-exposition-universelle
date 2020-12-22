9cc44ab06e0cbe430ab7610747fa427a784a3241
HakJu <gkrwnrh@gmail.com>
2020-12-23 01:44:25 +0900

    Refactor : MainViewController JSON데이터 할당

--- Expo1900/Expo1900/DetailViewController.swift
+++ Expo1900/Expo1900/DetailViewController.swift
@@ -1,8 +1,18 @@
 import UIKit
 
 class DetailViewController: UIViewController {
+    var entry: [Entry] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let jsonDecoder: JSONDecoder = JSONDecoder()
+        guard let dataAsset: NSDataAsset = NSDataAsset(name: "items") else { return }
+        
+        do{
+            self.entry = try jsonDecoder.decode([Entry].self, from: dataAsset.data)
+        } catch {
+            print(error.localizedDescription)
+        }
     }
 }
--- Expo1900/Expo1900/MainViewController.swift
+++ Expo1900/Expo1900/MainViewController.swift
@@ -1,22 +1,30 @@
 import UIKit
 
 class MainViewController: UIViewController {
-    @IBOutlet weak var textView: UITextView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var visitorLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var durationLabel: UILabel!
+    @IBOutlet weak var descriptionTextView: UITextView!
     
     var exposition: Exposition?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        textView.text = exposition?.title
-        
         let jsonDecoder: JSONDecoder = JSONDecoder()
-        guard let dataAsset: NSDataAsset = NSDataAsset(name: "exposition_universelle_1900") else { return }
+        guard let dataAsset: NSDataAsset = NSDataAsset(name: "exposition_universelle_1900") else { return print("에러") }
         
         do{
             self.exposition = try jsonDecoder.decode(Exposition.self, from: dataAsset.data)
         } catch {
             print(error.localizedDescription)
         }
+        
+        titleLabel.text = exposition?.title
+        visitorLabel.text = "방문객 : " + String(exposition!.visitors)
+        locationLabel.text = "개최지 : " + String(exposition!.location)
+        durationLabel.text = "개최기간 : " + String(exposition!.duration)
+        descriptionTextView.text = exposition?.description
     }
 }
