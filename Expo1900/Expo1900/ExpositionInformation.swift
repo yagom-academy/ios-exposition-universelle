ed23bb2b77145076b7fe0da32fa34c275ea08991
innieminnie <rkddlsl1001@gmail.com>
2020-12-22 22:46:04 +0900

    feat: ExpositionInformation에 init(from: Decoder) 메소드 추가

--- Expo1900/Expo1900/ExpositionInformation.swift
+++ Expo1900/Expo1900/ExpositionInformation.swift
@@ -8,13 +8,23 @@
 import Foundation
 
 struct ExpositionInformation: Decodable {
-    let title: String?
-    let visitors: Int?
-    let location: String?
-    let duration: String?
-    let description: String?
+    let title: String
+    let visitors: Int
+    let location: String
+    let duration: String
+    let description: String
     
     private enum CodingKeys: String, CodingKey {
         case title, visitors, location, duration, description
     }
+    
+    init(from decoder: Decoder) throws {
+        let requiredInformations = try decoder.container(keyedBy: CodingKeys.self)
+        title = (try? requiredInformations.decode(String.self, forKey: .title)) ?? ""
+        visitors = (try? requiredInformations.decode(Int.self, forKey: .visitors)) ?? 0
+        location = (try? requiredInformations.decode(String.self, forKey: .location)) ?? ""
+        duration = (try? requiredInformations.decode(String.self, forKey: .duration)) ?? ""
+        description = (try? requiredInformations.decode(String.self, forKey: .description)) ?? ""
+    }
 }
+
