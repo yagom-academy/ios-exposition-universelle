e60e9cc4e1f4b458f1c5bc75540d053f22f1e3f4
odong-Tree <ss12357@naver.com>
2020-12-22 19:52:58 +0900

    feat: CustomTableViewCell 파일 생성

---
+++ Expo1900/Expo1900/CustomTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  CustomTableViewCell.swift
+//  Expo1900
+//
+//  Created by 김동빈 on 2020/12/22.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
