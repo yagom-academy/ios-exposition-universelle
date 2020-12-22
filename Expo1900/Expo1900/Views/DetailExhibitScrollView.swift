683346dc68cb1ccc4720c1cb99f19efc4526128a
Hojoon Kim <elddy0948@hotmail.co.kr>
2020-12-22 22:08:51 +0900

    add : DetailExhibitScrollView Class 정의.

---
+++ Expo1900/Expo1900/Views/DetailExhibitScrollView.swift
@@ -0,0 +1,71 @@
+//
+//  DetailExhibitScrollView.swift
+//  Expo1900
+//
+//  Created by 김호준 on 2020/12/22.
+//
+
+import UIKit
+
+class DetailExhibitScrollView: UIScrollView {
+    
+    //MARK: - Views
+    private let exhibitInformationStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.alignment = .center
+        stackView.spacing = 8
+        stackView.distribution = .fill
+        return stackView
+    }()
+    
+    private let exhibitImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFit
+        imageView.clipsToBounds = true
+        return imageView
+    }()
+    
+    private let exhibitDescriptionTextView: UITextView = {
+        let textView = UITextView()
+        textView.textColor = .label
+        textView.isScrollEnabled = false
+        textView.isEditable = false
+        textView.font = .monospacedDigitSystemFont(ofSize: 15, weight: .regular)
+        return textView
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.translatesAutoresizingMaskIntoConstraints = false
+        exhibitInformationStackView.addArrangedSubview(exhibitImageView)
+        exhibitInformationStackView.addArrangedSubview(exhibitDescriptionTextView)
+        self.addSubview(exhibitImageView)
+        setUpConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    //MARK: - Public
+    public func configureExhibit(with exhibit: Exhibit?) {
+        guard let exhibit = exhibit else {
+            return
+        }
+        exhibitImageView.image = exhibit.exhibitImage
+        exhibitDescriptionTextView.text = exhibit.description
+    }
+    
+    //MARK: - Private
+    private func setUpConstraints() {
+        var constraints = [NSLayoutConstraint]()
+        constraints.append(exhibitInformationStackView.leadingAnchor.constraint(equalTo: self.contentLayoutGuide.leadingAnchor))
+        constraints.append(exhibitInformationStackView.trailingAnchor.constraint(equalTo: self.contentLayoutGuide.trailingAnchor))
+        constraints.append(exhibitInformationStackView.topAnchor.constraint(equalTo: self.contentLayoutGuide.topAnchor))
+        constraints.append(exhibitInformationStackView.bottomAnchor.constraint(equalTo: self.contentLayoutGuide.bottomAnchor))
+        constraints.append(exhibitInformationStackView.widthAnchor.constraint(equalTo: self.frameLayoutGuide.widthAnchor))
+        NSLayoutConstraint.activate(constraints)
+    }
+}
