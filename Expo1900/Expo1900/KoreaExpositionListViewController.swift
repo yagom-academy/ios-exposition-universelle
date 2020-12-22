c4803fbd0a5407e196515392405e9328a0322e79
yeon <jryoun1@naver.com>
2020-12-22 18:06:03 +0900

    feat: 두 번째 view인 KoreanExpositionListViewController 클래스 생성 및 navigationItem title과 navigationBar buttonButtonTitle 설정

--- Expo1900/Expo1900/KoreaExpositionListViewController.swift
+++ Expo1900/Expo1900/KoreaExpositionListViewController.swift
@@ -6,3 +6,15 @@
 //
 
 import Foundation
+import UIKit
+
+class KoreaExpositionListViewController: UIViewController {
+    @IBOutlet weak var tableView: UITableView!
+    private let decoder: JSONDecoder = JSONDecoder()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationController?.navigationBar.topItem?.backButtonTitle = "메인"
+        self.navigationItem.title = "한국의 출품작"
+    }
+}
