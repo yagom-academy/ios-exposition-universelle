6353d403734fa857a628f4a9c3da14880980a01d
Hojoon Kim <elddy0948@hotmail.co.kr>
2020-12-22 22:13:48 +0900

    feat : DetailViewController에 ScrollView를 읽어들여 ScrollView를 Configure해준다.
    DetailViewController에 ScrollView를 읽어들여 ScrollView를 Configure해준다.

--- Expo1900/Expo1900/Controllers/DetailViewController.swift
+++ Expo1900/Expo1900/Controllers/DetailViewController.swift
@@ -8,8 +8,28 @@
 import UIKit
 
 class DetailViewController: UIViewController {
+    
+    private var detailExhibitScrollView: DetailExhibitScrollView?
+    var exhibit: Exhibit?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        detailExhibitScrollView = DetailExhibitScrollView()
+        guard let detailExhibitScrollView = detailExhibitScrollView else {
+            return
+        }
+        view.addSubview(detailExhibitScrollView)
+        detailExhibitScrollView.configureExhibit(with: exhibit)
+        setupConstraints(scrollView: detailExhibitScrollView)
+    }
+    
+    //MARK: - Private
+    private func setupConstraints(scrollView: DetailExhibitScrollView) {
+        var constraints = [NSLayoutConstraint]()
+        constraints.append(scrollView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor))
+        constraints.append(scrollView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor))
+        constraints.append(scrollView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor))
+        constraints.append(scrollView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor))
+        NSLayoutConstraint.activate(constraints)
     }
 }
