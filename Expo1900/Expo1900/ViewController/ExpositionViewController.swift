34fa860c921cb3c2c709106e5f71c0108564a1f2
zziro95 <opers7878@gmail.com>
2020-12-22 14:10:25 +0900

    feat: decoding 구문 추가 및 아울렛 변수에 데이터값 적용

--- Expo1900/Expo1900/ViewController/ExpositionViewController.swift
+++ Expo1900/Expo1900/ViewController/ExpositionViewController.swift
@@ -13,5 +13,36 @@ class ExpositionViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let jsonDecoder: JSONDecoder = JSONDecoder()
+        let dataAssetName: String = "exposition_universelle_1900"
+        guard let dataAsset: NSDataAsset = NSDataAsset.init(name: dataAssetName) else { return }
+        
+        do {
+            self.expositionData = try jsonDecoder.decode(Exposition.self, from: dataAsset.data)
+        } catch DecodingError.dataCorrupted(let context) {
+            print("데이터가 손상되었거나 유효하지 않습니다.")
+            print(context.codingPath, context.debugDescription, context.underlyingError ?? "" , separator: "\n")
+        } catch DecodingError.keyNotFound(let codingkey, let context) {
+            print("주어진 키를 찾을수 없습니다.")
+            print(codingkey.intValue ?? Optional(nil)! , codingkey.stringValue , context.codingPath, context.debugDescription, context.underlyingError ?? "" , separator: "\n")
+        } catch DecodingError.typeMismatch(let type, let context) {
+            print("주어진 타입과 일치하지 않습니다.")
+            print(type.self , context.codingPath, context.debugDescription, context.underlyingError ?? "" , separator: "\n")
+        } catch DecodingError.valueNotFound(let type, let context) {
+            print("예상하지 않은 null 값이 발견되었습니다.")
+            print(type.self , context.codingPath, context.debugDescription, context.underlyingError ?? "" , separator: "\n")
+        } catch {
+            print("그외 에러가 발생했습니다.")
+        }
+        
+        guard let data = expositionData else { return }
+        
+        titleTextLabel.text = data.title
+        titleImageView.image = data.titleImage
+        visitorsTextLabel.text = "방문객 : " + String(data.visitors) + "명"
+        locationsTextLabel.text = "개최지 : " + data.location
+        durationTextLabel.text = "개최기간 : " + data.duration
+        descriptionTextView.text = data.description
     }
 }
