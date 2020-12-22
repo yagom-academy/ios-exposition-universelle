5fff3c5213d80840aef6d078f4d88025b6e64769
innieminnie <rkddlsl1001@gmail.com>
2020-12-22 13:07:49 +0900

    feat: 만국박람회 관련 화면 UI 배치

--- Expo1900/Expo1900/ViewController.swift
+++ Expo1900/Expo1900/ViewController.swift
@@ -7,12 +7,11 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var nextButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.navigationController?.isNavigationBarHidden = true
     }
-
-
 }
 
