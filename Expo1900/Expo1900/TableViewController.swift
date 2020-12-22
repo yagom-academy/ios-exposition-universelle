a165f9a964453e6317cf22d71410b40aa005c8c3
odong-Tree <ss12357@naver.com>
2020-12-22 20:04:07 +0900

    feat: 커스텀셀 구성요소 인터페이스빌더와 코드 연결

--- Expo1900/Expo1900/TableViewController.swift
+++ Expo1900/Expo1900/TableViewController.swift
@@ -3,6 +3,10 @@ import UIKit
 
 class TableViewController: UIViewController {
     
+    @IBOutlet weak var koreanItemImageView: UIImageView!
+    @IBOutlet weak var koreanItemTitleLabel: UILabel!
+    @IBOutlet weak var koreanItemShortDescriptionLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
