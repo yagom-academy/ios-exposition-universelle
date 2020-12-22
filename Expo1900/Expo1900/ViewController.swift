cc0d7a560f9929f030921ee1dfe22020212a4f3d
yeon <jryoun1@naver.com>
2020-12-22 17:44:13 +0900

    feat: 첫 화면 titleLabel을 셋팅해주는 함수 구현
    JSON파일에서 title이 줄바꿈이 없어서 이를 ( 기준으로 나눈 뒤, 다시 줄바꿈기호와 (를 붙여서 셋팅해준다.

--- Expo1900/Expo1900/ViewController.swift
+++ Expo1900/Expo1900/ViewController.swift
@@ -15,9 +15,9 @@ class ViewController: UIViewController {
     @IBOutlet weak var descriptionLabel: UILabel!
     
     override func viewWillAppear(_ animated: Bool) {
-            self.navigationController?.setNavigationBarHidden(true, animated: animated)
-            super.viewWillAppear(animated)
-        }
+        self.navigationController?.setNavigationBarHidden(true, animated: animated)
+        super.viewWillAppear(animated)
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,15 +30,25 @@ class ViewController: UIViewController {
     }
     
     private func decodeData() {
-            let jsonDecoder: JSONDecoder = JSONDecoder()
-            guard let dataAsset: NSDataAsset = NSDataAsset(name: "exposition_universelle_1900") else {
-                return
-            }
-            do {
-                let result = try jsonDecoder.decode(ParisExpositionInformation.self, from: dataAsset.data)
-            } catch {
-                print(error)
-            }
+        let jsonDecoder: JSONDecoder = JSONDecoder()
+        guard let dataAsset: NSDataAsset = NSDataAsset(name: "exposition_universelle_1900") else {
+            return
         }
+        do {
+            let result = try jsonDecoder.decode(ParisExpositionInformation.self, from: dataAsset.data)
+        } catch {
+            print(error)
+        }
+    }
+    
+    private func setTitleLabel(_ title: String) {
+        var seperatedTitle = title.components(separatedBy: "(")
+        seperatedTitle.insert("\n(", at: 1)
+        var resultTitle: String = ""
+        for text in seperatedTitle {
+            resultTitle += text
+        }
+        titleLabel.text = resultTitle
+    }
 }
 
