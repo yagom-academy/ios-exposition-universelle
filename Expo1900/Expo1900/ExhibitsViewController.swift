6e545b9a30b40f0fac90136c09142ae3b9f0f370
Hojoon Kim <elddy0948@hotmail.co.kr>
2020-12-22 21:11:57 +0900

    feat : 전시작의 데이터를 가져오는 기능 구현 getExhibitInformations()
    전시작의 데이터를 가져오는 기능 구현 getExhibitInformations()메서드를 통해 받아와
    exhibits 프로퍼티에 넣어두는 코드 구현

--- Expo1900/Expo1900/ExhibitsViewController.swift
+++ Expo1900/Expo1900/ExhibitsViewController.swift
@@ -8,8 +8,26 @@
 import UIKit
 
 class ExhibitsViewController: UIViewController {
+    
+    var exhibits: [Exhibit]?
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        getExhibitInformations()
+    }
+    
+    
+    //MARK: - Private
+    private func getExhibitInformations() {
+        if let data = NSDataAsset(name: "items")?.data {
+            let decoder = JSONDecoder()
+            do {
+                let taskData = try decoder.decode([Exhibit].self, from: data)
+                exhibits = taskData
+            } catch let error {
+                print(error)
+            }
+        }
     }
 }
