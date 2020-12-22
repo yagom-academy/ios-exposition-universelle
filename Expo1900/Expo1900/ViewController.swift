61c3c9378896aaf6543b4052c1dbaaad93496233
innieminnie <rkddlsl1001@gmail.com>
2020-12-22 22:47:34 +0900

    refactor: 메소드 decodeExpositionData, 메소드 updateUIFields 구현을 통한 viewDidLoad 과정 분리

--- Expo1900/Expo1900/ViewController.swift
+++ Expo1900/Expo1900/ViewController.swift
@@ -10,47 +10,52 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var expositionName: UILabel!
     @IBOutlet weak var expositionPoster: UIImageView!
-    
     @IBOutlet weak var numberOfVisitor: UILabel!
     @IBOutlet weak var expositionPlace: UILabel!
     @IBOutlet weak var expositionDuration: UILabel!
     @IBOutlet weak var expositionDescription: UITextView!
-    
     @IBOutlet weak var nextButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         self.navigationController?.isNavigationBarHidden = true
         expositionDescription.isEditable = false
         expositionDescription.isScrollEnabled = false
 
-        // MARK:- Decode
+        decodeExpositionData()
+    }
+    
+    private func decodeExpositionData() {
         let assetFile: String = "exposition_universelle_1900"
         guard let asset = NSDataAsset(name: assetFile) else {
             fatalError("Can not found data asset.")
         }
-        let decoder = JSONDecoder()
-        let decodedData = try? decoder.decode(ExpositionInformation.self, from: asset.data)
-
-        // Title
-        expositionName.text = decodedData?.title
-        // Image
+        
+        do {
+            let dataModel = try JSONDecoder().decode(ExpositionInformation.self, from: asset.data)
+            updateUIFields(with: dataModel)
+        } catch {
+            print("error: \(error)")
+        }
+    }
+    
+    private func updateUIFields(with model: ExpositionInformation) {
+        expositionName.text = model.title
         expositionPoster.image = #imageLiteral(resourceName: "poster")
-        // Visitor
+    
         let numberFormatter = NumberFormatter()
         numberFormatter.groupingSize = 3
         numberFormatter.numberStyle = .decimal
-        guard let visitor = numberFormatter.string(for: decodedData?.visitors) else { return }
+        guard let visitor = numberFormatter.string(for: model.visitors) else { return }
         numberOfVisitor.text = "방문객 : \(visitor) 명"
-        // Location
-        guard let location = decodedData?.location else { return }
-        expositionPlace.text = "개최지 : \(location)"
-        // Duration
-        guard let duration = decodedData?.duration else { return }
-        expositionDuration.text = "개최 기간 : \(duration)"
-        // Description
-        expositionDescription.text = decodedData?.description
+       
+        expositionPlace.text = "개최지 : \(model.location)"
+        expositionDuration.text = "개최 기간 : \(model.duration)"
+        expositionDescription.text = model.description
 
         nextButton.setTitle("한국의 출품작 보러가기", for: .normal)
     }
 }
+
 
