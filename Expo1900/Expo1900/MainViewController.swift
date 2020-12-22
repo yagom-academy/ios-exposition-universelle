eeb35e74e123f832bde7a3dc7c7a0d8c53c5ea01
HakJu <gkrwnrh@gmail.com>
2020-12-23 00:03:02 +0900

    feat : MainViewController 스토리보드 생성

--- Expo1900/Expo1900/MainViewController.swift
+++ Expo1900/Expo1900/MainViewController.swift
@@ -1,30 +1,14 @@
 import UIKit
 
-class MainViewController: UIViewController, UITableViewDataSource {
-    let cellIdentifier: String = "cell"
+class MainViewController: UIViewController {
+    @IBOutlet weak var textView: UITextView!
+    
     var exposition: Exposition?
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1 // 동적으로 표현하는 방법이 뭐가 있을까?
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: self.cellIdentifier, for: indexPath)
-        
-        let exposition: Exposition = self.exposition!
-        
-        cell.textLabel?.text = exposition.title
-        cell.imageView?.image = UIImage(named: "poster")
-        cell.detailTextLabel?.text = "방문객 : " + String(exposition.visitors)
-//        cell.detailTextLabel?.text = "개최지 : " + exposition.location
-        
-        return cell
-    }
-
-    @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        textView.text = exposition?.title
         
         let jsonDecoder: JSONDecoder = JSONDecoder()
         guard let dataAsset: NSDataAsset = NSDataAsset(name: "exposition_universelle_1900") else { return }
