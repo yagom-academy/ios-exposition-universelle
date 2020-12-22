9330283f052c0082a042699084d750e5ad6ff37b
iluxsm <be9.smile@gmail.com>
2020-12-22 20:34:52 +0900

    feat: JSON 데이터 받아와서 각 컨트롤에서 표현하도록 구현

--- Expo1900/Expo1900/ViewController.swift
+++ Expo1900/Expo1900/ViewController.swift
@@ -21,6 +21,36 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         self.navigationController?.isNavigationBarHidden = true
         expositionDescription.isEditable = false
+        expositionDescription.isScrollEnabled = false
+
+        // MARK:- Decode
+        let assetFile: String = "exposition_universelle_1900"
+        guard let asset = NSDataAsset(name: assetFile) else {
+            fatalError("Can not found data asset.")
+        }
+        let decoder = JSONDecoder()
+        let decodedData = try? decoder.decode(ExpositionInformation.self, from: asset.data)
+
+        // Title
+        expositionName.text = decodedData?.title
+        // Image
+        expositionPoster.image = #imageLiteral(resourceName: "poster")
+        // Visitor
+        let numberFormatter = NumberFormatter()
+        numberFormatter.groupingSize = 3
+        numberFormatter.numberStyle = .decimal
+        guard let visitor = numberFormatter.string(for: decodedData?.visitors) else { return }
+        numberOfVisitor.text = "방문객 : \(visitor) 명"
+        // Location
+        guard let location = decodedData?.location else { return }
+        expositionPlace.text = "개최지 : \(location)"
+        // Duration
+        guard let duration = decodedData?.duration else { return }
+        expositionDuration.text = "개최 기간 : \(duration)"
+        // Description
+        expositionDescription.text = decodedData?.description
+
+        nextButton.setTitle("한국의 출품작 보러가기", for: .normal)
     }
 }
 
