8fe24f11227d18b280e035e67f92b60b414c940c
zziro95 <opers7878@gmail.com>
2020-12-22 14:03:43 +0900

    feat: IBOutlet 선언 및 연결, Exposition 타입 변수 선언

--- Expo1900/Expo1900/ViewController/ExpositionViewController.swift
+++ Expo1900/Expo1900/ViewController/ExpositionViewController.swift
@@ -1,6 +1,15 @@
 import UIKit
 
 class ExpositionViewController: UITableViewController {
+    
+    @IBOutlet weak var titleTextLabel: UILabel!
+    @IBOutlet weak var titleImageView: UIImageView!
+    @IBOutlet weak var visitorsTextLabel: UILabel!
+    @IBOutlet weak var locationsTextLabel: UILabel!
+    @IBOutlet weak var durationTextLabel: UILabel!
+    @IBOutlet weak var descriptionTextView: UITextView!
+    
+    var expositionData: Exposition?
 
     override func viewDidLoad() {
         super.viewDidLoad()
