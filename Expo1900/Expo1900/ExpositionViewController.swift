994675d31c0dc2ed44ba8cea45d73850ddbf88a6
SM <hcooch2ch3@gmail.com>
2020-12-23 06:14:02 +0900

    feat: ExpositionViewController에서 expositionData 불러와서 View 내용 초기화하도록 구현

--- Expo1900/Expo1900/ExpositionViewController.swift
+++ Expo1900/Expo1900/ExpositionViewController.swift
@@ -9,14 +9,29 @@ import UIKit
 class ExpositionViewController: UIViewController {
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var englishTitleLabel: UILabel!
-    @IBOutlet weak var posterImageView: UIImageView!
     @IBOutlet weak var visitorsNumberLabel: UILabel!
     @IBOutlet weak var locationLabel: UILabel!
     @IBOutlet weak var durationLabel: UILabel!
     @IBOutlet weak var descriptionLabel: UILabel!
     
+    private let expositionDataModel = DataModel<Exposition>(fileName: "exposition_universelle_1900")
+    private var expositionData: Exposition?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        do {
+            self.expositionData = try expositionDataModel.requestData()
+        } catch {
+            print(error.localizedDescription)
+        }
+    
+        guard let expositionData = self.expositionData else { return }
+        titleLabel.text = expositionData.title
+        visitorsNumberLabel.text = "방문객 : \(expositionData.visitorsNumber)"
+        locationLabel.text = "개최지 : \(expositionData.location)"
+        durationLabel.text = "개최 기간 : \(expositionData.duration)"
+        descriptionLabel.text = expositionData.description
     }
 }
 
