d6ba18372d5ece9e54e908f9cd0c696e05376bb8
innieminnie <rkddlsl1001@gmail.com>
2020-12-23 12:47:39 +0900

    feat: ExhibitionWorkTableViewCell 추가

--- Expo1900/Expo1900/ExhibitionWorkTableViewCell.swift
+++ Expo1900/Expo1900/ExhibitionWorkTableViewCell.swift
@@ -8,16 +8,16 @@
 import UIKit
 
 class ExhibitionWorkTableViewCell: UITableViewCell {
-
+    @IBOutlet weak var workImage: UIImageView!
+    @IBOutlet weak var workName: UILabel!
+    @IBOutlet weak var workShortDescription: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
     }
 
 }
