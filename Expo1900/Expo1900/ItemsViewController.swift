380318d9032736e9e53d27101278964883d7b1ff
SM <hcooch2ch3@gmail.com>
2020-12-23 15:57:55 +0900

    feat: ItemsViewController에 TableView 추가

--- Expo1900/Expo1900/ItemsViewController.swift
+++ Expo1900/Expo1900/ItemsViewController.swift
@@ -8,22 +8,11 @@
 import UIKit
 
 class ItemsViewController: UIViewController {
-
+    @IBOutlet weak var itemsTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         self.navigationController?.isNavigationBarHidden = false
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
