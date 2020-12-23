16b3cf494c622090764899aa41a1aab6aabe247c
innieminnie <rkddlsl1001@gmail.com>
2020-12-23 22:04:25 +0900

    feat: 화면전환 시 데이터 이동을 위한 prepare, performSegue 메소드 구현

--- Expo1900/Expo1900/ExhibitionWorksListViewController.swift
+++ Expo1900/Expo1900/ExhibitionWorksListViewController.swift
@@ -8,15 +8,27 @@
 import UIKit
 
 class ExhibitionWorksListViewController: UIViewController {
-
+    
     private var exhibitionWorks = [ExhibitionWork]()
     
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let workDetailViewController = segue.destination as? WorkDetailViewController else {
+            return
+        }
+        
+        if let index = sender as? Int {
+            workDetailViewController.workTitle = exhibitionWorks[index].name
+            workDetailViewController.workImageData = exhibitionWorks[index].image ?? UIImage()
+            workDetailViewController.workDescriptionContents = exhibitionWorks[index].description
+            workDetailViewController.navigationItem.title = workDetailViewController.workTitle
+        }
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationController?.isNavigationBarHidden = false
         self.navigationController?.navigationBar.backItem?.title = "메인"
         self.navigationController?.navigationBar.topItem?.title = "한국의 출품작"
-        
         decodeExhibitionData()
     }
     
@@ -34,7 +46,9 @@ class ExhibitionWorksListViewController: UIViewController {
     }
 }
 extension ExhibitionWorksListViewController: UITableViewDelegate {
-    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "showWorkDetailView", sender: indexPath.row)
+    }
 }
 extension ExhibitionWorksListViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -49,7 +63,7 @@ extension ExhibitionWorksListViewController: UITableViewDataSource {
         cell.workImage.image = exhibitionWorks[indexPath.row].image
         cell.workName.text = exhibitionWorks[indexPath.row].name
         cell.workShortDescription.text = exhibitionWorks[indexPath.row].shortDescription
-
+        
         return cell
     }
 }
