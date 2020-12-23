f8c8a4e636e28c1690fe571973b0313045e7b3d2
zdodev <iver0945@gmail.com>
2020-12-23 13:50:48 +0900

    refactor: KoreaExpositionTableViewController 이름 변경

--- Expo1900/Expo1900/KoreaExpositionTableViewController.swift
+++ Expo1900/Expo1900/KoreaExpositionTableViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class KoreaExposition: UITableViewController {
+class KoreaExpositionTableViewController: UITableViewController {
     var koreaItemList: [KoreaItem]!
     
     override func viewDidLoad() {
