cea0ba30bc8d92479eea6bc472fb06abbd4ae7f2
innieminnie <rkddlsl1001@gmail.com>
2020-12-23 22:05:41 +0900

    feat: 3번째 화면에 대응하는 뷰컨트롤러 WorkDetailViewController생성 및 프로퍼티 설정

--- Expo1900/Expo1900/WorkDetailViewController.swift
+++ Expo1900/Expo1900/WorkDetailViewController.swift
@@ -8,22 +8,18 @@
 import UIKit
 
 class WorkDetailViewController: UIViewController {
-
+    @IBOutlet weak var workImage: UIImageView!
+    @IBOutlet weak var workDescription: UITextView!
+    
+    var workTitle = String()
+    var workImageData = UIImage()
+    var workDescriptionContents = String()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        workDescription.isEditable = false
+        
+        workImage.image = workImageData
+        workDescription.text = workDescriptionContents
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
