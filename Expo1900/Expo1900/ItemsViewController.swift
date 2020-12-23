a153865d361a4913a6d1f259a3860a2e988c521c
SM <hcooch2ch3@gmail.com>
2020-12-23 16:31:53 +0900

    feat: ItemsViewController의 itemsDataModel에서 데이터 불러오도록 하고 테이블뷰로 데이터 나타나도록 구현

--- Expo1900/Expo1900/ItemsViewController.swift
+++ Expo1900/Expo1900/ItemsViewController.swift
@@ -10,9 +10,39 @@ import UIKit
 class ItemsViewController: UIViewController {
     @IBOutlet weak var itemsTableView: UITableView!
     
+    private let cellIdentifier = "item"
+    private let itemsDataModel = DataModel<[Item]>(fileName: "items")
+    private var items: [Item]?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         self.navigationController?.isNavigationBarHidden = false
+        
+        do {
+            self.items = try itemsDataModel.requestData()
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
+}
+
+extension ItemsViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        guard let itemsCount = self.items?.count else { return 0 }
+        return itemsCount
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let itemsTableViewCell = itemsTableView.dequeueReusableCell(withIdentifier: self.cellIdentifier, for: indexPath) as? ItemsTableViewCell else { return UITableViewCell() }
+        
+        guard let item = self.items?[indexPath.row] else { return UITableViewCell() }
+        let itemImage = UIImage(named: item.imageName)
+        
+        itemsTableViewCell.nameLabel.text = item.name
+        itemsTableViewCell.shortDescriptionLabel.text = item.shortDescription
+        itemsTableViewCell.itemImageView.image = itemImage
+        
+        return itemsTableViewCell
     }
 }
