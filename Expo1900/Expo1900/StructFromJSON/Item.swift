5a1cee0d6e1ef75b00c4c2b732fd216d224a23cb
KyungminLee <kyungmin.lee.dev@gmail.com>
2020-12-24 03:17:01 +0900

    Feat : Item 구조체 추가, JSON 대응 프로퍼티 추가

---
+++ Expo1900/Expo1900/StructFromJSON/Item.swift
@@ -0,0 +1,15 @@
+//
+//  Item.swift
+//  Expo1900
+//
+//  Created by Kyungmin Lee on 2020/12/24.
+//
+
+import Foundation
+
+struct Item: Codable {
+    let name: String
+    let imageName: String
+    let shortDescription: String
+    let description: String
+}
