d7433b0b06dc862edca497226eeddb4b5003ec3e
KyungminLee <kyungmin.lee.dev@gmail.com>
2020-12-24 03:24:17 +0900

    Feat: CodingKeys 추가하여 JSON데이터에 대응하는 프로퍼티를 Swift 네이밍 컨벤션에 맞게 사용할 수 있도록 한다

--- Expo1900/Expo1900/StructFromJSON/Item.swift
+++ Expo1900/Expo1900/StructFromJSON/Item.swift
@@ -12,4 +12,11 @@ struct Item: Codable {
     let imageName: String
     let shortDescription: String
     let description: String
+    
+    enum CodingKeys: String, CodingKey {
+        case name
+        case imageName = "image_name"
+        case shortDescription = "short_desc"
+        case description = "desc"
+    }
 }
