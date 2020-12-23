4136292e2c67f16c03b4bd560fbb86f25fe9ce7a
innieminnie <rkddlsl1001@gmail.com>
2020-12-23 10:10:59 +0900

    feat: ExhibitionWorksListViewController 생성 및 TableView관련 프로토콜채택

--- Expo1900/Expo1900/ExhibitionWorksListViewController.swift
+++ Expo1900/Expo1900/ExhibitionWorksListViewController.swift
@@ -8,22 +8,28 @@
 import UIKit
 
 class ExhibitionWorksListViewController: UIViewController {
+    @IBOutlet weak var workListTableView: UITableView!
 
+    var tempArray = [1,2,3,4,5]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        self.navigationController?.isNavigationBarHidden = false
+    }
+}
+extension ExhibitionWorksListViewController: UITableViewDelegate {
+    
+}
+extension ExhibitionWorksListViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.tempArray.count
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell") else {
+            fatalError("error발생")
+        }
+        cell.textLabel?.text = "hi"
+        return cell
     }
-    */
-
 }
