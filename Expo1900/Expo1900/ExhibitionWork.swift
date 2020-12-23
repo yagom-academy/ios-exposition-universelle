4433292dc599ab70f9cda7e00231f5e1bc9b2b0d
innieminnie <rkddlsl1001@gmail.com>
2020-12-23 10:46:00 +0900

    feat: 구조체 ExhibitionWork의 init() 메소드 추가

--- Expo1900/Expo1900/ExhibitionWork.swift
+++ Expo1900/Expo1900/ExhibitionWork.swift
@@ -8,15 +8,12 @@
 import UIKit
 
 struct ExhibitionWork: Decodable {
-    let name: String?
-    private let imageName: String?
-    let shortDescription: String?
-    let description: String?
+    let name: String
+    private let imageName: String
+    let shortDescription: String
+    let description: String
     var image: UIImage? {
-        if let imageName = self.imageName {
-            return UIImage(named: imageName)
-        }
-        return nil
+        return UIImage(named: self.imageName)
     }
     
     private enum CodingKeys: String, CodingKey {
@@ -24,6 +21,14 @@ struct ExhibitionWork: Decodable {
         case imageName = "image_name"
         case shortDescription = "short_desc"
         case description = "desc"
+    }
+    
+    init(from decoder: Decoder) throws {
+        let requiredInformations = try decoder.container(keyedBy: CodingKeys.self)
+        name = (try? requiredInformations.decode(String.self, forKey: .name)) ?? ""
+        imageName = (try? requiredInformations.decode(String.self, forKey: .imageName)) ?? ""
+        shortDescription = (try? requiredInformations.decode(String.self, forKey: .shortDescription)) ?? ""
+        description = (try? requiredInformations.decode(String.self, forKey: .description)) ?? ""
     }
     
 }
