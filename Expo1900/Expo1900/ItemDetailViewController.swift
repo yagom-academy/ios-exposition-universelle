8d3d51fafc2e645711f3ae62a87b41772db09352
SM <hcooch2ch3@gmail.com>
2020-12-23 16:43:32 +0900

    feat: ItemDetailViewController 추가

---
+++ Expo1900/Expo1900/ItemDetailViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ItemDetailViewController.swift
+//  Expo1900
+//
+//  Created by 임성민 on 2020/12/23.
+//
+
+import UIKit
+
+class ItemDetailViewController: UIViewController {    
+    @IBOutlet weak var itemImageView: UIImageView!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
