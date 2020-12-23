b59a4e6fcfee1abf6a28300ec08107d4093ae430
SM <hcooch2ch3@gmail.com>
2020-12-23 15:15:23 +0900

    feat: ItemsViewController 추가

--- Expo1900/Expo1900/ItemsViewController.swift
+++ Expo1900/Expo1900/ItemsViewController.swift
@@ -12,7 +12,7 @@ class ItemsViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        self.navigationController?.isNavigationBarHidden = false
     }
     
 
