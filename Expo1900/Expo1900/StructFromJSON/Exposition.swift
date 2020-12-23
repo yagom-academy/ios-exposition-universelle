0f54d608646789e3160368d86826f3d1ef3a7d6e
KyungminLee <kyungmin.lee.dev@gmail.com>
2020-12-24 03:04:19 +0900

    Feat : Exposition 타입을 구조체로 구현

---
+++ Expo1900/Expo1900/StructFromJSON/Exposition.swift
@@ -0,0 +1,16 @@
+//
+//  Exposition.swift
+//  Expo1900
+//
+//  Created by Kyungmin Lee on 2020/12/24.
+//
+
+import Foundation
+
+struct Exposition {
+    let title: String
+    let visitors: UInt
+    let location: String
+    let duration: String
+    let description: String
+}
