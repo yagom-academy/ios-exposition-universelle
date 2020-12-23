9e1c17b59bade6b4c8e48ab0dec000e54bab7629
sole <subuchappyv2@gmail.com>
2020-12-23 21:17:31 +0900

    feat: exposition decoding

--- Expo1900/Expo1900/ExpositionViewController.swift
+++ Expo1900/Expo1900/ExpositionViewController.swift
@@ -13,12 +13,27 @@ class ExpositionViewController: UIViewController {
     @IBOutlet weak private var locationLabel: UILabel!
     @IBOutlet weak private var durationLabel: UILabel!
     @IBOutlet weak private var descriptionLabel: UILabel!
+    
+    private var exposition: Exposition?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
 }
 
+extension ExpositionViewController {
+    
+    private func decodeExpositionData() {
+        
+        let jasonDecoder = JSONDecoder()
+        guard let assetData: NSDataAsset = NSDataAsset(name: "exposition_universelle_1990") else {
+            return
+        }
+        guard let expositionData = try? jasonDecoder.decode(Exposition.self, from: assetData.data) else {
+            return
+        }
+        exposition = expositionData
+    }
+}
