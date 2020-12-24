6e568ff7d1cd41df1c6abf676a1fc64c8244bf3b
iluxsm <be9.smile@gmail.com>
2020-12-24 09:44:18 +0900

    feat: TableView 커스텀 셀 IBOutlet 연결

--- Expo1900/Expo1900/CustomTableViewCell.swift
+++ Expo1900/Expo1900/CustomTableViewCell.swift
@@ -8,16 +8,12 @@
 import UIKit
 
 class CustomTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var thumbnail: UIImageView!
+    @IBOutlet weak var workName: UILabel!
+    @IBOutlet weak var workDescription: UILabel!
 
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-
 }
