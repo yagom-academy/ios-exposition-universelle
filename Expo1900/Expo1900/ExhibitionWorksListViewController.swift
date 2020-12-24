3a662644458ab7ad67c11124f92739d809d08c00
iluxsm <be9.smile@gmail.com>
2020-12-24 09:47:26 +0900

    feat: WorksListTableView 구현 및 WorkViewController로 데이터 전송 준비 구현

--- Expo1900/Expo1900/ExhibitionWorksListViewController.swift
+++ Expo1900/Expo1900/ExhibitionWorksListViewController.swift
@@ -9,27 +9,54 @@ import UIKit
 
 class ExhibitionWorksListViewController: UIViewController {
     @IBOutlet weak var workListTableView: UITableView!
-
-    var tempArray = [1,2,3,4,5]
+    
+    var datasets = [ExhibitionWork]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationController?.isNavigationBarHidden = false
+        self.workListTableView.rowHeight = 100
+        
+        let assetFile: String = "items"
+        guard let asset = NSDataAsset(name: assetFile) else {
+            fatalError("Can not found data asset.")
+        }
+        
+        do {
+            datasets = try JSONDecoder().decode([ExhibitionWork].self, from: asset.data)
+        } catch {
+            print("error: \(error)")
+        }
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "cellSegue" {
+            guard let vc = segue.destination as? ExhibitionWorkViewController else { return }
+            guard let sender = sender as? Int else { return }
+            vc.imageFile = datasets[sender].image
+            vc.descriptionData = datasets[sender].description
+        }
     }
 }
+
 extension ExhibitionWorksListViewController: UITableViewDelegate {
-    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "cellSegue", sender: datasets[indexPath.row])
+    }
 }
+
 extension ExhibitionWorksListViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.tempArray.count
+        return datasets.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell") else {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell") as? CustomTableViewCell else {
             fatalError("error발생")
         }
-        cell.textLabel?.text = "hi"
+        cell.thumbnail.image = datasets[indexPath.row].image
+        cell.workName.text = datasets[indexPath.row].name
+        cell.workDescription.text = datasets[indexPath.row].shortDescription
         return cell
     }
 }
