2f01058b25c8a4408ec4fb3b79bcfbbbb7481ae1
iluxsm <be9.smile@gmail.com>
2020-12-24 09:48:44 +0900

    feat: 이전 화면에서 전달받은 데이터 받을 변수 및 IBOutlet 생성

--- Expo1900/Expo1900/ExhibitionWorkViewController.swift
+++ Expo1900/Expo1900/ExhibitionWorkViewController.swift
@@ -8,22 +8,15 @@
 import UIKit
 
 class ExhibitionWorkViewController: UIViewController {
-
+    
+    @IBOutlet weak var workImage: UIImageView!
+    @IBOutlet weak var workDescription: UITextView!
+    var imageFile: UIImage?
+    var descriptionData: String?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        workImage.image = imageFile
+        workDescription.text = descriptionData
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
