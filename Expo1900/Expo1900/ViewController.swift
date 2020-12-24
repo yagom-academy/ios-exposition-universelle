46787791fb16dc80640abccf01e3d543e6ae9a1b
taetae <taetae.dev@gmail.com>
2020-12-24 19:03:55 +0900

    Add: 메인 뷰 구현
    Label, imageView 등 초기화면 구성에 필요한 요소 추가

--- Expo1900/Expo1900/ViewController.swift
+++ Expo1900/Expo1900/ViewController.swift
@@ -8,9 +8,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var visitorLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var durationLabel: UILabel!
+    @IBOutlet weak var discriptionTextView: UITextView!
+    
+    var expositionParis: ExpositionParis!
+        
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let jsonDecoder: JSONDecoder = JSONDecoder()
+        
+        guard let dataAsset = NSDataAsset(name: "exposition_universelle_1900") else {
+            return
+        }
+        do {
+            self.expositionParis = try jsonDecoder.decode(ExpositionParis.self, from: dataAsset.data)
+        } catch {
+            debugPrint("Error")
+        }
+        
+        func setTitle() {
+            let title: [String] = expositionParis.title.components(separatedBy: "(")
+            titleLabel.text = title[0] + "\n(" + title[1]
+            titleLabel.numberOfLines = 0
+        }
+        visitorLabel.text = String(expositionParis.visitors)
+        locationLabel.text = expositionParis.location
+        durationLabel.text = expositionParis.duration
+        discriptionTextView.text = expositionParis.description
+        
     }
 
 
