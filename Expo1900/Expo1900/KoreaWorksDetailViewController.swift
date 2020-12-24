c47273f91bc23810648f17626a895f2fe8804704
sole <subuchappyv2@gmail.com>
2020-12-24 09:06:38 +0900

    feat: koreaworks에서 정보받을준비

--- Expo1900/Expo1900/KoreaWorksDetailViewController.swift
+++ Expo1900/Expo1900/KoreaWorksDetailViewController.swift
@@ -8,22 +8,13 @@
 import UIKit
 
 class KoreaWorksDetailViewController: UIViewController {
+    
+    @IBOutlet weak var imgView: UIImageView!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+    var koreaWorksInfo: KoreaWorks?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
