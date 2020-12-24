467cd0bc3aad52dc75e89b4bb08b1d8c275bca00
innieminnie <rkddlsl1001@gmail.com>
2020-12-24 12:15:25 +0900

    refactor: merge로 인한 충돌 해결

--- Expo1900/Expo1900/ExhibitionWorksListViewController.swift
+++ Expo1900/Expo1900/ExhibitionWorksListViewController.swift
@@ -10,89 +10,58 @@ import UIKit
 class ExhibitionWorksListViewController: UIViewController {
 
     @IBOutlet weak var workListTableView: UITableView!    
-    var datasets = [ExhibitionWork]()
-
-    //private var exhibitionWorks = [ExhibitionWork]()
+    private var exhibitionWorks = [ExhibitionWork]()
     
-//     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-//         guard let workDetailViewController = segue.destination as? WorkDetailViewController else {
-//             return
-//         }
-        
-//         if let index = sender as? Int {
-//             workDetailViewController.workTitle = exhibitionWorks[index].name
-//             workDetailViewController.workImageData = exhibitionWorks[index].image ?? UIImage()
-//             workDetailViewController.workDescriptionContents = exhibitionWorks[index].description
-//             workDetailViewController.navigationItem.title = workDetailViewController.workTitle
-//         }
-        
-//     }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "showDetail" {
+            guard let destinationViewController = segue.destination as? ExhibitionWorkViewController else { return }
+            guard let sender = sender as? Int else { return }
+            destinationViewController.imageFile = exhibitionWorks[sender].image
+            destinationViewController.descriptionData = exhibitionWorks[sender].description
+            destinationViewController.navigationItem.title = exhibitionWorks[sender].name
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationController?.isNavigationBarHidden = false
-        self.navigationController?.navigationBar.backItem?.title = "메인"
+        self.navigationController?.navigationBar.backItem?.title = "메"
         self.navigationController?.navigationBar.topItem?.title = "한국의 출품작"
         decodeExhibitionData()
     }
     
     private func decodeExhibitionData() {
-        self.workListTableView.rowHeight = 100
-
         let assetFile: String = "items"
         guard let asset = NSDataAsset(name: assetFile) else {
             fatalError("Can not found data asset.")
         }
         
         do {
-//             exhibitionWorks = try JSONDecoder().decode([ExhibitionWork].self, from: asset.data)
-//         } catch {
-//             print("error: \(error)")
-//         }
-            datasets = try JSONDecoder().decode([ExhibitionWork].self, from: asset.data)
+            exhibitionWorks = try JSONDecoder().decode([ExhibitionWork].self, from: asset.data)
         } catch {
             print("error: \(error)")
-        }
-    }
-    
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if segue.identifier == "cellSegue" {
-            guard let vc = segue.destination as? ExhibitionWorkViewController else { return }
-            guard let sender = sender as? Int else { return }
-            vc.imageFile = datasets[sender].image
-            vc.descriptionData = datasets[sender].description
         }
     }
 }
 
 extension ExhibitionWorksListViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        performSegue(withIdentifier: "cellSegue", sender: datasets[indexPath.row])
+        performSegue(withIdentifier: "showDetail", sender: indexPath.row)
     }
 }
 
 extension ExhibitionWorksListViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return datasets.count
+        return exhibitionWorks.count
     }
-    
-//     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-//         guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as? ExhibitionWorkTableViewCell else {
-//             fatalError("error발생")
-//         }
-        
-//         cell.workImage.image = exhibitionWorks[indexPath.row].image
-//         cell.workName.text = exhibitionWorks[indexPath.row].name
-//         cell.workShortDescription.text = exhibitionWorks[indexPath.row].shortDescription
-        
-//     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell") as? CustomTableViewCell else {
             fatalError("error발생")
         }
-        cell.thumbnail.image = datasets[indexPath.row].image
-        cell.workName.text = datasets[indexPath.row].name
-        cell.workDescription.text = datasets[indexPath.row].shortDescription
+        cell.thumbnail.image = exhibitionWorks[indexPath.row].image
+        cell.workName.text = exhibitionWorks[indexPath.row].name
+        cell.workDescription.text = exhibitionWorks[indexPath.row].shortDescription
         return cell
     }
 }
