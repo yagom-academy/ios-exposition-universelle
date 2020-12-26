f6101d6096e00aa3b4c7699e685d90a02396b39b
taetae <taetae.dev@gmail.com>
2020-12-26 22:07:35 +0900

    Add: 테이블뷰 셀에 대한 Outlet 추가

---
+++ Expo1900/Expo1900/EntryTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  EntryTableViewCell.swift
+//  Expo1900
+//
+//  Created by 김태형 on 2020/12/26.
+//
+
+import UIKit
+
+class EntryTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var entryImage: UIImageView!
+    @IBOutlet weak var entryTitle: UILabel!
+    @IBOutlet weak var entryShortDescription: UILabel!
+    
+}
