e26aad186b9387d8d97c756cfc2a09adc62f65e9
taetae <taetae.dev@gmail.com>
2020-12-26 22:05:41 +0900

    Add: 메인 뷰 컨트롤러 추가

--- Expo1900/Expo1900/ViewController.swift
+++ Expo1900/Expo1900/ViewController.swift
@@ -7,7 +7,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var visitorLabel: UILabel!
     @IBOutlet weak var locationLabel: UILabel!
@@ -15,9 +15,10 @@ class ViewController: UIViewController {
     @IBOutlet weak var discriptionTextView: UITextView!
     
     var expositionParis: ExpositionParis!
-        
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         let jsonDecoder: JSONDecoder = JSONDecoder()
         
         guard let dataAsset = NSDataAsset(name: "exposition_universelle_1900") else {
@@ -29,18 +30,22 @@ class ViewController: UIViewController {
             debugPrint("Error")
         }
         
-        func setTitle() {
-            let title: [String] = expositionParis.title.components(separatedBy: "(")
-            titleLabel.text = title[0] + "\n(" + title[1]
-            titleLabel.numberOfLines = 0
-        }
+        setTitle()
+        
         visitorLabel.text = String(expositionParis.visitors)
         locationLabel.text = expositionParis.location
         durationLabel.text = expositionParis.duration
         discriptionTextView.text = expositionParis.description
         
     }
-
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        navigationController?.isNavigationBarHidden = true
+    }
+    
+    func setTitle() {
+        let title: [String] = expositionParis.title.components(separatedBy: "(")
+        titleLabel.text = title[0] + "\n(" + title[1]
+        titleLabel.numberOfLines = 0
+    }
 }
-
