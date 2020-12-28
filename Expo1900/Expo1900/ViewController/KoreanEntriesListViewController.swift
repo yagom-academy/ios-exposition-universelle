71706d8923b3daedef8438debcb5223f235ffff6
GREENOVER <humains29@gmail.com>
2020-12-29 01:50:43 +0900

    refactor: 에러 얼럿 구현

--- Expo1900/Expo1900/ViewController/KoreanEntriesListViewController.swift
+++ Expo1900/Expo1900/ViewController/KoreanEntriesListViewController.swift
@@ -24,17 +24,22 @@ class KoreanEntriesListViewController: UITableViewController {
         } catch DecodingError.dataCorrupted(let context) {
             print("데이터가 손상되었거나 유효하지 않습니다.")
             print(context.codingPath, context.debugDescription, context.underlyingError ?? "" , separator: "\n")
+            showAlertMessage("데이터가 손상되었거나 유효하지 않습니다.")
         } catch DecodingError.keyNotFound(let codingkey, let context) {
             print("주어진 키를 찾을수 없습니다.")
             print(codingkey.intValue ?? Optional(nil)! , codingkey.stringValue , context.codingPath, context.debugDescription, context.underlyingError ?? "" , separator: "\n")
+            showAlertMessage("주어진 키를 찾을수 없습니다.")
         } catch DecodingError.typeMismatch(let type, let context) {
             print("주어진 타입과 일치하지 않습니다.")
             print(type.self , context.codingPath, context.debugDescription, context.underlyingError ?? "" , separator: "\n")
+            showAlertMessage("주어진 타입과 일치하지 않습니다.")
         } catch DecodingError.valueNotFound(let type, let context) {
             print("예상하지 않은 null 값이 발견되었습니다.")
             print(type.self , context.codingPath, context.debugDescription, context.underlyingError ?? "" , separator: "\n")
+            showAlertMessage("예상하지 않은 null 값이 발견되었습니다.")
         } catch {
             print("그외 에러가 발생했습니다.")
+            showAlertMessage("그외 에러가 발생했습니다.")
         }
     }
     
@@ -73,5 +78,15 @@ class KoreanEntriesListViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return UITableView.automaticDimension
+    }
+    
+    func showAlertMessage(_ errorMessage: String) {
+        let alert = UIAlertController(title: nil, message: errorMessage, preferredStyle: UIAlertController.Style.alert)
+        
+        let okAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+        
+        alert.addAction(okAction)
+        
+        present(alert, animated: true, completion: nil)
     }
 }
