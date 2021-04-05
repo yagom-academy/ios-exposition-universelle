b542b1a1bc866fcb6bc5674cd7f101f4b8e95e23
ryan-son <ryan.son1002@gmail.com>
2021-04-05 22:43:53 +0900

    Feat: ExpoIntroduction 및 Artwork 구조체 작성
    - readme 타임라인 업데이트

---
+++ Expo1900/Expo1900/Artwork.swift
@@ -0,0 +1,15 @@
+//
+//  Artwork.swift
+//  Expo1900
+//
+//  Created by Ryan-Son on 2021/04/05.
+//
+
+import Foundation
+
+struct Artwork {
+    let name: String
+    let imageName: String
+    let shortDescription: String
+    let description: String
+}
---
+++ Expo1900/Expo1900/ExpoIntroduction.swift
@@ -0,0 +1,17 @@
+//
+//  ExpoIntroduction.swift
+//  Expo1900
+//
+//  Created by Ryan-Son on 2021/04/05.
+//
+
+import Foundation
+
+struct ExpoIntroduction {
+    let title: String
+    let visitors: Int
+    let location: String
+    let duration: String
+    let description: String
+    var artworks: [Artwork]
+}
