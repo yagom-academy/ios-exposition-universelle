14d8c16aab487755c6e1b603bd0a1b8e6b2f2dfd
Wody95 <kwu0626@gmail.com>
2021-04-05 18:55:15 +0900

    feat : Table View를 활용한 items 데이터 불러와보기

--- Expo1900/Expo1900/ViewController.swift
+++ Expo1900/Expo1900/ViewController.swift
@@ -8,11 +8,56 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    var items: [Item] = []
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        let jsonDecoder = JSONDecoder()
+        
+        guard let itemsData: NSDataAsset = NSDataAsset(name: "items") else {
+            return
+        }
+        
+        do {
+            self.items = try jsonDecoder.decode([Item].self, from: itemsData.data)
+        } catch {
+            print(error.localizedDescription)
+        }
+        self.tableView.reloadData()
     }
+}
 
+extension ViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(indexPath.description + "을 만졌어!!")
+    }
+}
 
+extension ViewController: UITableViewDataSource {
+//    func numberOfSections(in tableView: UITableView) -> Int {
+//        return 10
+//    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cellIdentifier: String = "MyCell"
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath)
+        
+        cell.textLabel?.text = items[indexPath.row].name
+        cell.detailTextLabel?.text = items[indexPath.row].shortDesc
+        
+        return cell
+    }
 }
 
