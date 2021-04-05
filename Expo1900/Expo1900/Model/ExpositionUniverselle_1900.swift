5e4f3bb8de3c02ad53026c47cd527a5edffd672c
Tak <oit5538@gamil.com>
2021-04-05 21:30:18 +0900

    feat: Model 타입 구현

---
+++ Expo1900/Expo1900/Model/ExpositionUniverselle_1900.swift
@@ -0,0 +1,32 @@
+//
+//  ExpositionUniverselle_1900.swift
+//  Expo1900
+//
+//  Created by 오인탁 on 2021/04/05.
+//
+
+import Foundation
+
+struct ExpoItem: Codable {
+    enum CodingKeys: String, CodingKey {
+        case title
+        case visitors
+        case location
+        case duration
+        case description
+    }
+    let title: String
+    let visitors: Int
+    let location: String
+    let duration: String
+    let description: String
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        title = (try? container.decode(String.self, forKey: .title)) ?? ""
+        visitors = (try? container.decode(Int.self, forKey: .visitors)) ?? 0
+        location = (try? container.decode(String.self, forKey: .location)) ?? ""
+        duration = (try? container.decode(String.self, forKey: .duration)) ?? ""
+        description = (try? container.decode(String.self, forKey: .description)) ?? ""
+    }
+}
---
+++ Expo1900/Expo1900/Model/KoreaItem.swift
@@ -0,0 +1,29 @@
+//
+//  KoreaItem.swift
+//  Expo1900
+//
+//  Created by 오인탁 on 2021/04/05.
+//
+
+import Foundation
+
+struct KoreaItems: Codable {
+    enum CodingKeys: String, CodingKey {
+        case name
+        case imageName = "image_name"
+        case shortDesc = "short_desc"
+        case desc
+    }
+    let name: String
+    let imageName: String
+    let shortDesc: String
+    let desc: String
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        name = (try? container.decode(String.self, forKey: .name)) ?? ""
+        imageName = (try? container.decode(String.self, forKey: .imageName)) ?? ""
+        shortDesc = (try? container.decode(String.self, forKey: .shortDesc)) ?? ""
+        desc = (try? container.decode(String.self, forKey: .desc)) ?? ""
+    }
+}
