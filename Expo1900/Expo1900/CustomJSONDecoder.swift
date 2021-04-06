579ae0160f7dc0498f9177605c5bdfe3e89326b4
ryan-son <ryan.son1002@gmail.com>
2021-04-06 14:34:18 +0900

    Chore: CustomJSONDecoder 타입 문서화 주석 추가
    - 향후 사용자를 위해 타입과 메서드에 문서화 주석 추가
    - 향후 프로젝트 진행 중 로직 변경이 없거나 적을 것으로 예상하여 주석 추가함.
    - 로직 변경 시 문서화 주석 수정 예정
    - 단순 메서드이므로 향후 UI 개발 시 프로토콜 초기구현으로 구현 방식 변경 가능함

--- Expo1900/Expo1900/CustomJSONDecoder.swift
+++ Expo1900/Expo1900/CustomJSONDecoder.swift
@@ -8,20 +8,21 @@
 import Foundation
 import UIKit
 
+/// Decodable 프로토콜을 준수하는 타입 T에 대해 파일 이름 입력만으로 JSON 디코딩을 도와주는 메서드를 제공하는 타입.
 class CustomJSONDecoder<T> where T: Decodable {
-    private var decodedResult: T?
-
+    /// -  JSON 파일 이름을 전달인자로 받아 지정된 타입으로 디코딩하여 결과를 반환한다.
+    /// - 전달인자에 유효하지 않은 JSON 파일을 입력할 경우 nil을 반환한다.
     public func decode(jsonFileName: String) -> T? {
+        var decodedResult: T?
         let jsonDecoder = JSONDecoder()
-
         guard let jsonData: NSDataAsset = NSDataAsset(name: jsonFileName) else { return nil }
 
         do  {
-            self.decodedResult = try jsonDecoder.decode(T.self, from: jsonData.data)
+            decodedResult = try jsonDecoder.decode(T.self, from: jsonData.data)
         } catch {
             print(error.localizedDescription)
         }
 
-        return self.decodedResult
+        return decodedResult
     }
 }
