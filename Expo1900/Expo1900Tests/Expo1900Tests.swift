e223c473f28ec5b0f08173270006d68725814a43
eunseo5355 <eunseo5355@naver.com>
2021-04-07 03:32:19 +0900

    :white_check_mark: JSON 파싱 테스트 구현
    - exposition_universelle_1900.json & items.json

---
+++ Expo1900/Expo1900Tests/Expo1900Tests.swift
@@ -0,0 +1,44 @@
+//
+//  Expo1900Tests.swift
+//  Expo1900Tests
+//
+//  Created by 배은서 on 2021/04/07.
+//
+
+import XCTest
+@testable import Expo1900
+
+class Expo1900Tests: XCTestCase {
+
+    var decoder = JSONDecoder()
+    
+    func test_expoInfo() {
+        guard let dataAsset = NSDataAsset.init(name: "exposition_universelle_1900") else {
+            XCTFail("File Not Found")
+            return
+        }
+        
+        do {
+            let expoInfo = try decoder.decode(ExpoInfo.self, from: dataAsset.data)
+            print(expoInfo)
+        } catch {
+            XCTFail("Fail Decoding")
+        }
+    }
+    
+    func test_entryWorkItem() {
+        var entryWorkItems: [EntryWorkItem]
+        guard let dataAsset = NSDataAsset.init(name: "items") else {
+            XCTFail("File Not Found")
+            return
+        }
+        
+        do {
+            entryWorkItems = try decoder.decode([EntryWorkItem].self, from: dataAsset.data)
+            print(entryWorkItems)
+        } catch {
+            XCTFail("Fail Decoding")
+        }
+    }
+
+}
