483493d2d43caa54d2b9647a4f64b7603563c29b
stevenkim18 <11882@naver.com>
2021-04-07 16:56:15 +0900

    refactor: 전시회 정보 테스트에 예외처리 코드 수정 try? -> try로 수정

--- Expo1900/Expo1900Tests/Expo1900Tests.swift
+++ Expo1900/Expo1900Tests/Expo1900Tests.swift
@@ -11,29 +11,30 @@ import XCTest
 class Expo1900Tests: XCTestCase {
     
     var items: [ExhibitedItem]!
-    var info: ExhibitionInfomation!
-    var jsonDecoder: JSONDecoder!
+//    var info: ExhibitionInfomation!
+    var jsonDecoder = JSONDecoder()
     var itemJsonData: NSDataAsset!
     var exhibitionJsonData: NSDataAsset!
     
     override func setUpWithError() throws {
         items = []
-        jsonDecoder = JSONDecoder()
+//        jsonDecoder = JSONDecoder()
         
         itemJsonData = NSDataAsset(name: "items", bundle: Bundle(for: Expo1900Tests.self))
         items = try? jsonDecoder.decode([ExhibitedItem].self, from: itemJsonData.data)
         
-        exhibitionJsonData = NSDataAsset(name: "exposition_universelle_1900", bundle: Bundle(for: Expo1900Tests.self))
-        info = try? jsonDecoder.decode(ExhibitionInfomation.self, from: exhibitionJsonData.data)
-        
+//        exhibitionJsonData = NSDataAsset(name: "exposition_universelle_1900", bundle: Bundle(for: Expo1900Tests.self))
+//        info = try? jsonDecoder.decode(ExhibitionInfomation.self, from: exhibitionJsonData.data)
+        print("setup")
         try super.setUpWithError()
     }
 
     override func tearDownWithError() throws {
         items = nil
-        info = nil
-        jsonDecoder = nil
+//        info = nil
+//        jsonDecoder = nil
         itemJsonData = nil
+        print("tearDown")
         try super.tearDownWithError()
     }
 
@@ -42,11 +43,18 @@ class Expo1900Tests: XCTestCase {
     }
     
     func testExhibitionInformation() {
-        XCTAssertEqual(info.title, "파리 만국박람회 1900(L'Exposition de Paris 1900)")
-        XCTAssertEqual(info.visitors, 48130300)
-        XCTAssertEqual(info.location, "프랑스 파리")
-        XCTAssertEqual(info.duration, "1900. 04. 14 - 1900. 11. 12")
-        XCTAssertNotNil(info.description)
-        XCTAssertTrue(info.description.contains("1900년"))
+        let exhibitionJsonData = NSDataAsset(name: "exposition_universelle_1900", bundle: Bundle(for: Expo1900Tests.self))
+        
+        do {
+            let info = try jsonDecoder.decode(ExhibitionInfomation.self, from: exhibitionJsonData!.data)
+            XCTAssertEqual(info.title, "파리 만국박람회 1900(L'Exposition de Paris 1900)")
+            XCTAssertEqual(info.visitors, 48130300)
+            XCTAssertEqual(info.location, "프랑스 파리")
+            XCTAssertEqual(info.duration, "1900. 04. 14 - 1900. 11. 12")
+            XCTAssertNotNil(info.description)
+            XCTAssertTrue(info.description.contains("1900년"))
+        } catch {
+            print("decode error")
+        }
     }
 }
