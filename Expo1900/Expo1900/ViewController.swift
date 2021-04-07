5d89c8130dc83f9e8461de868974bac3d53cf64f
Wody95 <kwu0626@gmail.com>
2021-04-07 20:07:13 +0900

    feat : 메인화면 초안 완성

--- Expo1900/Expo1900/ViewController.swift
+++ Expo1900/Expo1900/ViewController.swift
@@ -7,7 +7,33 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var visitorsLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var durationLabel: UILabel!
+    @IBOutlet weak var descLabel: UILabel!
+    @IBOutlet weak var itemsbutton: UIButton!
+    
+    var expositionUniverselles: ExpositionUniverselle = ExpositionUniverselle(title: "", visitors: 0, location: "", duration: "", desc: "")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let jsonDecoder = JSONDecoder()
+        guard let expositionUniversellesData: NSDataAsset = NSDataAsset(name: "exposition_universelle_1900") else {
+            return
+        }
+        do {
+            self.expositionUniverselles = try jsonDecoder.decode(ExpositionUniverselle.self, from: expositionUniversellesData.data)
+        } catch {
+            print(error.localizedDescription)
+        }
+        
+        titleLabel.text = expositionUniverselles.title
+        visitorsLabel.text = String(expositionUniverselles.visitors)
+        locationLabel.text = expositionUniverselles.location
+        durationLabel.text = expositionUniverselles.duration
+        descLabel.text = expositionUniverselles.desc
     }
 }
