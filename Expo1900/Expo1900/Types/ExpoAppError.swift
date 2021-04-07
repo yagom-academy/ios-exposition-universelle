8084a779bb208b9e91606ffe820894c65da5982c
ryan-son <ryan.son1002@gmail.com>
2021-04-07 14:03:17 +0900

    Feat: ExpoAppError 타입에 debugDescription 추가
    - 디버깅 시 참고할 수 있는 문구 추가
    - `CustomDebugStringConvertible` 프로토콜 활용
    - `Artwork` 타입의 `CodingKeys` 타입의 접근 제한 설정 (`private`)
    - 향후 외부에서 접근 소요 없음

--- Expo1900/Expo1900/Types/ExpoAppError.swift
+++ Expo1900/Expo1900/Types/ExpoAppError.swift
@@ -5,7 +5,16 @@
 //  Created by Ryan-Son on 2021/04/07.
 //
 
-enum ExpoAppError: Error {
+enum ExpoAppError: Error, CustomDebugStringConvertible {
   case invalidJSONFile
   case foundNil
+  
+  var debugDescription: String {
+    switch self {
+    case .invalidJSONFile:
+      return "📃 존재하지 않는 JSON 파일이에요. 파일 이름을 다시 확인해주세요!"
+    case .foundNil:
+      return "😵 이 값은 nil이에요!"
+    }
+  }
 }
