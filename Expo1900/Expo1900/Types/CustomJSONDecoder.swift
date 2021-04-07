d2ca36ded044ec4a5b48fc8b37050a50ea6c3c29
ryan-son <ryan.son1002@gmail.com>
2021-04-07 12:44:55 +0900

    Feat: `decode(jsonFileName:)` 메서드 에러 처리 방식 변경
    - JSON 파일을 찾을 수 없는 경우 에러를 던지도록 변경

--- Expo1900/Expo1900/Types/CustomJSONDecoder.swift
+++ Expo1900/Expo1900/Types/CustomJSONDecoder.swift
@@ -10,12 +10,15 @@ import UIKit
 
 /// Decodable 프로토콜을 준수하는 타입 T에 대해 파일 이름 입력만으로 JSON 디코딩을 도와주는 메서드를 제공하는 타입.
 class CustomJSONDecoder<T> where T: Decodable {
-    /// -  JSON 파일 이름을 전달인자로 받아 지정된 타입으로 디코딩하여 결과를 반환한다.
-    /// - 전달인자에 유효하지 않은 JSON 파일을 입력할 경우 nil을 반환한다.
-    public func decode(jsonFileName: String) -> T? {
+    /// JSON 파일 이름을 전달인자로 받아 지정된 타입으로 디코딩 결과를 반환한다.
+    /// - 전달인자에 유효하지 않은 JSON 파일을 입력할 경우 `ExpoAppError.invalidJSONFile` 에러를 던진다.
+    /// -  Parameter jsonFileName: JSON 파일 이름을 `String` 타입으로 작성한다.
+    public func decode(jsonFileName: String) throws -> T? {
         var decodedResult: T?
         let jsonDecoder = JSONDecoder()
-        guard let jsonData: NSDataAsset = NSDataAsset(name: jsonFileName) else { return nil }
+        guard let jsonData: NSDataAsset = NSDataAsset(name: jsonFileName) else {
+            throw ExpoAppError.invalidJSONFile
+        }
 
         do  {
             decodedResult = try jsonDecoder.decode(T.self, from: jsonData.data)
--- Expo1900/Expo1900/Types/ExpoAppError.swift
+++ Expo1900/Expo1900/Types/ExpoAppError.swift
@@ -7,6 +7,6 @@
 
 import Foundation
 
-enum ExpoAppError {
+enum ExpoAppError: Error {
     case invalidJSONFile
 }
