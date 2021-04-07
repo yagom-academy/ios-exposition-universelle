b07dd3fe1eeaab300aa78051f077f9dc5d0fa4c6
kangkyung <rudgns0508@naver.com>
2021-04-07 23:31:28 +0900

    Feat: 첫 화면 json데이터 연결
    - titleLabel, titleImageView, visitorsLabel, locationLabel, durationLabel,
      descriptionLabel에 데이터 연결

--- Expo1900/Expo1900/ViewController.swift
+++ Expo1900/Expo1900/ViewController.swift
@@ -7,12 +7,35 @@
 import UIKit
 
 class ViewController: UIViewController {
+  @IBOutlet var titleLabel: UILabel!
+  @IBOutlet var titleImageView: UIImageView!
+  @IBOutlet var visitorsLabel: UILabel!
+  @IBOutlet var locationLabel: UILabel!
+  @IBOutlet var durationLabel: UILabel!
+  @IBOutlet var descriptionLabel: UILabel!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    updateUI()
+  }
+  
+  func updateUI() {
+    let jsonFile = JsonFile(fileName: "exposition_universelle_1900")
+    do {
+      let expositionUniverselle = try jsonFile.decode(type: Exposition())
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+      titleLabel.text = expositionUniverselle.title
+      titleImageView.image = UIImage(named: "poster.jpg")
+      visitorsLabel.text = "\(expositionUniverselle.visitors)"
+      locationLabel.text = expositionUniverselle.location
+      durationLabel.text = expositionUniverselle.duration
+      descriptionLabel.text = expositionUniverselle.description
+    } catch {
+      print(error)
     }
-
-
+    
+    
+  }
 }
 
