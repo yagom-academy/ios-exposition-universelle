dbc2faf458555b15e748273457eef9ae4936bf26
blanche37 <wkddbsghk16@gmail.com>
2021-04-08 13:55:58 +0900

    test: ExpositionUniverselle1900() 추가

--- Expo1900/Expo1900Tests/Expo1900Tests.swift
+++ Expo1900/Expo1900Tests/Expo1900Tests.swift
@@ -8,5 +8,16 @@
 import XCTest
 @testable import Expo1900
 class Expo1900Tests: XCTestCase {
-
+    
+    func test_ExpositionUniverselle1900() {
+        let jsonDecoder = JSONDecoder()
+        
+        guard let data: NSDataAsset = NSDataAsset.init(name: "exposition_universelle_1900") else { return }
+        do {
+            try jsonDecoder.decode(ExpositionUniverselle1900.self, from: data.data)
+        } catch {
+            XCTFail()
+        }
+    }
+    
 }
