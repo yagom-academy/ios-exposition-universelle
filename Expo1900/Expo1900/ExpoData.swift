c6f51c6257a78416e6475ce1dded582769294e45
inwoodev <dlsdn1207@gmail.com>
2021-04-08 22:46:10 +0900

    Refactor : ViewController 메서드 추가 - parseExpoData() 추가 - parseItemsData() 추가

--- Expo1900/Expo1900/ExpoData.swift
+++ Expo1900/Expo1900/ExpoData.swift
@@ -7,19 +7,11 @@
 
 import Foundation
 
-struct Expo: Codable {
+struct Expo: Decodable {
     var title: String
     var visitors: Int
     var location: String
     var duration: String
     var description: String
-    
-    init(from decoder: Decoder) throws {
-        let container = try decoder.container(keyedBy: CodingKeys.self)
-            title = (try? container.decode(String.self, forKey: .title)) ?? ""
-            visitors = (try? container.decode(Int.self, forKey: .visitors)) ?? 0
-            location = (try? container.decode(String.self, forKey: .location)) ?? ""
-            duration = (try? container.decode(String.self, forKey: .duration)) ?? ""
-            description = (try? container.decode(String.self, forKey: .description)) ?? ""
-    }
+
 }
