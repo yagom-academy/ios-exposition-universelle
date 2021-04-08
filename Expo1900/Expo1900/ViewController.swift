ed2dfde7a749146a809d8e2a6876a75af26bff54
stevenkim18 <11882@naver.com>
2021-04-08 21:43:11 +0900

    feat: 전시회 제목, 이미지, 설명 outlet 선언 및 초기화 구현

--- Expo1900/Expo1900/ViewController.swift
+++ Expo1900/Expo1900/ViewController.swift
@@ -8,8 +8,29 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var exhibitionTitleLabel: UILabel!
+    
+    @IBOutlet weak var exhibitionPosterImage: UIImageView!
+    
+    @IBOutlet weak var exhibitionDescriptionLabel: UILabel!
+    
     override func viewDidLoad() {
-        super.viewDidLoad()        
+        super.viewDidLoad()
+        
+        initValues()
+    }
+    
+    private func initValues() {
+        let jsonDecoder = JSONDecoder()
+        
+        guard let exhibitionJsonData = NSDataAsset(name: "exposition_universelle_1900") else { return }
+        
+        guard let exhibitionInfomation = try? jsonDecoder.decode(ExhibitionInfomation.self, from: exhibitionJsonData.data) else { return }
+        
+        exhibitionTitleLabel.text = exhibitionInfomation.newlineTitle
+        exhibitionTitleLabel.textAlignment = .center
+        
+        exhibitionDescriptionLabel.text = exhibitionInfomation.description
     }
 }
 
