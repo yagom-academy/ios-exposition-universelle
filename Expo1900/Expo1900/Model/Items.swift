5de1ce75ed2c88539fb6ba0267d77dd73263ba72
summer0817 <step33@naver.com>
2021-04-08 22:38:03 +0900

    test: 모델 타입 디코딩 테스트 - nil 및 내용확인 유닛테스트 진행

--- Expo1900/Expo1900/Model/Items.swift
+++ Expo1900/Expo1900/Model/Items.swift
@@ -7,13 +7,13 @@
 
 import Foundation
 
-struct Items: Decodable{
+struct Items: Decodable {
     let name: String
     let imageName: String
     let shortDescription: String
     let description: String
     
-    private enum CodingKeys: String, CodingKey {
+    enum CodingKeys: String, CodingKey {
         case name
         case imageName = "image_name"
         case shortDescription = "short_desc"
