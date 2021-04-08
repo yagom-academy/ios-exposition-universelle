515c97730e6cc1c5cd70350806e768cf2b198490
Ian <isnn0103@gmail.com>
2021-04-08 19:38:22 +0900

    - 이 저장소를 자신의 저장소로 fork하여 프로젝트를 진행합니다
    Feat : JSON 데이터 오류 대비 코드

--- Expo1900/Expo1900/ExpoItem.swift
+++ Expo1900/Expo1900/ExpoItem.swift
@@ -7,15 +7,24 @@
 
 import Foundation
 
-struct ExpoItem: Decodable {
+class ExpoItem: Decodable {
     let name: String
     let imageName: String
     let shortDesc: String
     let desc: String
     
-    enum CodingKeys: String, CodingKey {
+    private enum CodingKeys: String, CodingKey {
         case name, desc
         case imageName = "image_name"
         case shortDesc = "short_desc"
     }
+    
+    required init(from decoder: Decoder) throws {
+        let expoItemContainer = try decoder.container(keyedBy: CodingKeys.self)
+        self.name = (try expoItemContainer.decode(String?.self, forKey: .name)) ?? ""
+        self.imageName = (try expoItemContainer.decode(String?.self, forKey: .imageName)) ?? ""
+        self.shortDesc = (try expoItemContainer.decode(String?.self, forKey: .shortDesc)) ?? ""
+        self.desc = (try expoItemContainer.decode(String?.self, forKey: .desc)) ?? ""
+    }
+    
 }
