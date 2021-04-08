69ffbe6c0a79b552dc5591760a8e337e9314b7e3
sjsj <boromir21@naver.com>
2021-04-08 21:15:26 +0900

    [Feat] 만국박람회 파싱 모델 타입 구현

---
+++ Expo1900/Expo1900/ExpoMainViewController.swift
@@ -0,0 +1,18 @@
+//
+//  Expo1900 - ViewController.swift
+//  Created by yagom. 
+//  Copyright © yagom academy. All rights reserved.
+// 
+
+import UIKit
+
+class ExpoMainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Expo1900/Expo1900/ExpoUniverselle1900.swift
@@ -0,0 +1,16 @@
+//
+//  ExpoUniverselle1900.swift
+//  Expo1900
+//
+//  Created by TORI on 2021/04/08.
+//
+
+import Foundation
+
+struct ExpoUniverselle: Decodable {
+    let title: String
+    let visitors: Int
+    let location: String
+    let duration: String
+    let description: String
+}
