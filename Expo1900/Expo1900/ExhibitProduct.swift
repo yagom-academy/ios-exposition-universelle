a7fdbe4c4ca92bb15309600312cfc3759e2fcee2
helloworldjay <sj.seungjin.baek@gmail.com>
2021-04-09 15:11:53 +0900

    [#3]feat: implement type for getting data from JSON file

---
+++ Expo1900/Expo1900/ExhibitProduct.swift
@@ -0,0 +1,16 @@
+//
+//  ExhibitProduct.swift
+//  Expo1900
+//
+//  Created by Seungjin Baek on 2021/04/09.
+//
+
+import Foundation
+
+struct ExhibitProduct: Decodable {
+    var name: String
+    var imageName: String
+    var shortDescription: String
+    var description: String
+}
+
