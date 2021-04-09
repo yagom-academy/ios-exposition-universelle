1deaefc3061a4ea176406005d1023ccd2b4f9a6c
DragonTigerXYZ <yb1229@kpu.ac.kr>
2021-04-09 16:57:54 +0900

    [Feat] #1 Expo 구조체 생성

---
+++ Expo1900/Expo1900/Model/Expo.swift
@@ -0,0 +1,16 @@
+//
+//  Expo.swift
+//  Expo1900
+//
+//  Created by YB on 2021/04/07.
+//
+
+import Foundation
+
+struct Expo: Decodable {
+    let title: String
+    let visitors: Int
+    let location: String
+    let duration: String
+    let description: String
+}
