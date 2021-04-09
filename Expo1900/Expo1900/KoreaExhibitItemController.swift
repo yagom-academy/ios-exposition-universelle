fb9f6fe9daab0d085538ac4104c9af84b2520ae0
Sookim18 <scstnghks@naver.com>
2021-04-09 15:52:09 +0900

    Feat : 네비게이션 컨트롤러 추가 - KoreaExhibitItemController : 한국의 출품작목록을 테이블뷰로 보여주는 뷰컨트롤러

---
+++ Expo1900/Expo1900/KoreaExhibitItemController.swift
@@ -0,0 +1,16 @@
+//
+//  KoreaExhibitItemController.swift
+//  Expo1900
+//
+//  Created by sookim on 2021/04/09.
+//
+
+import Foundation
+import UIKit
+
+class KoreaExhibitItemController: UIViewController {
+    override func viewDidLoad() {
+        title = "한국의 출품작"
+        
+    }
+}
