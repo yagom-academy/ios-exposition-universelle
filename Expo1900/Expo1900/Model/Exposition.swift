86d8e26229e5c6dce41b67ecda5151c6a91638f9
duckbok <duckbok22@gmail.com>
2021-04-09 14:34:24 +0900

    [Feat] #1 Exposition Type 정의

---
+++ Expo1900/Expo1900/Model/Exposition.swift
@@ -0,0 +1,16 @@
+//
+//  Exposition.swift
+//  Expo1900
+//
+//  Created by duckbok on 2021/04/09.
+//
+
+import Foundation
+
+struct Exposition: Codable {
+    let title: String
+    let visitors: String
+    let location: String
+    let duration: String
+    let description: String
+}
