9c70df22f77234c26c17a69016ebb6fce38ecdda
ryan-son <ryan.son1002@gmail.com>
2021-04-11 01:04:59 +0900

    Feat: 초기화면 Navigation Bar 숨김 설정

--- Expo1900/Expo1900/Controller/ExpoIntroductionViewController.swift
+++ Expo1900/Expo1900/Controller/ExpoIntroductionViewController.swift
@@ -36,6 +36,15 @@ class ExpoIntroductionViewController: UIViewController {
     
   }
 
+  override func viewWillAppear(_ animated: Bool) {
+    super.viewWillAppear(animated)
+    self.navigationController?.isNavigationBarHidden = true
+  }
+  
+  override func viewWillDisappear(_ animated: Bool) {
+    super.viewWillDisappear(animated)
+    self.navigationController?.isNavigationBarHidden = false
+  }
   
   
 }
