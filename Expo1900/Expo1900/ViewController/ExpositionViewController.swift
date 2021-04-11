7dd39a9cc9d5c76bfbf9b99cfabf959013148a7f
kane-young <lyw2100@naver.com>
2021-04-12 00:11:58 +0900

    Feat : Exposition 첫 화면 UI 구성

---
+++ Expo1900/Expo1900/ViewController/ExpositionViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ExpositionViewController.swift
+//  Expo1900
+//
+//  Created by 이영우 on 2021/04/12.
+//
+
+import UIKit
+
+class ExpositionViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        
+    }
+}
