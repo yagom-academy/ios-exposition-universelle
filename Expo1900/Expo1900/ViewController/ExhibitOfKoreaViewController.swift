a1b8e57e1bf5a4fbaed85822d62656863d64444c
cjm <stepsw112@gmail.com>
2021-04-11 23:24:37 +0900

    Feat: TableViewCell 레이아웃변경

--- Expo1900/Expo1900/ViewController/ExhibitOfKoreaViewController.swift
+++ Expo1900/Expo1900/ViewController/ExhibitOfKoreaViewController.swift
@@ -33,8 +33,8 @@ class ExhibitOfKoreaViewController: UIViewController {
             print(error.localizedDescription)
         }
         
-        tableView.estimatedRowHeight = 120
-        tableView.rowHeight = UITableView.automaticDimension
+//        tableView.estimatedRowHeight = UITableView.automaticDimension
+//        tableView.rowHeight = UITableView.automaticDimension
         
     }
     
