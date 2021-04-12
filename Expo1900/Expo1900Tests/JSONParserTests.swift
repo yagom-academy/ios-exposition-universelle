d6ca91e2a0b50f8e75df722cea777ef687e7ccae
duckbok <duckbok22@gmail.com>
2021-04-12 17:14:09 +0900

    [Test] #3 JSONParser의 메서드가 Result 타입을 쓰도록 테스트 변경

--- Expo1900/Expo1900Tests/JSONParserTests.swift
+++ Expo1900/Expo1900Tests/JSONParserTests.swift
@@ -10,43 +10,33 @@ import XCTest
 
 class JSONParserTests: XCTestCase {
     
-    func test_에셋카탈로그에_없는_JSON데이터_파일_이름이_들어오면_nil을_반환한다() {
-        XCTAssertNil(JSONParser<Exposition>.extractData(from: ""))
+    func test_에셋카탈로그에_없는_JSON데이터_파일_이름이_들어오면_invalidAssetName_에러를_가진다() {
+        let sut = JSONParser<Exposition>.parse("")
+            
+        XCTAssertEqual(sut, .failure(.invalidAssetName("")))
     }
     
-    func test_에셋카탈로그에_있는_JSON데이터_파일_이름이_들어오면_NSDataAsset을_반환한다() {
-        XCTAssertNotNil(JSONParser<Exposition>.extractData(from: "items"))
-    }
-    
-    func test_NSDataAsset이_단일타입의_프로퍼티와_다르면_nil을_반환한다() {
-        guard let dataAsset = JSONParser<Exposition>.extractData(from: "items") else { return }
+    func test_에셋카탈로그에_있지만_타입의_프로퍼티와_다르면_dataCorrupted_에러를_가진다() {
+        let sut = JSONParser<Exposition>.parse("items")
         
-        XCTAssertNil(JSONParser<Exposition>.convert(from: dataAsset))
+        XCTAssertEqual(sut, .failure(.dataCorrupted("items")))
     }
     
-    func test_NSDataAsset이_단일타입의_프로퍼티와_일치하면_해당_인스턴스를_반환한다() {
-        guard let dataAsset = JSONParser<Exposition>.extractData(from: "exposition_universelle_1900") else { return }
+    func test_에셋카탈로그에_있고_단일타입의_프로퍼티와_일치하면_해당_인스턴스를_가진다() {
+        let sut = JSONParser<Exposition>.parse("exposition_universelle_1900")
         
-        XCTAssertNotNil(JSONParser<Exposition>.convert(from: dataAsset))
+        XCTAssertNotNil(try sut.get())
     }
     
-    func test_NSDataAsset이_배열타입의_프로퍼티와_다르면_nil을_반환한다() {
-        guard let dataAsset = JSONParser<[Entry]>.extractData(from: "exposition_universelle_1900") else { return }
+    func test_에셋카탈로그에_있지만_배열타입의_프로퍼티와_다르면_dataCorrupted_에러를_가진다() {
+        let sut = JSONParser<[Entry]>.parse("exposition_universelle_1900")
         
-        XCTAssertNil(JSONParser<[Entry]>.convert(from: dataAsset))
+        XCTAssertEqual(sut, .failure(.dataCorrupted("exposition_universelle_1900")))
     }
     
-    func test_NSDataAsset이_배열타입의_프로퍼티와_일치하면_해당_인스턴스를_반환한다() {
-        guard let dataAsset = JSONParser<[Entry]>.extractData(from: "items") else { return }
+    func test_에셋카탈로그에_있고_배열타입의_프로퍼티와_일치하면_해당_인스턴스를_가진다() {
+        let sut = JSONParser<[Entry]>.parse("items")
         
-        XCTAssertNotNil(JSONParser<[Entry]>.convert(from: dataAsset))
-    }
-    
-    func test_JSON데이터_파일이름과_주어진_타입이_다르면_parse를_실패한다() {
-        XCTAssertNil(JSONParser<[Entry]>.parse("exposition_universelle_1900"))
-    }
-    
-    func test_JSON데이터_파일이름과_주어진_타입이_같으면_parse를_성공한다() {
-        XCTAssertNotNil(JSONParser<[Entry]>.parse("items"))
+        XCTAssertNotNil(try sut.get())
     }
 }
