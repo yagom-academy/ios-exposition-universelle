347ca46a0a968cd2a32f65110d216741f8e53e7e
eunseo5355 <eunseo5355@naver.com>
2021-04-13 01:14:02 +0900

    ✨ Expo 정보를 나타내는 ViewController 구현
    - JSON 파싱하는 loadJsonData() 구현
    - 파싱된 데이터로 UI 업데이트 시켜주는 updateUI() 구현
    - Navigation Bar 숨기는 기능 추가
    - 한국 출품작 보러가기 버튼을 누르면 화면 전환하는 touchUpKoreaEntryWorkButton() 구현

---
+++ Expo1900/Expo1900/ExpoViewController.swift
@@ -0,0 +1,54 @@
+//
+//  Expo1900 - ExpoViewController.swift
+//  Created by yagom. 
+//  Copyright © yagom academy. All rights reserved.
+// 
+
+import UIKit
+
+final class ExpoViewController: UIViewController {
+
+    @IBOutlet private weak var expoTitleLabel: UILabel!
+    @IBOutlet private weak var visitorsLabel: UILabel!
+    @IBOutlet private weak var locationLabel: UILabel!
+    @IBOutlet private weak var durationLabel: UILabel!
+    @IBOutlet private weak var descriptionLabel: UILabel!
+    @IBOutlet private weak var KoreaEntryWorkButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        updateUI()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        self.navigationController?.isNavigationBarHidden = true
+    }
+    
+    func loadJsonData() -> ExpoInfo? {
+        let decoder = JSONDecoder()
+        guard let dataAsset = NSDataAsset.init(name: "exposition_universelle_1900") else { return nil }
+        
+        do {
+            return try decoder.decode(ExpoInfo.self, from: dataAsset.data)
+        } catch let error as NSError {
+            print(error.localizedDescription)
+            return nil
+        }
+    }
+    
+    func updateUI() {
+        let expoInformation = loadJsonData()
+        expoTitleLabel.text = expoInformation?.title
+        locationLabel.text = expoInformation?.location
+        durationLabel.text = expoInformation?.duration
+        descriptionLabel.text = expoInformation?.description
+        guard let visitors = expoInformation?.visitors else { return }
+        visitorsLabel.text = String(visitors) + "명"
+    }
+    
+    @IBAction func touchUpKoreaEntryWorkButton() {
+        guard let koreaEntryWorkViewController = self.storyboard?.instantiateViewController(identifier: "entryWorkVC") as? EntryWorkViewController else { return }
+        self.navigationController?.pushViewController(koreaEntryWorkViewController, animated: false)
+    }
+}
+
