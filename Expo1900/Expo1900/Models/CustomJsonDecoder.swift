28636cdaee9fb1a5672a919e4cee2c53e9d2a4fb
Wody95 <kwu0626@gmail.com>
2021-04-12 22:52:29 +0900

    refactor : jsonParser 메서드를 CustomJsonDecode 모델로 변경

---
+++ Expo1900/Expo1900/Models/CustomJsonDecoder.swift
@@ -0,0 +1,8 @@
+//
+//  CustomJsonDecoder.swift
+//  Expo1900
+//
+//  Created by 기원우 on 2021/04/12.
+//
+
+import Foundation
---
+++ Expo1900/Expo1900/Models/ExpoError.swift
@@ -0,0 +1,12 @@
+//
+//  ErrorType.swift
+//  Expo1900
+//
+//  Created by 기원우 on 2021/04/12.
+//
+
+import Foundation
+
+enum Error: Error {
+    case <#case#>
+}
