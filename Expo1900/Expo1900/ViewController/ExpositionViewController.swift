4d0b3570971b20ed3845260aa9a4f554b197c84a
kane-young <lyw2100@naver.com>
2021-04-12 13:50:44 +0900

    Feat : Add IBOutlet variation

--- Expo1900/Expo1900/ViewController/ExpositionViewController.swift
+++ Expo1900/Expo1900/ViewController/ExpositionViewController.swift
@@ -8,6 +8,16 @@
 import UIKit
 
 class ExpositionViewController: UIViewController {
+
+    @IBOutlet private weak var titleLabel: UILabel!
+    @IBOutlet private weak var visitorsLabel: UILabel!
+    @IBOutlet private weak var posterImageView: UIImageView!
+    @IBOutlet private weak var locationLabel: UILabel!
+    @IBOutlet private weak var durationLabel: UILabel!
+    @IBOutlet private weak var descriptionLabel: UILabel!
+    @IBOutlet private weak var itemsListPageButton: UIButton!
+    @IBOutlet private weak var leftOnButtonImageView: UIImageView!
+    @IBOutlet private weak var rightOnButtonImageView: UIImageView!
     
     override func viewDidLoad() {
         
