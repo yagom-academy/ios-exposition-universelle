0291c225fdf262e4d3db13dac6c5d79bfc3d0d64
kane-young <lyw2100@naver.com>
2021-04-12 15:00:40 +0900

    Feat : ListTableView 사용할 Cell 생성

---
+++ Expo1900/Expo1900/View/KoreanEntryTableViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  KoreanEntryTableViewCell.swift
+//  Expo1900
+//
+//  Created by 이영우 on 2021/04/12.
+//
+
+import UIKit
+
+class KoreanEntryTableViewCell: UITableViewCell {
+    @IBOutlet weak var itemShortDescLabel: UILabel!
+    @IBOutlet weak var itemTitleLabel: UILabel!
+    @IBOutlet weak var itemImageView: UIImageView!
+    
+    func setCell(_ data: Item) {
+        itemTitleLabel.text = data.name
+        itemImageView.image = UIImage(named: data.imageName)
+        itemShortDescLabel.text = data.shortDesc
+    }
+}
