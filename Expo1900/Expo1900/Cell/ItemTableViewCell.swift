7a8891078b75bde509f2c3b662e9458c11611009
stevenkim18 <11882@naver.com>
2021-04-12 20:20:26 +0900

    docs: 앱 화면 gif 파일 추가

--- Expo1900/Expo1900/Cell/ItemTableViewCell.swift
+++ Expo1900/Expo1900/Cell/ItemTableViewCell.swift
@@ -12,15 +12,12 @@ class ItemTableViewCell: UITableViewCell {
     @IBOutlet weak var itemImage: UIImageView!
     @IBOutlet weak var itemTitle: UILabel!
     @IBOutlet weak var itemShortDescription: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
     }
-    
 }
