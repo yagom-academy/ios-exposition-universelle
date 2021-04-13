0c9c3e1275839b4240c4e3baf69f505ce4fbd939
jaemuyeo <jemking94@gmail.com>
2021-04-14 03:10:59 +0900

    Feat. Main화면 UI구성, 아웃렛 연결

--- Expo1900/Expo1900/Controller/MainViewController.swift
+++ Expo1900/Expo1900/Controller/MainViewController.swift
@@ -7,6 +7,12 @@
 import UIKit
 
 class MainViewController: UIViewController {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var visitorsLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var durationLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
