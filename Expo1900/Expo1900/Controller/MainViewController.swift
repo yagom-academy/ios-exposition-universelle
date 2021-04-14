5175f38495539918fea5a6d08e0bfc902eaad439
jaemuyeo <jemking94@gmail.com>
2021-04-15 03:11:38 +0900

    Feat. exposition UI적용

--- Expo1900/Expo1900/Controller/MainViewController.swift
+++ Expo1900/Expo1900/Controller/MainViewController.swift
@@ -19,6 +19,7 @@ class MainViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         decodeData(assetName: ExpositionContents.dataAssetName)
+        expositionInformation()
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -41,12 +42,36 @@ class MainViewController: UIViewController {
             print(ErrorData.dataDecodingError.errorMassege)
         }
     }
+    
+    private func expositionInformation() {
+        guard let exposition = exposition else { return }
+        let visitorNumber = numberThousandUnitComma(to: exposition.visitors)
+        let titleText = titleTextLineBreak(to: exposition.title)
+        titleLabel.text = titleText
+        visitorsLabel.text = ExpositionContents.visitorsText + visitorNumber + ExpositionContents.personsText
+        locationLabel.text = ExpositionContents.locationText + exposition.location
+        durationLabel.text = ExpositionContents.durationText + exposition.duration
+        descriptionLabel.text = exposition.description
+    }
+    
+    private func numberThousandUnitComma(to number: Int) -> String {
+        let numberFormatter = NumberFormatter()
+        numberFormatter.numberStyle = .decimal
+        guard let text = numberFormatter.string(from: NSNumber(value: number)) else {
+            return String(number)
+        }
+        return text
+    }
+    
+    private func titleTextLineBreak(to title: String) -> String {
+        return title.replacingOccurrences(of: "(", with: "\n(")
+    }
 }
 
 extension MainViewController {
     struct ExpositionContents {
         static let dataAssetName = "exposition_universelle_1900"
-        static let visitoersText = "방문객 : "
+        static let visitorsText = "방문객 : "
         static let personsText = " 명"
         static let locationText = "개최지 : "
         static let durationText = "개최 기간 : "
