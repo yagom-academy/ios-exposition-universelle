4a59f76365647d7a29b614675a70173281bf7057
jaemuyeo <jemking94@gmail.com>
2021-04-15 00:47:08 +0900

    Feat. Exposition 디코딩, Text Contents 정의

--- Expo1900/Expo1900/Controller/MainViewController.swift
+++ Expo1900/Expo1900/Controller/MainViewController.swift
@@ -13,10 +13,12 @@ class MainViewController: UIViewController {
     @IBOutlet weak var locationLabel: UILabel!
     @IBOutlet weak var durationLabel: UILabel!
     @IBOutlet weak var descriptionLabel: UILabel!
+    
+    var exposition: Exposition?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        decodeData(assetName: ExpositionContents.dataAssetName)
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -29,5 +31,25 @@ class MainViewController: UIViewController {
         self.navigationController?.isNavigationBarHidden = false
     }
     
+    private func decodeData(assetName: String) {
+        let expositionDecoder = JSONDecoder()
+        guard let assetData = NSDataAsset(name: assetName) else { return }
+        
+        do {
+            exposition = try expositionDecoder.decode(Exposition.self, from: assetData.data)
+        } catch {
+            print(ErrorData.dataDecodingError.errorMassege)
+        }
+    }
+}
+
+extension MainViewController {
+    struct ExpositionContents {
+        static let dataAssetName = "exposition_universelle_1900"
+        static let visitoersText = "방문객 : "
+        static let personsText = " 명"
+        static let locationText = "개최지 : "
+        static let durationText = "개최 기간 : "
+    }
 }
 
