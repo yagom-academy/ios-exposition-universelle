86b731ca6f569b95de8217f28079ae62a259e035
sjsj <boromir21@naver.com>
2021-04-14 14:27:52 +0900

    [Feat] 커스텀 스택뷰 생성 커스텀 스택뷰 안에 ExpoMainViewController에 필요한 모든 오브젝트들을 코드로 선언

---
+++ Expo1900/Expo1900/ExpoMainStackView.swift
@@ -0,0 +1,30 @@
+//
+//  ExpoMainStackView.swift
+//  Expo1900
+//
+//  Created by TORI on 2021/04/14.
+//
+
+import UIKit
+
+class ExpoMainStackView: UIStackView {
+
+    let mainStackView = UIStackView()
+    let visitorsStackView = UIStackView()
+    let locationStackView = UIStackView()
+    let durationStackView = UIStackView()
+    let itemsListStackView = UIStackView()
+
+    let expoTitle = UILabel()
+    let expoPoster = UIImageView()
+    let visitorsTitle = UILabel()
+    let visitorsText = UILabel()
+    let locationTitle = UILabel()
+    let locationText = UILabel()
+    let durationTitle = UILabel()
+    let durationText = UILabel()
+    let expoDescription = UILabel()
+    let itemsListLeftImage = UIImageView()
+    let itemsList = UIButton(type: .system)
+    let itemsListRightImage = UIImageView()
+}
