ebba151bad7c4c8a92260074828023f4b9609211
summer0817 <step33@naver.com>
2021-04-14 11:39:53 +0900

    Feat: 메인화면 제목 줄바꿈 - 특정 문자를 변경하여 제목 줄바꿈 구현

--- Expo1900/Expo1900/ViewController/ExpositonViewController.swift
+++ Expo1900/Expo1900/ViewController/ExpositonViewController.swift
@@ -7,6 +7,7 @@
 import UIKit
 
 class ExpositionViewController: UIViewController {
+    
     @IBOutlet weak var expositionTitle: UILabel!
     @IBOutlet weak var expositionVisitor: UILabel!
     @IBOutlet weak var expositionLocation: UILabel!
@@ -25,11 +26,11 @@ class ExpositionViewController: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         navigationController?.setNavigationBarHidden(true, animated: animated) }
-
+    
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         navigationController?.setNavigationBarHidden(false, animated: animated) }
-
+    
     func decodeExposition() throws {
         let jsonDecoder: JSONDecoder = JSONDecoder()
         guard let dataAsset: NSDataAsset = NSDataAsset.init(name: "exposition_universelle_1900") else { throw ExpositionError.readFile}
@@ -38,6 +39,9 @@ class ExpositionViewController: UIViewController {
         numberFormatter.numberStyle = .decimal
         guard let visitor = numberFormatter.string(from: NSNumber(value: exposition.visitors)) else { throw ExpositionError.numberToString }
         
+        let titleText = exposition.title
+        let titleLine = titleText.replacingOccurrences(of: "(", with: "\n(")
+        expositionTitle.text = titleLine
         expositionVisitor.text = "방문객 : " + visitor + " 명"
         expositionLocation.text = "개최지 : " + exposition.location
         expositionDuration.text = "개최 기간 : " + exposition.duration
