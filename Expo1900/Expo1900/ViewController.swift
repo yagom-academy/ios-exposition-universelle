df9000f5f1cf717b47139aa8f8e0ab3008f763c3
duckbok <duckbok22@gmail.com>
2021-04-15 15:44:17 +0900

    [Feat] #8 Main 화면과 asset catalog 연결

--- Expo1900/Expo1900/ViewController.swift
+++ Expo1900/Expo1900/ViewController.swift
@@ -7,12 +7,36 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var expoTitleLabel: UILabel!
+    @IBOutlet weak var expoImageView: UIImageView!
+    @IBOutlet weak var expoVisitorsLabel: UILabel!
+    @IBOutlet weak var expoLocationLabel: UILabel!
+    @IBOutlet weak var expoDurationLabel: UILabel!
+    @IBOutlet weak var expoDescriptionLabel: UILabel!
+    @IBOutlet weak var flagImageView1: UIImageView!
+    @IBOutlet weak var flagImageView2: UIImageView!
+    
+    var parisianExpoData: Exposition?
+    var koreanEntryData: [Exposition.Entry]?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        parisianExpoData = try? JSONParser<Exposition>.parse("exposition_universelle_1900").get()
+        koreanEntryData = try? JSONParser<[Exposition.Entry]>.parse("items").get()
+        
+        setMainViewData()
     }
-
-
+    
+    func setMainViewData() {
+        expoTitleLabel.text = parisianExpoData?.formattedTitle
+        expoTitleLabel.numberOfLines = 0
+        expoImageView.image = UIImage(named: "poster")
+        expoVisitorsLabel.text = parisianExpoData?.formattedVisitors
+        expoLocationLabel.text = parisianExpoData?.location
+        expoDurationLabel.text = parisianExpoData?.duration
+        expoDescriptionLabel.text = parisianExpoData?.description
+        expoDescriptionLabel.numberOfLines = 0
+        flagImageView1.image = UIImage(named: "flag")
+        flagImageView2.image = UIImage(named: "flag")
+    }
 }
-
