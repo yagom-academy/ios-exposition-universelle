23d287720da5afb7f7a19ea3cb0a2ade833b5923
jaemuyeo <jemking94@gmail.com>
2021-04-15 17:32:37 +0900

    Feat. TableView에 데이터불러오기

--- Expo1900/Expo1900/Controller/ItemListViewController.swift
+++ Expo1900/Expo1900/Controller/ItemListViewController.swift
@@ -8,11 +8,36 @@
 import UIKit
 
 class ItemListViewController: UIViewController {
+    
+    var items: [ExhibitionWorks] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        decodeData(assetName: "items")
     }
     
+    private func decodeData(assetName: String) {
+        let itemsDecoder = JSONDecoder()
+        guard let assetData = NSDataAsset(name: assetName) else { return }
+        
+        do {
+            self.items = try itemsDecoder.decode([ExhibitionWorks].self, from: assetData.data)
+        } catch {
+            print(ErrorData.dataDecodingError.errorMassege)
+        }
+    }
+}
+
+extension ItemListViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "itemCell", for: indexPath) as? ItemTableViewCell else {
+            return UITableViewCell()
+        }
+        cell.itemInformation(data: items[indexPath.row])
+        return cell
+    }
 }
