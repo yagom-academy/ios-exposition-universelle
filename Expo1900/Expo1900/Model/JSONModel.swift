be13e4cdee5fb4b61736bef7dbf1a7f670843818
duckbok <duckbok22@gmail.com>
2021-04-15 16:02:14 +0900

    [Feat] #9 Exposition의 문자열 포맷팅 연산 프로퍼티 추가
    - 박람회 방문자 수 반환하는 프로퍼티 테스트
    - 박람회 제목 '(' 기준으로 개행하여 반환하는 프로퍼티 테스트

--- Expo1900/Expo1900/Model/JSONModel.swift
+++ Expo1900/Expo1900/Model/JSONModel.swift
@@ -5,12 +5,36 @@
 //  Created by duckbok on 2021/04/09.
 //
 
+import Foundation
+
 struct Exposition: Decodable, Equatable {
     let title: String
     let visitors: Int
     let location: String
     let duration: String
     let description: String
+    
+    var formattedVisitors: String? {
+        get {
+            let visitorsCount: NSNumber = visitors as NSNumber
+            let formatter = NumberFormatter()
+            
+            formatter.numberStyle = .decimal
+            guard let formatted = formatter.string(from: visitorsCount) else { return nil }
+            return formatted + " 명"
+        }
+    }
+    
+    var formattedTitle: String? {
+        get {
+            var elements = title.components(separatedBy: "(")
+            var formatted = elements.removeFirst()
+            
+            formatted = elements.reduce(formatted) { $0 + "\n(" + $1 }
+
+            return formatted
+        }
+    }
     
     struct Entry: Decodable, Equatable {
         private enum CodingKeys: String, CodingKey {
