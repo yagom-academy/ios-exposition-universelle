91cd2e5e2586e3e5d0eb25537a89b532b742c274
duckbok <duckbok22@gmail.com>
2021-04-15 18:42:08 +0900

    [Feat] #10 출품작 세부사항 storyboard와 Controller 생성

--- Expo1900/Expo1900/ViewController/EntryTableViewController.swift
+++ Expo1900/Expo1900/ViewController/EntryTableViewController.swift
@@ -1,5 +1,5 @@
 //
-//  EntriesViewController.swift
+//  EntryTableViewController.swift
 //  Expo1900
 //
 //  Created by duckbok on 2021/04/15.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class EntriesViewController: UITableViewController {
+class EntryTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationItem.title = "한국의 출품작"
@@ -16,10 +16,12 @@ class EntriesViewController: UITableViewController {
     // MARK: - Table view data source
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return 1
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return UITableViewCell()
+        let entryCell:UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "entryCell", for: indexPath)
+        
+        return entryCell
     }
 }
---
+++ Expo1900/Expo1900/ViewController/EntryViewController.swift
@@ -0,0 +1,14 @@
+//
+//  EntryViewController.swift
+//  Expo1900
+//
+//  Created by duckbok on 2021/04/15.
+//
+
+import UIKit
+
+class EntryViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
