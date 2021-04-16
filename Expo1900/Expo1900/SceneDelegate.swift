55156e9ad5bb1f73187daeffdb1e6a1670deb721
Neph <tony3322@naver.com>
2021-04-16 12:04:59 +0900

    [52] Fix : rootView 두번 나오는 것 수정

--- Expo1900/Expo1900/SceneDelegate.swift
+++ Expo1900/Expo1900/SceneDelegate.swift
@@ -10,10 +10,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
-        
-        window?.windowScene = windowScene
-        window?.makeKeyAndVisible()
         let mainViewController = UINavigationController(rootViewController: ExpoIntroductionViewController())
         window?.rootViewController = mainViewController
+        window?.windowScene = windowScene
+        window?.makeKeyAndVisible()
     }
 }
