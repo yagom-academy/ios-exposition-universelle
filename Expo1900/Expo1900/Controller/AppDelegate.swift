51fad8aa4b6749b0f8eaad6e8d13311c70ae2d8d
ryan-son <ryan.son1002@gmail.com>
2021-04-16 18:59:59 +0900

    Chore: AppDelegate 타입 final 키워드 적용
    - 더 이상 상속하여 사용할 필요가 없으므로 적용

--- Expo1900/Expo1900/Controller/AppDelegate.swift
+++ Expo1900/Expo1900/Controller/AppDelegate.swift
@@ -7,7 +7,7 @@
 import UIKit
 
 @main
-class AppDelegate: UIResponder, UIApplicationDelegate {
+final class AppDelegate: UIResponder, UIApplicationDelegate {
   
   var shouldSupportAllOrientation: Bool = false
   
