9695461a38003f777576a725b205b1fe5914963a
duckbok <duckbok22@gmail.com>
2021-04-16 19:51:06 +0900

    [Fix] #8 Main storyboard 스크롤바 및 라벨 수정

--- Expo1900/Expo1900/ViewController/ExpositionViewController.swift
+++ Expo1900/Expo1900/ViewController/ExpositionViewController.swift
@@ -7,20 +7,21 @@
 import UIKit
 
 class ExpositionViewController: UIViewController {
-    @IBOutlet weak var expoTitleLabel: UILabel!
-    @IBOutlet weak var expoImageView: UIImageView!
-    @IBOutlet weak var expoVisitorsLabel: UILabel!
-    @IBOutlet weak var expoLocationLabel: UILabel!
-    @IBOutlet weak var expoDurationLabel: UILabel!
-    @IBOutlet weak var expoDescriptionLabel: UILabel!
-    @IBOutlet weak var flagImageView1: UIImageView!
-    @IBOutlet weak var flagImageView2: UIImageView!
+    @IBOutlet weak var expoTitleLabel: UILabel?
+    @IBOutlet weak var expoImageView: UIImageView?
+    @IBOutlet weak var expoVisitorsLabel: UILabel?
+    @IBOutlet weak var expoLocationLabel: UILabel?
+    @IBOutlet weak var expoDurationLabel: UILabel?
+    @IBOutlet weak var expoDescriptionLabel: UILabel?
+    @IBOutlet weak var flagImageView1: UIImageView?
+    @IBOutlet weak var flagImageView2: UIImageView?
     
-    var parisianExpoData: Exposition?
+    var exposition: Exposition?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        parisianExpoData = try? JSONParser<Exposition>.parse("exposition_universelle_1900").get()
+        
+        exposition = try? JSONParser<Exposition>.parse("exposition_universelle_1900").get()
         
         setExpositionViewData()
     }
@@ -34,15 +35,17 @@ class ExpositionViewController: UIViewController {
     }
     
     func setExpositionViewData() {
-        expoTitleLabel.text = parisianExpoData?.formattedTitle
-        expoTitleLabel.numberOfLines = 0
-        expoImageView.image = UIImage(named: "poster")
-        expoVisitorsLabel.text = parisianExpoData?.formattedVisitors
-        expoLocationLabel.text = parisianExpoData?.location
-        expoDurationLabel.text = parisianExpoData?.duration
-        expoDescriptionLabel.text = parisianExpoData?.description
-        expoDescriptionLabel.numberOfLines = 0
-        flagImageView1.image = UIImage(named: "flag")
-        flagImageView2.image = UIImage(named: "flag")
+        expoTitleLabel?.text = exposition?.formattedTitle
+        
+        expoImageView?.image = UIImage(named: "poster")
+        
+        expoVisitorsLabel?.text = exposition?.formattedVisitors
+        expoLocationLabel?.text = exposition?.location
+        expoDurationLabel?.text = exposition?.duration
+        
+        expoDescriptionLabel?.text = exposition?.description
+        
+        flagImageView1?.image = UIImage(named: "flag")
+        flagImageView2?.image = UIImage(named: "flag")
     }
 }
