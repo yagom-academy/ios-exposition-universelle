9823d2d681c88af44a73af362d3676fa57578db8
duckbok <duckbok22@gmail.com>
2021-04-16 12:08:01 +0900

    [Style] #10 entryData 이름 확장성 있게 변경

--- Expo1900/Expo1900/ViewController/EntryTableViewController.swift
+++ Expo1900/Expo1900/ViewController/EntryTableViewController.swift
@@ -8,28 +8,29 @@
 import UIKit
 
 class EntryTableViewController: UITableViewController {
-    var koreanEntryData: [Exposition.Entry]?
+    var entries: [Exposition.Entry]?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        koreanEntryData = try? JSONParser<[Exposition.Entry]>.parse("items").get()
+        
+        entries = try? JSONParser<[Exposition.Entry]>.parse("items").get()
         navigationItem.title = "한국의 출품작"
     }
 
     // MARK: - Table view data source
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-            return koreanEntryData?.count ?? 0
+        return entries?.count ?? 0
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "entryCell", for: indexPath)
         guard let entryCell: EntryTableViewCell = cell as? EntryTableViewCell else { return cell }
-        guard let koreanEntries: [Exposition.Entry] = koreanEntryData else { return cell }
+        guard let entries: [Exposition.Entry] = entries else { return cell }
 
-        entryCell.entryImageView.image = UIImage(named: koreanEntries[indexPath.row].imageName)
-        entryCell.entryNameLabel.text = koreanEntries[indexPath.row].name
-        entryCell.entryShortDescriptionLabel.text = koreanEntries[indexPath.row].shortDescription
+        entryCell.entryImageView.image = UIImage(named: entries[indexPath.row].imageName)
+        entryCell.entryNameLabel.text = entries[indexPath.row].name
+        entryCell.entryShortDescriptionLabel.text = entries[indexPath.row].shortDescription
         
         return entryCell
     }
