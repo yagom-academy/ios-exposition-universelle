130f1e8cebec46a4e8d89dbf9dbfc6064352c8d6
duckbok <duckbok22@gmail.com>
2021-04-16 18:39:41 +0900

    [Feat] #11 출품작 상세 내용 custom class 정의

--- Expo1900/Expo1900/View/EntryView.swift
+++ Expo1900/Expo1900/View/EntryView.swift
@@ -1,5 +1,5 @@
 //
-//  EntryViewCell.swift
+//  EntryView.swift
 //  Expo1900
 //
 //  Created by duckbok on 2021/04/16.
@@ -8,13 +8,75 @@
 import UIKit
 
 class EntryView: UIView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    private let entryScrollView = UIScrollView()
+    private let entryStackView = UIStackView()
+    private let entryImageView = UIImageView()
+    private let entryDescriptionLabel = UILabel()
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        self.addSubview(entryScrollView)
+        
+        setEntryScrollView()
+        setEntryStackView()
+        setEntryImageView()
+        setEntryDescriptionLabel(in: .body)
     }
-    */
+    
+    func putEntryData(from entry: Exposition.Entry) {
+        entryImageView.image = UIImage(named: entry.imageName)
+        entryDescriptionLabel.text = entry.description
+    }
+    
+    private func setEntryScrollView() {
+        entryScrollView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            entryScrollView.topAnchor.constraint(equalTo: self.topAnchor),
+            entryScrollView.bottomAnchor.constraint(equalTo: self.bottomAnchor),
+            entryScrollView.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+            entryScrollView.trailingAnchor.constraint(equalTo: self.trailingAnchor)
+        ])
+        
+        entryScrollView.addSubview(entryStackView)
+    }
+    
+    private func setEntryStackView() {
+        entryStackView.axis = .vertical
+        entryStackView.spacing = 10
+        entryStackView.translatesAutoresizingMaskIntoConstraints = false
+        entryStackView.layoutMargins = UIEdgeInsets(top: 20, left: 16, bottom: 20, right: 16)
+        entryStackView.isLayoutMarginsRelativeArrangement = true
+        
+        let heightConstraint = entryStackView.heightAnchor.constraint(equalTo: entryScrollView.frameLayoutGuide.heightAnchor)
+        heightConstraint.priority = .defaultLow
+        
+        NSLayoutConstraint.activate([
+            entryStackView.topAnchor.constraint(equalTo: entryScrollView.topAnchor),
+            entryStackView.bottomAnchor.constraint(equalTo: entryScrollView.bottomAnchor),
+            entryStackView.leadingAnchor.constraint(equalTo: entryScrollView.leadingAnchor),
+            entryStackView.trailingAnchor.constraint(equalTo: entryScrollView.trailingAnchor),
+            entryStackView.widthAnchor.constraint(lessThanOrEqualTo: entryScrollView.widthAnchor, multiplier: 1),
+            heightConstraint
+        ])
+        
+        entryStackView.addArrangedSubview(entryImageView)
+        entryStackView.addArrangedSubview(entryDescriptionLabel)
+    }
+    
+    private func setEntryImageView() {
+        entryImageView.contentMode = .scaleAspectFit
+        entryImageView.translatesAutoresizingMaskIntoConstraints = false
 
+        NSLayoutConstraint.activate([
+            entryImageView.heightAnchor.constraint(lessThanOrEqualTo: entryImageView.widthAnchor)
+        ])
+    }
+    
+    private func setEntryDescriptionLabel(in textStyle: UIFont.TextStyle) {
+        entryDescriptionLabel.font = UIFont.preferredFont(forTextStyle: textStyle)
+        entryDescriptionLabel.adjustsFontForContentSizeCategory = true
+        entryDescriptionLabel.numberOfLines = 0
+    }
 }
