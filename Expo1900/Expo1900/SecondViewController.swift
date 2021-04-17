4dcfdce04afc69ad4055f5ac4baa516ff30b381c
unme88 <sunny.on.a.roll@gmail.com>
2021-04-17 14:05:23 +0900

    feat: Second View Controller json decoder 테이블뷰 구현 koreanTitle과 shortDescription만 일단 나오게 해줌 (이미지 추가해야 함)

--- Expo1900/Expo1900/SecondViewController.swift
+++ Expo1900/Expo1900/SecondViewController.swift
@@ -5,15 +5,48 @@
 //  Created by Sunny on 2021/04/14.
 //
 
+//struct KoreanItem : Decodable {
+//    let koreanTitle : String
+//    let englishTitle : String
+//    let shortDescription : String
+//    let fullDescription : String
+
 import UIKit
 
-class SecondViewController: UIViewController {
-
-
+class SecondViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-
-
+        
+        let jsonDecoder: JSONDecoder = JSONDecoder()
+        guard let dataAsset: NSDataAsset = NSDataAsset(name: "items") else { return }
+        
+        do {
+            self.koreanItems = try jsonDecoder.decode([KoreanItem].self, from: dataAsset.data)
+        } catch {
+            print(error.localizedDescription)
         }
+
+        self.tableView.reloadData()
+        }
+    
+    @IBOutlet weak var tableView: UITableView!
+    let cellIdentifier: String = "cell"
+    var koreanItems: [KoreanItem] = []
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.koreanItems.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: self.cellIdentifier, for: indexPath)
+        
+        let item: KoreanItem = self.koreanItems[indexPath.row]
+        
+        cell.textLabel?.text = item.koreanTitle
+        cell.detailTextLabel?.text = item.shortDescription
+        return cell
+    }
+    
 }
