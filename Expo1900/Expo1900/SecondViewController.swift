677b03d807a4807b39d7ef52d7523e3ea9877423
unme88 <sunny.on.a.roll@gmail.com>
2021-04-17 12:57:25 +0900

    feat: Show segue이용한 Second View Controller로의 화면 전환

--- Expo1900/Expo1900/SecondViewController.swift
+++ Expo1900/Expo1900/SecondViewController.swift
@@ -1,53 +1,19 @@
-////
-////  SecondViewController.swift
-////  Expo1900
-////
-////  Created by Sunny on 2021/04/14.
-////
 //
-//import UIKit
+//  SecondViewController.swift
+//  Expo1900
 //
-//class ViewController: UIViewController {
+//  Created by Sunny on 2021/04/14.
 //
-//    @IBOutlet weak var tableView: UITableView!
-//    let cellIdentifier: String = "cell"
-////    var exposition: ExpoItem?
-//
-////    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-////        return self.exposition.count
-////    }
-//
-////    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-////        let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: self.cellIdentifier, for: indexPath)
-////
-////        let expoItem: ExpoItem = self.exposition[indexPath.row]
-////        cell.textLabel?.text = expoItem.expoTitle
-////        cell.detailTextLabel?.text = expoItem.description
-////        return cell
-////    }
-//
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//        // Do any additional setup after loading the view.
-//
-//        var exposition: ExpoItem
-//
-//        let jsonDecoder: JSONDecoder = JSONDecoder()
-//        guard let dataAsset: NSDataAsset = NSDataAsset(name: "exposition") else {
-//            return
-//        }
-//
-//        do {
-//            exposition = try jsonDecoder.decode(ExpoItem.self, from: dataAsset.data)
-//            print(exposition.expoTitle)
-//            print(exposition.description)
-//            print(exposition.location)
-//            print(exposition.numberOfVisitors)
-//            print(exposition.openingPeriod)
-//        } catch {
-//            print(error.localizedDescription)
-//        }
-//
-////        self.tableView.reloadData()
-//        }
-//}
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+
+
+        }
+}
