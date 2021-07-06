73cc2e66a7311247b510a0fdefee6f502ddbea16
jgkim1008 <rlawnsrjs100@naver.com>
2021-07-06 14:44:59 +0900

    feat : MVC 패턴 적용, KoreaExposition 구조체 생성
    * Json 데이터를 디코더할 구조체 타입 생성
    * Json 키를 camelCase로 변환하는 enum 구현

---
+++ Expo1900/Expo1900/Model/KoreaExposition.swift
@@ -0,0 +1,36 @@
+//
+//  KoreaExposition.swift
+//  Expo1900
+//
+//  Created by 김준건 on 2021/07/06.
+//
+
+import Foundation
+
+
+struct KoreaExposition {
+    let name: String
+    let imageName: String
+    let shortDescription: String
+    let description: String
+    
+    enum CodingKeys: String, CodingKey {
+        case name
+        case imageName = "image_name"
+        case shortDescription = "short_desc"
+        case description = "desc"
+    }
+}
+
+
+//extension KoreaExposition: Decodable {
+//    init(from decoder: Decoder) throws {
+//        let values = try decoder.container(keyedBy: CodingKeys.self)
+//        name = try values.decode(String.self, forKey: .name)
+//        imageName = try values.decode(String.self, forKey: .imageName)
+//        shortDescription = try values.decode(String.self, forKey: .shortDescription)
+//        description = try values.decode(String.self, forKey: .description)
+//    }
+//}
+
+
