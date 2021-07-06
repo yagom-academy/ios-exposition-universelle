dc7bca021c42dd856b3212bbc56617526602033c
hrjy6278 <hrjy6278@naver.com>
2021-07-06 12:00:53 +0900

    feat: ExpositionItem 구조체 타입 구현

--- Expo1900/Expo1900/Model/ExpositionItem.swift
+++ Expo1900/Expo1900/Model/ExpositionItem.swift
@@ -6,3 +6,17 @@
 //
 
 import Foundation
+
+struct ExpositionItem: Codable {
+    private(set) var name: String
+    private(set) var imageName: String
+    private(set) var shortDescription: String
+    private(set) var description: String
+    
+    private enum CondingKeys: String, CodingKey {
+        case name
+        case imageName = "image_name"
+        case shortDescription = "short_desc"
+        case description = "desc"
+    }
+}
