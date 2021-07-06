e4970b65b78d20a2a840065c4d67dcff55cb12ea
soo941226 <rhrnakrnakr@naver.com>
2021-07-06 14:49:01 +0900

    chore: Rename a file and directories
    * Other->App
    * Controller->ViewController
    * ViewController->MainViewController
    * View->Storyboard
    * Model->JSONModel

--- Expo1900/Expo1900/ViewController/MainViewController.swift
+++ Expo1900/Expo1900/ViewController/MainViewController.swift
@@ -6,13 +6,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
 }
 
