0315ee604e96b4c02d9dc80856a6bbbffb4b382e
jyoon54 <jiwonyoon@Jiwonui-MacBookPro.local>
2021-07-06 14:58:18 +0900

    feat : parseToExposition, parseToItems 함수 추가

--- Expo1900/Expo1900/Parser.swift
+++ Expo1900/Expo1900/Parser.swift
@@ -8,10 +8,19 @@
 import Foundation
 
 struct Parser {
+    let fileName: String
+//    var expo: Exposition? {
+//
+//    }
+//    var items: [Item]? {
+//
+//    }
+    
     func readLocalJSONFile(forName name: String) -> Data? {
         do {
             if let bundlePath = Bundle.main.path(forResource: name, ofType: "json"),
                let jsonData = try String(contentsOfFile: bundlePath).data(using: .utf8) {
+                print(bundlePath)
                 return jsonData
             }
         } catch {
@@ -19,4 +28,25 @@ struct Parser {
         }
         return nil
     }
+    
+    func parseToExposition(data: Data) -> Exposition? {
+        do {
+            let expo = try JSONDecoder().decode(Exposition.self, from: data)
+            return expo
+        } catch {
+            print(error)
+        }
+        return nil
+    }
+    
+    func parseToItems(data: Data) -> [KoreanItem]? {
+        do {
+            let items = try JSONDecoder().decode([KoreanItem].self, from: data)
+            return items
+        } catch {
+            print(error)
+        }
+        return nil
+    }
+    
 }
--- Expo1900/Expo1900/ViewController.swift
+++ Expo1900/Expo1900/ViewController.swift
@@ -4,6 +4,11 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+//        let path = "exposition"
+//        let parser = Parser()
+//        print(path)
+//        print(parser.readLocalJSONFile(forName: path))
+//        //JSONFile -> DATA -> Exposition/Items
     }
 }
 
