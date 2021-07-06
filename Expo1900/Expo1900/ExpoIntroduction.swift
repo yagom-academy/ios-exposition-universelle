3b9594daf589bd70771035af2fef5d6df38e9160
yoonju lee <rsyjlee@gmail.com>
2021-07-06 17:19:15 +0900

    feat: 첫 화면의 데이터를 담을 ExpoIntroduction 타입 정의

---
+++ Expo1900/Expo1900/ExpoIntroduction.swift
@@ -0,0 +1,16 @@
+//
+//  ExpoIntroduction.swift
+//  Expo1900
+//
+//  Created by 이윤주 on 2021/07/06.
+//
+
+import Foundation
+
+struct ExpoIntroduction: Codable {
+    let title: String
+    let visitors: UInt
+    let location: String
+    let duration: String
+    let description: String
+}
