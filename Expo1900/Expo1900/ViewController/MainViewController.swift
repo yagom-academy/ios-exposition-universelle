372e2e6c0a6855243e89de7cca68a3b2bcc8e75c
soo941226 <rhrnakrnakr@naver.com>
2021-07-06 17:28:59 +0900

    feat: Add IBOutlets of MainView
    * labels about title, visitors, location, duration, description

--- Expo1900/Expo1900/ViewController/MainViewController.swift
+++ Expo1900/Expo1900/ViewController/MainViewController.swift
@@ -8,9 +8,13 @@ import UIKit
 
 class MainViewController: UIViewController {
 
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var visitorsLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var durationLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-    
 }
-
