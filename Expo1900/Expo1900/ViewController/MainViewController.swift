a2a82b424e2abfe912397c2e20830c4170c2de44
joey-ful <isjoeyful@gmail.com>
2021-07-06 19:03:10 +0900

    test: Add decoding test about Exposition type

--- Expo1900/Expo1900/ViewController/MainViewController.swift
+++ Expo1900/Expo1900/ViewController/MainViewController.swift
@@ -18,3 +18,4 @@ class MainViewController: UIViewController {
         super.viewDidLoad()
     }
 }
+
---
+++ Expo1900/Expo1900Tests/Expo1900Tests.swift
@@ -0,0 +1,25 @@
+//
+//  Expo1900Tests.swift
+//  Expo1900Tests
+//
+//  Created by 홍정아 on 2021/07/06.
+//
+@testable import Expo1900
+import XCTest
+
+class Expo1900Tests: XCTestCase {
+    class Test: JSONDecodable {
+        typealias T = Exposition
+    }
+    let cut = Test()
+    
+    func test_파일이름이exposition블라블라일때_Exposition의title은_파리만국박람회블라블라() {
+        //Given
+        let fileName = "exposition_universelle_1900"
+        let expectInputValue = "파리 만국박람회 1900(L'Exposition de Paris 1900)"
+        //When
+        let expectResult = try? cut.decodeJSON(fileName: fileName)
+        //Then
+        XCTAssertEqual(expectInputValue, expectResult?.title)
+    }
+}
