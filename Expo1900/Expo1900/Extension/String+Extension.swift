176141fe2a6b755b72c7b54d62565e87427c3cd4
soo941226 <rhrnakrnakr@naver.com>
2021-07-07 17:51:01 +0900

    feat: Add method styleLabelHeaders
    * Declare static variables in String+Extension.swift
    * Use upper things in MainViewController.swift

---
+++ Expo1900/Expo1900/Extension/String+Extension.swift
@@ -0,0 +1,21 @@
+//
+//  String+Extension.swift
+//  Expo1900
+//
+//  Created by kjs on 2021/07/07.
+//
+
+import Foundation
+
+extension String {
+    static let expositionFileName = "exposition_universelle_1900"
+    static let bracket = "("
+    static let newLine = "\n"
+    static let zero = "0"
+    static let visitor = "방문객 : "
+    static let location = "개최지 : "
+    static let duration = "개최 기간 : "
+    static let colon = ": "
+    static let blank = ""
+    static let people = " 명"
+}
--- Expo1900/Expo1900/ViewController/MainViewController.swift
+++ Expo1900/Expo1900/ViewController/MainViewController.swift
@@ -7,17 +7,17 @@
 import UIKit
 
 class MainViewController: UIViewController {
-
+    
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var visitorsLabel: UILabel!
     @IBOutlet weak var locationLabel: UILabel!
     @IBOutlet weak var durationLabel: UILabel!
     @IBOutlet weak var descriptionLabel: UILabel!
-    let assetFileName = "exposition_universelle_1900"
     
     override func viewDidLoad() {
         super.viewDidLoad()
         initView()
+        styleLabelHeaders()
     }
 }
 
@@ -25,7 +25,7 @@ extension MainViewController: JSONDecodable {
     typealias T = Exposition
     
     func getExposition() -> Exposition? {
-        return try? decodeJSON(fileName: assetFileName)
+        return try? decodeJSON(fileName: .expositionFileName)
     }
     
     func initView() {
@@ -35,19 +35,37 @@ extension MainViewController: JSONDecodable {
         let titleText = formattingTitle(title: currentExposition.title)
         let visitorsText = formattingVisitors(visitors: currentExposition.visitors)
         titleLabel.text = titleText
-        visitorsLabel.text = visitorsText
-        locationLabel.text = currentExposition.location
-        durationLabel.text = currentExposition.duration
+        visitorsLabel.text = .visitor + visitorsText + .people
+        locationLabel.text = .location + currentExposition.location
+        durationLabel.text = .duration + currentExposition.duration
         descriptionLabel.text = currentExposition.description
     }
     
     func formattingTitle(title: String) -> String {
-        return title.replacingOccurrences(of: "(", with: "\n(")
+        return title.replacingOccurrences(of: String.bracket, with: String.newLine + String.bracket)
     }
     
     func formattingVisitors(visitors: UInt) -> String {
         let numberFormatter = NumberFormatter()
         numberFormatter.numberStyle = .decimal
-        return numberFormatter.string(from: NSNumber(value: visitors)) ?? "0"
+        return numberFormatter.string(from: NSNumber(value: visitors)) ?? .zero
+    }
+    
+    func modifySize(of range: String, in label: UILabel) {
+        guard let text = label.text else {
+            return
+        }
+        
+        let fontSize = UIFont.systemFont(ofSize: label.font.pointSize + 4)
+        let attributedStr = NSMutableAttributedString(string: text)
+        attributedStr.addAttribute(.font, value: fontSize, range: (text as NSString).range(of: range.replacingOccurrences(of: String.colon, with: String.blank)))
+        
+        label.attributedText = attributedStr
+    }
+    
+    func styleLabelHeaders() {
+        modifySize(of: .visitor , in: visitorsLabel)
+        modifySize(of: .location , in: locationLabel)
+        modifySize(of: .duration , in: durationLabel)
     }
 }
