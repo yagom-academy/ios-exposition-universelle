945101853e2b28242fbe47e7446842db782af1a9
taeyoung-Kim-KR <kty72301@naver.com>
2021-07-07 14:46:00 +0900

    test: 테스트 네이밍 변경,불필요 테스트 케이스 삭제

--- Expo1900/Expo1900Tests/Expo1900Tests.swift
+++ Expo1900/Expo1900Tests/Expo1900Tests.swift
@@ -11,53 +11,23 @@ import XCTest
 class Expo1900Tests: XCTestCase {
     let decoder = JSONDecoder()
     
-    func test_유니버셜1900_JSON데이터를_가지고온_후에_InternationalExposition_객체를생성하여_title을_출력하면_한국박람회는_아니다() {
+    func test_JSONData_exposition_universelle_1900_titleShouldHaveValue() {
         //given
         guard let dataAsset: NSDataAsset = NSDataAsset.init(name: "exposition_universelle_1900") else {
           return
         }
         
         //when
-        let paris = try? decoder.decode(InternationalExposition.self, from: dataAsset.data)
-        let result = paris?.title
-        let expectResult = "한국박람회"
-        
-        //then
-        XCTAssertNotEqual(result, expectResult)
-    }
-    
-    func test_itmes_JSON데이터를_가지고온_후에_KoreaExposition_객체를생성하여_첫작품의_name을_출력하면_직지심체요절는_아니다() {
-        //given
-        guard let dataAsset: NSDataAsset = NSDataAsset.init(name: "items") else {
-          return
-        }
-        
-        //when
-        let koreaExpositions = try? decoder.decode([KoreaExposition].self, from: dataAsset.data)
-        let result = koreaExpositions?.last?.name
-        let expectResult = "직지심체요절"
-        
-        //then
-        XCTAssertNotEqual(result, expectResult)
-    }
-    
-    func test_유니버셜1900_JSON데이터를_가지고온_후에_InternationalExposition_객체를생성하여_title을_출력하면_파리만국박람회_1900이_나온다() {
-        //given
-        guard let dataAsset: NSDataAsset = NSDataAsset.init(name: "exposition_universelle_1900") else {
-          return
-        }
-        
-        //when
-        let paris = try? decoder.decode(InternationalExposition.self, from: dataAsset.data)
-        let result = paris?.title
+        let internationalExpositionData = try? decoder.decode(InternationalExposition.self, from: dataAsset.data)
+        let result = internationalExpositionData?.title
         let expectResult = "파리 만국박람회 1900(L\'Exposition de Paris 1900)"
         
         //then
         XCTAssertEqual(result, expectResult)
     }
     
-    func test_itmes_JSON데이터를_가지고온_후에_KoreaExposition_객체를생성하여_첫작품의_name을_출력하면_뭐가_나온다() {
-        //givien
+    func test_JSONData_items_nameShouldHaveValue() {
+        //given
         guard let dataAsset: NSDataAsset = NSDataAsset.init(name: "items") else {
           return
         }
@@ -71,3 +41,4 @@ class Expo1900Tests: XCTestCase {
         XCTAssertEqual(result, expectResult)
     }
 }
+
