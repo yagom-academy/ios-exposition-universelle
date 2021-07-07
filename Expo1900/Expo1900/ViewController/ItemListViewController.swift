1f0588b0de9a9852e5972a68211fb6e0ce7ae416
jaemuyeo <jemking94@gmail.com>
2021-07-07 18:19:36 +0900

    feat: Add NavigationController
    * Hide NavigationBar from RootViewController only
    * Create ItemListViewController

---
+++ Expo1900/Expo1900/ViewController/ItemListViewController.swift
@@ -0,0 +1,12 @@
+//
+//  ItemListViewController.swift
+//  Expo1900
+//
+//  Created by 잼킹 on 2021/07/07.
+//
+
+import UIKit
+
+class ItemListViewController: UIViewController {
+    
+}
--- Expo1900/Expo1900/ViewController/MainViewController.swift
+++ Expo1900/Expo1900/ViewController/MainViewController.swift
@@ -19,6 +19,16 @@ class MainViewController: UIViewController {
         initView()
         styleLabelHeaders()
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.navigationController?.isNavigationBarHidden = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        self.navigationController?.isNavigationBarHidden = false
+    }
 }
 
 extension MainViewController: JSONDecodable {
