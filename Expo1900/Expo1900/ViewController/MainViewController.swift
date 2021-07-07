f997ecbb0f11089f3b0f2e73f1fd3bdc7ecf544f
joey-ful <isjoeyful@gmail.com>
2021-07-07 16:51:57 +0900

    feat: Add feature about JSONdecoding in MainViewController
    * Add a line break in the titleLabel with formattingTitle(title: String)
    * Format visitors number with formattingVisitors(visitors: UInt)

--- Expo1900/Expo1900/ViewController/MainViewController.swift
+++ Expo1900/Expo1900/ViewController/MainViewController.swift
@@ -13,9 +13,41 @@ class MainViewController: UIViewController {
     @IBOutlet weak var locationLabel: UILabel!
     @IBOutlet weak var durationLabel: UILabel!
     @IBOutlet weak var descriptionLabel: UILabel!
+    let assetFileName = "exposition_universelle_1900"
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        initView()
     }
 }
 
+extension MainViewController: JSONDecodable {
+    typealias T = Exposition
+    
+    func getExposition() -> Exposition? {
+        return try? decodeJSON(fileName: assetFileName)
+    }
+    
+    func initView() {
+        guard let currentExposition = getExposition() else {
+            return
+        }
+        let titleText = formattingTitle(title: currentExposition.title)
+        let visitorsText = formattingVisitors(visitors: currentExposition.visitors)
+        titleLabel.text = titleText
+        visitorsLabel.text = visitorsText
+        locationLabel.text = currentExposition.location
+        durationLabel.text = currentExposition.duration
+        descriptionLabel.text = currentExposition.description
+    }
+    
+    func formattingTitle(title: String) -> String {
+        return title.replacingOccurrences(of: "(", with: "\n(")
+    }
+    
+    func formattingVisitors(visitors: UInt) -> String {
+        let numberFormatter = NumberFormatter()
+        numberFormatter.numberStyle = .decimal
+        return numberFormatter.string(from: NSNumber(value: visitors)) ?? "0"
+    }
+}
