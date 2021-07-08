a4ec55140eab4cb84f1527dab8285d06be85de75
taelee42 <ritmakid@gmail.com>
2021-07-08 19:20:56 +0900

    feat: ExpositionListCell을 만들고 tableView의 cell과 연결

---
+++ Expo1900/Expo1900/Controller/ExpositionListCell.swift
@@ -0,0 +1,14 @@
+//
+//  ExpositionListCell.swift
+//  Expo1900
+//
+//  Created by Terry Lee on 2021/07/08.
+//
+
+import UIKit
+
+class ExpositionListCell: UITableViewCell {
+    @IBOutlet weak var itemImage: UIImageView!
+    @IBOutlet weak var name: UILabel!
+    @IBOutlet weak var shortDescription: UILabel!
+}
