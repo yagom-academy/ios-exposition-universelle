0ac01e6a175601434843ed4862ffa8cba0d9cfcd
YongHoonJJo <starctak@gmail.com>
2021-07-08 22:23:34 +0900

    feat: JSONParser 타입 및 타입 메서드 추가, 테스트 코드 작성

---
+++ Expo1900/Expo1900/Util/JSONParser.swift
@@ -0,0 +1,27 @@
+//
+//  JSONParser.swift
+//  Expo1900
+//
+//  Created by YongHoon JJo on 2021/07/08.
+//
+
+import UIKit
+
+enum JSONParserError: Error {
+    case invalidName
+    case failedToDecode
+}
+
+struct JSONParser {
+    static func parse<T: Decodable>(name: String, type: T.Type) -> Result<T, JSONParserError> {
+        guard let asset = NSDataAsset(name: name) else {
+            return .failure(.invalidName)
+        }
+        let decoder = JSONDecoder()
+        guard let result = try? decoder.decode(type, from: asset.data) else {
+            return .failure(.failedToDecode)
+        }
+        
+        return .success(result)
+    }
+}
