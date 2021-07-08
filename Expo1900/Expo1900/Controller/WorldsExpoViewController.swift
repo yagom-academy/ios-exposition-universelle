c5d11e910cb21d29edc6cb4c0c7f941a7510f3c1
ictechgy <ictechgy@gmail.com>
2021-07-08 18:49:39 +0900

    feat: 초기화면 스토리보드 구현
    * WoldsExpoViewController class와 Storyboard에 만든 UI 요소 연결

---
+++ Expo1900/Expo1900/Controller/WorldsExpoViewController.swift
@@ -0,0 +1,28 @@
+//
+//  Expo1900 - ViewController.swift
+//  Created by yagom. 
+//  Copyright © yagom academy. All rights reserved.
+// 
+
+import UIKit
+
+class WorldsExpoViewController: UIViewController {
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var introductionImageView: UIImageView!
+    @IBOutlet weak var visitorCountLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var durationLabel: UILabel!
+    @IBOutlet weak var descriptionTextView: UITextView!
+    @IBOutlet weak var nationFlagImageView1: UIImageView!
+    @IBOutlet weak var nationFlagImageView2: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func showEntriesButtonDidTap(_ sender: UIButton) {
+    }
+    
+}
+
