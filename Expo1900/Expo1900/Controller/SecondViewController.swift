cab7314aac36cb734d6fdb46fe328ead8a939c70
산체스 <xognsl87@gmail.com>
2021-07-08 18:50:42 +0900

    feat: SecondViewController 내 Decoder 기능

--- Expo1900/Expo1900/Controller/SecondViewController.swift
+++ Expo1900/Expo1900/Controller/SecondViewController.swift
@@ -8,22 +8,21 @@
 import UIKit
 
 class SecondViewController: UIViewController {
+    @IBOutlet weak var tableView: UITableView!
+    var informationOfKoreanHeritages: [InformationOfKoreanHeritage] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        let jsonDecoder: JSONDecoder = JSONDecoder()
+        guard let dataAsset: NSDataAsset = NSDataAsset(name: "items") else {
+            return
+        }
+        do {
+            self.informationOfKoreanHeritages = try jsonDecoder.decode([InformationOfKoreanHeritage].self, from: dataAsset.data)
+        } catch {
+            print(error.localizedDescription)
+        }
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
