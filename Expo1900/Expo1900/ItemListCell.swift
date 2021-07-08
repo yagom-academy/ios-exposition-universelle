f092f8e9f099217e77136a3e7d70f682480cd7d6
blanche37 <wkddbsghk16@gmail.com>
2021-07-08 20:52:59 +0900

    feat: itemListViewController 추가 및 파일과 연결, items파싱

---
+++ Expo1900/Expo1900/ItemListCell.swift
@@ -0,0 +1,23 @@
+//
+//  ItemListCell.swift
+//  Expo1900
+//
+//  Created by yun on 2021/07/08.
+//
+
+import UIKit
+
+class ItemListCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Expo1900/Expo1900/ItemListViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ItemListViewController.swift
+//  Expo1900
+//
+//  Created by yun on 2021/07/08.
+//
+
+import UIKit
+
+class ItemListViewController: UIViewController {
+
+    var itemList: [KoreanItem] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let jsonDecoder = JSONDecoder()
+        guard let dataAsset = NSDataAsset(name: "items") else {
+            return
+        }
+        do {
+        itemList = try jsonDecoder.decode([KoreanItem].self, from: dataAsset.data)
+        } catch {
+            print(error.localizedDescription)
+        }
+
+    }
+}
+
+extension ItemListViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+}
