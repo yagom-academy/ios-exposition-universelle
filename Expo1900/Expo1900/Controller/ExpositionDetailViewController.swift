1028fb4ece264c933aee6fff3b55af9ad62db986
Ldoy <leedoy7989@gmail.com>
2021-07-09 15:00:53 +0900

    feat: ExpositionDetailViewController 파일 추가, IBOutlet 연결
    -  스토리보드 imageView, label 추가

---
+++ Expo1900/Expo1900/Controller/ExpositionDetailViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ExpositionDetailViewController.swift
+//  Expo1900
+//
+//  Created by Do Yi Lee on 2021/07/09.
+//
+
+import UIKit
+
+class ExpositionDetailViewController: UIViewController {
+	@IBOutlet weak var itemImage: UIImageView!
+	@IBOutlet weak var itemDescriptionLabel: UILabel!
+	var itemName: String = ""
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+	
+}
