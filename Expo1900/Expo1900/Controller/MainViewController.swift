9a3ce90ac8533986dcda969a9160fed9e33c4f7f
YongHoonJJo <starctak@gmail.com>
2021-07-09 17:19:59 +0900

    feat: MainViewController 에 데이터 바인딩
    - exposition_universelle_1900 JSON 파일의 데이터 가져온 후 IBOutlet 이미지 및 텍스트에 데이터 연결.
    - 넘버 포맷터를 이용하여 3자리마다 콤마 적용
    - MainViewController 의 NavigationBar : viewWillAppear() 시 hidden(true),  viewWillDisappear() 시 hidden(false) 처리.

--- Expo1900/Expo1900/Controller/MainViewController.swift
+++ Expo1900/Expo1900/Controller/MainViewController.swift
@@ -7,19 +7,47 @@
 import UIKit
 
 class MainViewController: UIViewController {
-
-	@IBOutlet weak var titleLabel: UILabel!
-	@IBOutlet weak var mainImage: UIImageView!
-	@IBOutlet weak var visitorsLabel: UILabel!
-	@IBOutlet weak var locationLabel: UILabel!
-	@IBOutlet weak var durationLabel: UILabel!
-	@IBOutlet weak var descriptionLabel: UILabel!
-	@IBOutlet weak var leftFlagImage: UIImageView!
-	@IBOutlet weak var rightFlagImage: UIImageView!
-	
-	override func viewDidLoad() {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var mainImage: UIImageView!
+    @IBOutlet weak var visitorsLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var durationLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var leftFlagImage: UIImageView!
+    @IBOutlet weak var rightFlagImage: UIImageView!
+    
+    override func viewDidLoad() {
         super.viewDidLoad()
+        
+        guard let exposition = try? JSONParser.parse(name: "exposition_universelle_1900", type: Exposition.self).get() else {
+            return
+        }
+        
+        let numberFommater = NumberFormatter()
+        numberFommater.numberStyle = .decimal
+        guard let visitors = numberFommater.string(for: exposition.visitors) else {
+            return
+        }
+        
+        titleLabel.text = exposition.title
+        mainImage.image = exposition.image
+        visitorsLabel.text = "\(visitors) 명"
+        locationLabel.text = exposition.location
+        durationLabel.text = exposition.duration
+        // descriptionLabel.text = exposition.description
+        // leftFlagImage.image = UIImage(named: "flag")
+        // rightFlagImage.image = UIImage(named: "flag")
     }
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.setNavigationBarHidden(true, animated: false)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.setNavigationBarHidden(false, animated: false)
+    }
 }
 
