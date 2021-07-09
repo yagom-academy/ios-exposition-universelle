9d092615d0a0dbc869e69937b18d296fd40f185f
taelee42 <ritmakid@gmail.com>
2021-07-09 14:34:56 +0900

    feat: 테이블 뷰 data source 필수 메서드 구현

--- Expo1900/Expo1900/Controller/ExpositionListViewController.swift
+++ Expo1900/Expo1900/Controller/ExpositionListViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class ExpositionListViewController: UIViewController {
 	private let cellReuseIdentifier = "ExpositionListCell"
+    var expositionItems: [ExpositionItem]?
 	
 	@IBOutlet var expositionTableView: UITableView!
 	
@@ -16,18 +17,25 @@ class ExpositionListViewController: UIViewController {
 		super.viewDidLoad()
 		expositionTableView.delegate = self
 		expositionTableView.dataSource = self
+        
+        expositionItems = try? JSONParser.parse(name: "items", type: [ExpositionItem].self).get()
 	}
 }
 
 extension ExpositionListViewController: UITableViewDelegate, UITableViewDataSource {
 	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-		return 1
+        return expositionItems?.count ?? 0
 	}
 	
 	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
 		guard let cell = tableView.dequeueReusableCell(withIdentifier: cellReuseIdentifier, for: indexPath) as? ExpositionListCell else {
 			return UITableViewCell()
 		}
+        
+        let expositionItem = expositionItems?[indexPath.row]
+        cell.name.text = expositionItem?.name
+        cell.shortDescription.text = expositionItem?.shortDescription
+        cell.itemImage.image = expositionItem?.image
 		return cell
 	}
 }
