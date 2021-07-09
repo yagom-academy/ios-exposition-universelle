12a09063df800c6e89e98021397cbaa764dd7b25
Ldoy <leedoy7989@gmail.com>
2021-07-09 16:36:55 +0900

    feat: MainViewController 스토리 보드에 UI요소 추가 및 IBOutlet 연결
    - viewcontroller 이름을 MainViewController로 변경
    - label의 line을 0으로 변경
    - stack view, scroll view 설정

---
+++ Expo1900/Expo1900/Controller/MainViewController.swift
@@ -0,0 +1,25 @@
+//
+//  Expo1900 - ViewController.swift
+//  Created by yagom. 
+//  Copyright © yagom academy. All rights reserved.
+// 
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+	@IBOutlet weak var titleLabel: UILabel!
+	@IBOutlet weak var mainImage: UIImageView!
+	@IBOutlet weak var visitorsLabel: UILabel!
+	@IBOutlet weak var locationLabel: UILabel!
+	@IBOutlet weak var durationLabel: UILabel!
+	@IBOutlet weak var descriptionLabel: UILabel!
+	@IBOutlet weak var leftFlagImage: UIImageView!
+	@IBOutlet weak var rightFlagImage: UIImageView!
+	
+	override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
+
