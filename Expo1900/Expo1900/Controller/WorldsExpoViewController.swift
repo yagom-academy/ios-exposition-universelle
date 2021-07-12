faa6599e7fc5fb9540505f6c2bc7af63d5efe066
ictechgy <ictechgy@gmail.com>
2021-07-12 23:07:53 +0900

    refactor: WorldsExpoViewContoller에 대한 전반적인 리팩토링
    * fileName String literal을 별도의 변수로 분리
    * 태극기 이미지에 대한 이미지 뷰 outlet 삭제
    * 불필요한 개행 제거 및 가독성을 위한 개행 추가
    * Access Control 설정
    * 데이터를 가져와서 레이블에 설정하는 메소드를 extension으로 이동시킨 뒤에 세부기능별로 메소드를 분리
    * 메소드명 및 변수명을 직관적으로 변경
    * MARK 주석 추가

--- Expo1900/Expo1900/Controller/WorldsExpoViewController.swift
+++ Expo1900/Expo1900/Controller/WorldsExpoViewController.swift
@@ -7,19 +7,18 @@
 import UIKit
 
 class WorldsExpoViewController: UIViewController {
-    @IBOutlet weak var titleLabel: UILabel!
-    @IBOutlet weak var introductionImageView: UIImageView!
-    @IBOutlet weak var visitorCountLabel: UILabel!
-    @IBOutlet weak var locationLabel: UILabel!
-    @IBOutlet weak var durationLabel: UILabel!
-    @IBOutlet weak var descriptionTextView: UITextView!
-    @IBOutlet weak var nationFlagImageView1: UIImageView!
-    @IBOutlet weak var nationFlagImageView2: UIImageView!
+    //MARK: IBOutlets
+    @IBOutlet weak private var titleLabel: UILabel!
+    @IBOutlet weak private var introductionImageView: UIImageView!
+    @IBOutlet weak private var visitorCountLabel: UILabel!
+    @IBOutlet weak private var locationLabel: UILabel!
+    @IBOutlet weak private var durationLabel: UILabel!
+    @IBOutlet weak private var descriptionTextView: UITextView!
     
+    //MARK: Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        setComponent()
+        queryData()
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -31,19 +30,27 @@ class WorldsExpoViewController: UIViewController {
         super.viewWillDisappear(animated)
         navigationController?.setNavigationBarHidden(false, animated: true)
     }
-    
-    func setComponent() {
-        let parsedResult = ParsingManager.shared.parse(name: "exposition_universelle_1900", to: ExpoIntroduction.self)
-        switch parsedResult {
-        case .success(let parsedData):
-            titleLabel.text = parsedData.title
-            visitorCountLabel.text = String(parsedData.visitors)
-            locationLabel.text = parsedData.location
-            durationLabel.text = parsedData.duration
-            descriptionTextView.text = parsedData.description
-        case .failure(let parsedError):
-            showAlert(error: parsedError)
-        }
-    }
 }
 
+//MARK:- Bind data with contents
+extension WorldsExpoViewController {
+    private func queryData() {
+        let expoIntroductionFileName = "exposition_universelle_1900"
+        let parsedResult = ParsingManager.shared.parse(name: expoIntroductionFileName, to: ExpoIntroduction.self)
+        
+        switch parsedResult {
+        case .success(let introductionContents):
+            bind(with: introductionContents)
+        case .failure(let parsingError):
+            showAlert(error: parsingError)
+        }
+    }
+    
+    private func bind(with introductionContents: ExpoIntroduction) {
+        titleLabel.text = introductionContents.title
+        visitorCountLabel.text = String(introductionContents.visitors)
+        locationLabel.text = introductionContents.location
+        durationLabel.text = introductionContents.duration
+        descriptionTextView.text = introductionContents.description
+    }
+}
