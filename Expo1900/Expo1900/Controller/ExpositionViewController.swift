37e12ba381bbd2d8be1d5c5357b6b65543b91eac
YongwooKim <keepingitflow@gmail.com>
2021-07-12 19:09:07 +0900

    feat : Exposition ViewController 화면 구현
    ViewController IBOulets 연결 및 JSONDecoder를 이용해서 Exposition 인스턴스 바인딩 및 화면에 UI  업데이트

--- Expo1900/Expo1900/Controller/ExpositionViewController.swift
+++ Expo1900/Expo1900/Controller/ExpositionViewController.swift
@@ -12,9 +12,28 @@ class ExpositionViewController: UIViewController {
     @IBOutlet weak var locationLabel: UILabel!
     @IBOutlet weak var durationLabel: UILabel!
     @IBOutlet weak var descriptionLabel: UILabel!
+    var exposition: Exposition?
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let jsonDecoder = JSONDecoder()
+        
+        guard let dataAsset = NSDataAsset(name: "exposition_universelle_1900") else {
+            return
+        }
+        
+        do {
+            self.exposition = try jsonDecoder.decode(Exposition.self, from: dataAsset.data)
+        } catch {
+            print(error.localizedDescription)
+        }
+        
+        titleLabel.text = exposition?.title
+        visitorsLabel.text = exposition?.visitors?.description
+        locationLabel.text = exposition?.location
+        durationLabel.text = exposition?.duration
+        descriptionLabel.text = exposition?.description
     }
 }
 
