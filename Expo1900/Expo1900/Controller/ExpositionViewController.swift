c971b15fe37f8f55101b35628c3a370abdca6ede
soll4u <dasollpark@gmail.com>
2021-07-12 23:26:36 +0900

    feat: ExpositionViewController UI 변경 ScrollView 구현, numberFormatter 적용, updateUI() 기능 분리, 프로퍼티 감시자가 옵저버로 추가된 MVC 패턴 적용

--- Expo1900/Expo1900/Controller/ExpositionViewController.swift
+++ Expo1900/Expo1900/Controller/ExpositionViewController.swift
@@ -1,39 +1,55 @@
 //
-//  Expo1900 - ViewController.swift
-//  Created by yagom. 
+//  Expo1900 - ExpositionViewController.swift
+//  Created by Marco, Soll, Yescoach.
 //  Copyright © yagom academy. All rights reserved.
 // 
 
 import UIKit
 
 class ExpositionViewController: UIViewController {
+    
+    // MARK: @IBOutlets
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var visitorsLabel: UILabel!
     @IBOutlet weak var locationLabel: UILabel!
     @IBOutlet weak var durationLabel: UILabel!
     @IBOutlet weak var descriptionLabel: UILabel!
-    var exposition: Exposition?
     
+    // MARK: Properties
+    var exposition: Exposition? {
+        didSet {
+            self.updateUI()
+        }
+    }
+    let numberFormatter = NumberFormatter()
+    let expositionDataIdentifier = "exposition_universelle_1900"
+    
+    // MARK: Life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let jsonDecoder = JSONDecoder()
+        numberFormatter.numberStyle = .decimal
         
-        guard let dataAsset = NSDataAsset(name: "exposition_universelle_1900") else {
+        let jsonDecoder = JSONDecoder()
+        guard let dataAsset = NSDataAsset(name: expositionDataIdentifier) else {
             return
         }
-        
         do {
             self.exposition = try jsonDecoder.decode(Exposition.self, from: dataAsset.data)
         } catch {
             print(error.localizedDescription)
         }
-        
+    }
+    
+    // MARK: Functions
+    func updateUI() {
         titleLabel.text = exposition?.title
-        visitorsLabel.text = exposition?.visitors?.description
+        guard let stringNumber = numberFormatter.string(from: NSNumber(value: (exposition?.visitors)!)) else {
+            return
+        }
+        visitorsLabel.text = stringNumber
         locationLabel.text = exposition?.location
         durationLabel.text = exposition?.duration
         descriptionLabel.text = exposition?.description
     }
 }
-
