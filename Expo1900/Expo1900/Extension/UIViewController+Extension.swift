6e441786196fc3c49f2ff1640ec0b0fad7153f5d
ictechgy <ictechgy@gmail.com>
2021-07-13 15:01:59 +0900

    refactor: UIViewController Extension에 대한 리팩토링
    * MARK 주석 추가
    * 가독성을 위한 개행 추가
    * 매직 리터럴을 별도의 상수로 선언

--- Expo1900/Expo1900/Extension/UIViewController+Extension.swift
+++ Expo1900/Expo1900/Extension/UIViewController+Extension.swift
@@ -7,10 +7,13 @@
 
 import UIKit
 
+//MARK:- Error Related Alert
 extension UIViewController {
     func showAlert(error: Error) {
+        let okActionTitle = "확인"
         let alert = UIAlertController(title: nil, message: error.localizedDescription, preferredStyle: .alert)
-        let okAction = UIAlertAction(title: "확인", style: .default)
+        let okAction = UIAlertAction(title: okActionTitle, style: .default)
+        
         alert.addAction(okAction)
         self.present(alert, animated: true)
     }
