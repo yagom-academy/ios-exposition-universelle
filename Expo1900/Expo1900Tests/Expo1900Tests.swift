aaf8508b681a93e1dce569cd264e0f83918503ee
ictechgy <ictechgy@gmail.com>
2021-07-13 19:44:00 +0900

    test: parse메소드에 대한 테스트 코드 추가
    * ParsingManager의 Parse메소드가 rtf형식의 파일에 대해 어떻게 처리를 수행하는지 검증하는 테스트 코드 작성

--- Expo1900/Expo1900Tests/Expo1900Tests.swift
+++ Expo1900/Expo1900Tests/Expo1900Tests.swift
@@ -9,15 +9,15 @@ import XCTest
 @testable import Expo1900
 
 class Expo1900Tests: XCTestCase {
-    //TODO: ParsingManager의 Parse메소드가 무결한 데이터에 대해 잘 파싱을 수행하는지, 그리고 그 데이터를 모델로 잘 변환하는지
-    //      ParsingManager의 Parse메소드가 존재하지 않는 파일에 대해 어떻게 처리를 수행하는지
-    //      존재하는 파일이기는 하나 JSON파일이 아니거나 형식이 올바르지 않은경우
+    //TODO:
+    //      존재하는 파일이기는 하나 JSON파일이 아니거나 -> rtf
+    //                          형식이 올바르지 않은경우
     //      데이터와 모델의 타입이 상호 호환되지 않는경우
     let sut = ParsingManager.shared
     
     func test_테스트파일을_가져와서파싱했을때_모델타입에적절한데이터가들어올것이다() {
         //given
-        let expectInputFileName = "test_expo"
+        let expectInputFileName = "daejeon_expo"
         
         //when
         let outputValue = sut.parse(from: expectInputFileName, to: ExpoIntroduction.self)
@@ -34,6 +34,18 @@ class Expo1900Tests: XCTestCase {
         //when
         let outputValue = sut.parse(from: expectInputFileName, to: ExpoIntroduction.self)
         let expectResult: Result<ExpoIntroduction, ParsingError> = .failure(.dataSetNotFound)
+        
+        //then
+        XCTAssertEqual(outputValue, expectResult)
+    }
+    
+    func test_rtf파일을_가져와서파싱했을때_에러가발생할것이다() {
+        //given
+        let expectInputFileName = "daejeon_expo2"
+        
+        //when
+        let outputValue = sut.parse(from: expectInputFileName, to: ExpoIntroduction.self)
+        let expectResult: Result<ExpoIntroduction, ParsingError> = .failure(.decodingFailed)
         
         //then
         XCTAssertEqual(outputValue, expectResult)
