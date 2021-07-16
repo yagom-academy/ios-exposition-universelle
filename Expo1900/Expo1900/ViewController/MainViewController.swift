c611d2fb441f7d70e3e0bf2845db952782a572c7
joey-ful <isjoeyful@gmail.com>
2021-07-16 15:47:26 +0900

    feat: Add accessibility features to MainViewController
    * Apply dynamic type
    * Add voice over
    * Adjust title font size
    * Modify label texts in storyboard

--- Expo1900/Expo1900/ViewController/MainViewController.swift
+++ Expo1900/Expo1900/ViewController/MainViewController.swift
@@ -13,6 +13,7 @@ class MainViewController: UIViewController {
     @IBOutlet private weak var locationLabel: UILabel!
     @IBOutlet private weak var durationLabel: UILabel!
     @IBOutlet private weak var descriptionLabel: UILabel!
+    @IBOutlet weak var nextButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -34,11 +35,51 @@ class MainViewController: UIViewController {
 extension MainViewController {
     private func initView() {
         let currentExposition = ParsedExposition()
-        titleLabel.text = currentExposition.title
-        visitorsLabel.text = currentExposition.visitors
-        locationLabel.text = currentExposition.location
-        durationLabel.text = currentExposition.duration
-        descriptionLabel.text = currentExposition.description
+        initTitleLabel(title: currentExposition.title)
+        initVisitorLabel(parsedExposition: currentExposition)
+        initDurationLabel(parsedExposition: currentExposition)
+        initLocationLabel(parsedExposition: currentExposition)
+        initDescriptionLabel(parsedExposition: currentExposition)
+//        nextButton.adjustsImageSizeForAccessibilityContentSizeCategory = true
+        nextButton.titleLabel?.adjustsFontForContentSizeCategory = true
+        nextButton.titleLabel?.numberOfLines = .zero
+    }
+    
+    private func initTitleLabel(title: String?) {
+        titleLabel.text = title
+        titleLabel.accessibilityLabel = NSLocalizedString("제목:", comment: "제목")
+        titleLabel.accessibilityValue = NSLocalizedString(title ?? .blank, comment: "내용")
+    }
+    
+    private func initVisitorLabel(parsedExposition: ParsedExposition) {
+        let prefix = ParsedExposition.Prefix.visitor
+        let value = parsedExposition.visitors?.replacingOccurrences(of: prefix, with: String.blank)
+        visitorsLabel.text = parsedExposition.visitors
+        visitorsLabel.accessibilityLabel = prefix
+        visitorsLabel.accessibilityValue = value
+    }
+    
+    private func initDurationLabel(parsedExposition: ParsedExposition) {
+        let prefix = ParsedExposition.Prefix.duration
+        let value = parsedExposition.duration?.replacingOccurrences(of: prefix, with: String.blank)
+        durationLabel.text = parsedExposition.duration
+        durationLabel.accessibilityLabel = prefix
+        durationLabel.accessibilityValue = value
+    }
+    
+    private func initLocationLabel(parsedExposition: ParsedExposition) {
+        let prefix = ParsedExposition.Prefix.location
+        let value = parsedExposition.location?.replacingOccurrences(of: prefix, with: String.blank)
+        locationLabel.text = parsedExposition.location
+        locationLabel.accessibilityLabel = prefix
+        locationLabel.accessibilityValue = value
+    }
+    
+    private func initDescriptionLabel(parsedExposition: ParsedExposition) {
+        let prefix = "내용:"
+        descriptionLabel.text = parsedExposition.description
+        descriptionLabel.accessibilityLabel = prefix
+        descriptionLabel.accessibilityValue = parsedExposition.description
     }
     
     private func styleLabelHeaders() {
@@ -51,11 +92,16 @@ extension MainViewController {
         guard let text = label.text else {
             return
         }
-        let targetRange = (text as NSString)
+        let headRange = (text as NSString)
             .range(of: range.replacingOccurrences(of: String.colonAndSpace, with: String.blank))
-        let fontSize = UIFont.systemFont(ofSize: label.font.pointSize + 4)
+        let tailRange = NSRange(headRange.length...(text.count - 1))
+        
+        let titleFontSize = UIFont.preferredFont(forTextStyle: .title3)
+        let bodyFontSize = UIFont.preferredFont(forTextStyle: .body)
+        
         let attributedString = NSMutableAttributedString(string: text)
-        attributedString.addAttribute(.font, value: fontSize, range: targetRange)
+        attributedString.addAttribute(.font, value: titleFontSize, range: headRange)
+        attributedString.addAttribute(.font, value: bodyFontSize, range: tailRange)
         
         label.attributedText = attributedString
     }
