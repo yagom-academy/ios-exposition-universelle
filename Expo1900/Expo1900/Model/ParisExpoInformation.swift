ee88e697aa9cd873075562795c8c865fb7605f93
hoBahk <qudgh849@gmail.com>
2021-12-06 17:59:59 +0900

    feat: ParisExpoInformation JSON 포멧 데이터 모델 타입 구현 #1

---
+++ Expo1900/Expo1900/Model/ParisExpoInformation.swift
@@ -0,0 +1,25 @@
+//
+//  ParisExpoInformation.swift
+//  Expo1900
+//
+//  Created by 박병호 on 2021/12/06.
+//
+
+import Foundation
+
+struct ParisExpoInformation: Codable {
+    let title: String
+    let visitors: Int
+    let location: String
+    let duration: String
+    let description: String
+
+    enum CodingKeys: CodingKey {
+        case title
+        case visitors
+        case location
+        case duration
+        case description
+    }
+}
+
