46e9a9e326be5e7f002517eeb82412ac4cb447c8
hwangjeha <hyhpwang@gmail.com>
2021-12-06 15:48:52 +0900

    feat: ExpoInformation 타입 구현 #1

---
+++ Expo1900/Expo1900/Model/ExpoInformation.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+struct ExpoInformation: Decodable {
+    let title: String
+    let visitors: Int
+    let location: String
+    let duration: String
+    let description: String
+}
