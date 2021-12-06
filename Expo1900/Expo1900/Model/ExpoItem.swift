1c6458a3c7fe8e82ef8d5d5459472b223ac47b6f
hoBahk <qudgh849@gmail.com>
2021-12-06 16:51:07 +0900

    feat: JSON 포멧 데이터 모델 타입 구현 #1

---
+++ Expo1900/Expo1900/Model/ExpoItem.swift
@@ -0,0 +1,23 @@
+//
+//  ExpoItem.swift
+//  Expo1900
+//
+//  Created by 박병호 on 2021/12/06.
+//
+
+import Foundation
+
+struct ExpoItem: Codable {
+    let name: String
+    let imageName: String
+    let shortDesc: String
+    let desc: String
+
+    enum CodingKeys: String, CodingKey {
+        case name
+        case imageName = "image_name"
+        case shortDesc = "short_desc"
+        case desc
+    }
+}
+
