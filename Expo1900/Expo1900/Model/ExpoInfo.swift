1af2b6ed79f947a4870072722df59d6d4654c64e
namu <simjh27@gmail.com>
2021-12-06 20:36:06 +0900

    feat: ExpoInfo 모델타입 정의

---
+++ Expo1900/Expo1900/Model/ExpoInfo.swift
@@ -0,0 +1,10 @@
+
+import Foundation
+
+struct ExpoInfo: Decodable {
+    let title: String
+    let visitors: Int
+    let location: String
+    let duration: String
+    let description: String
+}
