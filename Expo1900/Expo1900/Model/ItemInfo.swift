e88ef52aac4eaa0c6eb0f3a8925bdc221f18fceb
forestjae <forestsj30@gmail.com>
2021-12-07 16:40:25 +0900

    feat: ItemInfo 모델 타입 정의

---
+++ Expo1900/Expo1900/Model/ItemInfo.swift
@@ -0,0 +1,16 @@
+
+import Foundation
+
+struct ItemInfo: Decodable {
+    let name: String
+    let imageName: String
+    let shortDescription: String
+    let description: String
+    
+    enum CodingKeys: String, CodingKey {
+        case name = "name"
+        case imageName = "image_name"
+        case shortDescription = "short_desc"
+        case description = "desc"
+    }
+}
