26af217d6983ab44d5609ee4ed9c193bf3536d9f
July911 <min95911@nate.com>
2021-12-07 17:35:07 +0900

    feat: ExpositionItem 타입 내부의 중첩타입으로 Codingkeys 열거형 생성 #1
    JSON 데이터의 key와 사용자 정의 프로퍼티 타입의 이름이 달라 Codingkey 프로토콜을 채택

--- Expo1900/Expo1900/Model/ExpositionItem.swift
+++ Expo1900/Expo1900/Model/ExpositionItem.swift
@@ -5,4 +5,12 @@ struct ExpositionItem {
     let imageName: String
     let shortDescription: String
     let description: String
+
+    enum Codingkeys: String, CodingKey {
+        case name
+        case imageName = "image_name"
+        case shortDescription = "short_desc"
+        case description = "desc"
+    }
+
 }
