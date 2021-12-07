d87a2730b5171c770bd8fadecfea498ba606d811
jane <siyeon0616@naver.com>
2021-12-07 17:56:15 +0900

    feat: ExpositionItem, Exposition 타입에 Decodable 프로토콜 채택 #1

--- Expo1900/Expo1900/Model/ExpositionItem.swift
+++ Expo1900/Expo1900/Model/ExpositionItem.swift
@@ -1,8 +1,15 @@
 import Foundation
 
-struct ExpositionItem {
+struct ExpositionItem: Decodable {
     let name: String
     let imageName: String
     let shortDescription: String
     let description: String
+    
+    enum CodingKeys: String, CodingKey {
+        case name
+        case imageName
+        case shortDescription = "short_desc"
+        case description = "desc"
+    }
 }
