ec3b5c0ad8a3b9f583e1a8b9545f06a2857eb783
ICS-Asan <carpediem1015@gmail.com>
2021-12-07 10:58:09 +0900

    feat: ExpoItems, ExpoMainInfo 파일 생성 #1

---
+++ Expo1900/Expo1900/ExpoItems.swift
@@ -0,0 +1,8 @@
+//
+//  ExpoItems.swift
+//  Expo1900
+//
+//  Created by Seul Mac on 2021/12/07.
+//
+
+import Foundation
---
+++ Expo1900/Expo1900/ExpoMainInfo.swift
@@ -0,0 +1,8 @@
+//
+//  ExpoMainInfo.swift
+//  Expo1900
+//
+//  Created by Seul Mac on 2021/12/07.
+//
+
+import Foundation
