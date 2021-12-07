d13cbfd1a98257445ff36e001650b59db5f0e504
junbangg <wnstjr4620@naver.com>
2021-12-07 12:12:44 +0900

    feat: ExpoItems 타입 구현 #2
    - JSON 데이터와 매칭할 타입 구현

--- Expo1900/Expo1900/ExpoItems.swift
+++ Expo1900/Expo1900/ExpoItems.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+
+typealias ExpoItems = [ExpoItem]
+
+struct ExpoItem: Codable {
+    let name: String
+    let imageName: String
+    let summary: String
+    let explanation: String
+
+    enum CodingKeys: String, CodingKey {
+        case name
+        case imageName = "image_name"
+        case summary = "short_desc"
+        case explanation = "desc"
+    }
+}
