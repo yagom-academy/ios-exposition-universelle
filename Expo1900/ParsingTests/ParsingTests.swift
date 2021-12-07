07305bfb547b1711b1e19202578aa44fb9b87d75
namu <simjh27@gmail.com>
2021-12-07 17:30:46 +0900

    test: items에 대한 Parsing이 적절하게 이루어지는지 테스트하는 코드 구현

--- Expo1900/ParsingTests/ParsingTests.swift
+++ Expo1900/ParsingTests/ParsingTests.swift
@@ -9,7 +9,7 @@ class ParsingTests: XCTestCase {
     override func tearDownWithError() throws {
     }
     
-    func testParsingItems() {
+    func testParsingExpoInfo() {
         guard let expo1900 = NSDataAsset(name: "exposition_universelle_1900", bundle: .main)?.data else {
             XCTFail()
             return
@@ -17,6 +17,16 @@ class ParsingTests: XCTestCase {
         let decoder = JSONDecoder()
         
         XCTAssertNoThrow(try decoder.decode(ExpoInfo.self, from: expo1900))
+    }
+    
+    func testParsingItems() {
+        guard let items = NSDataAsset(name: "items", bundle: .main)?.data else {
+            XCTFail()
+            return
+        }
+        let decoder = JSONDecoder()
+        
+        XCTAssertNoThrow(try decoder.decode([ItemInfo].self, from: items))
     }
 }
 
