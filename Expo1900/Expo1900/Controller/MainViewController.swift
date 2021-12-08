cc88de1f7f90d213cc5e13f395313e1072808899
yim2627 <yim2627@gmail.com>
2021-12-08 18:16:45 +0900

    ✨ feat: Main 화면 Storyboard UI 구성 및 아울렛변수 연결 #2

--- Expo1900/Expo1900/Controller/MainViewController.swift
+++ Expo1900/Expo1900/Controller/MainViewController.swift
@@ -7,6 +7,13 @@
 import UIKit
 
 class MainViewController: UIViewController {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var posterImageView: UIImageView!
+    @IBOutlet weak var visitorsLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var durationLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
