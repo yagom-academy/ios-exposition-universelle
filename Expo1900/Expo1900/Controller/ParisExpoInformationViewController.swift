c61267d41acbb87d326d6126bbae690e8ee2d5b8
hoBahk <qudgh849@gmail.com>
2021-12-08 22:01:07 +0900

    feat: JSON 데이터를 메인 화면의 Label, ImageView 에 넣어주는 메서드 구현 #2

--- Expo1900/Expo1900/Controller/ParisExpoInformationViewController.swift
+++ Expo1900/Expo1900/Controller/ParisExpoInformationViewController.swift
@@ -17,8 +17,23 @@ class ParisExpoInformationViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        setLabel()
+        setImage()
         // Do any additional setup after loading the view.
     }
     
+    func setImage() {
+        parisExpoPosterImage.image = UIImage(named: "poster")
+    }
+    
+    func setLabel() {
+        let jsonName = "exposition_universelle_1900"
+        let result = JSONParser<ParisExpoInformation>.decode(from: jsonName)
+
+        titleLabel.text = result?.title
+        visitorLabel.text = result?.visitors.description
+        locationLabel.text = result?.location
+        durationLabel.text = result?.duration
+        descriptionLabel.text = result?.description
+    }
 }
