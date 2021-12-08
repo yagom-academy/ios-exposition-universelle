6a38861a08ddbd4d4f0b03b09d8d2924e24ea557
aCafela-coffee <acafelacoffee@gmail.com>
2021-12-08 15:51:53 +0900

    refactor: ExpositionInformation 계산 프로퍼티 추가
    화면에 표시할 값을 계산 프로퍼티로 만듬

--- Expo1900/Expo1900/Model/ExpositionInformation.swift
+++ Expo1900/Expo1900/Model/ExpositionInformation.swift
@@ -1,7 +1,34 @@
+import Foundation
+
 struct ExpositionInformation: Decodable {
     let title: String
-    let visitors: Int
-    let location: String
-    let duration: String
+    private let visitorsInformation: Int
+    private let locationInformation: String
+    private let durationInformation: String
     let description: String
+    
+    var visitors: String {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .decimal
+        guard let visitorsNumber = formatter.string(for: visitorsInformation) else {
+            return "잘못된 방문객 정보"
+        }
+        return "방문객 : \(visitorsNumber)명"
+    }
+    
+    var location: String {
+        return "개최지 : \(locationInformation)"
+    }
+    
+    var duration: String {
+        return "개최 기간 : \(durationInformation)"
+    }
+    
+    private enum CodingKeys: String, CodingKey {
+        case title
+        case visitorsInformation = "visitors"
+        case locationInformation = "location"
+        case durationInformation = "duration"
+        case description
+    }
 }
