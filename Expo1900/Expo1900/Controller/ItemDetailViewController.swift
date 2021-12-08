76796fdbb890e77540e335f9d2e35f63c0c3922d
yim2627 <yim2627@gmail.com>
2021-12-08 16:05:55 +0900

    ✨ feat: 각 ViewController 생성 후 VC 클래스 생성 #2 #3 #4

---
+++ Expo1900/Expo1900/Controller/ItemDetailViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ItemDetailViewController.swift
+//  Expo1900
+//
+//  Created by 임지성 on 2021/12/08.
+//
+
+import UIKit
+
+class ItemDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
