15b5f2c21cb86fbeab9941a0a501637c05fa6c69
Jager-yoo <y73447jh@gmail.com>
2021-12-08 21:24:24 +0900

    feat: IBOutlet 으로 메인 화면의 UILabel, UIImageView 연결 #2
    - 메인 화면(파리만국박람회정보) 뷰컨 새로 생성하고 스토리보드와 연결

---
+++ Expo1900/Expo1900/Controller/ParisExpoInformationViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ParisExpoInformationViewController.swift
+//  Expo1900
+//
+//  Created by 예거 on 2021/12/08.
+//
+
+import UIKit
+
+class ParisExpoInformationViewController: UIViewController {
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var parisExpoPosterImage: UIImageView!
+    @IBOutlet weak var visitorLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var durationLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+}
