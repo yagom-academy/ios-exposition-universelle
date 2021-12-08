33dd802090a0aa208e6d3f9cbff5b37043fb2610
Ari <lee_ari95@icloud.com>
2021-12-08 18:06:01 +0900

    ♻️  refactor: JSONParse 타입의 decode 메소드 반환타입을 Result로 수정 #1
    - 옵셔널이 아닌 에러나 데이터를 반환할 수 있도록 개선
    - 에러처리에 따른 JSONError 타입 추가

--- Expo1900/Expo1900/Model/JSONParse.swift
+++ Expo1900/Expo1900/Model/JSONParse.swift
@@ -1,12 +1,25 @@
 import UIKit
 
+enum JSONError: LocalizedError {
+    case notFound
+    
+    var errorDescription: String? {
+        switch self {
+        case .notFound:
+            return "데이터를 불러오지 못했습니다."
+        }
+    }
+}
+
 enum JSONParse<T: Decodable> {
-    static func decode(fileName: String) -> T? {
+    static func decode(fileName: String) -> Result<T, JSONError>  {
         guard let dataAsset: NSDataAsset = NSDataAsset(name: fileName) else {
-            return nil
+            return .failure(.notFound)
         }
         let decoder: JSONDecoder = JSONDecoder()
-        let decodedData: T? = try? decoder.decode(T.self, from: dataAsset.data)
-        return decodedData
+        guard let decodedData: T = try? decoder.decode(T.self, from: dataAsset.data) else {
+            return .failure(.notFound)
+        }
+        return .success(decodedData)
     }
 }
