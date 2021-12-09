a2bf795610428a8546ecd85404eb63800f884b89
forestjae <forestsj30@gmail.com>
2021-12-09 15:01:03 +0900

    feat: UI요소 IBOutlet 추가

--- Expo1900/Expo1900/Controller/ViewController.swift
+++ Expo1900/Expo1900/Controller/ViewController.swift
@@ -7,7 +7,13 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var visitorValueLabel: UILabel!
+    @IBOutlet weak var locationValueLabel: UILabel!
+    @IBOutlet weak var durationValueLabel: UILabel!
+    @IBOutlet weak var descriptionTextView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
