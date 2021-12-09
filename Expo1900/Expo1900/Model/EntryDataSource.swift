d9186204c3c3cefa24188f73eb292c82f1eda967
Jager-yoo <y73447jh@gmail.com>
2021-12-10 01:21:53 +0900

    refactor: 사용하지 않는 numberOfSections 메서드 삭제 #2

--- Expo1900/Expo1900/Model/EntryDataSource.swift
+++ Expo1900/Expo1900/Model/EntryDataSource.swift
@@ -9,10 +9,6 @@ import UIKit
 
 class EntryDataSource: NSObject, UITableViewDataSource {
     
-    func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
-    }
-    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return KoreanEntry.count
     }
