9bcb348abdcf9ca21b5d7394d52bfa0564d852ab
forestjae <forestsj30@gmail.com>
2021-12-09 20:34:06 +0900

    feat: ItemsViewController 생성 및 스토리보드 연결

---
+++ Expo1900/Expo1900/Controller/ItemsViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ItemsViewController.swift
+//  Expo1900
+//
+//  Created by 이승재 on 2021/12/09.
+//
+
+import UIKit
+
+class ItemsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setTitle()
+    }
+    
+    func setTitle() {
+        self.title = "한국의 출품작"
+    }
+}
