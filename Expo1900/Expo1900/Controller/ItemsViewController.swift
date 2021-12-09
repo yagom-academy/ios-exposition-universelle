6ced2ae4a302587f34694c045318e15a93dade51
namu <simjh27@gmail.com>
2021-12-09 23:08:31 +0900

    feat: ItemsViewController TableView DataSource 프로토콜 채택 및 기본 메서드 구현

--- Expo1900/Expo1900/Controller/ItemsViewController.swift
+++ Expo1900/Expo1900/Controller/ItemsViewController.swift
@@ -9,12 +9,25 @@ import UIKit
 
 class ItemsViewController: UIViewController {
 
+    @IBOutlet weak var itemsTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setTitle()
+        itemsTableView.dataSource = self
     }
     
     func setTitle() {
         self.title = "한국의 출품작"
+    }
+}
+
+extension ItemsViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
     }
 }
