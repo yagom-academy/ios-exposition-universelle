176ae8592c5a1449e5cb3c5144676bb6ba1b68f9
llghdud921 <llghdud921@naver.com>
2021-12-09 20:43:16 +0900

    feat: CustomButtonStackView 초기 설정 메소드 구현 #4

--- Expo1900/Expo1900/Controller/ViewController.swift
+++ Expo1900/Expo1900/Controller/ViewController.swift
@@ -2,8 +2,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var customButtonStackView: CustomButtonStackView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        customButtonStackView.setUp()
     }
 
 
--- Expo1900/Expo1900/View/CustomButtonStackView.swift
+++ Expo1900/Expo1900/View/CustomButtonStackView.swift
@@ -4,4 +4,18 @@ class CustomButtonStackView: UIStackView {
     @IBOutlet private weak var leftImageView: UIImageView!
     @IBOutlet private weak var button: UIButton!
     @IBOutlet private weak var rightImageView: UIImageView!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    func setUp() {
+        leftImageView.image = UIImage(named: "flag")
+        rightImageView.image = UIImage(named: "flag")
+        button.titleLabel?.text = "한국의 출품작 보러가기"
+    }
 }
