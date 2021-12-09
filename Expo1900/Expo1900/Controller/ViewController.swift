2656e373d52f6bd8b446d3453e9d5295f967b3df
yeha <dev.yeha@gmail.com>
2021-12-09 16:17:00 +0900

    feat: ExpoMainInformation JSON 파일을 디코딩하는 기능 구현 #7

--- Expo1900/Expo1900/Controller/ViewController.swift
+++ Expo1900/Expo1900/Controller/ViewController.swift
@@ -1,6 +1,6 @@
 //
 //  Expo1900 - ViewController.swift
-//  Created by yagom. 
+//  Created by yeha.
 //  Copyright © yagom academy. All rights reserved.
 // 
 
@@ -8,11 +8,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let expoMainInformationJSONFile = "exposition_universelle_1900"
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        guard let expoMainInformation = decodeExpoMainInformationJsonData() else {
+            return
+        }
     }
 
-
+    func decodeExpoMainInformationJsonData() -> ExpoMainInformation? {
+        var expoMainInformation: ExpoMainInformation?
+        let jsonDecoder = JSONDecoder()
+        
+        do {
+            let jsonData = try convertToNSDataAsset(from: expoMainInformationJSONFile)
+            expoMainInformation = try jsonDecoder.decode(ExpoMainInformation.self, from: jsonData.data)
+        } catch {
+            print(error.localizedDescription)
+        }
+        return expoMainInformation
+    }
+    
+    func convertToNSDataAsset(from file: String) throws -> NSDataAsset  {
+        guard let jsonData = NSDataAsset(name: file) else {
+            throw fatalError()
+        }
+        return jsonData
+    }
 }
 
