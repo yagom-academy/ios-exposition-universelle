827713aaeb8060cf48cd03629a511c0092354dce
Jager-yoo <y73447jh@gmail.com>
2021-12-09 23:55:11 +0900

    feat: EntryDetailViewController 파일 생성 #2
    - 스토리보드 UI 구현
    - Image, Label -> IBOutlet 연결

---
+++ Expo1900/Expo1900/Controller/EntryDetailViewController.swift
@@ -0,0 +1,19 @@
+//
+//  EntryDetailViewController.swift
+//  Expo1900
+//
+//  Created by 유재호 on 2021/12/09.
+//
+
+import UIKit
+
+class EntryDetailViewController: UIViewController {
+    @IBOutlet weak var entryImage: UIImageView!
+    @IBOutlet weak var entryDescriptionLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
