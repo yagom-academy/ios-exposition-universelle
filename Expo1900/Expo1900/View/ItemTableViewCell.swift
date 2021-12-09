ac23537a746d09cf9a735b4e33957c22102d3a40
forestjae <forestsj30@gmail.com>
2021-12-09 22:58:30 +0900

    feat: ItemTableViewCell타입 추가 및 셀 내부 UI요소 IBOutlet 연결

---
+++ Expo1900/Expo1900/View/ItemTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  ItemTableViewCell.swift
+//  Expo1900
+//
+//  Created by 이승재 on 2021/12/09.
+//
+
+import UIKit
+
+class ItemTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var itemImageView: UIImageView!
+    @IBOutlet weak var itemTitleLabel: UILabel!
+    @IBOutlet weak var itemDescriptionLabel: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
