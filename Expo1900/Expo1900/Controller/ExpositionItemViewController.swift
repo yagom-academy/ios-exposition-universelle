16a1841ef812556ebd95be62e88d550645645f18
ChaminLee <trueman9512@naver.com>
2021-12-09 16:28:32 +0900

    feat: ExpositionItemViewController 추가 #2
    - ExpositionItem내 UITextView -> UILabel 변경
    - IBOutlet 생성

---
+++ Expo1900/Expo1900/Controller/ExpositionItemViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ExpositionItemViewController.swift
+//  Expo1900
+//
+//  Created by 이차민 on 2021/12/09.
+//
+
+import UIKit
+
+class ExpositionItemViewController: UIViewController {
+
+    @IBOutlet weak var itemImage: UIImageView!
+    @IBOutlet weak var itemDescriptionLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
