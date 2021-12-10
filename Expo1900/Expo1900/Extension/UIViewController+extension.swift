d0bf64cd0bbb9f3babff1044e14b09e93b5066a7
yim2627 <yim2627@gmail.com>
2021-12-10 14:27:32 +0900

    ♻️  refactor: 얼럿을 HIG에 맞게 수정 #5

--- Expo1900/Expo1900/Extension/UIViewController+extension.swift
+++ Expo1900/Expo1900/Extension/UIViewController+extension.swift
@@ -2,7 +2,7 @@ import UIKit
 
 extension UIViewController {
     func showAlert(message: String) {
-        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
+        let cancelAction = UIAlertAction(title: "OK", style: .default)
         let alert = UIAlertController(title: nil, message: message, preferredStyle: .alert)
         alert.addAction(cancelAction)
         self.present(alert, animated: true)
