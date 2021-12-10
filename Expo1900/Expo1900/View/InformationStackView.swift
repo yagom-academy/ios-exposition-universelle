5a82c495a50c9ddba75acc68ef4b1c9d1fdde30e
hwangjeha <hyhpwang@gmail.com>
2021-12-10 11:04:20 +0900

    feat: InformationStackView 뷰 속성 및 텍스트 형식 적용 #4
    - visitors 형식 변환을 위한 DecimalNumberFormatter 구현

--- Expo1900/Expo1900/View/InformationStackView.swift
+++ Expo1900/Expo1900/View/InformationStackView.swift
@@ -17,14 +17,23 @@ class InformationStackView: UIStackView {
     }
     
     func setUp(data: ExpoInformation) {
-        titleLabel.text = data.title
+        self.spacing = 10
+        
+        titleLabel.textAlignment = .center
+        visitorsLabel.textAlignment = .center
+        locationLabel.textAlignment = .center
+        durationLabel.textAlignment = .center
+        
         titleLabel.numberOfLines = 2
-        titleLabel.font = .preferredFont(forTextStyle: .title1)
-        posterImageView.image = UIImage(named: "poster")
-        visitorsLabel.text = "\(data.visitors)"
-        locationLabel.text = data.location
-        durationLabel.text = data.duration
-        descriptionLabel.text = data.description
         descriptionLabel.numberOfLines = 0
+        
+        titleLabel.font = .preferredFont(forTextStyle: .title1)
+        
+        titleLabel.text = data.title
+        posterImageView.image = UIImage(named: "poster")
+        visitorsLabel.text = "방문객 : " + DecimalNumberFormatter.string(for: data.visitors) + " 명"
+        locationLabel.text = "개최지 : " + data.location
+        durationLabel.text = "개최 기간 : " + data.duration
+        descriptionLabel.text = data.description
     }
 }
