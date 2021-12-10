a9d85706051cbd4c4d4f45ad5098f52735b5e2c0
Ari <lee_ari95@icloud.com>
2021-12-10 10:59:12 +0900

    ✨ feat: ItemDetailViewController 화면 구성과 IBOutlet 연결 #4

--- Expo1900/Expo1900/Controller/ItemDetailViewController.swift
+++ Expo1900/Expo1900/Controller/ItemDetailViewController.swift
@@ -1,29 +1,11 @@
-//
-//  ItemDetailViewController.swift
-//  Expo1900
-//
-//  Created by 임지성 on 2021/12/08.
-//
-
 import UIKit
 
 class ItemDetailViewController: UIViewController {
-
+    
+    @IBOutlet weak var itemImageView: UIImageView!
+    @IBOutlet weak var itemDescription: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
