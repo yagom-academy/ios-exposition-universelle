e342214a094470d9046cd449d3b5555602998552
namu <simjh27@gmail.com>
2021-12-10 16:30:22 +0900

    feat: 세번째 화면 viewDidLoad에서 데이터가 뷰에 표시되도록 구현

--- Expo1900/Expo1900/Controller/ItemDetailViewController.swift
+++ Expo1900/Expo1900/Controller/ItemDetailViewController.swift
@@ -5,11 +5,12 @@ class ItemDetailViewController: UIViewController {
     
     var itemInfo: ItemInfo
     
-    @IBOutlet weak var itemImageView: UIImageView!
-    @IBOutlet weak var itemDescriptionTextView: UITextView!
+    @IBOutlet private weak var itemImageView: UIImageView!
+    @IBOutlet private weak var itemDescriptionTextView: UITextView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setViews()
     }
     
     init?(coder: NSCoder, data: ItemInfo) {
@@ -19,5 +20,10 @@ class ItemDetailViewController: UIViewController {
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setViews() {
+        self.itemImageView.image = UIImage(named: "\(itemInfo.imageName)")
+        self.itemDescriptionTextView.text = itemInfo.description
     }
 }
