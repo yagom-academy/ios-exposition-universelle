e5fe3272de3061b6edfb10a9b4988ff10d3790b9
smart8612 <smart8612@gmail.com>
2021-12-10 16:56:39 +0900

    feat: JSON 데이터를 불러와서 메인 화면에 데이터를 반영하는 기능 구현 #4

--- Expo1900/Expo1900/Controller/ViewController.swift
+++ Expo1900/Expo1900/Controller/ViewController.swift
@@ -13,6 +13,19 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        guard let data = NSDataAsset(name: "exposition_universelle_1900") else {
+            fatalError()
+        }
+        
+        let decoder = JSONDecoder()
+        let expositionData = try? decoder.decode(Exposition.self, from: data.data)
+        
+        titleLabel.text = expositionData?.title
+        imageView.image = UIImage(named: "poster")
+        visitorCountLabel.text = expositionData?.visitors.description
+        locationLabel.text = expositionData?.location
+        durationLabel.text = expositionData?.duration
+        descriptionLabel.text = expositionData?.description
     }
     
 
