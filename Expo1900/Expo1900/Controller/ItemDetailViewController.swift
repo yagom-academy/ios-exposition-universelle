0085ce9f5f42025b50f8ebceae0b6ec87f00e301
forestjae <forestsj30@gmail.com>
2021-12-10 12:19:39 +0900

    feat: ItemDetailViewController 생성 및 IBOutlet 추가

---
+++ Expo1900/Expo1900/Controller/ItemDetailViewController.swift
@@ -0,0 +1,12 @@
+
+import UIKit
+
+class ItemDetailViewController: UIViewController {
+    
+    @IBOutlet weak var itemImageView: UIImageView!
+    @IBOutlet weak var itemDescriptionTextView: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
