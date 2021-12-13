b52669843c94b5a5a782f153509377fc042b5a58
horeng2 <huski.a.a@gmail.com>
2021-12-13 20:28:18 +0900

    chore: pull 실수로 재커밋

--- Expo1900/Expo1900/Model/Exposition.swift
+++ Expo1900/Expo1900/Model/Exposition.swift
@@ -2,7 +2,7 @@ import Foundation
 
 struct Exposition: Decodable {
     let title: String
-    let visitors: Int
+    let visitorsCount: Int
     let location: String
     let duration: String
     let description: String
