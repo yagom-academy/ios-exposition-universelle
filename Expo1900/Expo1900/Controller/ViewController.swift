3b7312d302d7ce27e621e7856500d4a0b0cd9bd0
horeng2 <huski.a.a@gmail.com>
2021-12-14 00:54:52 +0900

    feat: TableViewController 및 NavigationController 추가로 두개의 뷰 연결 #4

--- Expo1900/Expo1900/Controller/ViewController.swift
+++ Expo1900/Expo1900/Controller/ViewController.swift
@@ -14,6 +14,16 @@ class ViewController: UIViewController {
     @IBOutlet weak var viewKoreaEnrtyListButton: UIButton!
     @IBOutlet weak var descriptionTextView: UITextView!
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.setNavigationBarHidden(true, animated: animated)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.setNavigationBarHidden(false, animated: animated)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         loadExpositionInformation()
