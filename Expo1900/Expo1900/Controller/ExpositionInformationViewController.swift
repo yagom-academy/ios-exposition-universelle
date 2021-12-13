98015d61f939ce84305748ea937de8e3e955ca50
yeahg-dev <yeahg.dev@gmail.com>
2021-12-13 18:38:13 +0900

    refactor: ExpositionInformation 화면 navigationBar 안보이게 구현

--- Expo1900/Expo1900/Controller/ExpositionInformationViewController.swift
+++ Expo1900/Expo1900/Controller/ExpositionInformationViewController.swift
@@ -20,6 +20,16 @@ class ExpositionInformationViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setExpositionInformation()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.setNavigationBarHidden(true, animated: animated)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.setNavigationBarHidden(false, animated: animated)
         setNavigationItem()
     }
     
