d4ee5e8f2017929e56072953accbf5130a8be3d4
oksunwoo <oksunwo@naver.com>
2021-12-14 17:02:53 +0900

    feat: 셀에 담아주기 위한 JSON 데이터를 파싱하는 메서드 구현 #4

--- Expo1900/Expo1900/Controller/TableViewController.swift
+++ Expo1900/Expo1900/Controller/TableViewController.swift
@@ -1,7 +1,8 @@
 import UIKit
 
 class TableViewController: UITableViewController {
-
+    @IBOutlet weak var entryListTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
@@ -15,6 +16,14 @@ extension TableViewController {
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 10 // 임시 값
+    }
+
+    func decodedEntryData() throws -> Entry {
+        guard let entryData = try parseEntryJSON() else {
+            throw ExpositionError.failJSONParsing
+        }
+        
+        return entryData
     }
 }
 
