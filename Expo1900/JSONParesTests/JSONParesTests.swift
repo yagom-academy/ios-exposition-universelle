9a778aed99f3a5845fc96e9ad41cadf56a40c627
Ari <lee_ari95@icloud.com>
2021-12-14 11:15:31 +0900

    ✅ test: 컴파일에러 발생했던 테스트코드 전반적으로 수정 #6

--- Expo1900/JSONParesTests/JSONParesTests.swift
+++ Expo1900/JSONParesTests/JSONParesTests.swift
@@ -2,20 +2,22 @@ import XCTest
 
 class JSONParesTests: XCTestCase {
     func test_Decode_파리만국박람회() {
-        let exposition = JSONParse<Exposition>.decode(fileName: "exposition_universelle_1900")
+        let parsing = JSONParse<Exposition>.decode(fileName: FileName.exposition)
+        let result: Exposition? = try? parsing.get()
         
-        XCTAssertEqual(exposition?.visitors, 48130300)
-        XCTAssertEqual(exposition?.location, "프랑스 파리")
-        XCTAssertTrue(exposition!.title.contains("파리 만국박람회"))
-        XCTAssertTrue(exposition!.description.contains("도래를 환영하고"))
+        XCTAssertEqual(result?.visitors, 48130300)
+        XCTAssertEqual(result?.location, "프랑스 파리")
+        XCTAssertTrue(result!.title.contains("파리 만국박람회"))
+        XCTAssertTrue(result!.description.contains("도래를 환영하고"))
     }
     
     func test_Decode_아이템() {
-        let expositionItem = JSONParse<[ExpositionItem]>.decode(fileName: "items")
-        
-        XCTAssertEqual(expositionItem!.count, 13)
-        XCTAssertTrue(expositionItem![0].name.contains("직지"))
-        XCTAssertTrue(expositionItem![1].name.contains("불경"))
-        XCTAssertTrue(expositionItem![2].name.contains("팔만"))
+        let parsing = JSONParse<[ExpositionItem]>.decode(fileName: FileName.items)
+        let result: [ExpositionItem]? = try? parsing.get()
+
+        XCTAssertEqual(result?.count, 13)
+        XCTAssertTrue(result![0].name.contains("직지"))
+        XCTAssertTrue(result![1].name.contains("불경"))
+        XCTAssertTrue(result![2].name.contains("팔만"))
     }
 }
