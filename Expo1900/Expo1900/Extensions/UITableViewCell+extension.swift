4d807e8289397bc9f4e4ff4071a1c0dad62721a5
hoBahk <qudgh849@gmail.com>
2021-12-15 21:16:26 +0900

    refactor: extension 타입 변경 #2
    - NSObject -> UITableViewCell (reuseIdentifier 타입 프로퍼티)

--- Expo1900/Expo1900/Extensions/UITableViewCell+extension.swift
+++ Expo1900/Expo1900/Extensions/UITableViewCell+extension.swift
@@ -1,13 +1,13 @@
 //
-//  NSObject+extension.swift
+//  UITableViewCell+extension.swift
 //  Expo1900
 //
 //  Created by 박병호 on 2021/12/15.
 //
 
-import Foundation
+import UIKit
 
-extension NSObject {
+extension UITableViewCell {
     static var reuseIdentifier: String {
         return String(describing: self)
     }
