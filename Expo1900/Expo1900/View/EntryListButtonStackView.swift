86f31928d76a54ef0673062e613a4bef3180eaad
wooyani77 <wooyani77@gmail.com>
2021-12-15 14:53:57 +0900

    refactor: InformationStackView alignment center로 변경 #7

--- Expo1900/Expo1900/View/EntryListButtonStackView.swift
+++ Expo1900/Expo1900/View/EntryListButtonStackView.swift
@@ -12,11 +12,17 @@ class EntryListButtonStackView: UIStackView {
     override func awakeFromNib() {
         super.awakeFromNib()
         setUpView()
+        setUpLayout()
     }
     
     private func setUpView() {
         leftImageView.image = UIImage(named: "flag")
         rightImageView.image = UIImage(named: "flag")
         entryListButton.setTitle("한국의 출품작 보러가기", for: .normal)
+    }
+    
+    private func setUpLayout() {
+        entryListButton.titleLabel?.adjustsFontSizeToFitWidth = true
+        entryListButton.titleLabel?.numberOfLines = 1
     }
 }
