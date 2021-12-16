c5cf031cd47b136c025f8cc03c7b6ffd15fcc391
llghdud921 <llghdud921@naver.com>
2021-12-16 16:44:09 +0900

    refactor: ExpoInformationView 내 image accessibility 적용 #7

--- Expo1900/Expo1900/View/InformationStackView.swift
+++ Expo1900/Expo1900/View/InformationStackView.swift
@@ -14,6 +14,7 @@ class InformationStackView: UIStackView {
     
     override func awakeFromNib() {
         super.awakeFromNib()
+        setUpAccessibilty()
         setUpStackViewSpacing()
         setUpLabelAlignment()
         setUpLabelTextLines()
@@ -41,6 +42,11 @@ class InformationStackView: UIStackView {
         durationLabel.attributedText = durationAttributedText
         
         descriptionLabel.text = data.description
+    }
+    
+    private func setUpAccessibilty() {
+        posterImageView.isAccessibilityElement = true
+        posterImageView.accessibilityLabel = "파리 만국박람회 포스터"
     }
     
     private func setUpStackViewSpacing() {
