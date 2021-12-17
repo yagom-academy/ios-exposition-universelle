9ab4e35cc744e2a4d9c070b7914f1f756dd783af
zoe <kny0651@naver.com>
2021-12-17 17:41:24 +0900

    feat: Cell의 각 요소에 Voice Over 설정

--- Expo1900/Expo1900/View/EntryTableViewCell.swift
+++ Expo1900/Expo1900/View/EntryTableViewCell.swift
@@ -13,6 +13,13 @@ class EntryTableViewCell: UITableViewCell {
     }
     
     func configureAccessibility(from entry: Entry) {
-        accessibilityHint = "누르면 \(entry.name) 상세정보 화면으로 이동합니다"
+        guard let entryNameLabel = entryNameLabel,
+              let entryShortDescriptionLabel = entryShortDescriptionLabel else {
+                  return
+              }
+        accessibilityElements = [entryNameLabel, entryShortDescriptionLabel]
+        
+        entryNameLabel.accessibilityHint = "누르면 \(entry.name) 상세정보 화면으로 이동합니다"
+        entryShortDescriptionLabel.accessibilityHint = "누르면 \(entry.name) 상세정보 화면으로 이동합니다"
     }
 }
