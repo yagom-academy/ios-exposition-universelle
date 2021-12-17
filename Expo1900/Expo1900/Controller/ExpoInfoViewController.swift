e1f91047c2dba4faab2951c53198d6007499faf3
namu <simjh27@gmail.com>
2021-12-17 17:01:53 +0900

    refactor: setViewsToDefault메서드를 세부기능별 함수 분리

--- Expo1900/Expo1900/Controller/ExpoInfoViewController.swift
+++ Expo1900/Expo1900/Controller/ExpoInfoViewController.swift
@@ -32,39 +32,66 @@ class ExpoInfoViewController: UIViewController {
 // MARK: - Private Method
 
 extension ExpoInfoViewController {
+    
     private func setViewsToDefault() {
         self.navigationController?.navigationBar.topItem?.title =
             ExpoStringLiteral.expoInfoTitle.text
+        setViewsWithParsedData()
+        configureTitleLabel()
+        configureVisitorValueLabel()
+        configureLocationValueLabel()
+        configureDurationValueLabel()
+        configureTransitionButton()
+    }
+    
+    private func setViewsWithParsedData() {
         do {
             let data = try Parser.parsedExpoInfo()
             self.titleLabel.text = data.formattedTitle
             self.visitorValueLabel.text = data.formattedVisitors
             self.locationValueLabel.text = data.location
             self.durationValueLabel.text = data.duration
-            self.titleLabel.numberOfLines = .zero
-            self.titleLabel.font = ExpoFont.expoInfoTitleFont
-            self.titleLabel.adjustsFontForContentSizeCategory = true
-            self.visitorValueLabel.font = ExpoFont.expoInfoBodyFont
-            self.visitorValueLabel.adjustsFontForContentSizeCategory = true
-            self.locationValueLabel.font = ExpoFont.expoInfoBodyFont
-            self.locationValueLabel.adjustsFontForContentSizeCategory = true
-            self.durationValueLabel.font = ExpoFont.expoInfoBodyFont
-            self.durationValueLabel.adjustsFontForContentSizeCategory = true
-            self.durationValueLabel.accessibilityLabel = durationValueLabel.text?.replacingOccurrences(of: "-", with: "부터")
-            self.descriptionTextView.setAttribute(with: " " + data.description)
-            self.transitionButton.titleLabel?.font = ExpoFont.expoInfoBodyFont
-            self.transitionButton.titleLabel?.adjustsFontForContentSizeCategory = true
-            self.transitionButton.titleLabel?.numberOfLines = 0
-            self.transitionButton.titleLabel?.textAlignment = .center
-            
-            guard let transitionButtonTitleLabelHeightAnchor = self.transitionButton.titleLabel?.heightAnchor else {
-                return
-            }
-            self.transitionButton.translatesAutoresizingMaskIntoConstraints = false
-            self.transitionButton.titleLabel?.translatesAutoresizingMaskIntoConstraints = false
-            self.transitionButton.heightAnchor.constraint(equalTo: transitionButtonTitleLabelHeightAnchor).isActive = true
+            self.durationValueLabel.accessibilityLabel =
+                data.duration.replacingOccurrences(of: "-", with: "부터")
+            self.descriptionTextView.setAttribute(with: data.description)
         } catch let error {
             showAlert(message: error.localizedDescription)
+        }
+    }
+    
+    private func configureTitleLabel() {
+        self.titleLabel.numberOfLines = .zero
+        self.titleLabel.font = ExpoFont.expoInfoTitleFont
+        self.titleLabel.adjustsFontForContentSizeCategory = true
+    }
+    
+    private func configureVisitorValueLabel() {
+        self.visitorValueLabel.font = ExpoFont.expoInfoBodyFont
+        self.visitorValueLabel.adjustsFontForContentSizeCategory = true
+    }
+    
+    private func configureLocationValueLabel() {
+        self.locationValueLabel.font = ExpoFont.expoInfoBodyFont
+        self.locationValueLabel.adjustsFontForContentSizeCategory = true
+    }
+    
+    private func configureDurationValueLabel() {
+        self.durationValueLabel.font = ExpoFont.expoInfoBodyFont
+        self.durationValueLabel.adjustsFontForContentSizeCategory = true
+    }
+    
+    private func configureTransitionButton() {
+        self.transitionButton.titleLabel?.font = ExpoFont.expoInfoBodyFont
+        self.transitionButton.titleLabel?.adjustsFontForContentSizeCategory = true
+        self.transitionButton.titleLabel?.numberOfLines = .zero
+        self.transitionButton.titleLabel?.textAlignment = .center
+        self.transitionButton.translatesAutoresizingMaskIntoConstraints = false
+        self.transitionButton.titleLabel?.translatesAutoresizingMaskIntoConstraints = false
+        if let transitionButtonTitleLabelHeightAnchor =
+            self.transitionButton.titleLabel?.heightAnchor {
+            self.transitionButton.heightAnchor
+                .constraint(equalTo: transitionButtonTitleLabelHeightAnchor)
+                .isActive = true
         }
     }
     
