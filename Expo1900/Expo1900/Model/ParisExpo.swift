1e0b9acd6b273d9740e3acdec20f0c0b0f2f4c2b
Quokkaa <rktrkdml@gmail.com>
2022-04-11 21:32:00 +0900

    feat: ParisExpo 타입 구현

---
+++ Expo1900/Expo1900/Model/ParisExpo.swift
@@ -0,0 +1,15 @@
+//
+//  ParisExpo.swift
+//  Expo1900
+//
+//  Created by LIMGAUI on 2022/04/11.
+
+import Foundation
+
+struct ParisExpo: Codable {
+    var title: String
+    var visitors: Int
+    var location: String
+    var duration: String
+    var description: String
+}
