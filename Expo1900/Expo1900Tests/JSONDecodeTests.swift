2ef809dc1f8be3187ba95af3850914a11753b082
Lingo <llingo@kakao.com>
2022-04-11 21:39:36 +0900

    test: JSONDecoder 인스턴스를 전역으로 분리

--- Expo1900/Expo1900Tests/JSONDecodeTests.swift
+++ Expo1900/Expo1900Tests/JSONDecodeTests.swift
@@ -10,21 +10,31 @@ import XCTest
 
 final class JSONDecodeTests: XCTestCase {
   
+  private var decoder: JSONDecoder?
+  
+  override func setUpWithError() throws {
+    try super.setUpWithError()
+    self.decoder = JSONDecoder()
+  }
+  
+  override func tearDownWithError() throws {
+    try super.tearDownWithError()
+    self.decoder = nil
+  }
+  
   func testDecode_WhenExpoDataProvided_ShouldNotThrowError() {
     // given when then
-    let decoder = JSONDecoder()
     if let path = Bundle.main.path(forResource: "exposition_universelle_1900", ofType: "json"),
        let data = try? Data(contentsOf: URL(fileURLWithPath: path)) {
-      XCTAssertNoThrow(try decoder.decode(Expo.self, from: data))
+      XCTAssertNoThrow(try decoder?.decode(Expo.self, from: data))
     }
   }
   
   func testDecode_WhenExpoItemDataProvided_ShouldNotThrowError() {
     // given when then
-    let decoder = JSONDecoder()
     if let path = Bundle.main.path(forResource: "items", ofType: "json"),
        let data = try? Data(contentsOf: URL(fileURLWithPath: path)) {
-      XCTAssertNoThrow(try decoder.decode(ExpoItem.self, from: data))
+      XCTAssertNoThrow(try decoder?.decode(ExpoItem.self, from: data))
     }
   }
 }
