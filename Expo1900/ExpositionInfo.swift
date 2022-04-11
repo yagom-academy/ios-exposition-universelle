59c54755816ab095dee5ea9553ad0087c18f64a6
dnwhd0112 <dnwhd0112@naver.com>
2022-04-11 20:26:27 +0900

    feat: ExpositionInfo 구조체 구현

---
+++ Expo1900/ExpositionInfo.swift
@@ -0,0 +1,16 @@
+//
+//  ExpositionInfo.swift
+//  Expo1900
+//
+//  Created by 우롱차, marisol on 2022/04/11.
+//
+
+import Foundation
+
+struct ExpositionInfo {
+    var title: String
+    var visitors: Int
+    var location: String
+    var duration: String
+    var description: String
+}
