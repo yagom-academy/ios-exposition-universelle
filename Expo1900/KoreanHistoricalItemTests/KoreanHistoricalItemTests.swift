863cd1876b20331b77880ba13e24e5a02e2437d8
Tiana <97kth01@naver.com>
2022-04-11 22:07:41 +0900

    test : KoreanHistoricalItemTests 구현

---
+++ Expo1900/KoreanHistoricalItemTests/KoreanHistoricalItemTests.swift
@@ -0,0 +1,45 @@
+//
+//  KoreanHistoricalItemTests.swift
+//  KoreanHistoricalItemTests
+//
+//  Created by 김태현 on 2022/04/11.
+//
+
+import XCTest
+@testable import Expo1900
+
+class KoreanHistoricalItemTests: XCTestCase {
+    var sut: KoreanHistoricalItem!
+    
+    override func setUpWithError() throws {
+        sut = KoreanHistoricalItem(name: "name", imageName: "image_name", shortDesciption: "short_desc", description: "desc")
+    }
+    
+    override func tearDownWithError() throws {
+        sut = nil
+    }
+    
+    func test_jsondata를_decoder했을때_값추출이_정상적으로되는지() throws {
+        let json = """
+        [
+            {
+                "name": "직지심체요절",
+                "image_name": "jikji",
+                "short_desc": "백운화상 경한(景閑)이 1372년에 초록한 불교 서적",
+                "desc": "《'백운화상초록불조직지심체요절》(白雲和尙抄錄佛祖直指心體要節)은 백운화상 경한(景閑)이 1372년"
+            }
+        ]
+        """.data(using: .utf8)!
+        
+        let decoder = JSONDecoder()
+        let items = try decoder.decode([KoreanHistoricalItem].self, from: json)
+        
+        for item in items {
+            XCTAssertEqual(item.name, "직지심체요절")
+            XCTAssertEqual(item.imageName, "jikji")
+            XCTAssertEqual(item.shortDesciption, "백운화상 경한(景閑)이 1372년에 초록한 불교 서적")
+            XCTAssertEqual(item.description, "《'백운화상초록불조직지심체요절》(白雲和尙抄錄佛祖直指心體要節)은 백운화상 경한(景閑)이 1372년")
+        }
+    }
+    
+}
