ccb6868757b522bdaab19cb735890718d5a05014
dnwhd0112 <dnwhd0112@naver.com>
2022-04-11 19:53:37 +0900

    Feat: Modal 타입 구현

---
+++ Expo1900/Images.swift
@@ -0,0 +1,14 @@
+//
+//  Images.swift
+//  Expo1900
+//
+//  Created by 우롱차, marisol on 2022/04/11.
+//
+
+import Foundation
+
+struct Image {
+    var filename: String
+    var idiom: String
+    var scale: String
+}
---
+++ Expo1900/Info.swift
@@ -0,0 +1,13 @@
+//
+//  Info.swift
+//  Expo1900
+//
+//  Created by 우롱차, marisol on 2022/04/11.
+//
+
+import Foundation
+
+struct info {
+    var author: String
+    var version: Int
+}
