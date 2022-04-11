954e27c2b0056763254119b1adff90a766acadec
Eddy <kimkyunghun3@naver.com>
2022-04-11 20:46:54 +0900

    ✨[FEAT] : 만국박람회 포스터 Decodable 구조체 구현

--- Expo1900/Expo1900/WorldFairPoster.swift
+++ Expo1900/Expo1900/WorldFairPoster.swift
@@ -6,3 +6,11 @@
 //
 
 import Foundation
+
+struct WorldFairPoster: Codable {
+    var title: String
+    var visitors: Int
+    var location: String
+    var duration: String
+    var description: String
+}
