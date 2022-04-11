59433474cda4aec27163beac0c7224ebd9e95601
Red <seryprk@gmail.com>
2022-04-11 22:19:45 +0900

    💎feat : Item 구조체 구현

---
+++ Expo1900/Expo1900/Model.swift
@@ -0,0 +1,22 @@
+//
+//  Model.swift
+//  Expo1900
+//
+//  Created by Doogie, Red on 2022/04/11.
+//
+
+import Foundation
+
+struct Item: Codable {
+    let name: String
+    let imageName: String
+    let shortDescription: String
+    let description: String
+    
+    private enum CodingKeys: String, CodingKey {
+        case name
+        case imageName = "image_name"
+        case shortDescription = "short_desc"
+        case description = "desc"
+    }
+}
