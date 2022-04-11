487120007b29eab71d1193b5a0f44c94a767e297
Taeangel <wngks2050@gmail.com>
2022-04-11 18:29:17 +0900

    feat: Model 폴더에 Expo.swift 파일 생성
    구조체 Expo에 Decodable 프로토콜 채택

---
+++ Expo1900/Expo1900/Model/Expo.swift
@@ -0,0 +1,14 @@
+//
+//  Exposition.swift
+//  Expo1900
+//
+//  Created by song on 2022/04/11.
+//
+struct Expo: Decodable {
+  let title: String
+  let cicitors: String
+  let location: String
+  let duration: String
+  let description: String
+}
+
