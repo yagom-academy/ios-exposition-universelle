1357bfb958afabdea5172add02627f1dc4454890
Taeangel <wngks2050@gmail.com>
2022-04-12 18:49:15 +0900

    refactor: Expo와 Heritage 프로퍼티를 옵셔널 타입으로 변경, Codable을 Decodable로 변경
    값이 존재하지 않을때 fatal오류를 방지하기 위해 옵셔널 타입으로 변경
    Encodable을 사용하지 않아 Codable을 Decodable로 변경

--- Expo1900/Expo1900/Model/Expo.swift
+++ Expo1900/Expo1900/Model/Expo.swift
@@ -5,10 +5,10 @@
 //  Created by Taeangel, dudu on 2022/04/11.
 //
 
-struct Expo: Codable {
-  let title: String
-  let visitors: Int
-  let location: String
-  let duration: String
-  let description: String
+struct Expo: Decodable {
+  let title: String?
+  let visitors: Int?
+  let location: String?
+  let duration: String?
+  let description: String?
 }
--- Expo1900/Expo1900/Model/Heritage.swift
+++ Expo1900/Expo1900/Model/Heritage.swift
@@ -5,11 +5,11 @@
 //  Created by Taeangel, dudu on 2022/04/11.
 //
 
-struct Heritage: Codable {
-  let name: String
-  let imageName: String
-  let shortDescription: String
-  let description: String
+struct Heritage: Decodable {
+  let name: String?
+  let imageName: String?
+  let shortDescription: String?
+  let description: String?
   
   enum CodingKeys: String, CodingKey {
     case name
