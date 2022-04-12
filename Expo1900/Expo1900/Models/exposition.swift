279bfa3ae93e44f03d5d678260ecff3384d54703
malrang <ruehd7924@gmail.com>
2022-04-12 14:24:13 +0900

    ✨[feat]: JSON 데이터를 매칭할 exposition 파일 및 struct 추가

---
+++ Expo1900/Expo1900/Models/exposition.swift
@@ -0,0 +1,16 @@
+//
+//  exposition.swift
+//  Expo1900
+//
+//  Created by 김동욱 on 2022/04/12.
+//
+
+import Foundation
+
+struct Exposition: Codable {
+    let title: String
+    let visitors: Int
+    let location: String
+    let duration: String
+    let description: String
+}
