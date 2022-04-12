46323ce3087d48f62711135931e42f88597f600a
dudu <whqtkf12@naver.com>
2022-04-12 19:01:20 +0900

    chore: 잘못입력한 주석 제거

--- Expo1900/DecodeTests/DecodeTests.swift
+++ Expo1900/DecodeTests/DecodeTests.swift
@@ -9,8 +9,6 @@ import XCTest
 @testable import Expo1900
 
 class DecodeTests: XCTestCase {
-  //asseName이 준비됬을때, ParseManager<Expo>.parse를 수행하면
-  //결과가 nil이 아니여야 한다
   func test_assetName이_준비됐을때_Expo타입의_PaseManager_parse의_결과가_Nil이_아니여야한다() {
     //given
     let assetName = "exposition_universelle_1900"
