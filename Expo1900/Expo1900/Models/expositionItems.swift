b7d84d656c88b116e06858507b8bd473884fd102
malrang <ruehd7924@gmail.com>
2022-04-12 14:43:03 +0900

    ✨[feat]: ExpositionItems 에 맞게 디코딩 해주는 parse() 메서드 구현

--- Expo1900/Expo1900/Models/expositionItems.swift
+++ Expo1900/Expo1900/Models/expositionItems.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import UIKit
 
 struct ExpositionItems: Codable {
     let name: String
@@ -18,5 +19,11 @@ struct ExpositionItems: Codable {
         case imageName = "image_name"
         case shortDescription = "short_desc"
         case description = "desc"
+    }
+    
+    static func parse() -> [ExpositionItems]? {
+        guard let asset = NSDataAsset(name: "ExpositionItems") else { return nil }
+        let expositionItems = try? JSONDecoder().decode([ExpositionItems].self, from: asset.data)
+        return expositionItems
     }
 }
