c0f463a92410d4cf143fdb1c3adc12d715a0c723
saafaaari <111dltldnjs@naver.com>
2022-04-12 11:05:16 +0900

    add: Expo Model 생성

---
+++ Expo1900/Expo1900/Model/Expo.swift
@@ -0,0 +1,14 @@
+//
+//  Expo.swift
+//  Expo1900
+//
+//  Created by 사파리, papri on 2022/04/12.
+//
+
+import Foundation
+
+struct Expo: Codable {
+    let title: String
+    let visitors: Int
+    let location, duration, description: String
+}
