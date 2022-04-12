298a8025b429fee5a6fe824e7ecdc0c579155040
GoMyungju171 <mooooongdu171@gmail.com>
2022-04-12 14:31:16 +0900

    ✨[feat]: JSON 데이터를 매칭할 ExpositionItems 파일 및 struct 추가

---
+++ Expo1900/Expo1900/Models/expositionItems.swift
@@ -0,0 +1,22 @@
+//
+//  expositionItems.swift
+//  Expo1900
+//
+//  Created by 고명주 on 2022/04/12.
+//
+
+import Foundation
+
+struct ExpositionItems: Codable {
+    let name: String
+    let imageName: String
+    let shortDescription: String
+    let description: String
+    
+    enum CodingKeys: String, CodingKey {
+        case name
+        case imageName = "image_name"
+        case shortDescription = "short_desc"
+        case description = "desc"
+    }
+}
