521190a9050cd333de11c30fd65713e3db57afb1
Quokkaa <rktrkdml@gmail.com>
2022-04-13 16:59:46 +0900

    refactor: test를 위한 Equatable 채택

--- Expo1900/ParisExpoTests/ParisExpoTests.swift
+++ Expo1900/ParisExpoTests/ParisExpoTests.swift
@@ -8,6 +8,12 @@
 import XCTest
 @testable import Expo1900
 
+extension ParisExpo: Equatable {
+    public static func == (lhs: ParisExpo, rhs: ParisExpo) -> Bool {
+        return lhs.title == rhs.title && lhs.description == rhs.description && lhs.duration == rhs.duration && lhs.visitors == rhs.visitors && lhs.location == rhs.location
+    }
+}
+
 final class ParisExpoTests: XCTestCase {
     func test_jsondata를_decode했을때_값추출이_정상적으로되는지() throws {
         let json = """
@@ -23,10 +29,12 @@ final class ParisExpoTests: XCTestCase {
         let decoder = JSONDecoder()
         let item = try decoder.decode(ParisExpo.self, from: json)
         
-        XCTAssertEqual(item.title, "파리 만국박람회 1900(L'Exposition de Paris 1900)")
-        XCTAssertEqual(item.visitors, 48130300)
-        XCTAssertEqual(item.location, "프랑스 파리")
-        XCTAssertEqual(item.duration, "1900. 04. 14 - 1900. 11. 12")
-        XCTAssertEqual(item.description, "1900년 파리 만국박람회(지금의 세계 박람회[World’s")
+        let result = ParisExpo(title: "파리 만국박람회 1900(L'Exposition de Paris 1900)",
+                               visitors: 48130300,
+                               location: "프랑스 파리",
+                               duration: "1900. 04. 14 - 1900. 11. 12",
+                               description: "1900년 파리 만국박람회(지금의 세계 박람회[World’s")
+        
+        XCTAssertEqual(result, item)
     }
 }
