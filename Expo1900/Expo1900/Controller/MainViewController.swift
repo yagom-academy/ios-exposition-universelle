8e213e4612614aebe738851b5ddf1b4f168ce826
Lingo <llingo@kakao.com>
2022-04-13 14:10:45 +0900

    feat: MainViewController UI 요소 연결

--- Expo1900/Expo1900/Controller/MainViewController.swift
+++ Expo1900/Expo1900/Controller/MainViewController.swift
@@ -7,7 +7,17 @@ import UIKit
 
 final class MainViewController: UIViewController {
   
+  @IBOutlet private weak var titleLabel: UILabel!
+  @IBOutlet private weak var posterImageView: UIImageView!
+  @IBOutlet private weak var visitorsLabel: UILabel!
+  @IBOutlet private weak var locationLabel: UILabel!
+  @IBOutlet private weak var durationLabel: UILabel!
+  @IBOutlet private weak var descriptionLabel: UILabel!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
+  }
+  
+  @IBAction private func didTapShowExpoItemsButton(_ sender: UIButton) {
   }
 }
