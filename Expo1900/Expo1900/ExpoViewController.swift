87882fa41bf8ea88e0b1a23c6a365705122a7b76
dudu <whqtkf12@naver.com>
2022-04-13 13:50:22 +0900

    feat: scrollView와 stackView의 제약 생성

--- Expo1900/Expo1900/ExpoViewController.swift
+++ Expo1900/Expo1900/ExpoViewController.swift
@@ -11,10 +11,51 @@ final class ExpoViewController: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
     attribute()
+    layout()
   }
   
   private func attribute() {
     view.backgroundColor = .systemBackground
+  }
+  
+  private func layout() {
+    let scrollView = UIScrollView()
+    scrollView.translatesAutoresizingMaskIntoConstraints = false
+    view.addSubview(scrollView)
+
+    scrollView.leadingAnchor.constraint(
+      equalTo: view.leadingAnchor
+    ).isActive = true
+    scrollView.trailingAnchor.constraint(
+      equalTo: view.trailingAnchor
+    ).isActive = true
+    scrollView.topAnchor.constraint(
+      equalTo: view.topAnchor
+    ).isActive = true
+    scrollView.bottomAnchor.constraint(
+      equalTo: view.bottomAnchor
+    ).isActive = true
+    
+    let stackView = UIStackView()
+    stackView.translatesAutoresizingMaskIntoConstraints = false
+    stackView.axis = .vertical
+    
+    scrollView.addSubview(stackView)
+    stackView.leadingAnchor.constraint(
+      equalTo: scrollView.leadingAnchor
+    ).isActive = true
+    stackView.trailingAnchor.constraint(
+      equalTo: scrollView.trailingAnchor
+    ).isActive = true
+    stackView.topAnchor.constraint(
+      equalTo: scrollView.topAnchor
+    ).isActive = true
+    stackView.bottomAnchor.constraint(
+      equalTo: scrollView.bottomAnchor
+    ).isActive = true
+    stackView.widthAnchor.constraint(
+      equalTo: scrollView.widthAnchor
+    ).isActive = true
   }
   
   private func prepareData() -> (Expo, UIImage)? {
