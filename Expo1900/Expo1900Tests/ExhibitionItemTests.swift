a5ae926ca8a7f5a28693e36035d8b7ee718be127
JoSH0318 <jsh9870@naver.com>
2022-04-13 23:00:59 +0900

    test: ExpositionItems Tests 코드 수정 및 추가
    * Decodable+Extension 파일 그룹 변경

--- Expo1900/Expo1900Tests/ExhibitionItemTests.swift
+++ Expo1900/Expo1900Tests/ExhibitionItemTests.swift
@@ -12,27 +12,36 @@ class ExhibitionItemTests: XCTestCase {
     private var sut: [ExhibitionItem]!
     
     override func setUpWithError() throws {
-        guard let asset = NSDataAsset(name: "items") else {
-            return
-        }
-        
-        self.sut = try? JSONDecoder().decode([ExhibitionItem].self, from: asset.data)
+        try super.setUpWithError()
+        self.sut = try? [ExhibitionItem].decode(from: "items").get()
     }
 
     override func tearDownWithError() throws {
+        try super.tearDownWithError()
         self.sut = nil
     }
     
-    func test_items_JSON데이터에서_decode된_첫번째객체의_name값이_올바른가() {
+    func test_decode_파일이_존재하지않을때_예상된_에러인가() {
+        //given
+        let expected: DecodingError = .fileNameNotFound
+        let fakeFileName = "a"
+        // when
+        // then
+        XCTAssertThrowsError(try [ExhibitionItem].decode(from: fakeFileName).get()) { error in
+            XCTAssertEqual(error as? DecodingError, expected)
+        }
+    }
+    
+    func test_decode_JSON데이터에서_decode된_첫번째객체의_name값이_올바른가() {
         //given
         let name = "직지심체요절"
         //when
-        let firstName = sut[0].name
+        let firstName = sut.first!.name
         //then
         XCTAssertEqual(firstName, name)
     }
     
-    func test_items_JSON데이터에서_decode된_객체의_수가_올바른가() {
+    func test_decode_JSON데이터에서_decode된_객체의_수가_올바른가() {
         //given
         let jsonCount = 13
         //when
