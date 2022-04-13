7aebfba01f38ddf6d2b4680bfe4c26a69ec811d3
dudu <whqtkf12@naver.com>
2022-04-13 17:59:56 +0900

    feat: HeritageCell 구현
    내부 view 생성 및 레이아웃 설정

---
+++ Expo1900/Expo1900/Controller/HeritageCell.swift
@@ -0,0 +1,77 @@
+//
+//  HeritageCell.swift
+//  Expo1900
+//
+//  Created by Taeangel, dudu on 2022/04/13.
+//
+
+import UIKit
+
+class HeritageCell: UITableViewCell {
+  override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+    super.init(style: style, reuseIdentifier: reuseIdentifier)
+    layout()
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  private let titleLabel: UILabel = {
+    let label = UILabel()
+    label.font = .preferredFont(forTextStyle: .title1)
+    return label
+  }()
+  
+  private let descriptionLabel: UILabel = {
+    let label = UILabel()
+    label.numberOfLines = 0
+    return label
+  }()
+  
+  private let heritageImageView: UIImageView = {
+    let imgView = UIImageView()
+    imgView.translatesAutoresizingMaskIntoConstraints = false
+    imgView.contentMode = .scaleAspectFit
+    return imgView
+  }()
+  
+  private func layout() {
+    let stackView = UIStackView(arrangedSubviews: [titleLabel, descriptionLabel])
+    stackView.translatesAutoresizingMaskIntoConstraints = false
+    
+    contentView.addSubview(stackView)
+    contentView.addSubview(heritageImageView)
+    
+    heritageImageView.widthAnchor.constraint(
+      equalToConstant: 100
+    ).isActive = true
+    heritageImageView.heightAnchor.constraint(
+      equalToConstant: 100
+    ).isActive = true
+    heritageImageView.leadingAnchor.constraint(
+      equalTo: contentView.leadingAnchor,
+      constant: 10
+    ).isActive = true
+    heritageImageView.trailingAnchor.constraint(
+      equalTo: stackView.trailingAnchor,
+      constant: -10
+    ).isActive = true
+    heritageImageView.centerYAnchor.constraint(
+      equalTo: contentView.centerYAnchor
+    ).isActive = true
+    
+    stackView.trailingAnchor.constraint(
+      equalTo: contentView.trailingAnchor,
+      constant: -10
+    ).isActive = true
+    stackView.topAnchor.constraint(
+      equalTo: contentView.topAnchor,
+      constant: 10
+    ).isActive = true
+    stackView.bottomAnchor.constraint(
+      equalTo: contentView.bottomAnchor,
+      constant: -10
+    ).isActive = true
+  }
+}
