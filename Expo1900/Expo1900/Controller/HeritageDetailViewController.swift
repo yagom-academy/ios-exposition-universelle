bdefe3cbf8488ac9cf5bf98a0be5b0d0777ea5d5
dudu <whqtkf12@naver.com>
2022-04-13 20:45:45 +0900

    feat: HeritageDetailViewController 생성
    custom initalizer 구현

---
+++ Expo1900/Expo1900/Controller/HeritageDetailViewController.swift
@@ -0,0 +1,32 @@
+//
+//  HeritageDetailViewController.swift
+//  Expo1900
+//
+//  Created by Taeangel, dudu on 2022/04/13.
+//
+
+import UIKit
+
+final class HeritageDetailViewController: UIViewController {
+  
+  private let heritage: Heritage
+  
+  init(heritage: Heritage) {
+    self.heritage = heritage
+    super.init()
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    attribute()
+  }
+  
+  private func attribute() {
+    view.backgroundColor = .systemBackground
+  }
+  
+}
