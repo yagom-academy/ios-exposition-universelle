97c4a80fcd7739c8a65a715d72d7990589867051
dudu <whqtkf12@naver.com>
2022-04-13 17:41:33 +0900

    feat: TableView를 생성하고 layout 설정

--- Expo1900/Expo1900/Controller/HeritageViewController.swift
+++ Expo1900/Expo1900/Controller/HeritageViewController.swift
@@ -8,15 +8,25 @@
 import UIKit
 
 class HeritageViewController: UIViewController {
+  private let tableView = UITableView()
   
   override func viewDidLoad() {
     super.viewDidLoad()
     attribute()
-    
+    layout()
   }
   
   private func attribute() {
     view.backgroundColor = .systemBackground
     title = "한국의 출품작"
+  }
+  
+  private func layout() {
+    view.addSubview(tableView)
+    tableView.translatesAutoresizingMaskIntoConstraints = false
+    tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+    tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+    tableView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+    tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
   }
 }
