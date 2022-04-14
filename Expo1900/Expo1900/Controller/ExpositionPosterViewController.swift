e44eefaecd218a6e3dab5a356174a65cfa5519a0
malrang <ruehd7924@gmail.com>
2022-04-14 17:23:14 +0900

    ✨[feat]: ExpositionPosterViewController 프로퍼티 구현

--- Expo1900/Expo1900/Controller/ExpositionPosterViewController.swift
+++ Expo1900/Expo1900/Controller/ExpositionPosterViewController.swift
@@ -7,11 +7,18 @@
 import UIKit
 
 class ExpositionPosterViewController: UIViewController {
-
+    
+    @IBOutlet weak var visitors: UILabel!
+    @IBOutlet weak var location: UILabel!
+    @IBOutlet weak var duration: UILabel!
+    
+    @IBOutlet weak var posterTitle: UILabel!
+    @IBOutlet weak var visitorsValue: UILabel!
+    @IBOutlet weak var locationValue: UILabel!
+    @IBOutlet weak var durationValue: UILabel!
+    @IBOutlet weak var descriptions: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        
     }
 }
-
