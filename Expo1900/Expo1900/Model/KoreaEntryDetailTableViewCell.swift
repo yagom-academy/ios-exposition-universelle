a4f25c12c465bc2de91eebc8b238c5afe317f833
Eddy <kimkyunghun3@naver.com>
2022-04-14 13:55:09 +0900

    ✨[FEAT] : KoreaEntryDetail Cell 파일 생성

---
+++ Expo1900/Expo1900/Model/KoreaEntryDetailTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  KoreaEntryDetailTableViewCell.swift
+//  Expo1900
+//
+//  Created by Eddy on 2022/04/14.
+//
+
+import UIKit
+
+class KoreaEntryDetailTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
