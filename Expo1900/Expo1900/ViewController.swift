3b8220fd04b90336e37751bd532c2de47073f301
papriOS <bluecrab30@naver.com>
2022-04-14 15:54:42 +0900

    feat: UI 타입에 Expo데이터 저장

--- Expo1900/Expo1900/ViewController.swift
+++ Expo1900/Expo1900/ViewController.swift
@@ -12,11 +12,20 @@ final class ViewController: UIViewController {
     @IBOutlet weak var visitorsLabel: UILabel!
     @IBOutlet weak var locationLabel: UILabel!
     @IBOutlet weak var durationLabel: UILabel!
-    @IBOutlet weak var decriptionTextView: UITextView!
+    @IBOutlet weak var decriptionLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        guard let expoInfomation = Expo.parsingJson(name: "exposition_universelle_1900") else {
+            return
+        }
+        titleLabel.text = expoInfomation.title
+        posterImageView.image = UIImage(named: "poster")
+        visitorsLabel.text = "방문객 : \(expoInfomation.visitors)"
+        locationLabel.text = "개최지 : \(expoInfomation.location)"
+        durationLabel.text = "개최기간 : \(expoInfomation.duration)"
+        decriptionLabel.text = expoInfomation.description
+        
     }
 
 
