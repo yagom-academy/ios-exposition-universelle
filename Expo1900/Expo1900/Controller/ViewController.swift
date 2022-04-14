ec4b0d5a9718dd322646385c81c7b95c520c9636
Eddy <kimkyunghun3@naver.com>
2022-04-14 14:41:46 +0900

    ✨[FEAT] : TableViewDataSource 내부 코드 구현

--- Expo1900/Expo1900/Controller/ViewController.swift
+++ Expo1900/Expo1900/Controller/ViewController.swift
@@ -15,3 +15,19 @@ class ViewController: UIViewController {
 
 }
 
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell: KoreaEntryDetailTableViewCell = tableView.dequeueReusableCell(withIdentifier: "KoreaEntryDetailTableViewCell") as? KoreaEntryDetailTableViewCell else {
+            return KoreaEntryDetailTableViewCell()
+        }
+        cell.koreaEntryImageView.image = UIImage(named: "god")
+        cell.titleLabel.text = "hello"
+        cell.shortDescriptionLabel.text = "good"
+        
+        return cell
+    }
+}
