5a6521b4f96c71b67343480a8233f7238ef62b56
minseong <msb0987@naver.com>
2022-04-14 18:31:50 +0900

    feat: 두 번째 VC 커스템 TableCell구현

--- Expo1900/Expo1900/Controller/ExhibitionItemsViewController.swift
+++ Expo1900/Expo1900/Controller/ExhibitionItemsViewController.swift
@@ -7,8 +7,41 @@
 
 import UIKit
 
-class ExhibitionItemsViewController: UIViewController {
+class ExhibitionItemsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.tableView.dataSource = self
+        self.tableView.delegate = self
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        guard let rowCount = decodeData()?.count else {
+            return .zero
+        }
+        
+        return rowCount
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        var content = cell.defaultContentConfiguration()
+        
+        content.image = UIImage(named: decodeData()![indexPath.row].imageName)
+        content.text = decodeData()![indexPath.row].name
+        content.secondaryText = decodeData()![indexPath.row].shortDescription
+        
+        cell.contentConfiguration = content
+        
+        return cell
+    }
+    
+    private func decodeData() -> [ExhibitionItem]? {
+        let fileName = "items"
+        let decodedData = try? [ExhibitionItem].decode(from: fileName)
+        
+        return decodedData
     }
 }
