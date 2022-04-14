b17a9b9787bb0fd74e0681f72ad0e12cbc18d1c1
JoSH0318 <jsh9870@naver.com>
2022-04-15 00:13:00 +0900

    refactor: custom cell 구현 및 tableView(_cellForRowAt :) 수정
    * class ExhibitionItemsTableViewCell 타입 생성 및 IBOutlet 연결
    * custom cell를 위한 스토리보드 수정
    * extension으로 ExhibitionItemsViewController 코드 분리

---
+++ Expo1900/Expo1900/Controller/ExhibitionItemsTableViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  ExhibitionItemsTableViewCell.swift
+//  Expo1900
+//
+//  Created by mmim, Minseong.
+//
+
+import UIKit
+
+class ExhibitionItemsTableViewCell: UITableViewCell {
+    @IBOutlet weak var posterImageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var subtitleLabel: UILabel!
+}
--- Expo1900/Expo1900/Controller/ExhibitionItemsViewController.swift
+++ Expo1900/Expo1900/Controller/ExhibitionItemsViewController.swift
@@ -7,35 +7,13 @@
 
 import UIKit
 
-class ExhibitionItemsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+class ExhibitionItemsViewController: UIViewController, UITableViewDelegate {
     @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.tableView.dataSource = self
         self.tableView.delegate = self
-    }
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        guard let rowCount = decodeData()?.count else {
-            return .zero
-        }
-        
-        return rowCount
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        
-        var content = cell.defaultContentConfiguration()
-        
-        content.image = UIImage(named: decodeData()![indexPath.row].imageName)
-        content.text = decodeData()![indexPath.row].name
-        content.secondaryText = decodeData()![indexPath.row].shortDescription
-        
-        cell.contentConfiguration = content
-        
-        return cell
     }
     
     private func decodeData() -> [ExhibitionItem]? {
@@ -45,3 +23,35 @@ class ExhibitionItemsViewController: UIViewController, UITableViewDataSource, UI
         return decodedData
     }
 }
+
+extension ExhibitionItemsViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        guard let rowCount = decodeData()?.count else {
+            return .zero
+        }
+        
+        return rowCount
+    }
+    
+    func tableView(_ tableView: UITableView,
+                   cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        guard let cell = tableView
+                .dequeueReusableCell(withIdentifier: "cell",
+                                     for: indexPath) as? ExhibitionItemsTableViewCell
+        else {
+            return UITableViewCell()
+        }
+        
+        guard let decodedData = decodeData() else {
+            return UITableViewCell()
+        }
+        
+        cell.posterImageView.image = UIImage(named: decodedData[indexPath.row].imageName)
+        cell.titleLabel.text = decodedData[indexPath.row].name
+        cell.subtitleLabel.text = decodedData[indexPath.row].shortDescription
+
+        return cell
+    }
+}
+
