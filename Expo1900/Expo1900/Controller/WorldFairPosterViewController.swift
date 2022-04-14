155044fa5247dda3188b237f1c102faa9290ebaf
Eddy <kimkyunghun3@naver.com>
2022-04-14 15:32:59 +0900

    ✨[FEAT] : 첫 번째 화면에 따른 ViewController 파일 생성 및 스토리보드 연결

---
+++ Expo1900/Expo1900/Controller/WorldFairPosterViewController.swift
@@ -0,0 +1,13 @@
+//
+//  WorldFairPosterViewController.swift
+//  Expo1900
+//
+//  Created by Eddy on 2022/04/14.
+//
+
+
+import UIKit
+
+class WorldFairPosterViewController: UIViewController {
+    
+}
