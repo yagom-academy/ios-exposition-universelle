b34a48ebb551eae883dfd9f7dd26c4b8220345de
Grumpy <sewoong2890@gmail.com>
2022-04-14 11:40:51 +0900

    ✨[feat]: ExpositionMainView 구조 생성 및 내비게이션 바 숨기기 기능 추가

--- Expo1900/Expo1900/Controller/ExpositionMainViewController.swift
+++ Expo1900/Expo1900/Controller/ExpositionMainViewController.swift
@@ -2,8 +2,20 @@ import UIKit
 
 class ExpositionMainViewController: UIViewController {
 
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
+        self.navigationController?.isNavigationBarHidden = true
+        
+//        self.navigationController!.navigationBar.setBackgroundImage(UIImage(), for: .default)
+//        self.navigationController!.navigationBar.shadowImage = UIImage()
+//        self.navigationController!.navigationBar.isTranslucent = true
+        
     }
+
+    
 }
 
--- Expo1900/Expo1900/Controller/KoreanEntryViewController.swift
+++ Expo1900/Expo1900/Controller/KoreanEntryViewController.swift
@@ -4,6 +4,13 @@ class KoreanEntryViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.navigationController?.isNavigationBarHidden = false
     }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        self.navigationController?.isNavigationBarHidden = true
+    }
+    
 }
 
