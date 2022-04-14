a45844bd8f1b7582ef0a87d85e0822fce952de79
Red <seryprk@gmail.com>
2022-04-15 00:43:25 +0900

    💎feat : getItems 메서드 구현 셀에 정보 할당

--- Expo1900/Expo1900/ListViewController.swift
+++ Expo1900/Expo1900/ListViewController.swift
@@ -9,12 +9,25 @@ import UIKit
 
 class ListViewController: UIViewController {
     @IBOutlet weak var listTableView: UITableView!
+    var items: [Item] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         listTableView.dataSource = self
         listTableView.delegate = self
+        self.items = getItems()
     }
+    
+    func getItems() -> [Item] {
+        var items: [Item] = []
+        do {
+        items = try [Item].decode(with: "items")
+        } catch {
+            showAlert()
+        }
+          return items
+    }
+    
     func showAlert() {
         let alert = UIAlertController(title: "오류", message: "데이터를 불러올 수 없습니다.", preferredStyle: .alert)
         let action = UIAlertAction(title: "확인", style: .default, handler: nil)
@@ -26,15 +39,15 @@ class ListViewController: UIViewController {
 
 extension ListViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        10
+        return items.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "itemCell", for: indexPath)
         var contents = cell.defaultContentConfiguration()
-        contents.attributedText = NSAttributedString(string: "직지심체요절", attributes: [ .font: UIFont.systemFont(ofSize: 20, weight: .bold)])
-        contents.secondaryText = "1900년 파리 만국박람회(지금의 세계 박람회[World’s Fair/Exposition/Expo])는 지난 세기를 기념하고 다음 세기를 향한 발전을 가속하자는 의미에서 1900년 4월 14일부터 11월 12일까지 프랑스 파리"
-        contents.image = UIImage(named: "jikji.png")
+        contents.attributedText = NSAttributedString(string: items[indexPath.row].name, attributes: [ .font: UIFont.systemFont(ofSize: 20, weight: .bold)])
+        contents.secondaryText = items[indexPath.row].shortDescription
+        contents.image = UIImage(named: items[indexPath.row].imageName)
         contents.imageProperties.maximumSize.width = CGFloat(100)
 
         cell.contentConfiguration = contents
