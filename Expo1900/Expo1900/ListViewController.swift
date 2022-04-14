7e0ffa42ee5603e34094c2a3a9f851eed60079e0
doogie97 <chris_97@naver.com>
2022-04-15 00:10:25 +0900

    💎feat : tableView구현, cell 요소 구현

--- Expo1900/Expo1900/ListViewController.swift
+++ Expo1900/Expo1900/ListViewController.swift
@@ -2,19 +2,39 @@
 //  ListViewController.swift
 //  Expo1900
 //
-//  Created by 박세리 on 2022/04/14.
+//  Created by Doogie, Red on 2022/04/14.
 //
 
 import UIKit
 
 class ListViewController: UIViewController {
-
+    @IBOutlet weak var listTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        listTableView.dataSource = self
+        listTableView.delegate = self
+    }
+}
 
-        // Do any additional setup after loading the view.
+extension ListViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        10
     }
     
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "itemCell", for: indexPath)
+        var contents = cell.defaultContentConfiguration()
+        contents.attributedText = NSAttributedString(string: "직지심체요절", attributes: [ .font: UIFont.systemFont(ofSize: 20, weight: .bold)])
+        contents.secondaryText = "1900년 파리 만국박람회(지금의 세계 박람회[World’s Fair/Exposition/Expo])는 지난 세기를 기념하고 다음 세기를 향한 발전을 가속하자는 의미에서 1900년 4월 14일부터 11월 12일까지 프랑스 파리"
+        contents.image = UIImage(named: "jikji.png")
+        contents.imageProperties.maximumSize.width = CGFloat(100)
 
+        cell.contentConfiguration = contents
+        return cell
+    }
+}
 
+extension ListViewController: UITableViewDelegate {
+    
 }
