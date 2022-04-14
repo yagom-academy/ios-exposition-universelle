3e4ac6d0b7a4c6d7904b81686f3a5b8802a1c40a
Tiana <97kth01@naver.com>
2022-04-14 13:15:19 +0900

    feat : uploadImages 메서드 구현

--- Expo1900/Expo1900/Controller/ParisExpoVC.swift
+++ Expo1900/Expo1900/Controller/ParisExpoVC.swift
@@ -4,9 +4,33 @@
 
 import UIKit
 
-class ParisExpoVC: UIViewController {
-
+final class ParisExpoVC: UIViewController {
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var mainImageView: UIImageView!
+    @IBOutlet weak var visitorsLabel: UILabel!
+    @IBOutlet weak var visitorsNumberLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var locationDescriptionLabel: UILabel!
+    @IBOutlet weak var durationLabel: UILabel!
+    @IBOutlet weak var durationDescriptionLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var leftImageView: UIImageView!
+    @IBOutlet weak var koreanItemsButton: UIButton!
+    @IBOutlet weak var rightImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        uploadImages()
     }
+    
+    func uploadImages() {
+        mainImageView.image = UIImage(named: "poster")
+        leftImageView.image = UIImage(named: "flag")
+        rightImageView.image = UIImage(named: "flag")
+    }
+}
+
+// MARK: method
+extension ParisExpo {
+    
 }
