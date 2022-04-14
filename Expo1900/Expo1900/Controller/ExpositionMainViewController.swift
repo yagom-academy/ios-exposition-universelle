f7188f2351bac52425fbbcd1cbb076c1b8638647
Donnie <seo_17@naver.com>
2022-04-14 12:57:39 +0900

    ✨[feat]: parseJSON, insertNewLine 메서드 구현 및 Exposition IBOulet 연결

--- Expo1900/Expo1900/Controller/ExpositionMainViewController.swift
+++ Expo1900/Expo1900/Controller/ExpositionMainViewController.swift
@@ -1,21 +1,33 @@
 import UIKit
 
 class ExpositionMainViewController: UIViewController {
-
     
+    @IBOutlet weak var expoTitleLabel: UILabel!
+    @IBOutlet weak var expoImageView: UIImageView!
+    @IBOutlet weak var expoVisitorsLabel: UILabel!
+    @IBOutlet weak var expoLocationLabel: UILabel!
+    @IBOutlet weak var expoDurationLabel: UILabel!
+    @IBOutlet weak var expoDescriptionLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
         self.navigationController?.isNavigationBarHidden = true
         
-//        self.navigationController!.navigationBar.setBackgroundImage(UIImage(), for: .default)
-//        self.navigationController!.navigationBar.shadowImage = UIImage()
-//        self.navigationController!.navigationBar.isTranslucent = true
-        
+        guard let decodedData = parseJSON() else { return }
+        expoTitleLabel.text = insertNewLine(value: decodedData.title)
     }
-
     
+    private func parseJSON() -> Exposition? {
+        let decoder = JSONDecoder()
+        guard let asset = NSDataAsset(name: "exposition_universelle_1900") else { return nil }
+        let data = try? decoder.decode(Exposition.self, from: asset.data)
+        return data
+    }
+    
+    private func insertNewLine(value: String) -> String {
+        var str = value
+        str.insert("\n", at: str.firstIndex(of: "(") ?? str.endIndex)
+        return str
+    }
 }
 
