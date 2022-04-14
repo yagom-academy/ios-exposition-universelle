65d0e51029db0b8d4f06b337aad77a141db04149
doogie97 <chris_97@naver.com>
2022-04-14 13:15:52 +0900

    ✏️chore : 타입 오타 수정

--- Expo1900/Expo1900/Model/Exposition.swift
+++ Expo1900/Expo1900/Model/Exposition.swift
@@ -7,7 +7,7 @@
 
 struct Exposition: Decodable {
     let title: String
-    let visitors: String
+    let visitors: Int
     let location: String
     let duration: String
     let description: String
