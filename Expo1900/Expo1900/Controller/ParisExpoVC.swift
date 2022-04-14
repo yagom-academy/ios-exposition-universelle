e94f4ad09493786bccb1f454c9fedb738ce084f4
Quokkaa <rktrkdml@gmail.com>
2022-04-14 12:09:23 +0900

    feat: Main Storyboard UI구성
    * 오토레이 아웃 설정

---
+++ Expo1900/Expo1900/Controller/ParisExpoVC.swift
@@ -0,0 +1,12 @@
+//  Expo1900 - ViewController.swift
+//  Created by Quokkaaa.
+//  Copyright © yagom academy. All rights reserved.
+
+import UIKit
+
+class ParisExpoVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
