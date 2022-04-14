b9c0d42af95f9c868e60bee10cca43019d2bc695
onetool <xogns8696@gmail.com>
2022-04-15 01:31:50 +0900

    ✨[FEAT] : WorldFairPosterViewController의 IBOulet연결

--- Expo1900/Expo1900/Controller/WorldFairPosterViewController.swift
+++ Expo1900/Expo1900/Controller/WorldFairPosterViewController.swift
@@ -8,6 +8,14 @@
 import UIKit
 
 final class WorldFairPosterViewController: UIViewController {
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var visitorLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var durationLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+    @IBOutlet weak var worldFairPosterImageView: UIImageView!
+    @IBOutlet weak var koreaFlagImageView: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
