26248d4f89bee62656a39ee78750fde1ce8e096c
Donnie <seo_17@naver.com>
2022-04-14 16:28:33 +0900

    ✨[feat]: EntryDetailViewController구현 및 UI 배치

---
+++ Expo1900/Expo1900/Controller/EntryDetailViewController.swift
@@ -0,0 +1,18 @@
+//
+//  EntryDetailViewController.swift
+//  Expo1900
+//
+//  Created by SeoDongyeon on 2022/04/14.
+//
+
+import UIKit
+
+class EntryDetailViewController: UIViewController {
+    
+    @IBOutlet weak var detailImageView: UIImageView!
+    @IBOutlet weak var detailDescriptionLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
