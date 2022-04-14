4c0c09a0f7c5e8da812ad1db6f86ae7212c0cd6a
GoMyungju171 <mooooongdu171@gmail.com>
2022-04-14 17:32:44 +0900

    ✨[feat]: Poster namespace 구현

---
+++ Expo1900/Expo1900/Controller/Poster.swift
@@ -0,0 +1,14 @@
+//
+//  Poster.swift
+//  Expo1900
+//
+//  Created by 고명주 on 2022/04/14.
+//
+
+import Foundation
+
+enum Poster {
+    static let visitors = "방문객 :"
+    static let location = "개최지 :"
+    static let durationn = "개최 기간 :"
+}
