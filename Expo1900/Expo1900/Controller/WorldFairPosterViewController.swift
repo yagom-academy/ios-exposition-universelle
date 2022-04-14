76894234ab3594c37c558b84238f48573cb102f6
Eddy <kimkyunghun3@naver.com>
2022-04-14 17:08:01 +0900

    ✨[FEAT] : 첫화면에 NavigationBar 숨기도록 구현

--- Expo1900/Expo1900/Controller/WorldFairPosterViewController.swift
+++ Expo1900/Expo1900/Controller/WorldFairPosterViewController.swift
@@ -9,4 +9,13 @@ import UIKit
 
 final class WorldFairPosterViewController: UIViewController {
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.setNavigationBarHidden(true, animated: animated)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.setNavigationBarHidden(false, animated: animated)
+    }
 }
