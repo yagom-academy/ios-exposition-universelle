f87720a5065061749c473f776b287d157bce681d
Tiana <97kth01@naver.com>
2022-04-15 00:48:59 +0900

    feat: identifier 타입 연산 프로퍼티로 정의

---
+++ Expo1900/Expo1900/Model/UIViewController+extension.swift
@@ -0,0 +1,14 @@
+//
+//  UIViewController+extension.swift
+//  Expo1900
+//
+//  Created by 김태현 on 2022/04/15.
+//
+
+import UIKit
+
+extension UIViewController {
+    static var identifier: String {
+        return String(describing: self)
+    }
+}
