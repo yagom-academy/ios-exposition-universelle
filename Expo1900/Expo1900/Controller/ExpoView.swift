07d51bd39217a7f34ff7d191793f6edc79b321f8
Taeangel <wngks2050@gmail.com>
2022-04-15 11:16:16 +0900

    refactor: ExpoViewController를 View와 Controller로 분리

---
+++ Expo1900/Expo1900/Controller/ExpoView.swift
@@ -0,0 +1,186 @@
+//
+//  ExpoView.swift
+//  Expo1900
+//
+//  Created by Taeangel, dudu on 2022/04/15.
+//
+
+import UIKit
+
+class ExpoView: UIView {
+  private enum Const {
+    enum Image {
+      static let width: CGFloat = 40
+      static let height: CGFloat = 40
+      static let name: String = "flag"
+    }
+    enum Scroll {
+      static let top: CGFloat = 15
+      static let bottom: CGFloat = -15
+      static let leading: CGFloat = 15
+      static let trailing: CGFloat = -15
+    }
+    
+    enum Stack {
+      static let top: CGFloat = 10
+      static let bottom: CGFloat = -10
+      static let leading: CGFloat = 10
+      static let trailing: CGFloat = -10
+      static let spacing: CGFloat = 10
+    }
+    
+    enum ButtonStack {
+      static let spacing: CGFloat = 30
+    }
+    
+    enum Button {
+      static let title: String = "한국의 출품작 보러가기"
+    }
+  }
+  
+  private lazy var baseScrollView: UIScrollView = {
+    let scrollView = UIScrollView()
+    scrollView.translatesAutoresizingMaskIntoConstraints = false
+    return scrollView
+  }()
+  
+  private lazy var baseStackView: UIStackView = {
+    let stackView = UIStackView()
+    
+    stackView.translatesAutoresizingMaskIntoConstraints = false
+    stackView.axis = .vertical
+    stackView.alignment = .center
+    stackView.spacing = Const.Stack.spacing
+    return stackView
+  }()
+  
+  lazy var titleLabel: UILabel = {
+    let label = UILabel()
+    label.numberOfLines = 2
+    label.textAlignment = .center
+    label.font = .preferredFont(forTextStyle: .title1)
+    return label
+  }()
+  
+  lazy var posterImageView: UIImageView = {
+    let imageView = UIImageView()
+    imageView.contentMode = .scaleAspectFit
+    return imageView
+  }()
+  
+  lazy var visitorLabel: UILabel = {
+    let label = UILabel()
+    return label
+  }()
+  
+  lazy var locationLabel: UILabel = {
+    let label = UILabel()
+    return label
+  }()
+  
+  lazy var durationLabel: UILabel = {
+    let label = UILabel()
+    return label
+  }()
+  
+  lazy var descriptionLabel: UILabel = {
+    let label = UILabel()
+    label.numberOfLines = 0
+    return label
+  }()
+  
+  private lazy var buttonStackView: UIStackView = {
+    let stackView = UIStackView()
+    stackView.distribution = .fill
+    stackView.spacing = Const.ButtonStack.spacing
+    return stackView
+  }()
+  
+  lazy var koreaHeritageButton: UIButton = {
+    let button = UIButton()
+    button.setTitleColor(.systemBlue, for: .normal)
+    button.setTitle(Const.Button.title, for: .normal)
+    return button
+  }()
+  
+  private lazy var leftFlagImageView: UIImageView = {
+    let imageView = UIImageView(image: UIImage(named: Const.Image.name))
+    imageView.translatesAutoresizingMaskIntoConstraints = false
+    imageView.contentMode = .scaleAspectFit
+    imageView.widthAnchor.constraint(equalToConstant: Const.Image.width).isActive = true
+    imageView.heightAnchor.constraint(equalToConstant: Const.Image.height).isActive = true
+    return imageView
+  }()
+  
+  private lazy var rightFlagImageView: UIImageView = {
+    let imageView = UIImageView(image: UIImage(named: Const.Image.name))
+    imageView.translatesAutoresizingMaskIntoConstraints = false
+    imageView.contentMode = .scaleAspectFit
+    imageView.widthAnchor.constraint(equalToConstant: Const.Image.width).isActive = true
+    imageView.heightAnchor.constraint(equalToConstant: Const.Image.height).isActive = true
+    return imageView
+  }()
+  
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+    setUp()
+    layout()
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+ 
+  private func setUp() {
+    backgroundColor = .systemBackground
+    
+    addSubview(baseScrollView)
+    baseScrollView.addSubview(baseStackView)
+  
+    buttonStackView.addArrangedSubview(leftFlagImageView)
+    buttonStackView.addArrangedSubview(koreaHeritageButton)
+    buttonStackView.addArrangedSubview(rightFlagImageView)
+
+    baseStackView.addArrangedSubview(titleLabel)
+    baseStackView.addArrangedSubview(posterImageView)
+    baseStackView.addArrangedSubview(visitorLabel)
+    baseStackView.addArrangedSubview(locationLabel)
+    baseStackView.addArrangedSubview(durationLabel)
+    baseStackView.addArrangedSubview(descriptionLabel)
+    baseStackView.addArrangedSubview(buttonStackView)
+  }
+  
+  private func layout() {
+    
+    //MARK: - baseScrollView
+    
+    NSLayoutConstraint.activate([
+      baseScrollView.topAnchor.constraint(
+        equalTo: self.topAnchor,
+        constant: Const.Scroll.top
+      ),
+      baseScrollView.bottomAnchor.constraint(
+        equalTo: self.bottomAnchor,
+        constant: Const.Scroll.bottom
+      ),
+      baseScrollView.leadingAnchor.constraint(
+        equalTo: self.leadingAnchor,
+        constant: Const.Scroll.leading
+      ),
+      baseScrollView.trailingAnchor.constraint(
+        equalTo: self.trailingAnchor,
+        constant: Const.Scroll.trailing
+      )
+    ])
+    
+    //MARK: - baseStackView
+    
+    NSLayoutConstraint.activate([
+      baseStackView.leadingAnchor.constraint(equalTo: baseScrollView.leadingAnchor),
+      baseStackView.trailingAnchor.constraint(equalTo: baseScrollView.trailingAnchor),
+      baseStackView.topAnchor.constraint(equalTo: baseScrollView.topAnchor),
+      baseStackView.bottomAnchor.constraint(equalTo: baseScrollView.bottomAnchor),
+      baseStackView.widthAnchor.constraint(equalTo: baseScrollView.widthAnchor)
+    ])
+  }
+}
--- Expo1900/Expo1900/Controller/ExpoViewController.swift
+++ Expo1900/Expo1900/Controller/ExpoViewController.swift
@@ -37,52 +37,12 @@ private extension String {
 
 final class ExpoViewController: UIViewController {
   
-  private let titleLabel: UILabel = {
-    let label = UILabel()
-    label.numberOfLines = 2
-    label.textAlignment = .center
-    label.font = .preferredFont(forTextStyle: .title1)
-    return label
-  }()
-  
-  private let posterImageView: UIImageView = {
-    let imageView = UIImageView()
-    imageView.contentMode = .scaleAspectFit
-    return imageView
-  }()
-  
-  private let visitorLabel: UILabel = {
-    let label = UILabel()
-    return label
-  }()
-  
-  private let locationLabel: UILabel = {
-    let label = UILabel()
-    return label
-  }()
-  
-  private let durationLabel: UILabel = {
-    let label = UILabel()
-    return label
-  }()
-  
-  private let descriptionLabel: UILabel = {
-    let label = UILabel()
-    label.numberOfLines = 0
-    return label
-  }()
-  
-  private let koreaHeritageButton: UIButton = {
-    let button = UIButton()
-    button.setTitleColor(.systemBlue, for: .normal)
-    button.setTitle("한국의 출품작 보러가기", for: .normal)
-    return button
-  }()
+  private lazy var baseView = ExpoView(frame: view.bounds)
   
   override func viewDidLoad() {
     super.viewDidLoad()
     attribute()
-    layout()
+    bind()
   }
   
   override func viewWillAppear(_ animated: Bool) {
@@ -98,7 +58,10 @@ final class ExpoViewController: UIViewController {
   private func attribute() {
     navigationItem.backButtonTitle = "메인"
     view.backgroundColor = .systemBackground
-    
+  }
+  
+  private func bind() {
+    view = baseView
     guard let (expo, poster) = prepareData() else {
       return
     }
@@ -109,18 +72,18 @@ final class ExpoViewController: UIViewController {
     
     expoTitle.insert("\n", at: targetIndex)
     
-    titleLabel.text = expoTitle
-    posterImageView.image = poster
-    visitorLabel.text = expo.visitors?.addComma()
-    locationLabel.text = expo.location
-    durationLabel.text = expo.duration
-    descriptionLabel.text = expo.description
+    baseView.titleLabel.text = expoTitle
+    baseView.posterImageView.image = poster
+    baseView.visitorLabel.text = expo.visitors?.addComma()
+    baseView.locationLabel.text = expo.location
+    baseView.durationLabel.text = expo.duration
+    baseView.descriptionLabel.text = expo.description
     
-    visitorLabel.attributedText = visitorLabel.text?.changeFontSize(insert: "방문객")
-    locationLabel.attributedText = locationLabel.text?.changeFontSize(insert: "개최지")
-    durationLabel.attributedText = durationLabel.text?.changeFontSize(insert: "개최 기간")
+    baseView.visitorLabel.attributedText = baseView.visitorLabel.text?.changeFontSize(insert: "방문객")
+    baseView.locationLabel.attributedText = baseView.locationLabel.text?.changeFontSize(insert: "개최지")
+    baseView.durationLabel.attributedText = baseView.durationLabel.text?.changeFontSize(insert: "개최 기간")
     
-    koreaHeritageButton.addTarget(
+    baseView.koreaHeritageButton.addTarget(
       self,
       action: #selector(didTapKoreaHeritageButton(_:)),
       for: .touchUpInside
@@ -141,80 +104,5 @@ final class ExpoViewController: UIViewController {
     }
     
     return (expo, poster)
-  }
-}
-
-//MARK: - Layout
-
-extension ExpoViewController {
-  
-  private func layout() {
-    let scrollView = UIScrollView()
-    scrollView.translatesAutoresizingMaskIntoConstraints = false
-    view.addSubview(scrollView)
-    
-    let scrollViewHorizontalMargin: CGFloat = 15
-    let scrollViewVerticalMargin: CGFloat = 20
-
-    scrollView.leadingAnchor.constraint(
-      equalTo: view.leadingAnchor,
-      constant: scrollViewHorizontalMargin
-    ).isActive = true
-    scrollView.trailingAnchor.constraint(
-      equalTo: view.trailingAnchor,
-      constant: -scrollViewHorizontalMargin
-    ).isActive = true
-    scrollView.topAnchor.constraint(
-      equalTo: view.topAnchor,
-      constant: scrollViewVerticalMargin
-    ).isActive = true
-    scrollView.bottomAnchor.constraint(
-      equalTo: view.bottomAnchor,
-      constant: -scrollViewVerticalMargin
-    ).isActive = true
-    
-    let stackView = UIStackView()
-    stackView.translatesAutoresizingMaskIntoConstraints = false
-    stackView.axis = .vertical
-    stackView.alignment = .center
-    stackView.spacing = 10
-    
-    scrollView.addSubview(stackView)
-    stackView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor).isActive = true
-    stackView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor).isActive = true
-    stackView.topAnchor.constraint(equalTo: scrollView.topAnchor).isActive = true
-    stackView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor).isActive = true
-    stackView.widthAnchor.constraint(equalTo: scrollView.widthAnchor).isActive = true
-
-    let buttonStackView = UIStackView()
-    buttonStackView.distribution = .fill
-    buttonStackView.spacing = 30
-    
-    let flagImageWidth: CGFloat = 40
-    let flagImageHeight: CGFloat = 40
-    
-    let leftFlagImageView = UIImageView(image: UIImage(named: "flag"))
-    leftFlagImageView.translatesAutoresizingMaskIntoConstraints = false
-    leftFlagImageView.contentMode = .scaleAspectFit
-    leftFlagImageView.widthAnchor.constraint(equalToConstant: flagImageWidth).isActive = true
-    leftFlagImageView.heightAnchor.constraint(equalToConstant: flagImageHeight).isActive = true
-    
-    let rightFlagImageView = UIImageView(image: UIImage(named: "flag"))
-    rightFlagImageView.translatesAutoresizingMaskIntoConstraints = false
-    rightFlagImageView.contentMode = .scaleAspectFit
-    rightFlagImageView.widthAnchor.constraint(equalToConstant: flagImageWidth).isActive = true
-    rightFlagImageView.heightAnchor.constraint(equalToConstant: flagImageHeight).isActive = true
-
-    buttonStackView.addArrangedSubview(leftFlagImageView)
-    buttonStackView.addArrangedSubview(koreaHeritageButton)
-    buttonStackView.addArrangedSubview(rightFlagImageView)
-
-    stackView.addArrangedSubview(titleLabel)
-    stackView.addArrangedSubview(posterImageView)
-    stackView.addArrangedSubview(visitorLabel)
-    stackView.addArrangedSubview(locationLabel)
-    stackView.addArrangedSubview(durationLabel)
-    stackView.addArrangedSubview(descriptionLabel)
-    stackView.addArrangedSubview(buttonStackView)
   }
 }
