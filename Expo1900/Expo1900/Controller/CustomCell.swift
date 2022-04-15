392df9608396bb2ce704000f0689f0eadb92a234
malrang <ruehd7924@gmail.com>
2022-04-15 12:20:06 +0900

    ✨[feat]: tableView 구현에 필요한 class CustomCell 구현 및 tableViewDatasource 구현

---
+++ Expo1900/Expo1900/Controller/CustomCell.swift
@@ -0,0 +1,15 @@
+//
+//  CustomCell.swift
+//  Expo1900
+//
+//  Created by 김동욱 on 2022/04/15.
+//
+
+import Foundation
+import UIKit
+
+class CustomCell: UITableViewCell {
+    @IBOutlet weak var itemImage: UIView!
+    @IBOutlet weak var itemTitle: UILabel!
+    @IBOutlet weak var itemShortDiscription: UILabel!
+}
--- Expo1900/Expo1900/Controller/EntryListViewController.swift
+++ Expo1900/Expo1900/Controller/EntryListViewController.swift
@@ -9,10 +9,32 @@ import UIKit
 
 class EntryListViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    let expositionItems = [ExpositionItems].parse(JsonFile.items)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableView.dataSource = self
+    }
+}
+
+extension EntryListViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return expositionItems?.count ?? .zero
     }
     
-    @IBOutlet weak var tableView: UITableView!
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: CustomCell = tableView.dequeueReusableCell(withIdentifier: "CustomCell", for: indexPath) as? CustomCell ?? CustomCell()
+        return cell
+    }
+}
+
+class CustomCell: UITableViewCell {
+    @IBOutlet weak var itemImage: UIView!
+    @IBOutlet weak var itemTitle: UILabel!
+    @IBOutlet weak var itemShortDiscription: UILabel!
+    override func prepareForReuse() {
+        super.prepareForReuse()
+    }
 }
