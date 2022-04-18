3e90eb66b65f83827b08cd70ef8409a584099230
onetool <xogns8696@gmail.com>
2022-04-18 11:44:40 +0900

    ✨[FEAT] : 오류 처리를 위한 showAlert 함수 생성

---
+++ Expo1900/Expo1900/Utils/Error/Alert.swift
@@ -0,0 +1,17 @@
+//
+//  ShowAlert.swift
+//  Expo1900
+//
+//  Created by 김태훈 on 2022/04/18.
+//
+
+import UIKit
+
+extension UIViewController {
+    func showAlert(alertTitle: String,
+                   okTitle: String?,
+                   okAction: (() -> ())? = nil,
+                   noTitle: String? = nil,
+                   noAction: (() -> ())? = nil) {
+    }
+}
