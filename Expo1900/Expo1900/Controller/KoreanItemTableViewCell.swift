b1ef79f9379e306412d6074779db6d282be0eb36
Tiana <97kth01@naver.com>
2022-04-18 21:02:12 +0900

    refactor: 아울렛 변수를 강제 추출에서 옵셔널로 변경

--- Expo1900/Expo1900/Controller/KoreanItemTableViewCell.swift
+++ Expo1900/Expo1900/Controller/KoreanItemTableViewCell.swift
@@ -8,13 +8,13 @@
 import UIKit
 
 class KoreanItemTableViewCell: UITableViewCell {
-    @IBOutlet weak var mainImageView: UIImageView!
-    @IBOutlet weak var titleLabel: UILabel!
-    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var mainImageView: UIImageView?
+    @IBOutlet weak var titleLabel: UILabel?
+    @IBOutlet weak var descriptionLabel: UILabel?
     
     func assignValue(from item: KoreanHistoricalItem) {
-        titleLabel.text = item.name
-        descriptionLabel.text = item.shortDesciption
-        mainImageView.image = UIImage(named: item.imageName)
+        titleLabel?.text = item.name
+        descriptionLabel?.text = item.shortDesciption
+        mainImageView?.image = UIImage(named: item.imageName)
     }
 }
