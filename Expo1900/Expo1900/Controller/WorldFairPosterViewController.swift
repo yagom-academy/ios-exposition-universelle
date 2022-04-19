ba7cb92a8c9ce1a994c6b8c6a0f0f9632a0214e4
onetool <xogns8696@gmail.com>
2022-04-19 12:54:00 +0900

    ♻️[REFACTOR] : 에러 처리 방법 변경 (alert -> throws)

--- Expo1900/Expo1900/Controller/WorldFairPosterViewController.swift
+++ Expo1900/Expo1900/Controller/WorldFairPosterViewController.swift
@@ -35,10 +35,9 @@ final class WorldFairPosterViewController: UIViewController {
         updateImageViews()
     }
     
-    private func decodeWorldFairPoster() {
+    private func decodeWorldFairPoster() throws -> WorldFairPoster {
         guard let worldFairPosterData = Parser<WorldFairPoster>.parse(name: "exposition_universelle_1900") else {
-            showAlert(alertTitle: "오류 발생, 다시 한번 실행해주세요", okTitle: "OK")
-            return
+            throw ExpoError.emptyValueError
         }
         return worldFairPosterData
     }
