2043ed40fa577c90857f53d6a1e43b182366733f
saafaaari <111dltldnjs@naver.com>
2022-04-19 14:31:59 +0900

    refactor: 테스트 케이스 네이밍 수정

--- Expo1900/Expo1900ModelTests/Expo1900ModelTests.swift
+++ Expo1900/Expo1900ModelTests/Expo1900ModelTests.swift
@@ -10,7 +10,7 @@ import XCTest
 
 class Expo1900ModelTests: XCTestCase {
 
-    func test_Heritage_Type_parsingJson_작동하는가() {
+    func test_Heritage_Type_parsingJson_작동하면_FirstItemTitle_Equal_직지심체요절() {
         //given
         let firstHeritageItemName = "직지심체요절"
         
@@ -22,14 +22,14 @@ class Expo1900ModelTests: XCTestCase {
         XCTAssertEqual(itemName, firstHeritageItemName)
     }
     
-    func test_Expo_Type_parsingJson_작동하는가() {
+    func test_Expo_Type_parsingJson_작동하면_ExpoTitle_Equal_파리만국박람회1900() {
         //given
-        let Expotitle = "파리 만국박람회 1900(L'Exposition de Paris 1900)"
+        let expoTitle = "파리 만국박람회 1900(L'Exposition de Paris 1900)"
         
         //when
         let title = Expo.parsingJson(name: "exposition_universelle_1900")?.title
         
         //then
-        XCTAssertEqual(title, Expotitle)
+        XCTAssertEqual(title, expoTitle)
     }
 }
