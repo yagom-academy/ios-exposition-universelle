98d46fb4a72988db1f187ca5c83248f21debc198
onetool <xogns8696@gmail.com>
2022-04-19 16:09:05 +0900

    ♻️[REFACTOR] : Enum으로 String 변경

--- Expo1900/Expo1900/Model/AssetSeeker.swift
+++ Expo1900/Expo1900/Model/AssetSeeker.swift
@@ -10,14 +10,14 @@ import Foundation
 struct AssetSeeker {
 
     func matchKoreaEntryDetailAsset() throws -> [KoreaEntryDetail] {
-        guard let unwrappedKoreaEntryData = Parser<[KoreaEntryDetail]>.parse(name: "items") else {
+        guard let unwrappedKoreaEntryData = Parser<[KoreaEntryDetail]>.parse(name: ExpoStringEnum.koreaEntryDataName) else {
             throw ExpoError.emptyValueError
         }
         return unwrappedKoreaEntryData
     }
     
     func matchWorldFairPosterAsset() throws -> WorldFairPoster {
-        guard let unwrppaedWorldFairPosterData = Parser<WorldFairPoster>.parse(name: "exposition_universelle_1900") else {
+        guard let unwrppaedWorldFairPosterData = Parser<WorldFairPoster>.parse(name: ExpoStringEnum.worldFairPosterName) else {
             throw ExpoError.emptyValueError
         }
         return unwrppaedWorldFairPosterData
