dec93387250b4ca682955c2776fd08e7a78e0043
dnwhd0112 <dnwhd0112@naver.com>
2022-04-19 15:23:06 +0900

    refactor: 테스트 객체 제거

--- Expo1900/JsonManager.swift
+++ Expo1900/JsonManager.swift
@@ -41,34 +41,3 @@ struct JsonManager: JsonManagerable {
         return expositionInfo
     }
 }
-
-
-struct JsonManager2: JsonManagerable {
-    func decodedItems() throws -> [Item] {
-        let jsonDecoder = JSONDecoder()
-         
-        guard let itemData = NSDataAsset(name: DataFileName.items) else {
-            throw ExpoError.noFileError
-        }
-        
-        guard let items = try? jsonDecoder.decode([Item].self, from: itemData.data) else {
-            throw ExpoError.decodingError
-        }
-        
-        return items
-    }
-    
-    func decodedExpositionInfo() throws -> ExpositionInfo {
-        let jsonDecoder = JSONDecoder()
-        
-        guard let expositionInfoData = NSDataAsset(name: DataFileName.expositionInfo) else {
-            throw ExpoError.noFileError
-        }
-  
-        guard let expositionInfo = try? jsonDecoder.decode(ExpositionInfo.self, from: expositionInfoData.data) else {
-            throw ExpoError.decodingError
-        }
-        
-        return expositionInfo
-    }
-}
