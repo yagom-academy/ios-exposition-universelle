982e6a6552133a1d81f84c6e42c471349eda55c6
Eddy <kimkyunghun3@naver.com>
2022-04-21 17:23:03 +0900

    ♻️[REFACTOR] : titleLabel, shortDescriptionLabel이 글자 크기에 맞게 늘어나도록 변경

--- Expo1900/Expo1900/Model/KoreaEntryDetailTableViewCell.swift
+++ Expo1900/Expo1900/Model/KoreaEntryDetailTableViewCell.swift
@@ -15,7 +15,9 @@ final class KoreaEntryDetailTableViewCell: UITableViewCell {
     func makeCell(koreaEntryData: KoreaEntryDetail) {
         koreaEntryImageView.image = UIImage(named: koreaEntryData.imageFileName)
         titleLabel.text = koreaEntryData.name
+        titleLabel.sizeToFit()
         shortDescriptionLabel.text = koreaEntryData.shortDescription
+        shortDescriptionLabel.sizeToFit()
         shortDescriptionLabel.numberOfLines = 0
         accessoryType = UITableViewCell.AccessoryType.disclosureIndicator
     }
