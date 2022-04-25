82a8a7e828780bb13670e4bb39ab851d3204be23
doogie97 <chris_97@naver.com>
2022-04-25 11:19:35 +0900

    ♻️refactor : 접근제어 추가

--- Expo1900/Expo1900/Controller/NavigationViewController.swift
+++ Expo1900/Expo1900/Controller/NavigationViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ExpoNavigationController: UINavigationController {
+final class ExpoNavigationController: UINavigationController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
