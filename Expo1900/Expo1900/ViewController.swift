2092cc979ab0be2fbee2246378ed22897189946b
웡빙 <wb277@naver.com>
2022-06-13 18:31:53 +0900

    feat : "DataManager" 구조체를 만들어서 JSONDecoder 구현

--- Expo1900/Expo1900/ViewController.swift
+++ Expo1900/Expo1900/ViewController.swift
@@ -7,12 +7,11 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    let dataManager = DataManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        dataManager.getData()
     }
-
-
 }
 
