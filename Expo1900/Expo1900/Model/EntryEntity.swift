b285e20ef335e1786da2369be16c9b64cef4f680
BaekGom <jihunmm02@gmail.com>
2022-06-13 21:59:35 +0900

    feat : "ViewController" 에서 사용될 UIImage로 변환하는 computed property 구현

--- Expo1900/Expo1900/Model/EntryEntity.swift
+++ Expo1900/Expo1900/Model/EntryEntity.swift
@@ -4,11 +4,18 @@
 //
 //  Created by Derrick kim on 2022/06/13.
 //
-
+import UIKit
 
 struct EntryEntity: Codable {
     let name: String
     let imageName: String
     let shortDescription: String
     let description: String
+}
+
+extension EntryEntity {
+    var convertedUIImage: UIImage? {
+        let uiImage = UIImage(named: self.imageName)
+        return uiImage
+    }
 }
