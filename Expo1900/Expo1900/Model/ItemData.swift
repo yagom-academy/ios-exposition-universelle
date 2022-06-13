fe70e920925d2e456081e9f72a87fa9c22388e95
apwierk2451 <apwierk2451@gmail.com>
2022-06-13 17:09:48 +0900

    feat: snake case와 축약 형태의 json을 swift형식으로 변환

--- Expo1900/Expo1900/Model/ItemData.swift
+++ Expo1900/Expo1900/Model/ItemData.swift
@@ -10,6 +10,14 @@ import Foundation
 struct Item: Codable {
     let name: String
     let imageName: String
-    let shortDesc: String
-    let desc: String
+    let shortDescription: String
+    let description: String
+    
+    enum CodingKeys: String, CodingKey {
+        case name
+        case imageName = "image_name"
+        case shortDescription = "short_desc"
+        case description = "desc"
+    }
 }
+
