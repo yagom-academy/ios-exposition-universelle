2e90e5e9c8c38e474748d790f5716413431b4a29
derrickkim0109 <derrickkim0109@naver.com>
2022-06-13 18:55:57 +0900

    chore : 불필요한 파일 삭제

--- Expo1900/Expo1900/Model/EntryEntity.swift
+++ Expo1900/Expo1900/Model/EntryEntity.swift
@@ -5,20 +5,10 @@
 //  Created by Derrick kim on 2022/06/13.
 //
 
-import Foundation
 
 struct EntryEntity: Codable {
     let name: String
     let imageName: String
     let shortDescription: String
     let description: String
-}
-
-extension EntryEntity {
-    enum CodingKeys: String, CodingKey {
-        case name
-        case imageName = "image_name"
-        case shortDescription = "short_desc"
-        case description
-    }
 }
