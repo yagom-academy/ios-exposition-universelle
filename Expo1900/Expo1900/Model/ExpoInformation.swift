76cc069fb394f85fc5b50696ca396d4a1fa72b9f
bard <schy2503@naver.com>
2022-06-13 20:12:14 +0900

    feat: ExpoInformation 구조체 구현

--- Expo1900/Expo1900/Model/ExpoInformation.swift
+++ Expo1900/Expo1900/Model/ExpoInformation.swift
@@ -6,3 +6,11 @@
 //
 
 import Foundation
+
+struct ExpoInformation: Codable {
+    var title: String
+    var visitors: Int
+    var location: String
+    var duration: String
+    var description: String
+}
