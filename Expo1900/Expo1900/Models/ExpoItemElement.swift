9ae7edaf7c80ddbc7a72b04f52ce4092b7e4236d
ZZBAE <jebyun2@gmail.com>
2022-06-13 18:03:37 +0900

    refactor: 파일 Description 수정
    - 작성자 팀원 이름으로 수정

--- Expo1900/Expo1900/Models/ExpoItemElement.swift
+++ Expo1900/Expo1900/Models/ExpoItemElement.swift
@@ -2,7 +2,7 @@
 //  ExpoItemElement.swift
 //  Expo1900
 //
-//  Created by 변재은 on 2022/06/13.
+//  Created by 재재, 보리사랑 on 2022/06/13.
 //
 
 import Foundation
--- Expo1900/Expo1900/Models/Exposition.swift
+++ Expo1900/Expo1900/Models/Exposition.swift
@@ -2,7 +2,7 @@
 //  Exposition.swift
 //  Expo1900
 //
-//  Created by 유한석 on 2022/06/13.
+//  Created by 재재, 보리사랑 on 2022/06/13.
 //
 
 import Foundation
