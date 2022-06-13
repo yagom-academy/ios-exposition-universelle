8b7cf62c234b52786ca5ebc5232997d20de6cc94
gjrjs31@naver.com <gjrjs31@naver.com>
2022-06-13 22:10:51 +0900

    feat: Create ExpoInformation Type
    - Set Property
    - Implement Method
    - Add Unit Test

---
+++ Expo1900/Expo1900/Model/ExpoInformation.swift
@@ -0,0 +1,35 @@
+//
+//  ExpoInformation.swift
+//  Expo1900
+//
+//  Created by 허건 on 2022/06/13.
+//
+
+struct ExpoInformation: Decodable {
+    private var title: String
+    private var visitors: Int
+    private var location: String
+    private var duration: String
+    private var description: String
+    
+    func getTitle() -> String {
+        return title
+    }
+    
+    func getVisitors() -> Int {
+        return visitors
+    }
+    
+    func getLocation() -> String {
+        return location
+    }
+    
+    func getDuration() -> String {
+        return duration
+    }
+    
+    func getDescription() -> String {
+        return description
+    }
+}
+
