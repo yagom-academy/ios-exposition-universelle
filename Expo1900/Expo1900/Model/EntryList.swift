dd92714a725d3c282d022a48b5a8252d24af96f4
bard <schy2503@naver.com>
2022-06-13 19:55:31 +0900

    sytle: EntryList구조체 프로퍼티 및 케이스 네이밍 변경

--- Expo1900/Expo1900/Model/EntryList.swift
+++ Expo1900/Expo1900/Model/EntryList.swift
@@ -10,13 +10,13 @@ import Foundation
 struct EntryList: Codable {
     var name: String
     var imageName: String
-    var shortDesc: String
-    var desc: String
+    var shortDescription: String
+    var description: String
     
     enum CodingKeys: String, CodingKey {
         case name
         case imageName = "image_name"
-        case shortDesc = "short_desc"
-        case desc
+        case shortDescription = "short_desc"
+        case description = "desc"
     }
 }
