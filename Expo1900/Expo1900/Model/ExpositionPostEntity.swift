afbe5f3f374c26d04f1243b9697092bc956e3fd8
derrickkim0109 <derrickkim0109@naver.com>
2022-06-13 22:03:35 +0900

    feat : "ExpositionPostEntity" 메인 Post 페이지 모델 타입 구현

---
+++ Expo1900/Expo1900/Model/ExpositionPostEntity.swift
@@ -0,0 +1,14 @@
+//
+//  ExpositionPost.swift
+//  Expo1900
+//
+//  Created by Derrick kim on 2022/06/13.
+//
+
+struct ExpositionPostEntity: Decodable {
+    let title: String
+    let visitors: Int
+    let location: String
+    let duration: String  
+    let description: String
+}
