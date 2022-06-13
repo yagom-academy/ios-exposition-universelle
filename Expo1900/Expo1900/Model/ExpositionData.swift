a8c9394c18dcd0dcec0d425e5df4fe09bab45bf4
웡빙 <wb277@naver.com>
2022-06-13 15:57:19 +0900

    feat : "ExpositionData" 와 "ItemData" 구조체 생성
    JSON 데이터와 매칭할 모델 타입 구현

---
+++ Expo1900/Expo1900/Model/ExpositionData.swift
@@ -0,0 +1,16 @@
+//
+//  ExpositionData.swift
+//  Expo1900
+//
+//  Created by 웡빙, bonf on 2022/06/13.
+//
+
+import Foundation
+
+struct ExpositionData {
+    let title: String
+    let visitors: Int
+    let location: String
+    let duration: String
+    let description: String
+}
---
+++ Expo1900/Expo1900/Model/ItemData.swift
@@ -0,0 +1,15 @@
+//
+//  ItemData.swift
+//  Expo1900
+//
+//  Created by 웡빙, bonf on 2022/06/13.
+//
+
+import Foundation
+
+struct Item: Codable {
+    let name: String
+    let imageName: String
+    let shortDesc: String
+    let desc: String
+}
