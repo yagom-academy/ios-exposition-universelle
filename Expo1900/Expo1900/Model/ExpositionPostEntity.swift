d95a2b5d039a393e252087709922c589222d06df
derrickkim0109 <derrickkim0109@naver.com>
2022-06-14 10:59:13 +0900

    feat : "ExpositionPostEntity" 내에서 방문자 수를 위한 numberFormatter 기능 구현

--- Expo1900/Expo1900/Model/ExpositionPostEntity.swift
+++ Expo1900/Expo1900/Model/ExpositionPostEntity.swift
@@ -5,10 +5,19 @@
 //  Created by Derrick kim on 2022/06/13.
 //
 
+import Foundation
 struct ExpositionPostEntity: Decodable {
     let title: String
     let visitors: Int
     let location: String
     let duration: String  
     let description: String
+}
+private extension Int {
+    func setNumberFormatter() -> String {
+        let numberFormatter = NumberFormatter()
+        numberFormatter.usesSignificantDigits = true
+        numberFormatter.numberStyle = .decimal
+        return numberFormatter.string(from: NSNumber(value: self)) ?? ""
+    }
 }
