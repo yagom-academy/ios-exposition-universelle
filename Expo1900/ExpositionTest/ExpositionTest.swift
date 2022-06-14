8479ddf23c3710dfdef78813b325a45c3035e9e4
derrickkim0109 <derrickkim0109@naver.com>
2022-06-14 15:57:41 +0900

    test : "ExpositionTest" Json 파일 불러오는 테스트코드 재 작성

--- Expo1900/ExpositionTest/ExpositionTest.swift
+++ Expo1900/ExpositionTest/ExpositionTest.swift
@@ -23,15 +23,27 @@ class ExpositionTest: XCTestCase {
     func test_Json데이터를_가져온다() {
         //given
         do {
-            guard
-                let jsonData = JsonParser.fetch("ExpositionPost"),
-                let post = try? JSONDecoder().decode(ExpositionPostEntity.self, from: jsonData)
-            else { return }
             //when
-            let data = try? JSONDecoder().decode(ExpositionPostEntity.self, from: jsonData)
+            let jsonData = JsonParser.fetch("ExpositionPost")
             
             //then
-            XCTAssertNotNil(data)
+            XCTAssertNotNil(jsonData)
+        } catch {
+        }
+    }
+    
+    func test_Json_데이터가_Entity에_들어있는지_확인한다() {
+        //given
+        let exposition: ExpositionPostEntity?
+        let expectation = "파리 만국박람회 1900(L\'Exposition de Paris 1900)"
+        do {
+            //when
+            let jsonData = JsonParser.fetch("ExpositionPost")
+        
+            let result = jsonData?.manufacture(.title)
+            
+            //then
+            XCTAssertEqual(result, expectation)
         } catch {
             
         }
