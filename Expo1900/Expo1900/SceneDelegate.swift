76e3ccf203cff807b872c23e8c38e0ea45e98034
derrickkim0109 <derrickkim0109@naver.com>
2022-06-14 16:26:28 +0900

    chore : "MainStoryBoard" 제거 및 Main 설정 제거

--- Expo1900/Expo1900/SceneDelegate.swift
+++ Expo1900/Expo1900/SceneDelegate.swift
@@ -10,7 +10,13 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+         
+        window = UIWindow(windowScene: windowScene)
+        let mainViewController = ExpositionPostViewController()
+
+        window?.rootViewController = mainViewController
+        window?.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
