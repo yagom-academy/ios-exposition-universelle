0d4a0c4f728249d420213f682258af23a7f6e0a5
minsson <mskaang@gmail.com>
2022-06-15 18:48:29 +0900

    feat: 메인 화면 내 모든 레이블을 채우는 setAllLabels 메서드 구현
    - do-catch 문 내에서 setAllLabels 메서드 실행
    - 레이블의 줄 수와 폰트 크기를 세팅하는 adjustLabelAttribute() 메서드 구현
    - UI 변수 접근제어 수준 private으로 변경

--- Expo1900/Expo1900/Sources/Controllers/MainViewController.swift
+++ Expo1900/Expo1900/Sources/Controllers/MainViewController.swift
@@ -1,3 +1,37 @@
+//
+//  Expo1900 - ViewController.swift
+//  Created by yagom. 
+//  Copyright © yagom academy. All rights reserved.
+// 
+
+import UIKit
+
+final class MainViewController: UIViewController {
+    @IBOutlet private weak var titleLabel: UILabel!
+    @IBOutlet private weak var expositionImageView: UIImageView!
+    @IBOutlet private weak var visitorsLabel: UILabel!
+    @IBOutlet private weak var locationLabel: UILabel!
+    @IBOutlet private weak var durationLabel: UILabel!
+    @IBOutlet private weak var descriptionLabel: UILabel!
+    
+    @IBOutlet private weak var leftFlagImageView: UIImageView!
+    @IBOutlet private weak var presentItemsButton: UIButton!
+    @IBOutlet private weak var rightFlagImageView: UIImageView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        adjustLabelAttribute()
+        setAllImages()
+        
+        do {
+            let decodedData = try decodeExpositionData()
+            setAllLabels(of: decodedData)
+        } catch let error as DataHandlingError {
+            print(error.description)
+        } catch {
+            print("Undexpected error: \(error)")
+        }
+    }
     
     private func decodeExpositionData() throws -> Exposition {
         guard let mainPage = NSDataAsset.init(name: "exposition_universelle_1900") else { throw DataHandlingError.invalidFile }
@@ -8,9 +42,27 @@
         
         return exposition
     }
+    
+    private func setAllLabels(of exposition: Exposition) {
+        titleLabel.text = exposition.title.replacingOccurrences(of: "(", with: "\n(")
+        visitorsLabel.text = "방문객 : \(exposition.visitors) 명"
+        locationLabel.text = "개최지 : \(exposition.location)"
+        durationLabel.text = "개최 기간 : \(exposition.duration)"
+        descriptionLabel.text = exposition.description
+    }
+    
     private func setAllImages() {
         expositionImageView.image = #imageLiteral(resourceName: "poster")
         
         leftFlagImageView.image = #imageLiteral(resourceName: "flag")
         rightFlagImageView.image = #imageLiteral(resourceName: "flag")
     }
+    
+    private func adjustLabelAttribute() {
+        descriptionLabel.numberOfLines = 0
+        
+        titleLabel.numberOfLines = 0
+        titleLabel.font = UIFont.preferredFont(forTextStyle: .title1)
+    }
+}
+
