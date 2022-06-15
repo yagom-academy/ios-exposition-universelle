c3e074fda204ef8bbfeb7a639f66bdd2176061d0
OldStarKR <tyr2293@naver.com>
2022-06-15 14:49:54 +0900

    feat: 이미지를 직접 가져오는 메서드 구현

--- Expo1900/Expo1900/Models/ExpositionEntry.swift
+++ Expo1900/Expo1900/Models/ExpositionEntry.swift
@@ -1,12 +1,17 @@
+import UIKit
 struct ExpositionEntry: Decodable {
     let name: String
-    let imageAddress: String
+    let imageName: String
     let shortDescription: String
     let description: String
     
+    func fetchImage(from path: String) -> UIImage? {
+        return UIImage(contentsOfFile: path) ?? nil
+    }
+    
     enum CodingKeys: String, CodingKey {
         case name
-        case imageAddress = "image_name"
+        case imageName = "image_name"
         case shortDescription = "short_desc"
         case description = "desc"
     }
