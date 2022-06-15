4ed9e6f0254b5025b70df08cf2ef308308ea5f46
minsson <mskaang@gmail.com>
2022-06-15 18:28:00 +0900

    feat: 메인화면 이미지 삽입
    - ViewController 네이밍 MainViewController로 변경
    - 메인화면 UI 추가 및 수정

---
+++ Expo1900/Expo1900/Sources/Controllers/MainViewController.swift
@@ -0,0 +1,6 @@
+    private func setAllImages() {
+        expositionImageView.image = #imageLiteral(resourceName: "poster")
+        
+        leftFlagImageView.image = #imageLiteral(resourceName: "flag")
+        rightFlagImageView.image = #imageLiteral(resourceName: "flag")
+    }
