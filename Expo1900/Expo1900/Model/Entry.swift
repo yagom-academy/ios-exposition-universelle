81e94a2c69bbfdc269078867dcf6fb8f5ac0d0c7
Hugh-github <tnghk4822@gmail.com>
2022-06-15 11:36:33 +0900

    refactor: Entry에서 채택하던 Identifiable 제거

--- Expo1900/Expo1900/Model/Entry.swift
+++ Expo1900/Expo1900/Model/Entry.swift
@@ -7,15 +7,13 @@
 
 import Foundation
 
-struct Entry: Codable, Identifiable {
-    var id: String
+struct Entry: Codable {
     let name: String
     let imageName: String
     let shortDescription: String
     let longDescription: String
     
     private enum CodingKeys: String, CodingKey {
-        case id
         case name
         case imageName = "image_name"
         case shortDescription = "short_desc"
