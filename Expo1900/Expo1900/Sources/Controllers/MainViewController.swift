121e284fb015a90797166a30f99e262f4fc1ab4a
minsson <mskaang@gmail.com>
2022-06-15 18:36:53 +0900

    feat: decodeExpositionData 메서드 구현 및 관련 에러 타입 정의
    - 메인화면에 표시할 Exposition 데이터를 관련 JSON 파일에서 파싱하는 메서드
    - 에러처리에 필요한 DataHandlingError 구현

--- Expo1900/Expo1900/Sources/Controllers/MainViewController.swift
+++ Expo1900/Expo1900/Sources/Controllers/MainViewController.swift
@@ -1,3 +1,13 @@
+    
+    private func decodeExpositionData() throws -> Exposition {
+        guard let mainPage = NSDataAsset.init(name: "exposition_universelle_1900") else { throw DataHandlingError.invalidFile }
+        
+        var exposition: Exposition
+        let jsonDecoder = JSONDecoder()
+        exposition = try jsonDecoder.decode(Exposition.self, from: mainPage.data)
+        
+        return exposition
+    }
     private func setAllImages() {
         expositionImageView.image = #imageLiteral(resourceName: "poster")
         
