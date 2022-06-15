a5009cc5592bc5becdab2400842c88c8388ef1fa
Judy <joo00ee@naver.com>
2022-06-15 15:35:45 +0900

    feat: 메인화면 UI 구현

--- Expo1900/Expo1900/Sources/Controllers/ViewController.swift
+++ Expo1900/Expo1900/Sources/Controllers/ViewController.swift
@@ -7,7 +7,19 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var expoImageView: UIImageView!
+    @IBOutlet weak var visitorsLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var durationLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+    @IBOutlet weak var leftFlagImageView: UIImageView!
+    @IBOutlet weak var presentItemsButton: UIButton!
+    @IBOutlet weak var rightFlagImageView: UIImageView!
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
