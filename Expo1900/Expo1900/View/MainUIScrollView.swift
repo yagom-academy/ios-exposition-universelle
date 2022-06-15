086e2d6deda7ddf70a8097460b71cb0f96489d58
finnn1 <tgss3535@gmail.com>
2022-06-15 15:46:42 +0900

    add: 스크롤뷰 파일 생성

---
+++ Expo1900/Expo1900/View/MainUIScrollView.swift
@@ -0,0 +1,124 @@
+//
+//  MainUIScrollView.swift
+//  Expo1900
+//
+//  Created by LeeChiheon on 2022/06/15.
+//
+
+import UIKit
+
+class MainUIScrollView: UIScrollView {
+    
+    let contentView = UIView()
+    
+    lazy var numberFormatter: NumberFormatter = {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .decimal
+        
+        return formatter
+    }()
+    
+    lazy var validJson = decodeJson()
+    
+    lazy var mainTitleLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.boldSystemFont(ofSize: 28)
+        label.text = validJson?.title
+        label.numberOfLines = 2
+        label.textAlignment = .center
+        return label
+    }()
+    
+    let mainImage: UIImageView = {
+        let imageView = UIImageView()
+        guard let image = UIImage(named: "poster.png") else { return UIImageView() }
+        imageView.image = image
+        return imageView
+    }()
+    
+    lazy var audienceLabel: UILabel = {
+        let label = UILabel()
+        guard let audienceNumber = validJson?.visitors,
+              let formattedNumber = numberFormatter.string(from: audienceNumber as NSNumber) else { return UILabel() }
+        
+        label.font = UIFont.systemFont(ofSize: 20)
+        label.text = "방문객 : \(formattedNumber) 명"
+        return label
+    }()
+    
+    lazy var venueLabel: UILabel = {
+        let label = UILabel()
+        guard let location = validJson?.location else { return UILabel() }
+        label.font = UIFont.systemFont(ofSize: 20)
+        label.text = "개최지 : \(location)"
+        return label
+    }()
+    
+    lazy var periodLabel: UILabel = {
+        let label = UILabel()
+        guard let duration = validJson?.duration else { return UILabel() }
+        label.font = UIFont.systemFont(ofSize: 20)
+        label.text = "개최기간 : \(duration)"
+        return label
+    }()
+    
+    lazy var descriptionLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 20)
+        label.text = validJson?.description
+        label.numberOfLines = 0
+        return label
+    }()
+    
+    lazy var mainStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [mainTitleLabel, mainImage, audienceLabel, venueLabel, periodLabel, descriptionLabel])
+        stackView.spacing = 10
+        stackView.axis = .vertical
+        stackView.distribution = .equalSpacing
+        stackView.alignment = .center
+        return stackView
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        addSubview(contentView)
+        contentView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            contentView.topAnchor.constraint(equalTo: topAnchor),
+            contentView.bottomAnchor.constraint(equalTo: bottomAnchor),
+            contentView.widthAnchor.constraint(equalTo: widthAnchor),
+            contentView.centerXAnchor.constraint(equalTo: centerXAnchor)
+        ])
+        
+        mainStackViewConfigure()
+        setupLabelConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func mainStackViewConfigure() {
+        backgroundColor = .white
+        addSubview(mainStackView)
+    }
+    
+    func setupLabelConstraints() {
+        mainStackView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            mainStackView.topAnchor.constraint(equalTo: contentView.topAnchor),
+            mainStackView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
+            mainStackView.widthAnchor.constraint(equalTo: contentView.widthAnchor),
+            mainStackView.centerXAnchor.constraint(equalTo: contentView.centerXAnchor)
+        ])
+    }
+    
+    func decodeJson() -> Expo? {
+        let decoder = JSONDecoder()
+        
+        guard let fileLocation = Bundle.main.url(forResource: "exposition_universelle_1900", withExtension: "json"),
+              let data = try? Data(contentsOf: fileLocation),
+              let expoInfo =  try? decoder.decode(Expo.self, from: data) else { return nil }
+        return expoInfo
+    }
+}
