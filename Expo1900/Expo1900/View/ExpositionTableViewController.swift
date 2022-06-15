ab11a10c797ad13e9f65ba0d86b05bebd515f585
derrickkim0109 <derrickkim0109@naver.com>
2022-06-15 17:00:00 +0900

    feat : "ExpositionTableViewController" 다음 페이지 생성

---
+++ Expo1900/Expo1900/View/ExpositionTableViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ExpositionTableViewController.swift
+//  Expo1900
+//
+//  Created by Derrick kim on 2022/06/15.
+//
+
+import UIKit
+
+class ExpositionTableViewController: UITableViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationController?.navigationBar.isHidden = false
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 0
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+}
