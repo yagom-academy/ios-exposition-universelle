5b5967251c479a8dd256b9fc31e4205e95bec548
yeeton37 <lyeeun37@naver.com>
2022-06-15 16:26:10 +0900

    feat: IBOutlet 프로퍼티 생성 및 스토리보드와 dataAsset을 연결

--- Expo1900/Expo1900/ViewController.swift
+++ Expo1900/Expo1900/ViewController.swift
@@ -7,10 +7,54 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var expositionUniverselle: ExpositionUniverselle? = nil
 
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var posterImage: UIImageView!
+    @IBOutlet weak var visitorsLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var durationLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let jsonDecoder: JSONDecoder = JSONDecoder()
+        guard let dataAsset: NSDataAsset = NSDataAsset(name: "exposition_universelle_1900") else {
+            return
+        }
+        
+        do {
+            self.expositionUniverselle = try jsonDecoder.decode(ExpositionUniverselle.self, from: dataAsset.data)
+        } catch {
+            print("error")
+        }
+        let title = expositionUniverselle?.title
+        let changeTitle = title?.replacingOccurrences(of: "(", with: "\n(")
+        
+        titleLabel.text = changeTitle
+        titleLabel.numberOfLines = 0
+        titleLabel.font = UIFont.systemFont(ofSize: 25.0)
+        
+        let numberformatter = NumberFormatter()
+        numberformatter.numberStyle = .decimal
+        
+        guard let result = numberformatter.string(for: expositionUniverselle?.visitors) else {
+            return
+        }
+        
+        guard let location = expositionUniverselle?.location,
+        let duration = expositionUniverselle?.duration else {
+            return
+        }
+        
+        visitorsLabel.text = "방문객 : \(result) 명"
+        locationLabel.text = "개최지 : \(location)"
+        durationLabel.text = "개최기간 : \(duration)"
+        descriptionLabel.text = expositionUniverselle?.description
+        
+        descriptionLabel.lineBreakMode = .byWordWrapping
+        descriptionLabel.numberOfLines = 0
     }
 
 
