3b9e465904fc05f8bcc8e6a31a414944ff4ffb96
unchain123 <unchain123@kakao.com>
2022-06-16 15:46:32 +0900

    feat: 두번째 스토리보드 데이터연결

--- Expo1900/Expo1900/Controller/Cell/CustomTableViewCell.swift
+++ Expo1900/Expo1900/Controller/Cell/CustomTableViewCell.swift
@@ -4,20 +4,20 @@
 //
 //  Created by unchain,bard on 2022/06/16.
 //
-
 import UIKit
 
 class CustomTableViewCell: UITableViewCell {
-
+    @IBOutlet weak var koreaEntryImage: UIImageView!
+    @IBOutlet weak var koreaEntryTitle: UILabel!
+    @IBOutlet weak var shortDescription: UILabel!
+    var detailedDescription: String?
+    var cellImage: UIImageView?
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
     }
-
 }
--- Expo1900/Expo1900/Controller/EntryListViewController.swift
+++ Expo1900/Expo1900/Controller/EntryListViewController.swift
@@ -7,23 +7,33 @@
 
 import UIKit
 
-class EntryListViewController: UIViewController {
-
+class EntryListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var secondNavigationItem: UINavigationItem!
+    let entryList = try! JsonParser.parseEntryList()
+    let firstViewController = ViewController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        self.tableView.dataSource = self
+        self.tableView.delegate = self
+        secondNavigationItem.title = "한국의 출품작"
+        self.navigationController?.isNavigationBarHidden = false
+    }
+  
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return entryList.count
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: CustomTableViewCell = tableView.dequeueReusableCell(withIdentifier: "customCell", for: indexPath) as! CustomTableViewCell
+        
+        cell.koreaEntryTitle.text = entryList[indexPath.row].name
+        cell.shortDescription.text = entryList[indexPath.row].shortDescription
+        cell.koreaEntryImage.image = UIImage(named: entryList[indexPath.row].imageName)
+        cell.accessoryType = .disclosureIndicator
+        cell.detailedDescription = entryList[indexPath.row].description
+        
+        return cell
     }
-    */
-
 }
