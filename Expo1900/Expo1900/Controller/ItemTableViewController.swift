d650369ca3160c8ab4e3bd527223ccd0b9af8c67
finnn1 <tgss3535@gmail.com>
2022-06-16 12:51:47 +0900

    feat: 셀과 컨트롤러의 데이터 연동

--- Expo1900/Expo1900/Controller/ItemTableViewController.swift
+++ Expo1900/Expo1900/Controller/ItemTableViewController.swift
@@ -15,17 +15,20 @@ class ItemTableViewController: UITableViewController {
         super.viewDidLoad()
         tableView.register(ItemTableViewCell.self, forCellReuseIdentifier: "entryCell")
     }
-
-    // MARK: - Table view data source
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         guard let entryInfo = entryInfo else { return 0 }
-
         return entryInfo.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "entryCell",for: indexPath)
+        guard let entryInfo = entryInfo else { return UITableViewCell() }
+        let cell = tableView.dequeueReusableCell(withIdentifier: "entryCell",for: indexPath) as! ItemTableViewCell
+        cell.itemInfo = entryInfo[indexPath.row]
         return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
     }
 }
--- Expo1900/Expo1900/View/ItemTableViewCell.swift
+++ Expo1900/Expo1900/View/ItemTableViewCell.swift
@@ -8,16 +8,39 @@
 import UIKit
 
 class ItemTableViewCell: UITableViewCell {
-   
-//    let itemImageView: UIImageView {
-//     let imageView = UIImageView()
-//     let image = UIImage(named: String)
-//    }
+    
+    var itemInfo: Entry? {
+        didSet {
+            testLabel.text = itemInfo?.name
+        }
+    }
+    
+    let testLabel: UILabel = {
+        let label = UILabel()
+        return label
+    }()
+    
+//    lazy var itemImageView: UIImageView = {
+//        let imageView = UIImageView()
+//        let image = UIImage(named: )
+//        return imageView
+//    }()
+    
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        addSubview(testLabel)
+        testLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            testLabel.topAnchor.constraint(equalTo: topAnchor),
+            testLabel.bottomAnchor.constraint(equalTo: bottomAnchor),
+            testLabel.leadingAnchor.constraint(equalTo: leadingAnchor),
+            testLabel.trailingAnchor.constraint(equalTo: trailingAnchor)
+        ])
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    
 }
