1ab59365df23ba73acdf45efe4691cd87ada0da5
BaekGom <jihunmm02@gmail.com>
2022-06-16 15:32:21 +0900

    feat :  "ExpositionTableView" 생성 및 JSON 데이터 호출

--- Expo1900/Expo1900/Controller/ExpositionTableViewController.swift
+++ Expo1900/Expo1900/Controller/ExpositionTableViewController.swift
@@ -8,9 +8,31 @@
 import UIKit
 
 final class ExpositionTableViewController: UITableViewController {
+    private let expositionTableView: UITableView = {
+        let tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        return tableView
+    }()
+    
+    var entryEntity: [EntryEntity]?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationController?.navigationBar.isHidden = false
+        view.addSubview(expositionTableView)
+        
+        NSLayoutConstraint.activate([
+            expositionTableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            expositionTableView.topAnchor.constraint(equalTo: view.topAnchor),
+            expositionTableView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            expositionTableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+        ])
+        
+        expositionTableView.register(ExpositionTableViewCell.self, forCellReuseIdentifier: "ExpositionTableViewCell")
+        
+        expositionTableView.rowHeight = UITableView.automaticDimension
+        expositionTableView.estimatedRowHeight = UITableView.automaticDimension
+        entryEntity = JsonParser<[EntryEntity]>.fetch("EntryEntity")
     }
     
     override func numberOfSections(in tableView: UITableView) -> Int {
@@ -18,6 +40,16 @@ final class ExpositionTableViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if let entryEntity = entryEntity {
+            return entryEntity.count
+        }
         return 0
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = expositionTableView.dequeueReusableCell(withIdentifier: "ExpositionTableViewCell", for: indexPath) as? ExpositionTableViewCell else {
+            return UITableViewCell()
+        }
+        return cell
     }
 }
---
+++ Expo1900/Expo1900/View/ExpositionTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ExpositionTableViewCell.swift
+//  Expo1900
+//
+//  Created by Baek on 2022/06/16.
+//
+
+import UIKit
+
+class ExpositionTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
