bffa52d2a34498431cdb60071684e1937388045f
OldStarKR <tyr2293@naver.com>
2022-06-17 00:22:51 +0900

    refactor: 출품작 모델 일부 프로퍼티 접근 제한 설정

--- Expo1900/Expo1900/Models/ExpositionEntry.swift
+++ Expo1900/Expo1900/Models/ExpositionEntry.swift
@@ -2,15 +2,16 @@ import UIKit
 
 struct ExpositionEntry: Decodable {
     let name: String
-    let imageName: String
     let shortDescription: String
     let description: String
+    
+    private let imageName: String
     
     var thumbnail: UIImage? {
         return UIImage(named: imageName)
     }
     
-    enum CodingKeys: String, CodingKey {
+    private enum CodingKeys: String, CodingKey {
         case name
         case imageName = "image_name"
         case shortDescription = "short_desc"
