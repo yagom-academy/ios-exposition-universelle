198abd1f9e10ef5a048b6dc440b64e6985f5dfb6
unchain123 <unchain123@kakao.com>
2022-06-16 14:04:14 +0900

    feat: numberFormatting하는 함수 구현

---
+++ Expo1900/Expo1900/Model/Extension/Int+extension.swift
@@ -0,0 +1,18 @@
+//
+//  Int+extension.swift
+//  Expo1900
+//
+//  Created by unchain, bard on 2022/06/16.
+//
+
+import Foundation
+
+extension Int {
+    func formatNumber() -> String? {
+        let numberformatter = NumberFormatter()
+        numberformatter.numberStyle = .decimal
+        
+        guard let formattedResult = numberformatter.string(from: self as NSNumber) else { return nil }
+        return formattedResult
+    }
+}
