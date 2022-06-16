478606ebf98dcb008c18d0121e80186baf17de10
unchain123 <unchain123@kakao.com>
2022-06-16 14:42:41 +0900

    add: 뷰컨트롤러 파일 생성 및 커스텀테이블뷰셀 파일 생성

---
+++ Expo1900/Expo1900/Controller/Cell/CustomTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  CustomTableViewCell.swift
+//  Expo1900
+//
+//  Created by unchain,bard on 2022/06/16.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
