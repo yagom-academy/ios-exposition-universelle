552639027f1904fbc9b4d8c9eb4e45481f460ed5
MinsuJeon <chso294@naver.com>
2022-06-16 20:47:03 +0900

    feat: KoreaEntryViewController 생성 및 필수 메서드 구현

--- Expo1900/Expo1900/KoreaEntryViewController.swift
+++ Expo1900/Expo1900/KoreaEntryViewController.swift
@@ -14,10 +14,51 @@ class KoreaEntryViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         parseExpositionUniverselleData()
-
+        
         tableView.delegate = self
         tableView.dataSource = self
+        
+        navigationItem.title = "한국의 출품작"
+        //        navigationItem.backButtonTitle = "메인"
+        //        self.navigationController?.navigationBar.topItem?.title = "한국의 출품작"
+        //        navigationController?.navigationBar.topItem?.backButtonTitle = "메인"
     }
+    
+    //    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    //        guard segue.identifier == "transferToDetailsVC" else {
+    //            return
+    //        }
+    //
+    //        guard let destination = segue.destination as? KoreaEntryDetailsViewController,
+    //              let index = self.tableView.indexPathForSelectedRow?.row else {
+    //                  return
+    //              }
+    //
+    //        destination.detailImage!.image = UIImage(named: koreaEntry[index].imageName)
+    //    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?){
+        if segue.identifier == "transferToDetailsVC" {
+            let destination = segue.destination as? KoreaEntryDetailsViewController
+            if let index = self.tableView.indexPathForSelectedRow?.row {
+                destination?.someImage = koreaEntry[index].imageName
+                destination?.someDescription = koreaEntry[index].description
+                destination?.entryTitle = koreaEntry[index].name
+            }
+        }
+    }
+    
+//    override func prepare(for segue: UIStoryboardSegue, sender: Any?){
+//        if segue.identifier == "transferToDetailsVC" {
+//            let destination = segue.destination as? KoreaEntryDetailsViewController
+//            if let index = self.tableView.indexPathForSelectedRow?.row {
+//                let imageName = koreaEntry[index].imageName
+//                
+//                destination?.someImageView = UIImageView(image: UIImage(named: imageName))
+//                destination?.someDescription = UILabel(koreaEntry[index].description)
+//            }
+//        }
+//    }
     
     private func parseExpositionUniverselleData() {
         let jsonDecoder: JSONDecoder = JSONDecoder()
@@ -41,6 +82,8 @@ class KoreaEntryViewController: UIViewController {
             print("unknown error!!")
         }
     }
+    
+    
 }
 
 extension KoreaEntryViewController: UITableViewDataSource, UITableViewDelegate {
@@ -60,12 +103,19 @@ extension KoreaEntryViewController: UITableViewDataSource, UITableViewDelegate {
         content.text = koreaEntry[indexPath.row].name
         content.secondaryText = koreaEntry[indexPath.row].shortDescription
         content.image = UIImage(named: koreaEntry[indexPath.row].imageName)
-        content.imageProperties.maximumSize.height = 30
-        content.imageProperties.maximumSize.width = 30
+        //        content.imageProperties.topAnchor.constraint(equalTo: content.text).isActive = true
+        
+        content.imageProperties.maximumSize.width = 50
+        content.imageProperties.maximumSize.height = 50
+        //        content.textProperties.adjustsFontSizeToFitWidth = true
+        
+        // 더 알아보는 걸로...!
+        //        content.imageProperties.reservedLayoutSize = CGSize(width: 5, height: 10)
         
         cell.contentConfiguration = content
         cell.accessoryType = .disclosureIndicator
         
         return cell
     }
+    
 }
