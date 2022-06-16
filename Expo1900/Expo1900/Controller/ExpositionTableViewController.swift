b24fce7ab6140bc463d3b5235c5a2ed56ed16dc6
derrickkim0109 <derrickkim0109@naver.com>
2022-06-16 16:52:46 +0900

    refactor : "ExpositionTableViewController" TableView 초기 설정 함수화

--- Expo1900/Expo1900/Controller/ExpositionTableViewController.swift
+++ Expo1900/Expo1900/Controller/ExpositionTableViewController.swift
@@ -7,19 +7,61 @@
 
 import UIKit
 
-final class ExpositionTableViewController: UITableViewController {
+final class ExpositionTableViewController: UIViewController {
     private let expositionTableView: UITableView = {
         let tableView = UITableView()
         tableView.translatesAutoresizingMaskIntoConstraints = false
         return tableView
     }()
     
-    var entryEntity: [EntryEntity]?
+    private var entryEntity: [EntryEntity]?
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        configureUI()
+        
+        entryEntity = JsonParser<[EntryEntity]>.fetch("EntryEntity")
+    }
+}
+
+extension ExpositionTableViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if let entryEntity = entryEntity {
+            return entryEntity.count
+        }
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 150
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = self.expositionTableView.dequeueReusableCell(withIdentifier: "ExpositionTableViewCell", for: indexPath) as? ExpositionTableViewCell else {
+            return UITableViewCell()
+        }
+        cell.accessoryType = .disclosureIndicator
+        
+        if let data = entryEntity?[indexPath.row] {
+            cell.configure(with: data)
+        }
+        
+        return cell
+    }
+}
+
+extension ExpositionTableViewController {
+    func configureUI() {
+        expositionTableView.delegate = self
+        expositionTableView.dataSource = self
+        
         self.navigationController?.navigationBar.isHidden = false
-        view.addSubview(expositionTableView)
+        self.title = "한국의 출품작"
+        self.view.addSubview(expositionTableView)
+        
+        expositionTableView.rowHeight = UITableView.automaticDimension
+        expositionTableView.estimatedRowHeight = UITableView.automaticDimension
         
         NSLayoutConstraint.activate([
             expositionTableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
@@ -29,27 +71,5 @@ final class ExpositionTableViewController: UITableViewController {
         ])
         
         expositionTableView.register(ExpositionTableViewCell.self, forCellReuseIdentifier: "ExpositionTableViewCell")
-        
-        expositionTableView.rowHeight = UITableView.automaticDimension
-        expositionTableView.estimatedRowHeight = UITableView.automaticDimension
-        entryEntity = JsonParser<[EntryEntity]>.fetch("EntryEntity")
-    }
-    
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        return 0
-    }
-    
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        if let entryEntity = entryEntity {
-            return entryEntity.count
-        }
-        return 0
-    }
-    
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let cell = expositionTableView.dequeueReusableCell(withIdentifier: "ExpositionTableViewCell", for: indexPath) as? ExpositionTableViewCell else {
-            return UITableViewCell()
-        }
-        return cell
     }
 }
