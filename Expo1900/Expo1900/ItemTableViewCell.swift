841e640dd00c064da2bf246fece59a0f150c78dc
apwierk2451 <apwierk2451@gmail.com>
2022-06-16 19:39:35 +0900

    feat : custom cell 생성
    - cell 의 컴포넌트 생성
    - autoLayout

--- Expo1900/Expo1900/ItemTableViewCell.swift
+++ Expo1900/Expo1900/ItemTableViewCell.swift
@@ -9,22 +9,69 @@ import UIKit
 
 class ItemTableViewCell: UITableViewCell {
     
-    let label: UILabel = {
+    let nameLabel: UILabel = {
         let label = UILabel()
-        label.font = UIFont.systemFont(ofSize: 16)
+        label.font = UIFont.systemFont(ofSize: 25)
+        label.numberOfLines = 0
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+    let shortDescriptionLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 16)
+        label.numberOfLines = 0
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    let imageNameView: UIImageView = {
+        let image = UIImageView()
+        image.translatesAutoresizingMaskIntoConstraints = false
+        return image
+    }()
+    
+    lazy var stackView: UIStackView = {
+        let stack = UIStackView()
+        stack.translatesAutoresizingMaskIntoConstraints = false
+        stack.axis = .horizontal
+        stack.distribution = .fill
+        stack.alignment = .fill
+        stack.addArrangedSubview(imageNameView)
+        stack.addArrangedSubview(subStackView)
+        return stack
+    }()
+    
+    lazy var subStackView: UIStackView = {
+        let stack = UIStackView()
+        stack.translatesAutoresizingMaskIntoConstraints = false
+        stack.axis = .vertical
+        stack.distribution = .fill
+        stack.alignment = .leading
+        stack.addArrangedSubview(nameLabel)
+        stack.addArrangedSubview(shortDescriptionLabel)
+        return stack
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: .default, reuseIdentifier: reuseIdentifier)
+        self.addSubview(stackView)
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
-
+    
+    override func updateConstraints() {
+        stackView.heightAnchor.constraint(equalTo: self.heightAnchor).isActive = true
+        stackView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 10).isActive = true
+        stackView.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -10).isActive = true
+        
+        super.updateConstraints()
+    }
+    
+    func generateImage(name: String) -> UIImage? {
+        guard let image = UIImage(named: name) else { return nil }
+        return image
+    }
 }
