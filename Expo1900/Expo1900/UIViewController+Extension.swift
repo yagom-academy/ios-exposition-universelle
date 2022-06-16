4f96ebdceb12e8c5fd70ba3669ef5cc20d484ebb
웡빙 <wb277@naver.com>
2022-06-16 16:09:03 +0900

    feat : UIViewController  Extension 추가
    ViewController 를 인스턴스화 하는 instantiate 타입메서드 추가

---
+++ Expo1900/Expo1900/UIViewController+Extension.swift
@@ -0,0 +1,22 @@
+//
+//  UIViewController+Extension.swift
+//  Expo1900
+//
+//  Created by 웡빙, bonf on 2022/06/16.
+//
+
+import Foundation
+import UIKit
+
+extension UIViewController {
+    class func instantiate(from storyboard: String = "Main", bundle: Bundle? = nil, identifier: String? = nil) -> Self {
+        let viewControllerIdentifier = identifier ?? String(describing: self)
+        let storyboard = UIStoryboard(name: storyboard, bundle: bundle)
+        guard let viewController = storyboard
+            .instantiateViewController(withIdentifier: viewControllerIdentifier) as? Self else {
+            preconditionFailure(
+                "Unable to instantiate view controller with identifier \(viewControllerIdentifier) as type \(type(of: self))")
+        }
+        return viewController
+    }
+}
