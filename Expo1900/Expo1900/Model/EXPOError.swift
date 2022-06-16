e3072f2f86b744c7bba0942e65acf83a5e65ca44
yeeton37 <lyeeun37@naver.com>
2022-06-17 01:32:03 +0900

    refactor: EXPOError파일 Model 폴더로 이동 및 리팩토링

--- Expo1900/Expo1900/Model/EXPOError.swift
+++ Expo1900/Expo1900/Model/EXPOError.swift
@@ -2,16 +2,16 @@
 //  ExpoError.swift
 //  Expo1900
 //
-//  Created by 전민수 on 2022/06/15.
+//  Created by 수꿍, yeton on 2022/06/15.
 //
 
 import Foundation
-enum ExpoError: Error {
+enum EXPOError: Error {
     case decoding
 }
 
-extension ExpoError: LocalizedError {
-    public var errorDescription: String? {
+extension EXPOError: LocalizedError {
+    var errorDescription: String? {
         switch self {
         case .decoding:
             return NSLocalizedString("decoding error", comment: "Description of Decoding Error")
