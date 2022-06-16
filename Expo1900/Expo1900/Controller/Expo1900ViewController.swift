9f174045a783f26394bd7897fd52ba6feca749d7
starting1231 <starting1231@naver.com>
2022-06-16 15:51:21 +0900

    feat: Create Expo1900 StackView, Labels, ImageView
    - create expo1900StackView property
    - create Labels(titleLabel, visitorsLabel, locationLabel, durationLabel, descriptionLabel)
    - create ImageView(posterImageView)

--- Expo1900/Expo1900/Controller/Expo1900ViewController.swift
+++ Expo1900/Expo1900/Controller/Expo1900ViewController.swift
@@ -14,10 +14,63 @@ class Expo1900ViewController: UIViewController {
         return scrollView
     }()
     
+    let expo1900StackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.alignment = .fill
+        stackView.distribution = .equalSpacing
+        stackView.spacing = 5
+        return stackView
+    }()
+    
+    let titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Title"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    let posterImageView: UIImageView = {
+        let imageView = UIImageView()
+        return imageView
+    }()
+    
+    let visitorsLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Visitors"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    let locationLabel: UILabel = {
+        let label = UILabel()
+        label.text = "location"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    let durationLabel: UILabel = {
+        let label = UILabel()
+        label.text = "duration"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    let descriptionLabel: UILabel = {
+        let label = UILabel()
+        label.text = "description"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.addSubview(expo1900ScrollView)
         setScrollViewConstraints()
+        [titleLabel, posterImageView, visitorsLabel, locationLabel, durationLabel, descriptionLabel]
+            .forEach {self.expo1900StackView.addArrangedSubview($0)}
+        self.expo1900ScrollView.addSubview(expo1900StackView)
     }
 
     private func setScrollViewConstraints() {
@@ -26,7 +79,6 @@ class Expo1900ViewController: UIViewController {
         expo1900ScrollView.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor, constant: 10).isActive = true
         expo1900ScrollView.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor, constant: -10).isActive = true
     }
-
 }
 
 
