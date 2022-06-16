5251c96810f224781b05240901d9fea088f50675
minsson <mskaang@gmail.com>
2022-06-16 14:52:13 +0900

    feat: 한국의 출품작 화면의 테이블뷰 내부에서 사용될 ItemCell UI 구현

--- Expo1900/Expo1900/Sources/Controllers/ItemListViewController.swift
+++ Expo1900/Expo1900/Sources/Controllers/ItemListViewController.swift
@@ -10,14 +10,4 @@ class ItemListViewController: UIViewController {
         super.viewWillAppear(animated)
         self.navigationController?.isNavigationBarHidden = false
     }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
---
+++ Expo1900/Expo1900/Sources/Models/ItemCell.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+class ItemCell: UITableViewCell {
+
+    @IBOutlet weak var itemImageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+
+}
