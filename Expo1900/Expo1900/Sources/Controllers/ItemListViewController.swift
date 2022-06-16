64ae24bdf2a20905e06263da630b43902039d5ae
minsson <mskaang@gmail.com>
2022-06-16 15:07:14 +0900

    feat: 한국의 출품작을 디코딩하는 메서드 구현

--- Expo1900/Expo1900/Sources/Controllers/ItemListViewController.swift
+++ Expo1900/Expo1900/Sources/Controllers/ItemListViewController.swift
@@ -2,12 +2,32 @@ import UIKit
 
 class ItemListViewController: UIViewController {
 
+    private var contents: [Content] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        do {
+            contents = try decodeContentData()
+        } catch let error as DataHandlingError {
+            print(error.description)
+        } catch {
+            print("Undexpected error: \(error)")
+        }
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         self.navigationController?.isNavigationBarHidden = false
     }
+    
+    private func decodeContentData() throws -> [Content] {
+        guard let koreanItems = NSDataAsset.init(name: "items") else { throw DataHandlingError.invalidFile }
+        
+        let jsonDecoder = JSONDecoder()
+        let contents = try jsonDecoder.decode([Content].self, from: koreanItems.data)
+        
+        return contents
+    }
+    
 }
