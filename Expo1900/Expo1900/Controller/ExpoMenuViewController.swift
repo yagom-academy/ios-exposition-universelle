589b9c149252311fd0f17f62ed9f35947e73e800
yusw10 <yusw10@naver.com>
2022-06-17 02:11:49 +0900

    feat: ExpoMenuViewController 추가
    - storyboard에 ExpoMenuViewController 추가

---
+++ Expo1900/Expo1900/Controller/ExpoMenuViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ExpoMenuViewController.swift
+//  Expo1900
+//
+//  Created by 유한석 on 2022/06/17.
+//
+
+import UIKit
+
+class ExpoMenuViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
