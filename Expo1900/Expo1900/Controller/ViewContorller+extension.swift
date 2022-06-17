54da74b05fc854640bf74d1f54b9d3df35b6ee72
unchain123 <unchain123@kakao.com>
2022-06-17 10:22:01 +0900

    feat: 첫번째 스토리보드 내 레이블 추가
    - 개최지, 방문객수, 개최기간, 만국박람회설명레이블 추가

--- Expo1900/Expo1900/Controller/ViewContorller+extension.swift
+++ Expo1900/Expo1900/Controller/ViewContorller+extension.swift
@@ -2,7 +2,7 @@
 //  ViewContorller+extension.swift
 //  Expo1900
 //
-//  Created by 김동용 on 2022/06/16.
+//  Created by unchain, bard on 2022/06/16.
 //
 import UIKit
 
@@ -22,22 +22,31 @@ extension ViewController {
                                      scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor)]
         NSLayoutConstraint.activate(scrollViewConstraints)
         
+        //MARK: - contentView
+        let contentView = UIView()
+        scrollView.addSubview(contentView)
+        contentView.translatesAutoresizingMaskIntoConstraints = false
+        let contentViewConstranints = [contentView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor),
+                           contentView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor),
+                           contentView.topAnchor.constraint(equalTo: scrollView.topAnchor),
+                           contentView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor),
+                                       contentView.widthAnchor.constraint(equalTo: scrollView.widthAnchor)]
+        NSLayoutConstraint.activate(contentViewConstranints)
         
         //MARK: - mainViewConstraints
         let stackView = UIStackView()
         
         stackView.axis = .vertical
-        stackView.distribution = .fillEqually
+        stackView.distribution = .fill
         stackView.alignment = .center
         stackView.spacing = 10
-        scrollView.addSubview(stackView)
+        contentView.addSubview(stackView)
         stackView.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
-            stackView.topAnchor.constraint(equalTo: scrollView.topAnchor),
-            stackView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor),
-            stackView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor),
-            stackView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor),
-            stackView.widthAnchor.constraint(equalTo: scrollView.widthAnchor)
+            stackView.topAnchor.constraint(equalTo: contentView.topAnchor),
+            stackView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 20),
+            stackView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -20),
+            stackView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor)
         ])
     
         //MARK: - titleLabel
@@ -55,6 +64,31 @@ extension ViewController {
         posterImage.image = UIImage(named: "poster")
         stackView.addArrangedSubview(posterImage)
     
+        //MARK: - visitorsLabel
+        let visitorsLabel = UILabel()
+        
+        visitorsLabel.text = "방문객 : \(decodeData.visitors.formatNumber() ?? "") 명"
+        stackView.addArrangedSubview(visitorsLabel)
+        
+        //MARK: - locationLabel
+        let locationLabel = UILabel()
+        
+        locationLabel.text = "개최지 : \(decodeData.location)"
+        stackView.addArrangedSubview(locationLabel)
+        
+        //MARK: - durationLabel
+        let durationLabel = UILabel()
+        
+        durationLabel.text = "개최기간 : \(decodeData.duration)"
+        stackView.addArrangedSubview(durationLabel)
+        
+        //MARK: - descriptionLabel
+        let descriptionLabel = UILabel()
+        
+        descriptionLabel.text = decodeData.description
+        descriptionLabel.numberOfLines = 0
+        descriptionLabel.textAlignment = .left
+        stackView.addArrangedSubview(descriptionLabel)
     }
     
 }
