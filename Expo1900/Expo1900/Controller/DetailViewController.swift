0542dc85a7b9ddae0aee3f473fa8501fb36a116a
yusw10 <yusw10@naver.com>
2022-06-17 23:12:19 +0900

    feat: ExpoMenuViewController의 TableView 기능 구현
    - expopItemTableView의 DataSource와 Delegate구현 및 기능 구현을 ExpoMenuViewContgroller에 위임
    - 셀 선택시 세부 화면으로 넘어가기 위한 didSelectRowAt 메서드 구현
    - DetailViewController 구현 및 storyboard에 뷰 추가

---
+++ Expo1900/Expo1900/Controller/DetailViewController.swift
@@ -0,0 +1,14 @@
+//
+//  DetailViewController.swift
+//  Expo1900
+//
+//  Created by 유한석 on 2022/06/17.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
--- Expo1900/Expo1900/Controller/ExpoMenuViewController.swift
+++ Expo1900/Expo1900/Controller/ExpoMenuViewController.swift
@@ -4,27 +4,49 @@
 //
 //  Created by 유한석 on 2022/06/17.
 //
-/*
-- iboutlet 연결
-- jsondata 디코딩해서 저장 >> 완료
-- ui 업데이트
-- delegate구현
-- datasource 구현
-*/
-//
 
 import UIKit
 
 class ExpoMenuViewController: UIViewController {
     var expoItemList: [ExpoItemElement] = []
     
+    @IBOutlet weak var expoItemTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        expoItemTableView.delegate = self
+        expoItemTableView.dataSource = self
         setExpositionData()
     }
     
     private func setExpositionData() {
         let expoItemList = loadJsonData(type: Array<ExpoItemElement>.self, "items", "json")
         self.expoItemList = expoItemList
+    }
+}
+
+extension ExpoMenuViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        guard let detailVC = storyboard?.instantiateViewController(withIdentifier: "DetailViewController") as? DetailViewController else {
+            return
+        }
+        self.navigationController?.pushViewController(detailVC, animated: true)
+    }
+}
+
+extension ExpoMenuViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return expoItemList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        //셀을 반환해야함 > 어떤셀? 우리가 만든 셀
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "ExpoMenuTableViewCell") as? ExpoMenuTableViewCell else {
+            return UITableViewCell()
+        }
+        let currentExpoItemElement = expoItemList[indexPath.row]
+        cell.settingLableView(name: currentExpoItemElement.name, shortDescription: currentExpoItemElement.shortDesc)
+        cell.settingImageView(name: currentExpoItemElement.imageName)
+        return cell
     }
 }
