f7588010a3475a77bc78c83ee41bbcf422e9f974
yusw10 <yusw10@naver.com>
2022-06-17 22:04:01 +0900

    feat: ExpoHomeViewController에서 ExpoItemElement 데이터 디코딩
    - Table View  컨텐츠의 데이터가 될 ExpoItemList를 뷰컨트롤러의 전역으로 선언
    - "items.json" 파일로부터 ExpoItemElement의 배열 인스턴스 디코딩 및 저장

--- Expo1900/Expo1900/Controller/ExpoMenuViewController.swift
+++ Expo1900/Expo1900/Controller/ExpoMenuViewController.swift
@@ -4,11 +4,27 @@
 //
 //  Created by 유한석 on 2022/06/17.
 //
+/*
+- iboutlet 연결
+- jsondata 디코딩해서 저장 >> 완료
+- ui 업데이트
+- delegate구현
+- datasource 구현
+*/
+//
 
 import UIKit
 
 class ExpoMenuViewController: UIViewController {
+    var expoItemList: [ExpoItemElement] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setExpositionData()
+    }
+    
+    private func setExpositionData() {
+        let expoItemList = loadJsonData(type: Array<ExpoItemElement>.self, "items", "json")
+        self.expoItemList = expoItemList
     }
 }
