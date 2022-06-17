83ba27de831d0a0467a6346ecbb3ac7c4bf6ef4a
Judy <joo00ee@naver.com>
2022-06-17 14:30:38 +0900

    feat: 출품작별 상세페이지의 UI 요소를 채우는 메서드 구현

--- Expo1900/Expo1900/Sources/Controllers/ItemViewController.swift
+++ Expo1900/Expo1900/Sources/Controllers/ItemViewController.swift
@@ -1,7 +1,6 @@
 import UIKit
 
 final class ItemViewController: UIViewController {
-
     @IBOutlet private weak var itemImageView: UIImageView!
     @IBOutlet private weak var descriptionLabel: UILabel!
     
@@ -9,5 +8,14 @@ final class ItemViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        descriptionLabel.numberOfLines = 0
+        setUIComponents()
+    }
+    
+    private func setUIComponents() {
+        guard let content = content else { return }
+        itemImageView.image = UIImage(named: content.imageName)
+        descriptionLabel.text = content.description
+        title = content.name
     }
 }
