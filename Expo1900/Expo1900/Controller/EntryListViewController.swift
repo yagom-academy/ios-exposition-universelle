d2478e3c2527df8e98e4d8b9d11abca91134c6a3
bard <schy2503@naver.com>
2022-06-17 17:19:30 +0900

    feat: 두번째 화면내 내비게이션 구현 및 테이블 뷰 생성 - UITableViewDelegate, UITableViewDataSource 프로토콜 채택

--- Expo1900/Expo1900/Controller/EntryListViewController.swift
+++ Expo1900/Expo1900/Controller/EntryListViewController.swift
@@ -7,11 +7,33 @@
 
 import UIKit
 
-class EntryListViewController: UIViewController {
-
+class EntryListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    let entryList: [EntryList] = {
+        var entryList: [EntryList] = [EntryList]()
+        do {
+            entryList = try JsonParser.parseEntryList()
+        } catch {
+            print(ParseError.unknown.description)
+        }
+        return entryList
+    }()
+    let tableView = UITableView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = .white
         self.navigationController?.isNavigationBarHidden = false
+        self.navigationItem.title = "한국의 출품작"
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return entryList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        UITableViewCell()
     }
 }
