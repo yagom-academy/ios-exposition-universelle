5fa77a6d48929683b3b711245dc7ced2deb09e9c
starting1231 <starting1231@naver.com>
2022-06-18 22:45:34 +0900

    feat: Create itemDetailScrollView
    - Create setScrollViewConstraints method for AutoLayout

--- Expo1900/Expo1900/Controller/ItemDetailViewController.swift
+++ Expo1900/Expo1900/Controller/ItemDetailViewController.swift
@@ -10,9 +10,26 @@ import UIKit
 class ItemDetailViewController: UIViewController {
     var koreaItem: KoreaItem?
     
+    let itemDetailScrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        return scrollView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = .systemBackground
+        self.navigationItem.title = koreaItem?.name
+        self.view.addSubview(itemDetailScrollView)
+        
+        setScrollViewConstraints()
+    }
+    
+    private func setScrollViewConstraints() {
+        itemDetailScrollView.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor).isActive = true
+        itemDetailScrollView.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor).isActive = true
+        itemDetailScrollView.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor).isActive = true
+        itemDetailScrollView.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor).isActive = true
     }
 }
 
