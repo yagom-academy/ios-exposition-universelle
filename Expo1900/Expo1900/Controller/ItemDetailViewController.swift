c71eba1799ce077f5550495d286992ec9b7b257e
gjrjs31@naver.com <gjrjs31@naver.com>
2022-06-18 22:51:11 +0900

    feat: Create itemDetailStackView
    - Create setStackViewConstraints method for AutoLayout

--- Expo1900/Expo1900/Controller/ItemDetailViewController.swift
+++ Expo1900/Expo1900/Controller/ItemDetailViewController.swift
@@ -15,14 +15,26 @@ class ItemDetailViewController: UIViewController {
         scrollView.translatesAutoresizingMaskIntoConstraints = false
         return scrollView
     }()
+
+    let itemDetailStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.alignment = .center
+        stackView.distribution = .equalSpacing
+        stackView.spacing = 10
+        return stackView
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = .systemBackground
         self.navigationItem.title = koreaItem?.name
         self.view.addSubview(itemDetailScrollView)
+        self.itemDetailScrollView.addSubview(itemDetailStackView)
         
         setScrollViewConstraints()
+        setStackViewConstraints()
     }
     
     private func setScrollViewConstraints() {
@@ -30,6 +42,13 @@ class ItemDetailViewController: UIViewController {
         itemDetailScrollView.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor).isActive = true
         itemDetailScrollView.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor).isActive = true
         itemDetailScrollView.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor).isActive = true
+    }
+    
+    private func setStackViewConstraints() {
+        itemDetailStackView.topAnchor.constraint(equalTo: itemDetailScrollView.contentLayoutGuide.topAnchor).isActive = true
+        itemDetailStackView.bottomAnchor.constraint(equalTo: itemDetailScrollView.contentLayoutGuide.bottomAnchor).isActive = true
+        itemDetailStackView.leadingAnchor.constraint(equalTo: itemDetailScrollView.frameLayoutGuide.leadingAnchor, constant: 10).isActive = true
+        itemDetailStackView.trailingAnchor.constraint(equalTo: itemDetailScrollView.frameLayoutGuide.trailingAnchor, constant: -10).isActive = true
     }
 }
 
