75774fea05ade1917afcfb7a7b6a716a2c706798
재재 <98302604+ZZBAE@users.noreply.github.com>
2022-06-20 16:55:05 +0900

    refactor: DetailViewController 리뷰 반영 final 추가
    Co-authored-by: Doe-Hyung KIM <kimdo2297@gmail.com>

--- Expo1900/Expo1900/Controller/DetailViewController.swift
+++ Expo1900/Expo1900/Controller/DetailViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class DetailViewController: UIViewController {
+final class DetailViewController: UIViewController {
     var expoItemElement: ExpoItemElement?
     
     @IBOutlet weak var ExpoItemImageView: UIImageView!
