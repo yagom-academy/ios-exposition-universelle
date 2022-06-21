73b25704bce6d730b92399a7a19bd91ff81189c6
unchain123 <unchain123@kakao.com>
2022-06-21 11:51:11 +0900

    refactor: 두번째 스토리보드 구현완료

--- Expo1900/Expo1900/Controller/EntryListViewController.swift
+++ Expo1900/Expo1900/Controller/EntryListViewController.swift
@@ -9,40 +9,47 @@ import UIKit
 
 class EntryListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     @IBOutlet weak var tableView: UITableView!
-    let entryList = try! JSONParser.parseEntryList()
-    let firstViewController = MainViewController()
+    var entryList: [EntryList]?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.tableView.dataSource = self
         self.tableView.delegate = self
         self.navigationController?.isNavigationBarHidden = false
+        fetchEntryList()
     }
     
     override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
         self.navigationController?.navigationBar.topItem?.title = "한국의 출품작"
     }
-  
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        super.prepare(for: segue, sender: sender)
+        guard let nextViewController: DescriptionViewController = segue.destination as? DescriptionViewController else { return }
+        guard let cell: CustomTableViewCell = sender as? CustomTableViewCell else { return }
+        
+        nextViewController.imageToSet = cell.koreaEntryImage
+        nextViewController.textToSet = cell.detailedDescription
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        guard let entryList = entryList else { return 0 }
+        
         return entryList.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell: CustomTableViewCell = tableView.dequeueReusableCell(withIdentifier: "customCell", for: indexPath) as! CustomTableViewCell
         
-        cell.koreaEntryTitle.text = entryList[indexPath.row].name
-        cell.shortDescription.text = entryList[indexPath.row].shortDescription
-        cell.koreaEntryImage.image = UIImage(named: entryList[indexPath.row].imageName)
-        cell.accessoryType = .disclosureIndicator
-        cell.detailedDescription = entryList[indexPath.row].description
+        updateCell(cell: cell, indexPath: indexPath)
         
         return cell
     }
     
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        guard let nextViewController: DescriptionViewController = segue.destination as? DescriptionViewController else { return }
-        guard let cell: CustomTableViewCell = sender as? CustomTableViewCell else { return }
+    func fetchEntryList() {
+        guard let entryList = JSONParser.fetch(fileName: "items", parsedItems: entryList) else { return }
         
-        nextViewController.imageToSet = cell.koreaEntryImage
-        nextViewController.textToSet = cell.detailedDescription
+        self.entryList = entryList
     }
 }
--- Expo1900/Expo1900/Model/Extension/EntryListViewController+extension.swift
+++ Expo1900/Expo1900/Model/Extension/EntryListViewController+extension.swift
@@ -4,3 +4,35 @@
 //
 //  Created by unchain, bard on 2022/06/21.
 //
+
+import UIKit
+
+extension EntryListViewController {
+    func updateCell(cell: CustomTableViewCell, indexPath: IndexPath) {
+        updatekoreaEntryImage(of: cell, indexPath)
+        updateKoreaEntryTitle(of: cell, indexPath)
+        updateShortDescription(of: cell, indexPath)
+        updateCellAccessoryType(of: cell)
+        updateDetailedDescription(of: cell, indexPath)
+    }
+    
+    private func updateCellAccessoryType(of cell: CustomTableViewCell) {
+        cell.accessoryType = .disclosureIndicator
+    }
+    
+    private func updateKoreaEntryTitle(of cell: CustomTableViewCell, _ indexPath: IndexPath) {
+        cell.koreaEntryTitle.text = entryList?[indexPath.row].name
+    }
+    
+    private func updatekoreaEntryImage(of cell: CustomTableViewCell, _ indexPath: IndexPath) {
+        cell.koreaEntryImage.image = UIImage(named: entryList?[indexPath.row].imageName ?? "")
+    }
+    
+    private func updateShortDescription(of cell: CustomTableViewCell, _ indexPath: IndexPath) {
+        cell.shortDescription.text = entryList?[indexPath.row].shortDescription
+    }
+    
+    private func updateDetailedDescription(of cell: CustomTableViewCell, _ indexPath: IndexPath) {
+        cell.detailedDescription = entryList?[indexPath.row].description
+    }
+}
