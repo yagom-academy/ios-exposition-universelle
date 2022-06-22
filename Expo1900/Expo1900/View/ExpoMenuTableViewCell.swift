d4f872fbfccaa7fc1689d97c52e9cc09fd08738a
ZZBAE <jebyun2@gmail.com>
2022-06-22 16:44:58 +0900

    refactor: Step2 추가 코멘트 기반 refactoring
    - IBOutlet 프로퍼티의 네이밍 수정 (소문자로 시작하는 컨벤션 준수)
    - ExpoMenuTableViewCell에서 setupUI 메서드 추가

--- Expo1900/Expo1900/View/ExpoMenuTableViewCell.swift
+++ Expo1900/Expo1900/View/ExpoMenuTableViewCell.swift
@@ -11,6 +11,11 @@ class ExpoMenuTableViewCell: UITableViewCell {
     @IBOutlet weak var expoItemImageView: UIImageView!
     @IBOutlet weak var expoItemNameLabel: UILabel!
     @IBOutlet weak var expoItemDescriptionLabel: UILabel!
+   
+    func setupUI(with expoItemElement: ExpoItemElement) {
+         setupLableView(name: expoItemElement.name, shortDescription: expoItemElement.shortDesc)
+         setupImageView(name: expoItemElement.imageName)
+    }
     
     func setupImageView(name: String) {
         self.expoItemImageView.image = UIImage(named: name)
