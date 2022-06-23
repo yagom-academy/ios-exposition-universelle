ebc7404cf32deafa369cec7568d1abdf78325ae1
OldStarKR <tyr2293@naver.com>
2022-06-23 21:31:19 +0900

    refactor: 출품작 테이블 뷰 셀 오토레이아웃 값 재설정

--- Expo1900/Expo1900/Views/KoreanEntryTableViewCell.swift
+++ Expo1900/Expo1900/Views/KoreanEntryTableViewCell.swift
@@ -11,6 +11,7 @@ final class KoreanEntryTableViewCell: UITableViewCell, ReuseIdentifying {
     private lazy var entryImageView: UIImageView = {
         let imageView = UIImageView()
         imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleAspectFit
         return imageView
     }()
     private lazy var titleLabel: UILabel = {
@@ -23,12 +24,13 @@ final class KoreanEntryTableViewCell: UITableViewCell, ReuseIdentifying {
         let shortDescriptionLabel = UILabel()
         shortDescriptionLabel.translatesAutoresizingMaskIntoConstraints = false
         shortDescriptionLabel.numberOfLines = 0
+        shortDescriptionLabel.lineBreakMode = .byWordWrapping
         return shortDescriptionLabel
     }()
     private lazy var descriptionStackView: UIStackView = {
         let stackView = UIStackView()
         stackView.translatesAutoresizingMaskIntoConstraints = false
-        stackView.spacing = 8
+        stackView.spacing = 5
         stackView.axis = .vertical
         return stackView
     }()
@@ -52,18 +54,20 @@ extension KoreanEntryTableViewCell {
     
     private func setupLayout() {
         NSLayoutConstraint.activate([
-            entryImageView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 8),
-            entryImageView.bottomAnchor.constraint(lessThanOrEqualTo: contentView.bottomAnchor, constant: 8),
+            entryImageView.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+            entryImageView.topAnchor.constraint(greaterThanOrEqualTo: contentView.topAnchor, constant: 8),
+            entryImageView.bottomAnchor.constraint(lessThanOrEqualTo: contentView.bottomAnchor, constant: -8),
             entryImageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 8),
             entryImageView.widthAnchor.constraint(equalTo: contentView.widthAnchor, multiplier: 0.2),
             entryImageView.widthAnchor.constraint(equalTo: entryImageView.heightAnchor)
         ])
         
         NSLayoutConstraint.activate([
+            descriptionStackView.centerYAnchor.constraint(equalTo: entryImageView.centerYAnchor),
+            descriptionStackView.topAnchor.constraint(greaterThanOrEqualTo: contentView.topAnchor, constant: 8),
+            descriptionStackView.bottomAnchor.constraint(lessThanOrEqualTo: contentView.bottomAnchor, constant: -8),
             descriptionStackView.leadingAnchor.constraint(equalTo: entryImageView.trailingAnchor, constant: 8),
-            descriptionStackView.topAnchor.constraint(equalTo: entryImageView.topAnchor),
-            descriptionStackView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
-            descriptionStackView.bottomAnchor.constraint(equalTo: entryImageView.bottomAnchor)
+            descriptionStackView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor)
         ])
     }
 }
