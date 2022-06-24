20d7260dc8fde5b9013826136af19e6a5c78988c
ZZBAE <jebyun2@gmail.com>
2022-06-24 18:14:10 +0900

    refactor: 리뷰 반영 AppDelegate에서 사용하지 않는 메서드 정리
    - AppUtility에서 lookOrientation 삭제

--- Expo1900/Expo1900/AppDelegate.swift
+++ Expo1900/Expo1900/AppDelegate.swift
@@ -31,12 +31,4 @@ struct AppUtility {
             delegate.orientationLock = orientation
         }
     }
-    
-    static func lockOrientation(_ orientation: UIInterfaceOrientationMask, andRotateTo rotateOrientation:UIInterfaceOrientation) {
-        
-        self.lockOrientation(orientation)
-        
-        UIDevice.current.setValue(rotateOrientation.rawValue, forKey: "orientation")
-        UINavigationController.attemptRotationToDeviceOrientation()
-    }
 }
