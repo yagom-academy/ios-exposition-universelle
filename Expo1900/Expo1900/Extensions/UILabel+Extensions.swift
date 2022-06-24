f6d70afb444e28c940414ee4287c5ebb20b044fb
derrickkim0109 <derrickkim0109@naver.com>
2022-06-24 10:32:02 +0900

    refactor : "UILabel" 배열 subscript값 처리 옵셔널 프로퍼티로 변경

--- Expo1900/Expo1900/Extensions/UILabel+Extensions.swift
+++ Expo1900/Expo1900/Extensions/UILabel+Extensions.swift
@@ -5,7 +5,6 @@
 //  Created by Baek on 2022/06/17.
 //
 
-import Foundation
 import UIKit
 
 extension UILabel {
@@ -13,7 +12,9 @@ extension UILabel {
         let font = UIFont.preferredFont(forTextStyle: style)
         let fullText = self.text ?? ""
         
-        let targetString = fullText.split(separator: seperator).map{ String($0) }[0]
+        guard let targetString = fullText.split(separator: seperator).map({ String($0) }).first else {
+            return
+        }
         
         let range = (fullText as NSString).range(of: targetString)
         let attributedString = NSMutableAttributedString(string: fullText)
