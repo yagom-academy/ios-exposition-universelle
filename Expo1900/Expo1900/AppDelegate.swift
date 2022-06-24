7cac277d6ffc30f85ff91271546558719a4ca59f
재재 <98302604+ZZBAE@users.noreply.github.com>
2022-06-24 17:53:30 +0900

    refactor: 리뷰 반영 컨벤션 정리
    Co-authored-by: Doe-Hyung KIM <kimdo2297@gmail.com>

--- Expo1900/Expo1900/AppDelegate.swift
+++ Expo1900/Expo1900/AppDelegate.swift
@@ -20,7 +20,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {}
 
     func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
-            return self.orientationLock
+        return self.orientationLock
     }
 }
 
