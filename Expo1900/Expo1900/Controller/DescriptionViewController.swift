27d4ad5449082b36e233b5e52527b84ae20c0147
bard <schy2503@naver.com>
2022-06-24 15:25:45 +0900

    refactor: 불필요한 프로퍼티 제거

--- Expo1900/Expo1900/Controller/DescriptionViewController.swift
+++ Expo1900/Expo1900/Controller/DescriptionViewController.swift
@@ -10,8 +10,6 @@ import UIKit
 final class DescriptionViewController: UIViewController {
     @IBOutlet private(set) weak var imageview: UIImageView!
     @IBOutlet private(set) weak var detailedDescription: UILabel!
-    var detailedDescriptionText: String?
-    var entryImage: UIImageView?
     var entryList: EntryList?
     
     override func viewDidLoad() {
