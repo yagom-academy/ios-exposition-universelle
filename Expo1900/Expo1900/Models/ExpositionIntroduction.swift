6b68a943e076d710ebf185ad1a573c7b74d3ffb8
jin <pearlisrich@naver.com>
2022-10-17 14:54:00 +0900

    feat: ExpositionIntroduction JSON model 생성

---
+++ Expo1900/Expo1900/Models/ExpositionIntroduction.swift
@@ -0,0 +1,14 @@
+//
+//  ExpositionIntroduction.swift
+//  Expo1900
+//
+//  Created by tottalE, Kyo on 10/17/22.
+//
+
+struct ExpositionIntroduction: Codable {
+    var title: String
+    var visitors: Int
+    var location: String
+    var duration: String
+    var description: String
+}
