258d370366ba847fc96f846f2cfb132069d17b72
jsa0224 <94514250+jsa0224@users.noreply.github.com>
2022-10-18 00:50:44 +0900

    feat: ExhibitionWork 파일 생성 & 구조체 타입 설정

---
+++ Expo1900/Expo1900/ExhibitionWork.swift
@@ -0,0 +1,14 @@
+//
+//  ExhibitionWork.swift
+//  Expo1900
+//
+//  Created by Som, Stone, Aaron on 2022/10/18.
+//
+
+struct ExhibitionWork: Codable {
+    let name: String
+    let image_name: String
+    let short_desc: String
+    let desc: String
+}
+
