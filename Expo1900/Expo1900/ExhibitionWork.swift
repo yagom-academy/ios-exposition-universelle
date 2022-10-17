8811299c20fc6f63e549d48b13ab6ec2fb8e2429
hashswim <ss991117@naver.com>
2022-10-18 01:03:34 +0900

    feat: ExpositionPoster Codable프로토콜 채택 및 ExhibitionWork CodingKeys 열거형 구현

--- Expo1900/Expo1900/ExhibitionWork.swift
+++ Expo1900/Expo1900/ExhibitionWork.swift
@@ -7,8 +7,14 @@
 
 struct ExhibitionWork: Codable {
     let name: String
-    let image_name: String
-    let short_desc: String
+    let imageName: String
+    let shortDesc: String
     let desc: String
+    
+    enum CodingKeys: String, CodingKey {
+        case name, desc
+        case imageName = "image_name"
+        case shortDesc = "short_desc"
+    }
 }
 
--- Expo1900/Expo1900/ExpositionPoster.swift
+++ Expo1900/Expo1900/ExpositionPoster.swift
@@ -5,7 +5,7 @@
 //  Created by Som, Stone, Aaron on 2022/10/18.
 //
 
-struct ExpositionPoster {
+struct ExpositionPoster: Codable {
     let title: String
     let visitors: Int
     let location: String
