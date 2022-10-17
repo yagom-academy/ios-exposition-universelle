cbc338fe19902d489a0055f931c92abbd9a278c2
lws2269 <lws2269@kakao.com>
2022-10-18 00:54:30 +0900

    feat: ExpositionPoster 타입 추가 구조체 설정

---
+++ Expo1900/Expo1900/ExpositionPoster.swift
@@ -0,0 +1,14 @@
+//
+//  ExpositionPoster.swift
+//  Expo1900
+//
+//  Created by Som, Stone, Aaron on 2022/10/18.
+//
+
+struct ExpositionPoster {
+    let title: String
+    let visitors: Int
+    let location: String
+    let durataion: String
+    let description: String
+}
