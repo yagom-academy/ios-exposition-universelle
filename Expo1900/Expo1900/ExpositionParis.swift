d66ae1653ab3e57707ca3b78ae4856e6531db2b1
woong <workplayhard1@naver.com>
2022-10-17 17:22:30 +0900

    feat: ExpositionParis 구조체 정의

---
+++ Expo1900/Expo1900/ExpositionParis.swift
@@ -0,0 +1,16 @@
+//
+//  ExpositionParis.swift
+//  Expo1900
+//
+//  Created by baem,woong on 2022/10/17.
+//
+
+import Foundation
+
+struct ExpositionParis: Codable {
+    let title: String
+    let visitors: Int
+    let location: String
+    let duration: String
+    let description: String
+}
