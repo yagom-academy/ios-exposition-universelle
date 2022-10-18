e1683a4c406ab6e1691f62111fba751f4c212bf2
yuvinrho <yvrho@naver.com>
2022-10-18 11:02:13 +0900

    feat: exposition.json 포멧의 데이터와 매칭할exposition타입 구현

---
+++ Expo1900/Expo1900/ExpositionUniverselle1900.swift
@@ -0,0 +1,9 @@
+//  Created by Rhovin, Aejong on 2022/10/18.
+
+struct ExpositionUniverselle1900: Codable {
+    let title: String
+    let visitors: Int
+    let location: String
+    let duration: String
+    let description: String
+}
