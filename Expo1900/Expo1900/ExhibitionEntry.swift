18528249fe0fafdade126a954245980d05ae171f
lxodud <xodud1998223@naver.com>
2022-10-18 10:50:37 +0900

    style: ExhibitionEntry 오타 수정

--- Expo1900/Expo1900/ExhibitionEntry.swift
+++ Expo1900/Expo1900/ExhibitionEntry.swift
@@ -1,11 +1,11 @@
 //
-//  ExibitionEntry.swift
+//  ExhibitionEntry.swift
 //  Expo1900
 //
 //  Created by Hamo, Mene on 2022/10/18.
 //
 
-struct ExibitionEntry: Decodable {
+struct ExhibitionEntry: Decodable {
     let name: String
     let imageName: String
     let shortDescription: String
