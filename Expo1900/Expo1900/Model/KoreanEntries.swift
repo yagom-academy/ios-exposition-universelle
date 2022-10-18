85eb3d014f4d60b5d2d500b30d84062f1e73a19d
Mangdi <lkw4670@naver.com>
2022-10-18 17:19:11 +0900

    refactor: KoreanEntries의 상수명 수정

--- Expo1900/Expo1900/Model/KoreanEntries.swift
+++ Expo1900/Expo1900/Model/KoreanEntries.swift
@@ -4,12 +4,13 @@
 struct KoreanEntries: Codable {
     let name: String
     let imageName: String
-    let shortDesc: String
-    let desc: String
+    let shortDescription: String
+    let description: String
     
     enum CodingKeys: String, CodingKey {
-        case name, desc
+        case name
         case imageName = "image_name"
-        case shortDesc = "short_desc"
+        case shortDescription = "short_desc"
+        case description = "desc"
     }
 }
