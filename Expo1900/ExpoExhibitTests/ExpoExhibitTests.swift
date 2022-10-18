0946d5a5965b070892699249eb5824fb486e3ecc
미니 <cow970814@naver.com>
2022-10-18 10:29:28 +0900

    test : 테스트 코드 초기 설정

---
+++ Expo1900/ExpoExhibitTests/ExpoExhibitTests.swift
@@ -0,0 +1,22 @@
+//
+//  ExpoExhibitTests.swift
+//  ExpoExhibitTests
+//
+//  Copyright (c) 2022 Minii All rights reserved.
+        
+
+import XCTest
+
+final class ExpoExhibitTests: XCTestCase {
+    override func setUpWithError() throws {
+        
+    }
+
+    override func tearDownWithError() throws {
+        
+    }
+
+    func testExample() throws {
+        
+    }
+}
