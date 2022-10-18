7dd2ed6afe174c59e2372eef0916b879b6e6b827
DragonYG <kioswe92@gmail.com>
2022-10-18 13:33:33 +0900

    feat: exposition_universelle_1900.json 파일과 매칭할 구조체 Exposition 구현

---
+++ Expo1900/Expo1900/Exposition.swift
@@ -0,0 +1,14 @@
+//
+//  Exposition.swift
+//  Expo1900
+//
+//  Created by Gundy, Dragon on 2022/10/18.
+//
+
+struct Exposition: Codable {
+    let title: String
+    let visitors: Int
+    let location: String
+    let duration: String
+    let description: String
+}
