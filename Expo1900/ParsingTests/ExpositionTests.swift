035167d0dcc34d4835750c791e1ce5eab0abbde0
jpush <lj845854@gmail.com>
2022-10-18 11:12:04 +0900

    feat: Exposition 타입 생성

---
+++ Expo1900/ParsingTests/ExpositionTests.swift
@@ -0,0 +1,29 @@
+//
+//  ExpoExhibitTests.swift
+//  ExpoExhibitTests
+//
+//  Copyright (c) 2022 Minii All rights reserved.
+        
+
+import XCTest
+@testable import Expo1900
+
+final class ExpositionTests: XCTestCase {
+    
+    var sut: Exposition!
+    
+    override func tearDownWithError() throws {
+        try super.tearDownWithError()
+        
+        sut = nil
+    }
+    
+    private func test_when_parsing_expo_then_not_nil() {
+        // given
+        
+        // when
+        
+        // then
+
+    }
+}
