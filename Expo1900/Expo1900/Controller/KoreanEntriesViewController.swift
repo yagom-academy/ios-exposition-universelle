410c22f1de72ef6df2de7c8f9843650741e01e15
zhilly11 <zhzh1x4@gmail.com>
2022-10-18 18:11:42 +0900

    feat: KoreanEntriesViewController 에 TableView 생성하는 코드 작성

---
+++ Expo1900/Expo1900/Controller/KoreanEntriesViewController.swift
@@ -0,0 +1,58 @@
+//  Expo1900 - KoreanEntriesViewController.swift
+//  Created by Mangdi, zhilly on 2022/10/18
+
+import UIKit
+
+class KoreanEntriesViewController: UIViewController {
+    
+    @IBOutlet weak var koreanEntriesTableView: UITableView!
+    
+    let cellIdentifier: String = "koreanEntryCell"
+    var koreanEntries: [KoreanEntries] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        loadKoreanEntries()
+    }
+    
+    func loadKoreanEntries() {
+        let jsonDecoder: JSONDecoder = JSONDecoder()
+        guard let dataAsset: NSDataAsset = NSDataAsset(name: "koreanEntryCell") else { return }
+        
+        do {
+            self.koreanEntries = try jsonDecoder.decode([KoreanEntries].self, from: dataAsset.data)
+        } catch {
+            return
+        }
+        
+        self.koreanEntriesTableView.reloadData()
+    }
+}
+
+extension KoreanEntriesViewController: UITableViewDataSource {
+
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.koreanEntries.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: UITableViewCell = koreanEntriesTableView.dequeueReusableCell(withIdentifier: self.cellIdentifier, for: indexPath)
+        let entry: KoreanEntries = self.koreanEntries[indexPath.row]
+        
+        var content = cell.defaultContentConfiguration()
+        //content.image = UIImage(named: entry.imageName)
+        content.text = entry.name
+        content.secondaryText = entry.shortDescription
+        cell.contentConfiguration = content
+        return cell
+    }
+}
+
+extension KoreanEntriesViewController: UITableViewDelegate {
+    
+}
