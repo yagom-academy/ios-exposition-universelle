b783992e2696322e2c706265d711bb420d86990e
sunnyCookie <107384230+sunny-maeng@users.noreply.github.com>
2022-10-18 11:17:19 +0900

    feat: ExpoInformation 구조체 선언

---
+++ Expo1900/Expo1900/ExpoInformation.swift
@@ -0,0 +1,16 @@
+//
+//  ExpoInformation.swift
+//  Expo1900
+//
+//  Created by 맹선아 on 2022/10/18.
+//
+
+import Foundation
+
+struct ExpoInformation: Codable {
+    let title: String
+    let visitors: Int
+    let location: String
+    let duration: String
+    let description: String
+}
