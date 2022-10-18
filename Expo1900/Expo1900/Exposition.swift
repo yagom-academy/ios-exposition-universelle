a4d14d6f1c49ce7701a92d096a99bb2f98d63150
yjjem <marksim0606@gmail.com>
2022-10-18 11:06:21 +0900

    feat: Exposition 구조체 추가

---
+++ Expo1900/Expo1900/Exposition.swift
@@ -0,0 +1,14 @@
+//
+//  Exposition.swift
+//  Expo1900
+//
+//  Created by Jeremy, 준호 on 2022/10/18.
+//
+
+struct Exposition: Codable {
+    var title: String
+    var visitors: Int
+    var location: String
+    var duration: String
+    var description: String
+}
