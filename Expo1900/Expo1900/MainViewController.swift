f935bb0f5a5216e07979209b176b90442448592d
lxodud <xodud1998223@naver.com>
2022-10-19 10:39:27 +0900

    feat: MainViewController IBOulet 연결

---
+++ Expo1900/Expo1900/MainViewController.swift
@@ -0,0 +1,21 @@
+//
+//  Expo1900 - MainViewController.swift
+//  Created by yagom. 
+//  Copyright © yagom academy. All rights reserved.
+// 
+
+import UIKit
+
+final class MainViewController: UIViewController {
+    @IBOutlet private weak var titleLabel: UILabel!
+    @IBOutlet private weak var visitorsLabel: UILabel!
+    @IBOutlet private weak var locationLabel: UILabel!
+    @IBOutlet private weak var durationLabel: UILabel!
+    @IBOutlet private weak var descriptionTextView: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
