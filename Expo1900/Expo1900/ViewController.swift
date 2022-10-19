bf43c373505c63d5f0d1f1dc161ddd1c08620996
sunnyCookie <107384230+sunny-maeng@users.noreply.github.com>
2022-10-19 10:18:09 +0900

    feat: JSON 포맷의 expoInformation 디코딩 코드 구현

--- Expo1900/Expo1900/ViewController.swift
+++ Expo1900/Expo1900/ViewController.swift
@@ -6,12 +6,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var expoInformation: [ExpoInformation] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let decoder: JSONDecoder = JSONDecoder()
+        guard let asset = NSDataAsset(name: "exposition_universelle_1900") else { return }
+
+        do {
+            expoInformation = try decoder.decode([ExpoInformation].self, from: asset.data)
+        } catch {
+            print(error.localizedDescription)
+        }
     }
-
-
 }
-
