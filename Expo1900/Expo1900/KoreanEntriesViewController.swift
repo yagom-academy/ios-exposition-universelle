8d56102d6f92e9f95bed32aea263b5b8fbf20610
Jae Hyung Kim <smfc.mene@gmail.com>
2022-10-19 10:36:32 +0800

    feat: 한국의 출품작 KoreanEntriesViewController 생성하고 IBOutlet 설정

---
+++ Expo1900/Expo1900/KoreanEntriesViewController.swift
@@ -0,0 +1,18 @@
+//
+//  KoreanEntriesViewController.swift
+//  Expo1900
+//
+//  Created by Hamo, Mene on 19/10/2022.
+//
+
+import UIKit
+
+final class KoreanEntriesViewController: UIViewController {
+    @IBOutlet private weak var entriesTableView: UITableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
