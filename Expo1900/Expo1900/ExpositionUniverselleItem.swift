c500f9e944cad42bf6919c4d7dbb006412f909d3
chacha <happyhy0429@gmail.com>
2022-10-19 22:14:11 +0900

    refactor: ExpositionUniverselleItem 타입 내부 불필요 기능 삭제

--- Expo1900/Expo1900/ExpositionUniverselleItem.swift
+++ Expo1900/Expo1900/ExpositionUniverselleItem.swift
@@ -1,13 +1,8 @@
-import UIKit
 struct ExpositionUniverselleItem: Codable {
     let name: String
-    private let imageName: String
+    let imageName: String
     let shortDescription: String
     let description: String
-    
-    var image: UIImage? {
-        return UIImage(named: self.imageName)
-    }
     
     enum CodingKeys: String, CodingKey {
         case name
