b1756d2c9a20bf0e502171bf9fb41cd0ddee86e1
woong <workplayhard1@naver.com>
2022-10-19 14:50:23 +0900

    feat: DetailEntryViewController 생성 및 View 요소 그리기 및 오토레이아웃 설정

---
+++ Expo1900/Expo1900/DetailEntryViewController.swift
@@ -0,0 +1,18 @@
+//
+//  DetailEntryViewController.swift
+//  Expo1900
+//
+//  Created by baem,woong on 2022/10/19.
+//
+
+import UIKit
+
+class DetailEntryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+    }
+    
+
+}
