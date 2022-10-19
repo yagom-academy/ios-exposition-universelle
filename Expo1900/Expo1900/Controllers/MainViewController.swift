1bcefa2c5d2fdcb1541cb1da04446e03419c496c
yuvinrho <yvrho@naver.com>
2022-10-19 15:15:20 +0900

    feat: decodeExposition() 정의

--- Expo1900/Expo1900/Controllers/MainViewController.swift
+++ Expo1900/Expo1900/Controllers/MainViewController.swift
@@ -7,12 +7,22 @@
 import UIKit
 
 class MainViewController: UIViewController {
-
+    @IBOutlet private weak var mainStackView: UIStackView!
+    private let jsonDecoder = JSONDecoder()
+    private var exposition: Exposition? = nil
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        exposition = decodeExposition("exposition_universelle_1900")
     }
-
+    
+    private func decodeExposition(_ file: String) -> Exposition? {
+        guard let expositionAsset: NSDataAsset = NSDataAsset(name: file) else { return nil }
+        let exposition = try? jsonDecoder.decode(Exposition.self, from: expositionAsset.data)
+        
+        return exposition
+    }
 
 }
 
