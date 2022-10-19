9708dd0a9f87d1de37739b7a87fc8fdb0dd5b060
lxodud <xodud1998223@naver.com>
2022-10-19 13:48:00 +0900

    feat: 출품작 상세 뷰 컨트롤러 EntryDetailViewController 구현 및 아울렛 연결

---
+++ Expo1900/Expo1900/EntryDetailViewController.swift
@@ -0,0 +1,18 @@
+//
+//  EntryDetailViewController.swift
+//  Expo1900
+//
+//  Created by 이태영 on 2022/10/19.
+//
+
+import UIKit
+
+final class EntryDetailViewController: UIViewController {
+    @IBOutlet private weak var entryImage: UIImageView!
+    @IBOutlet private weak var enteyDetailTextView: UITextView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
