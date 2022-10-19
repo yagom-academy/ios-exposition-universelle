db2a205db0a8ff6d83cde46726ed8e7b0fb536e6
Jae Hyung Kim <smfc.mene@gmail.com>
2022-10-19 09:57:26 +0800

    feat: json파일에서 expo정보를 불러오는 fetchExpoInformation 메서드 구현

--- Expo1900/Expo1900/MainViewController.swift
+++ Expo1900/Expo1900/MainViewController.swift
@@ -13,9 +13,25 @@ final class MainViewController: UIViewController {
     @IBOutlet private weak var durationLabel: UILabel!
     @IBOutlet private weak var descriptionTextView: UITextView!
     
+    var expositionUniverselle: ExpositionUniverselle?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        fetchExpoInformation()
+        
+    }
+    
+    private func fetchExpoInformation() {
+        let jsonDecoder: JSONDecoder = JSONDecoder()
+        guard let dataAsset: NSDataAsset = NSDataAsset(name: "exposition_universelle_1900") else {
+            return
+        }
+        
+        do {
+            self.expositionUniverselle = try jsonDecoder.decode(ExpositionUniverselle.self, from: dataAsset.data)
+        } catch {
+            print(error.localizedDescription)
+        }
     }
 }
 
