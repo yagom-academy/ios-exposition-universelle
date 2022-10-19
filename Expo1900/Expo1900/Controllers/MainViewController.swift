2c4203197ff62f82ff32f65289c64b116a78f82d
jonghancha <jonghancha@gmail.com>
2022-10-19 15:49:32 +0900

    feat: Exposition타입 연산프로퍼티 추가, mainStackView Label 추가

--- Expo1900/Expo1900/Controllers/MainViewController.swift
+++ Expo1900/Expo1900/Controllers/MainViewController.swift
@@ -15,6 +15,7 @@ class MainViewController: UIViewController {
         super.viewDidLoad()
         
         exposition = decodeExposition("exposition_universelle_1900")
+        configureView()
     }
     
     private func decodeExposition(_ file: String) -> Exposition? {
@@ -23,6 +24,67 @@ class MainViewController: UIViewController {
         
         return exposition
     }
-
+    
+    func configureView() {
+        let koreanTitleLabel: UILabel = {
+            let label = UILabel()
+            label.text = exposition?.koreanTitle
+            label.font = UIFont.preferredFont(forTextStyle: .largeTitle)
+            
+            return label
+        }()
+        
+        mainStackView.addArrangedSubview(koreanTitleLabel)
+        
+        let englishTitleLabel: UILabel = {
+            let label = UILabel()
+            label.text = exposition?.englishTitle
+            label.font = UIFont.preferredFont(forTextStyle: .title1)
+            
+            return label
+        }()
+        
+        mainStackView.addArrangedSubview(englishTitleLabel)
+        
+        let visitorsLabel: UILabel = {
+            let label = UILabel()
+            label.text = "방문객 : \(exposition?.visitors ?? 0)"
+            label.font = UIFont.preferredFont(forTextStyle: .title2)
+            
+            return label
+        }()
+        
+        mainStackView.addArrangedSubview(visitorsLabel)
+        
+        let locationLabel: UILabel = {
+            let label = UILabel()
+            label.text = exposition?.location
+            label.font = UIFont.preferredFont(forTextStyle: .title2)
+            
+            return label
+        }()
+        
+        mainStackView.addArrangedSubview(locationLabel)
+        
+        let durationLabel: UILabel = {
+            let label = UILabel()
+            label.text = exposition?.duration
+            label.font = UIFont.preferredFont(forTextStyle: .title2)
+            
+            return label
+        }()
+        
+        mainStackView.addArrangedSubview(durationLabel)
+        
+        let descriptionLabel: UILabel = {
+            let label = UILabel()
+            label.text = exposition?.description
+            label.font = UIFont.preferredFont(forTextStyle: .body)
+            label.numberOfLines = 0
+            
+            return label
+        }()
+        
+        mainStackView.addArrangedSubview(descriptionLabel)
+    }
 }
-
--- Expo1900/Expo1900/Models/Exposition.swift
+++ Expo1900/Expo1900/Models/Exposition.swift
@@ -2,6 +2,14 @@
 
 struct Exposition: Decodable {
     let title: String
+    var koreanTitle: String {
+        guard let firstBracketIndex = title.firstIndex(of: "(") else { return "" }
+        return String(title[..<firstBracketIndex])
+    }
+    var englishTitle: String {
+        guard let firstBracketIndex = title.firstIndex(of: "(") else { return "" }
+        return String(title[firstBracketIndex...])
+    }
     let visitors: Int
     let location: String
     let duration: String
