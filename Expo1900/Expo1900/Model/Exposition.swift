73e63d87f3f23a93241d1a4cac7dbaf903c2b165
lj-7-77 <swtw72@gmail.com>
2022-10-20 22:42:51 +0900

    feat: Exposition 구조체 연산프로퍼티 생성

--- Expo1900/Expo1900/Model/Exposition.swift
+++ Expo1900/Expo1900/Model/Exposition.swift
@@ -2,10 +2,30 @@
 //  Expo1900
 //  Created by inho, LJ on 2022/10/18.
 
+import UIKit
+
 struct Exposition: Decodable {
     let title: String
     let visitors: Int
     let location: String
     let duration: String
     let description: String
+    var visitorInformation: String {
+        return "방문객 : \(visitors) 명"
+    }
+    var locationInformation: String {
+        return "개최지 : \(location)"
+    }
+    var durationInformation: String {
+        return "개최 기간 : \(duration)"
+    }
+    var expositionImage: UIImage? {
+        return UIImage(named: "poster")
+    }
+    var flagImage: UIImage? {
+        return UIImage(named: "flag")
+    }
+    var exhibitButtonText: String {
+        return "한국의 출품작 보러가기"
+    }
 }
