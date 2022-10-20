15ddff9f60965bfe69d25f61fb92c33e05fed6b8
jsa0224 <94514250+jsa0224@users.noreply.github.com>
2022-10-20 16:55:23 +0900

    feat: setUpJSONData 메서드 추가

--- Expo1900/Expo1900/ExhibitionWorkListViewController.swift
+++ Expo1900/Expo1900/ExhibitionWorkListViewController.swift
@@ -15,7 +15,21 @@ class ExhibitionWorkListViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setUpJSONData()
+    }
+    
+    func setUpJSONData() {
+        let jsonDecoder: JSONDecoder = JSONDecoder()
+        guard let dataAsset: NSDataAsset = NSDataAsset(name: "items") else {
+            return
+        }
         
+        do {
+            exhibitionWork = try jsonDecoder.decode([ExhibitionWork].self, from: dataAsset.data)
+        } catch {
+            print(error.localizedDescription)
+        }
+        self.exhibitionWorkTableView.reloadData()
     }
 }
 
