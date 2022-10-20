e0764cc596ef33fcc8a917165b48c1176dfa8310
chacha <happyhy0429@gmail.com>
2022-10-20 20:32:21 +0900

    feat: ExpositionItems 화면 UI 구성요소 구현

---
+++ Expo1900/Expo1900/ExpositionItemsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ExpositionItemsViewController.swift
+//  Expo1900
+//
+//  Created by hy on 2022/10/20.
+//
+
+import UIKit
+
+class ExpositionItemsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
