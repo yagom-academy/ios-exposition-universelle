59b15500d36872b55e86e1d5e4b3d5b9a2a15465
lj-7-77 <swtw72@gmail.com>
2022-10-21 00:38:20 +0900

    feat: ExhibitTableViewCell 타입 생성

---
+++ Expo1900/Expo1900/Controller/ExhibitTableViewCell.swift
@@ -0,0 +1,11 @@
+//  ExhibitTableViewCell.swift
+//  Expo1900
+//  Created by inho, LJ  on 2022/10/21.
+
+import UIKit
+
+class ExhibitTableViewCell: UITableViewCell {
+    @IBOutlet weak var exhibitImageView: UIImageView!
+    @IBOutlet weak var exhibitNameLabel: UILabel!
+    @IBOutlet weak var exhibitShortDescriptionLabel: UILabel!
+}
