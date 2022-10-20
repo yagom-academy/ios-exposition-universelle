ff4fe842c53e0549c3df67d7cfa28f735daafe6a
yjjem <marksim0606@gmail.com>
2022-10-20 21:09:03 +0900

    feat: 데이터 추출 기능 추가, TableView 기능 구현

--- Expo1900/Expo1900/ExpoEntryViewController.swift
+++ Expo1900/Expo1900/ExpoEntryViewController.swift
@@ -8,33 +8,50 @@
 import UIKit
 
 final class ExpoEntryViewController: UIViewController {
-    @IBOutlet weak var ExpoEntryTableView: UITableView!
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    
+    @IBOutlet private weak var ExpoEntryTableView: UITableView!
+    
+    var expoEntries: [ExpoEntry] = []
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        configureExpoEntryDataFromJSON(name: "items")
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    private func configureExpoEntryDataFromJSON(name: String) {
+        let jsonDecoder: JSONDecoder = JSONDecoder()
+        guard let dataAsset: NSDataAsset = NSDataAsset(name: name) else { return }
+        
+        do {
+            self.expoEntries = try jsonDecoder.decode([ExpoEntry].self, from: dataAsset.data)
+        } catch {
+            print(error)
+        }
     }
-    */
-
 }
 
 extension ExpoEntryViewController: UITableViewDataSource {
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return expoEntries.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell: UITableViewCell = ExpoEntryTableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        configureExpoEntryContents(of: cell, with: expoEntries[indexPath.row])
+        
         return cell
+    }
+    
+    private func configureExpoEntryContents(of cell: UITableViewCell, with expoEntry: ExpoEntry) {
+        cell.imageView?.image = UIImage(named: expoEntry.imageName)
+        cell.textLabel?.text = expoEntry.name
+        cell.detailTextLabel?.text = expoEntry.shortDescription
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 90
     }
 }
 
