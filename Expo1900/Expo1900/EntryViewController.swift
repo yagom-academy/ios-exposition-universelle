0c3440b87a77c9ae4f090704415d9673cb82b88b
sunnyCookie <107384230+sunny-maeng@users.noreply.github.com>
2022-10-20 17:10:13 +0900

    feat: entryTableView 스토리보드 구성 및 outlet 연결

--- Expo1900/Expo1900/EntryViewController.swift
+++ Expo1900/Expo1900/EntryViewController.swift
@@ -8,11 +8,26 @@
 import UIKit
 
 class EntryViewController: UIViewController {
-
+    @IBOutlet weak var entryTableView: UITableView!
+    let cellIdetifier: String = "entryCell"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationController?.navigationBar.isHidden = false
         title = "한국의 출품작"
     }
+}
 
+extension EntryViewController: UITableViewDelegate {
+    
+}
+
+extension EntryViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return entries.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
 }
