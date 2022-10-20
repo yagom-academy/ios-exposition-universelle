93b163f3e8fb397cc76b64e4bacca84e1d5d0495
lnhk98 <hozzang666@gmai.com>
2022-10-20 18:04:44 +0900

    feat: exposition View UI요소에 IBOutlet 연결

--- Expo1900/Expo1900/Controller/ViewController.swift
+++ Expo1900/Expo1900/Controller/ViewController.swift
@@ -6,6 +6,16 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var expositionTitleLabel: UILabel!
+    @IBOutlet weak var expositionImageView: UIImageView!
+    @IBOutlet weak var expositionVisitorsLabel: UILabel!
+    @IBOutlet weak var expositionLocationLabel: UILabel!
+    @IBOutlet weak var expositionDurationLabel: UILabel!
+    @IBOutlet weak var expositionDescriptionTextView: UITextView!
+    @IBOutlet weak var leftFlagImageView: UIImageView!
+    @IBOutlet weak var showExhibitButton: UIButton!
+    @IBOutlet weak var rightFlagImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
