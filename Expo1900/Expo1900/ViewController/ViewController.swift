2347c7af5597121e5427f3297277ecd08d124288
미니 <cow970814@naver.com>
2022-10-20 19:04:32 +0900

    feat : 스크롤 뷰 제외 포스터 뷰 오토레이아웃 구현

--- Expo1900/Expo1900/ViewController/ViewController.swift
+++ Expo1900/Expo1900/ViewController/ViewController.swift
@@ -10,6 +10,7 @@ class ViewController: UIViewController {
     let titleLabel: UILabel = {
         let label = UILabel()
         label.font = .systemFont(ofSize: 20, weight: .bold)
+        label.text = "파리 박람회 1900"
         return label
     }()
     
@@ -20,49 +21,80 @@ class ViewController: UIViewController {
     
     let visitorsLabel: UILabel = {
         let label = UILabel()
+        label.textAlignment = .center
+        label.text = "방문객 : 10000000명"
         return label
     }()
     
     let locationLabel: UILabel = {
         let label = UILabel()
+        label.text = "개최지 : 프랑스 파리 프랑스 파리프랑스 파리 프랑스 파리 프랑스 파리 프랑스 파리 프랑스 파리 프랑스 파리"
         return label
     }()
     
     let durationLabel: UILabel = {
         let label = UILabel()
+        label.text = "개최 기간 : 1900.04.14 - 1900.11.12 1900.04.14 - 1900.11.12 1900.04.14 - 1900.11.12 1900.04.14 - 1900.11.12"
         return label
     }()
     
     let descriptionLabel: UILabel = {
         let label = UILabel()
+        label.numberOfLines = 0
+        let text: String = """
+보내는 되는 피부가 오아이스도 뜨거운지라, 피가 보배를 끓는 능히 것이다. 열락의 밥을 이것을 피다. 기관과 무엇을 충분히 이것은 설산에서 이것이다. 창공에 있을 희망의 듣기만 있는 못하다 생생하며, 생의 주며, 칼이다. 황금시대를 하였으며, 곳이 얼음에 위하여서, 듣기만 열락의 거친 사막이다. 청춘에서만 이 인간은 청춘 봄날의 천하를 사라지지 보라. 이것은 천자만홍이 불어 것이다. 품고 들어 인간의 이성은 힘있다. 청춘을 뜨거운지라, 앞이 같은 운다.
+
+타오르고 가장 우리의 있는 사막이다. 속잎나고, 얼마나 노래하며 청춘에서만 과실이 우리 피가 가진 황금시대다. 피에 있을 영락과 심장의 못하다 방황하여도, 그들은 그들은 약동하다. 이상이 그들에게 광야에서 구하지 곳으로 위하여 인생을 그들의 그들은 철환하였는가? 소리다.이것은 청춘 이상은 속잎나고, 같은 간에 그림자는 싶이 사막이다. 새가 우리의 이것이야말로 황금시대다. 커다란 그들의 이상 간에 충분히 위하여서, 찬미를 돋고, 부패뿐이다. 품으며, 타오르고 뛰노는 붙잡아 아름답고 장식하는 날카로우나 커다란 주는 보라. 황금시대를 인류의 예가 때에, 뜨고, 것은 따뜻한 우리는 있다. 거선의 힘차게 열락의 것이다.
+
+부패를 물방아 원대하고, 우는 현저하게 보는 하였으며, 것은 풀밭에 듣는다. 이상의 두손을 낙원을 장식하는 같은 끓는다. 품었기 피에 돋고, 우리의 반짝이는 트고, 날카로우나 아니다. 끝에 우리는 무한한 곳이 인생을 천자만홍이 말이다. 보이는 피가 열락의 주며, 것이다. 봄날의 전인 가치를 이것을 가는 이것이야말로 꾸며 아니다. 그들의 맺어, 속잎나고, 말이다. 꽃이 석가는 주며, 피어나기 피다. 소금이라 소리다.이것은 우리 갑 것이다. 그들의 대고, 들어 방황하여도, 뭇 일월과 풀밭에 열매를 위하여서.
+
+품고 고행을 있는 눈에 사랑의 뿐이다. 돋고, 그들을 듣기만 거친 힘차게 있는 피다. 있는 것은 자신과 이상을 놀이 이것이다. 품으며, 현저하게 붙잡아 그들의 능히 것이다. 오아이스도 보이는 꾸며 때에, 발휘하기 있을 불어 얼음이 이것이다. 인생의 듣기만 없으면, 놀이 뿐이다. 남는 이상이 이것이야말로 보는 그들은 이것은 있는 타오르고 보라. 찬미를 끓는 인간이 무한한 장식하는 우리는 있는 트고, 이것이다. 할지니, 어디 위하여, 커다란 피는 그들을 같은 듣기만 위하여서. 미인을 수 방황하였으며, 가슴이 모래뿐일 생생하며, 인생의 아니한 이것이다. 위하여 대고, 뭇 것이 오직 뿐이다.
+
+이성은 얼마나 뛰노는 가치를 예수는 이상이 청춘 얼음 이상 사막이다. 꾸며 있으며, 부패를 따뜻한 풀이 얼마나 얼마나 철환하였는가? 있음으로써 예가 곳이 커다란 말이다. 군영과 붙잡아 찬미를 갑 그리하였는가? 보이는 이성은 보이는 그들은 위하여서. 있는 같은 타오르고 행복스럽고 사라지지 얼음 하는 칼이다. 같은 있을 피는 그림자는 싸인 맺어, 역사를 듣는다. 품었기 얼마나 대한 사막이다. 그러므로 대중을 불어 그들은 모래뿐일 이것은 힘차게 살 쓸쓸하랴? 못할 품에 고행을 열락의 않는 천자만홍이 이것이다.
+
+찾아 때까지 인생에 같지 피어나기 이상이 새가 우리 사람은 황금시대다. 우리 꽃이 있음으로써 속에 너의 뿐이다. 가치를 그러므로 원질이 있다. 길을 우리는 꽃이 트고, 것은 지혜는 이것이다. 맺어, 용감하고 없는 위하여 피가 구하지 보내는 것은 때까지 철환하였는가? 미인을 거친 어디 같이, 뼈 보이는 수 이상 못하다 철환하였는가? 용기가 되는 있으며, 가치를 무엇을 끓는 가는 이상의 듣는다. 열락의 할지니, 실로 열매를 칼이다. 있을 쓸쓸한 인생을 아름다우냐?
+
+따뜻한 풀밭에 주는 것이다. 대고, 사랑의 없으면 이상 예수는 봄바람을 주는 생의 때문이다. 새 피가 것은 뜨고, 트고, 그들은 생의 생생하며, 교향악이다. 이상을 할지라도 인도하겠다는 철환하였는가? 기관과 우리 끝에 속에 우는 없으면 관현악이며, 칼이다. 예가 유소년에게서 인류의 할지니, 인생을 가지에 사막이다. 같이 현저하게 청춘의 하여도 풀이 유소년에게서 그들의 붙잡아 그리하였는가? 풍부하게 관현악이며, 할지니, 전인 목숨이 이상의 들어 인생에 이상의 말이다. 이상을 생명을 바이며, 현저하게 품에 끝까지 사막이다. 유소년에게서 보는 뛰노는 설레는 무엇을 충분히 피다. 열매를 뼈 품으며, 못할 찾아다녀도, 고행을 노년에게서 힘차게 동력은 끓는다.
+"""
+        label.text = text
         return label
     }()
     
-    lazy var entryStackView: UIStackView = {
-        let stackView = UIStackView()
-        let flag = UIImageView(image: UIImage(named: "flag"))
-        stackView.axis = .horizontal
-        
-        stackView.addArrangedSubview(flag)
-        stackView.addArrangedSubview(entrybutton)
-        stackView.addArrangedSubview(flag)
-        return stackView
+    let leftFlagImageView: UIImageView = {
+        let imageView = UIImageView(image: UIImage(named: "flag"))
+        imageView.contentMode = .scaleAspectFit
+        imageView.widthAnchor.constraint(equalToConstant: 50).isActive = true
+        imageView.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        return imageView
     }()
     
-    let entrybutton: UIButton = {
+    let rightFlagImageView: UIImageView = {
+        let imageView = UIImageView(image: UIImage(named: "flag"))
+        imageView.contentMode = .scaleAspectFit
+        imageView.widthAnchor.constraint(equalToConstant: 50).isActive = true
+        imageView.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        return imageView
+    }()
+
+    
+    let entryButton: UIButton = {
         let button = UIButton()
+        button.setTitleColor(.systemBlue, for: .normal)
         button.setTitle("한국의 출품작 보러가기", for: .normal)
         return button
     }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        
         setLayout()
     }
     
     private func setLayout() {
+        let stackView = UIStackView(arrangedSubviews: [leftFlagImageView, entryButton, rightFlagImageView])
+        
+        stackView.alignment = .center
+        
+        
         let layoutList: [UIView] = [
             titleLabel,
             posterImageView,
@@ -70,11 +102,42 @@ class ViewController: UIViewController {
             locationLabel,
             durationLabel,
             descriptionLabel,
+            stackView
         ]
+        let safeArea = view.safeAreaLayoutGuide
         
-        layoutList.forEach { view in
-            view.translatesAutoresizingMaskIntoConstraints = false
+        layoutList.forEach { element in
+            element.translatesAutoresizingMaskIntoConstraints = false
+            view.addSubview(element)
         }
+        
+        NSLayoutConstraint.activate([
+            titleLabel.topAnchor.constraint(equalTo: safeArea.topAnchor, constant: 16),
+            titleLabel.centerXAnchor.constraint(equalTo: safeArea.centerXAnchor),
+            
+            posterImageView.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 8),
+            posterImageView.centerXAnchor.constraint(equalTo: safeArea.centerXAnchor),
+            
+            visitorsLabel.topAnchor.constraint(equalTo: posterImageView.bottomAnchor, constant: 8),
+            visitorsLabel.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor, constant: 16),
+            visitorsLabel.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor, constant: -16),
+            
+            locationLabel.topAnchor.constraint(equalTo: visitorsLabel.bottomAnchor, constant: 8),
+            locationLabel.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor, constant: 16),
+            locationLabel.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor, constant: -16),
+            
+            durationLabel.topAnchor.constraint(equalTo: locationLabel.bottomAnchor, constant: 8),
+            durationLabel.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor, constant: 16),
+            durationLabel.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor, constant: -16),
+            
+            descriptionLabel.topAnchor.constraint(equalTo: durationLabel.bottomAnchor, constant: 8),
+            descriptionLabel.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor, constant: 16),
+            descriptionLabel.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor, constant: -16),
+            descriptionLabel.bottomAnchor.constraint(equalTo: stackView.topAnchor),
+            
+            stackView.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor, constant: 16),
+            stackView.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor, constant: -16),
+            stackView.bottomAnchor.constraint(equalTo: safeArea.bottomAnchor, constant: -16)
+        ])
     }
 }
-
