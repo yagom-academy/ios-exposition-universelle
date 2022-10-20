b4f229f24317e2a1f315f42127b5904733e1911f
wonbi92 <bin9239@gmail.com>
2022-10-20 18:05:25 +0900

    feat: DetailView에 이전에 선택한 cell에 해당하는 데이터가 보여지도록 구현

--- Expo1900/Expo1900/DetailViewController.swift
+++ Expo1900/Expo1900/DetailViewController.swift
@@ -8,13 +8,24 @@
 import UIKit
 
 class DetailViewController: UIViewController {
+    @IBOutlet weak var detailImage: UIImageView!
+    @IBOutlet weak var descriptionTextView: UITextView!
+    
     var entry: Entry?
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         navigationController?.navigationBar.isHidden = false
         title = entry?.name
+        
+        setupDetailView()
     }
+    
+    func setupDetailView() {
+        guard let entry = entry else { return }
 
+        detailImage.image = UIImage(named: entry.imageName)
+        descriptionTextView.text = entry.description
+    }
 }
