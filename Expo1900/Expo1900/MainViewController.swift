427102bd23018ab8f30d0d66ca591e2bd10135d7
Ayaan <onestar7984@gmail.com>
2022-10-20 18:08:34 +0900

    feat: MainVC 내부 IBOutlet 프로퍼티 구현 및 연결

---
+++ Expo1900/Expo1900/MainViewController.swift
@@ -0,0 +1,24 @@
+//
+//  Expo1900 - ViewController.swift
+//  Created by yagom. 
+//  Copyright © yagom academy. All rights reserved.
+// 
+
+import UIKit
+
+final class MainViewController: UIViewController {
+    @IBOutlet private weak var titleLabel: UILabel!
+    @IBOutlet private weak var posterImageView: UIImageView!
+    @IBOutlet private weak var visitorLabel: UILabel!
+    @IBOutlet private weak var locationLabel: UILabel!
+    @IBOutlet private weak var durationLabel: UILabel!
+    @IBOutlet private weak var descriptionTextView: UITextView!
+    @IBOutlet private weak var showKoreanItemListButton: UIButton!
+    @IBOutlet private weak var leftFlagImageView: UIImageView!
+    @IBOutlet private weak var rightFlagImageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
