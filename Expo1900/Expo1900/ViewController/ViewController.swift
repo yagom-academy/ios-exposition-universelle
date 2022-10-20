b2d76048af5a2d69d471f24d82df259806de6a2b
jpush <lj845854@gmail.com>
2022-10-20 12:15:44 +0900

    feat: 첫화면 요소 생성

--- Expo1900/Expo1900/ViewController/ViewController.swift
+++ Expo1900/Expo1900/ViewController/ViewController.swift
@@ -7,9 +7,74 @@
 import UIKit
 
 class ViewController: UIViewController {
+    let titleLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 20, weight: .bold)
+        return label
+    }()
+    
+    let posterImageView: UIImageView = {
+        let image = UIImageView(image: UIImage(named: "poster"))
+        return image
+    }()
+    
+    let visitorsLabel: UILabel = {
+        let label = UILabel()
+        return label
+    }()
+    
+    let locationLabel: UILabel = {
+        let label = UILabel()
+        return label
+    }()
+    
+    let durationLabel: UILabel = {
+        let label = UILabel()
+        return label
+    }()
+    
+    let descriptionLabel: UILabel = {
+        let label = UILabel()
+        return label
+    }()
+    
+    lazy var entryStackView: UIStackView = {
+        let stackView = UIStackView()
+        let flag = UIImageView(image: UIImage(named: "flag"))
+        stackView.axis = .horizontal
+        
+        stackView.addArrangedSubview(flag)
+        stackView.addArrangedSubview(entrybutton)
+        stackView.addArrangedSubview(flag)
+        return stackView
+    }()
+    
+    let entrybutton: UIButton = {
+        let button = UIButton()
+        button.setTitle("한국의 출품작 보러가기", for: .normal)
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        setLayout()
+    }
+    
+    private func setLayout() {
+        let layoutList: [UIView] = [
+            titleLabel,
+            posterImageView,
+            visitorsLabel,
+            locationLabel,
+            durationLabel,
+            descriptionLabel,
+        ]
+        
+        layoutList.forEach { view in
+            view.translatesAutoresizingMaskIntoConstraints = false
+        }
     }
 }
 
