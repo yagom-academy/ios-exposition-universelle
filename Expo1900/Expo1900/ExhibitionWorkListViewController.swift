193209173267b799f70a2055c8715ce7e258b528
jsa0224 <94514250+jsa0224@users.noreply.github.com>
2022-10-20 16:53:13 +0900

    feat: 스토리보드에 TableView 생성, ExhibitionWorkListViewController 소스파일 생성

---
+++ Expo1900/Expo1900/ExhibitionWorkListViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ExhibitionWorkListViewController.swift
+//  Expo1900
+//
+//  Created by Som, Stone, Aaron on 2022/10/20.
+//
+
+import UIKit
+
+class ExhibitionWorkListViewController: UIViewController {
+    @IBOutlet weak var exhibitionWorkTableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
+
+
