767bfe17fbe6f2aa1daa95e5379e0953d8ca16fb
hashswim <ss991117@naver.com>
2022-10-20 17:35:19 +0900

    feat: DetailWorkViewController 소스 파일 생성

---
+++ Expo1900/Expo1900/DetailWorkViewContorller.swift
@@ -0,0 +1,18 @@
+//
+//  DetailWorkViewContorller.swift
+//  Expo1900
+//
+//  Created by Som, Stone, Aaron on 2022/10/20.
+//
+
+import UIKit
+
+class DetailWorkViewContorller: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
