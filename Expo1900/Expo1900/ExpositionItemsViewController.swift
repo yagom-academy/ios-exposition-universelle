05b14aa3778cb717ebf0c11944b2b28de009ea6d
Ayaan <onestar7984@gmail.com>
2022-10-20 20:43:08 +0900

    feat: ExpositionItemsVC내부 TableView DataSource와 Delegate구현

--- Expo1900/Expo1900/ExpositionItemsViewController.swift
+++ Expo1900/Expo1900/ExpositionItemsViewController.swift
@@ -8,10 +8,24 @@
 import UIKit
 
 class ExpositionItemsViewController: UIViewController {
+    @IBOutlet private weak var tableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
     }
 }
+
+extension ExpositionItemsViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+}
+
+extension ExpositionItemsViewController: UITableViewDelegate { }
