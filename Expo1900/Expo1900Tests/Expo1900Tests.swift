121ff813c9e450ebd3c73f3e36886bb9f9463e27
sunnyCookie <107384230+sunny-maeng@users.noreply.github.com>
2022-10-20 21:47:08 +0900

    test: Entry구조체로 JSONDecoding이 제대로 실행되는지 확인하는 유닛 테스트 케이스 추가

--- Expo1900/Expo1900Tests/Expo1900Tests.swift
+++ Expo1900/Expo1900Tests/Expo1900Tests.swift
@@ -15,7 +15,14 @@ class Expo1900Tests: XCTestCase {
             from: "exposition_universelle_1900"
         )
         
-        XCTAssertEqual(result?.title, "파리 만국박람회 1900(L'Exposition de Paris 1900)")
-        XCTAssertEqual(result?.visitors, 48130300)
+        XCTAssertEqual(result?.mainTitle, "파리 만국박람회 1900\n(L'Exposition de Paris 1900)")
+        XCTAssertEqual(result?.decimalVisitor, "48,130,300")
+    }
+    
+    func test_Entry의디코딩이정상적으로이루어지는지() {
+        let result = JSONDecoder.decode([Entry].self, from: "items")
+        
+        XCTAssertEqual(result?[0].name , "직지심체요절")
+        XCTAssertNotEqual(result?[0].name, "불경")
     }
 }
