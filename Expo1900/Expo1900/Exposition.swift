30a56f7a35a5f044511e599ac24b2e9bf1bfb152
yjjem <marksim0606@gmail.com>
2022-10-20 12:23:35 +0900

    feat: 포스터와 각종 설명 표시 기능 추가

--- Expo1900/Expo1900/Exposition.swift
+++ Expo1900/Expo1900/Exposition.swift
@@ -4,6 +4,7 @@
 //
 //  Created by Jeremy, 준호 on 2022/10/18.
 //
+import Foundation
 
 struct Exposition: Decodable {
     let title: String
@@ -11,4 +12,19 @@ struct Exposition: Decodable {
     let location: String
     let duration: String
     let description: String
+    
+    var visitorsDescription: String {
+        let numberFormatter: NumberFormatter = NumberFormatter()
+        numberFormatter.numberStyle = .decimal
+        let formatted: String = numberFormatter.string(from: NSNumber(value: visitors)) ?? ""
+        return "방문객 : " + formatted + " 명"
+    }
+    
+    var locationDescription: String {
+        return "개최지 : " + self.location
+    }
+    
+    var durationDescription: String {
+        return "개최 기간 : " + self.duration
+    }
 }
--- Expo1900/Expo1900/ViewController.swift
+++ Expo1900/Expo1900/ViewController.swift
@@ -6,13 +6,42 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+final class ViewController: UIViewController {
+    
+    @IBOutlet private weak var titleLabel: UILabel!
+    @IBOutlet private weak var posterImage: UIImageView!
+    @IBOutlet private weak var visitorsLabel: UILabel!
+    @IBOutlet private weak var locationLabel: UILabel!
+    @IBOutlet private weak var durationLabel: UILabel!
+    @IBOutlet private weak var descriptionTextView: UITextView!
+    
+    private var exposition: Exposition?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        configureExpositionDataFromJSON(name: "exposition_universelle_1900")
+        configureExpositionContents()
     }
-
-
+    
+    private func configureExpositionDataFromJSON(name: String) {
+        let jsonDecoder: JSONDecoder = JSONDecoder()
+        guard let dataAsset: NSDataAsset = NSDataAsset(name: name) else { return }
+        
+        do {
+            self.exposition = try jsonDecoder.decode(Exposition.self, from: dataAsset.data)
+        } catch {
+            print(error)
+        }
+    }
+    
+    private func configureExpositionContents() {
+        titleLabel.text = exposition?.title
+        posterImage.image = UIImage(named: "poster")
+        visitorsLabel.text = exposition?.visitorsDescription
+        locationLabel.text = exposition?.locationDescription
+        durationLabel.text = exposition?.durationDescription
+        descriptionTextView.text = exposition?.description
+    }
+    
 }
 
