d0497a8f60b336f4343b974efc272ef46dbf269c
lnhk98 <hozzang666@gmai.com>
2022-10-21 00:21:50 +0900

    feat: ExhibitViewController와 테이블 뷰 생성

---
+++ Expo1900/Expo1900/Controller/ExhibitViewController.swift
@@ -0,0 +1,14 @@
+//  ExhibitViewController.swift
+//  Expo1900
+//  Created by inho, LJ  on 2022/10/21.
+
+import UIKit
+
+class ExhibitViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+}
