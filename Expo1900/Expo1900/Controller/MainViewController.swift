1dec9cd7d23c0194ef6e105ea0ae7df21d2fca6f
Jae Hyung Kim <smfc.mene@gmail.com>
2022-10-21 13:52:38 +0800

    refactor: String extension MainViewController로 이동하고 fileprivate 설정

--- Expo1900/Expo1900/Controller/MainViewController.swift
+++ Expo1900/Expo1900/Controller/MainViewController.swift
@@ -71,3 +71,29 @@ final class MainViewController: UIViewController {
     }
 }
 
+fileprivate extension String {
+    var expoVisitorInformation: String {
+        return ExpoConstant.visitor + ExpoConstant.colon + self + ExpoConstant.people
+    }
+    
+    var expoLocationInformation: String {
+        return ExpoConstant.location + ExpoConstant.colon + self
+    }
+    
+    var expoDurationInformation: String {
+        return ExpoConstant.duration + ExpoConstant.colon + self
+    }
+    
+    func createAttributed(target: String) -> NSAttributedString {
+        let fontSize = ExpoConstant.mediumFont
+        let attributed: NSMutableAttributedString = NSMutableAttributedString(string: self)
+        let targetRange = (self as NSString).range(of: target)
+        attributed.addAttribute(.font, value: fontSize, range: targetRange)
+        
+        return attributed
+    }
+    
+    func applyLineBreak() -> String {
+        return self.split(separator: ExpoConstant.leftBracket).joined(separator: "\n(")
+    }
+}
