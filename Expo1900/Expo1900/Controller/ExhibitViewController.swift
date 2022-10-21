5b1c1e2cc0690b5e9b9f911c7b8987ec69c9cff4
lnhk98 <hozzang666@gmai.com>
2022-10-21 13:37:07 +0900

    feat: ExhibitViewController에 UITableViewDataSource 채택 및 셀 구성

--- Expo1900/Expo1900/Controller/ExhibitViewController.swift
+++ Expo1900/Expo1900/Controller/ExhibitViewController.swift
@@ -5,10 +5,36 @@
 import UIKit
 
 class ExhibitViewController: UIViewController {
+    @IBOutlet weak var exhibitTableView: UITableView!
+    let cellIdentifier: String = "exhibitCell"
+    var exhibits: [Exhibit] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        exhibitTableView.dataSource = self
+    }
+}
+
+extension ExhibitViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return exhibits.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell: ExhibitTableViewCell = exhibitTableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as? ExhibitTableViewCell else {
+            let cell = UITableViewCell()
+            cell.textLabel?.text = "잘못된 셀"
+            
+            return cell
+        }
         
+        let exhibit: Exhibit = exhibits[indexPath.row]
+        
+        cell.exhibitNameLabel.text = exhibit.name
+        cell.exhibitShortDescriptionLabel.text = exhibit.shortDescription
+        cell.exhibitImageView.image = exhibit.image
+        
+        return cell
     }
 }
--- Expo1900/Expo1900/Model/Exhibit.swift
+++ Expo1900/Expo1900/Model/Exhibit.swift
@@ -2,11 +2,16 @@
 //  Expo1900
 //  Created by inho, LJ  on 2022/10/18.
 
+import UIKit
+
 struct Exhibit: Decodable {
     let name: String
     let imageName: String
     let shortDescription: String
     let description: String
+    var image: UIImage? {
+        return UIImage(named: imageName)
+    }
     
     enum CodingKeys: String, CodingKey {
         case name
