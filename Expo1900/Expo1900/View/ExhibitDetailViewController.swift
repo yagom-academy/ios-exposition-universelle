de0e11cf98373cce489c336f1fe92144f526b7cd
lj-7-77 <swtw72@gmail.com>
2022-10-21 15:30:27 +0900

    feat: ExhibitDetailViewController 생성 및 UI요소 배치

---
+++ Expo1900/Expo1900/View/ExhibitDetailViewController.swift
@@ -0,0 +1,12 @@
+//  ExhibitDetailViewController.swift
+//  Expo1900
+//  Created by inho, LJ  on 2022/10/21.
+
+import UIKit
+
+class ExhibitDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
