589584cf1ee064e67c5310a925399cf45d12654d
jpush <lj845854@gmail.com>
2022-10-21 13:45:49 +0900

    "feat: json 파싱한 데이터로 뷰 업데이트"

--- Expo1900/Expo1900/Model/Exposition.swift
+++ Expo1900/Expo1900/Model/Exposition.swift
@@ -3,6 +3,7 @@
 //  Exposition
 //
 //  Copyright (c) 2022 제이푸시, Minii All rights reserved.
+import Foundation
 
 struct Exposition: Decodable {
     let title: String
@@ -10,4 +11,23 @@ struct Exposition: Decodable {
     let location: String
     let duration: String
     let description: String
+    
+    var titleData: String {
+        return title.replacingOccurrences(of: "(", with: "\n(")
+    }
+    
+    var visitorsData: String {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .decimal
+        let formattedNumber = formatter.string(for: visitors) ?? "0"
+        return "방문객 : \(formattedNumber) 명"
+    }
+    
+    var locationData: String {
+        return "개최지 : \(location)"
+    }
+    
+    var durationData: String {
+        return "개최기간 : \(duration)"
+    }
 }
