dbe325bceb5476bbc4686e439846dfc964f59cb4
Ayaan <onestar7984@gmail.com>
2022-10-21 10:54:41 +0900

    feat: ExpositionItemDetailVC 내부 assignContentValue 메서드 구현

--- Expo1900/Expo1900/ExpositionItemDetailViewController.swift
+++ Expo1900/Expo1900/ExpositionItemDetailViewController.swift
@@ -11,9 +11,20 @@ class ExpositionItemDetailViewController: UIViewController {
     @IBOutlet private weak var itemImageView: UIImageView!
     @IBOutlet private weak var itemDescriptionTextView: UITextView!
     
-    var item: ExpositionUniverselleItem? 
+    var item: ExpositionUniverselleItem?
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        assignContentValue()
+    }
+    
+    private func assignContentValue() {
+        guard let item = item else {
+            return
+        }
+        
+        itemImageView.image = UIImage(named: item.imageName)
+        itemDescriptionTextView.text = item.description
+        itemDescriptionTextView.isScrollEnabled = false
     }
 }
