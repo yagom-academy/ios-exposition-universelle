755c2d6572665d0ad17d82f357a0b33d555018b9
jpush <lj845854@gmail.com>
2022-10-21 14:17:58 +0900

    refactor: 기능별 메서드 분리

--- Expo1900/Expo1900/ViewController/ViewController.swift
+++ Expo1900/Expo1900/ViewController/ViewController.swift
@@ -11,7 +11,14 @@ class ViewController: UIViewController {
     let scrollView: UIScrollView = {
         let scrollView = UIScrollView()
         scrollView.showsHorizontalScrollIndicator = false
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
         return scrollView
+    }()
+    
+    let contentView: UIView = {
+        let contentView = UIView()
+        contentView.translatesAutoresizingMaskIntoConstraints = false
+        return contentView
     }()
     
     let titleLabel: UILabel = {
@@ -51,6 +58,14 @@ class ViewController: UIViewController {
         return label
     }()
     
+    lazy var stackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [leftFlagImageView, entryButton, rightFlagImageView])
+        stackView.axis = .horizontal
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.alignment = .center
+        return stackView
+    }()
+    
     let leftFlagImageView: UIImageView = {
         let imageView = UIImageView(image: UIImage(named: "flag"))
         imageView.contentMode = .scaleAspectFit
@@ -66,7 +81,7 @@ class ViewController: UIViewController {
         imageView.heightAnchor.constraint(equalToConstant: 50).isActive = true
         return imageView
     }()
-
+    
     
     let entryButton: UIButton = {
         let button = UIButton()
@@ -84,34 +99,45 @@ class ViewController: UIViewController {
         setViewData()
     }
     
-    private func setLayout() {
-        let stackView = UIStackView(arrangedSubviews: [leftFlagImageView, entryButton, rightFlagImageView])
-        let safeArea = view.safeAreaLayoutGuide
+    private func decode() -> Exposition? {
+        let jsonDecoder = JSONDecoder()
+        let json = NSDataAsset(name: "exposition_universelle_1900")
+        
+        guard let result = try? jsonDecoder.decode(Exposition.self, from: json?.data ?? Data()) else {
+            return nil
+        }
+        
+        return result
+    }
+}
 
-        stackView.alignment = .center
-        
-        scrollView.translatesAutoresizingMaskIntoConstraints = false
-        
-        let contentView = UIView()
-        contentView.translatesAutoresizingMaskIntoConstraints = false
-        
+//MARK: - layout 설정
+extension ViewController {
+    private func setScrollView() {
+        let safeArea = view.safeAreaLayoutGuide
         view.addSubview(scrollView)
-        scrollView.addSubview(contentView)
         
         NSLayoutConstraint.activate([
             scrollView.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor),
             scrollView.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor),
             scrollView.topAnchor.constraint(equalTo: safeArea.topAnchor),
             scrollView.bottomAnchor.constraint(equalTo: safeArea.bottomAnchor),
-            
+        ])
+    }
+    
+    private func setContentView() {
+        scrollView.addSubview(contentView)
+        
+        NSLayoutConstraint.activate([
             contentView.topAnchor.constraint(equalTo: scrollView.topAnchor),
             contentView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor),
             contentView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor),
             contentView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor),
             contentView.widthAnchor.constraint(equalTo: scrollView.widthAnchor)
-            
         ])
-        
+    }
+    
+    private func setContentsChildView() {
         let layoutList: [UIView] = [
             titleLabel,
             posterImageView,
@@ -126,53 +152,85 @@ class ViewController: UIViewController {
             element.translatesAutoresizingMaskIntoConstraints = false
             contentView.addSubview(element)
         }
-        
+    }
+    
+    private func setTitleLabelLayout() {
         NSLayoutConstraint.activate([
             titleLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 16),
             titleLabel.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
-            
+        ])
+    }
+    
+    private func setPosterImageViewLayout() {
+        NSLayoutConstraint.activate([
             posterImageView.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 8),
             posterImageView.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
-            
+        ])
+    }
+    
+    private func setVisitorsLabelLayout() {
+        NSLayoutConstraint.activate([
             visitorsLabel.topAnchor.constraint(equalTo: posterImageView.bottomAnchor, constant: 8),
             visitorsLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 16),
             visitorsLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -16),
-            
+        ])
+    }
+    
+    private func setLocationLabelLayout() {
+        NSLayoutConstraint.activate([
             locationLabel.topAnchor.constraint(equalTo: visitorsLabel.bottomAnchor, constant: 8),
             locationLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 16),
             locationLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -16),
-            
+        ])
+    }
+    
+    private func setDurationLabelLayout() {
+        NSLayoutConstraint.activate([
             durationLabel.topAnchor.constraint(equalTo: locationLabel.bottomAnchor, constant: 8),
             durationLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 16),
             durationLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -16),
-            
+        ])
+    }
+    
+    private func setDescriptionLabelLayout() {
+        NSLayoutConstraint.activate([
             descriptionLabel.topAnchor.constraint(equalTo: durationLabel.bottomAnchor, constant: 8),
             descriptionLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 16),
             descriptionLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -16),
             descriptionLabel.bottomAnchor.constraint(equalTo: stackView.topAnchor),
-            
+        ])
+    }
+    
+    private func setStackViewLayout() {
+        NSLayoutConstraint.activate([
             stackView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 16),
             stackView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -16),
             stackView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -16)
         ])
     }
     
+    private func setLayout() {
+        setScrollView()
+        setContentView()
+        setContentsChildView()
+        
+        setTitleLabelLayout()
+        setPosterImageViewLayout()
+        setVisitorsLabelLayout()
+        setLocationLabelLayout()
+        setDurationLabelLayout()
+        setDescriptionLabelLayout()
+        setStackViewLayout()
+    }
+}
+
+//MARK: - Business Logic
+extension ViewController {
     private func setViewData() {
         titleLabel.text = exposition?.titleData
         visitorsLabel.text = exposition?.visitorsData
         locationLabel.text = exposition?.locationData
         durationLabel.text = exposition?.durationData
         descriptionLabel.text = exposition?.description
-    }
-    
-    private func decode() -> Exposition? {
-        let jsonDecoder = JSONDecoder()
-        let json = NSDataAsset(name: "exposition_universelle_1900")
-        
-        guard let result = try? jsonDecoder.decode(Exposition.self, from: json?.data ?? Data()) else {
-            return nil
-        }
-        
-        return result
     }
 }
