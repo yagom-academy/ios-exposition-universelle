4f62931b8812a91eda244788c094c62329a75126
sunnyCookie <107384230+sunny-maeng@users.noreply.github.com>
2022-10-25 12:29:59 +0900

    design: 방향전환시 첫 번째 뷰는 세로모드를 유지하고, 나머지 뷰들은 모든방향을 지원하도록 디자인

---
+++ Expo1900/Expo1900/Controller/ExpoNavigationController.swift
@@ -0,0 +1,12 @@
+//
+//  ExpoNavigationController.swift
+//  Created by sunnyCookie, Wonbi
+//
+ 
+import UIKit
+
+class ExpoNavigationController: UINavigationController {
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        return (topViewController as? ExpoMainViewController) != nil ? .portrait : .all
+    }
+}
