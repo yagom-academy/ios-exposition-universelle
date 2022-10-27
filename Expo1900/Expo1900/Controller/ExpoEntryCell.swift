cc778a9aa9fef4d0aef96567fec5c15431d5fa09
yjjem <marksim0606@gmail.com>
2022-10-27 11:33:25 +0900

    refactor: cell contentView의subview를 코드로 구성하도록 수정

--- Expo1900/Expo1900/Controller/ExpoEntryCell.swift
+++ Expo1900/Expo1900/Controller/ExpoEntryCell.swift
@@ -9,32 +9,53 @@ import UIKit
 
 class ExpoEntryCell: UITableViewCell {
     
-    @IBOutlet weak var cellImage: UIImageView!
-    @IBOutlet weak var stackView: UIStackView!
-    @IBOutlet weak var cellText: UILabel!
-    @IBOutlet weak var cellDetailText: UILabel!
+    private var entryImageView: UIImageView!
+    private var stackView: UIStackView!
+    private var entryNameLabel: UILabel!
+    private var entryShortDescriptionLabel: UILabel!
     
     override func awakeFromNib() {
         super.awakeFromNib()
         
-        cellImage.translatesAutoresizingMaskIntoConstraints = false
+        entryImageView = UIImageView()
+        entryImageView.translatesAutoresizingMaskIntoConstraints = false
+        self.contentView.addSubview(entryImageView)
+        
+        entryNameLabel = UILabel()
+        entryNameLabel.translatesAutoresizingMaskIntoConstraints = false
+        entryNameLabel.font = UIFont.preferredFont(forTextStyle: .title1)
+        entryNameLabel.adjustsFontForContentSizeCategory = true
+        entryNameLabel.numberOfLines = 0
+        
+        entryShortDescriptionLabel = UILabel()
+        entryShortDescriptionLabel.translatesAutoresizingMaskIntoConstraints = false
+        entryShortDescriptionLabel.font = UIFont.preferredFont(forTextStyle: .callout)
+        entryShortDescriptionLabel.adjustsFontForContentSizeCategory = true
+        entryShortDescriptionLabel.numberOfLines = 0
+        
+        stackView = UIStackView(arrangedSubviews: [entryNameLabel, entryShortDescriptionLabel])
         stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.alignment = .fill
+        stackView.distribution = .fill
+        stackView.contentMode = .scaleAspectFit
+        stackView.axis = .vertical
+        self.contentView.addSubview(stackView)
         
         NSLayoutConstraint.activate([
-            cellImage.widthAnchor.constraint(equalTo: cellImage.heightAnchor),
-            cellImage.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
-            cellImage.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 15.0),
-            cellImage.widthAnchor.constraint(equalTo: contentView.widthAnchor, multiplier: 0.2),
+            entryImageView.widthAnchor.constraint(equalTo: entryImageView.heightAnchor),
+            entryImageView.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+            entryImageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 15.0),
+            entryImageView.widthAnchor.constraint(equalTo: contentView.widthAnchor, multiplier: 0.2),
             stackView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
             contentView.bottomAnchor.constraint(equalTo: stackView.bottomAnchor, constant: 10),
-            stackView.leadingAnchor.constraint(equalTo: cellImage.trailingAnchor, constant: 8.0),
+            stackView.leadingAnchor.constraint(equalTo: entryImageView.trailingAnchor, constant: 8.0),
             stackView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 10)
         ])
     }
     
     func configureContentsView(image: UIImage?, name: String, shortDescription: String) {
-        self.cellImage?.image = image
-        self.cellText?.text = name
-        self.cellDetailText?.text = shortDescription
+        self.entryImageView.image = image
+        self.entryNameLabel.text = name
+        self.entryShortDescriptionLabel.text = shortDescription
     }
 }
