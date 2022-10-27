ecf7e52f70bf5feaa88be5a72c9c8c9909c4e0fe
junho <junhoL1046@gmail.com>
2022-10-27 10:49:07 +0900

    refactor: 두 번째 화면 cell의 제약 조건을 코드로 처리하도록 수정

--- Expo1900/Expo1900/Controller/ExpoEntryCell.swift
+++ Expo1900/Expo1900/Controller/ExpoEntryCell.swift
@@ -10,8 +10,27 @@ import UIKit
 class ExpoEntryCell: UITableViewCell {
     
     @IBOutlet weak var cellImage: UIImageView!
+    @IBOutlet weak var stackView: UIStackView!
     @IBOutlet weak var cellText: UILabel!
     @IBOutlet weak var cellDetailText: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        cellImage.translatesAutoresizingMaskIntoConstraints = false
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            cellImage.widthAnchor.constraint(equalTo: cellImage.heightAnchor),
+            cellImage.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+            cellImage.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 15.0),
+            cellImage.widthAnchor.constraint(equalTo: contentView.widthAnchor, multiplier: 0.2),
+            stackView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            contentView.bottomAnchor.constraint(equalTo: stackView.bottomAnchor, constant: 10),
+            stackView.leadingAnchor.constraint(equalTo: cellImage.trailingAnchor, constant: 8.0),
+            stackView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 10)
+        ])
+    }
     
     func configureContentsView(image: UIImage?, name: String, shortDescription: String) {
         self.cellImage?.image = image
