a35a48fc737014f3872af72f14442e573f79e9bb
Rhode-park <jinah.park.16@gmail.com>
2023-02-20 18:02:56 +0900

    feat: ExhibitItem 구조체 구현, viewDidLoad 내의 Decoding 구현

--- Expo1900/Expo1900/ViewController.swift
+++ Expo1900/Expo1900/ViewController.swift
@@ -1,18 +1,25 @@
 //
 //  Expo1900 - ViewController.swift
-//  Created by yagom. 
+//  Created by Christy, Rhode. 
 //  Copyright © yagom academy. All rights reserved.
 // 
 
 import UIKit
 
 class ViewController: UIViewController {
-
+    var items: [ExhibitItem] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        guard let assetItem = NSDataAsset.init(name: "items") else {
+            return
+        }
+        let decoder = JSONDecoder()
+        
+        do {
+            self.items = try decoder.decode([ExhibitItem].self, from: assetItem.data)
+        } catch {
+            print(error.localizedDescription)
+        }
     }
-
-
 }
-
