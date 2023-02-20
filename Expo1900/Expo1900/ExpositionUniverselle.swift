23b4abdf455d676ac07628bb3da9d777b6812c29
Hanseul Lee <61936306+christy-hs-lee@users.noreply.github.com>
2023-02-20 20:41:23 +0900

    feat: ExpositionUniverselle 구현

---
+++ Expo1900/Expo1900/ExpositionUniverselle.swift
@@ -0,0 +1,16 @@
+//
+//  ExpositionUniverselle.swift
+//  Expo1900
+//
+//  Created by Christy, Rhode on 2023/02/20.
+//
+
+import Foundation
+
+struct ExpositionUniverselle: Decodable {
+    var title: String
+    var visitors: Int
+    var location: String
+    var duration: String
+    var description: String
+}
--- Expo1900/Expo1900/ViewController.swift
+++ Expo1900/Expo1900/ViewController.swift
@@ -7,19 +7,8 @@
 import UIKit
 
 class ViewController: UIViewController {
-    var items: [ExhibitItem] = []
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        guard let assetItem = NSDataAsset.init(name: "items") else {
-            return
-        }
-        let decoder = JSONDecoder()
-        
-        do {
-            self.items = try decoder.decode([ExhibitItem].self, from: assetItem.data)
-        } catch {
-            print(error.localizedDescription)
-        }
     }
 }
