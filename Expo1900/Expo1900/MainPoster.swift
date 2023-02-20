a4827bfa63089f6a3f7d607cad587c183c641b7b
ROWAN <kimkj93@naver.com>
2023-02-20 14:53:01 +0900

    feat: MainPoster 타입 정의

---
+++ Expo1900/Expo1900/MainPoster.swift
@@ -0,0 +1,14 @@
+//
+//  MainBrochure.swift
+//  Expo1900
+//
+//  Created by 리지, Rowan on 2023/02/20.
+//
+
+struct MainPoster {
+    let title: String
+    let visitors: Int
+    let location: String
+    let duration: String
+    let description: String
+}
