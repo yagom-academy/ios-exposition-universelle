685c63d0c98a3cb137766693284ffcb64ccc84a9
kaki <ycg02109@naver.com>
2023-02-20 15:28:51 +0900

    refactor: 구조체 이름 변경

--- Expo1900/Expo1900/Models/ExpositionUniverselle.swift
+++ Expo1900/Expo1900/Models/ExpositionUniverselle.swift
@@ -1,11 +1,11 @@
 //
-//  Exposition.swift
+//  ExpositionUniverselle.swift
 //  Expo1900
 //
 //  Created by kaki, brody on 2023/02/20.
 //
 
-struct Exposition: Decodable {
+struct ExpositionUniverselle: Decodable {
     let title: String
     let visitors: Int
     let location: String
--- Expo1900/Expo1900/Models/ExpositionUniverselleItem.swift
+++ Expo1900/Expo1900/Models/ExpositionUniverselleItem.swift
@@ -1,11 +1,11 @@
 //
-//  Items.swift
+//  ExpositionUniverselleItem.swift
 //  Expo1900
 //
 //  Created by kaki, brody on 2023/02/20.
 //
 
-struct Item: Decodable {
+struct ExpositionUniverselleItem: Decodable {
     let name: String
     let imageName: String
     let shortDesc: String
