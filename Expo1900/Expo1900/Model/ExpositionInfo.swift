ec2a563631b42cf6e67c7314af7f25396f2844cc
Andrew-0411 <whk1140@naver.com>
2023-02-20 15:27:18 +0900

    feat: ExpositionInfo 타입 추가

---
+++ Expo1900/Expo1900/Model/ExpositionInfo.swift
@@ -0,0 +1,16 @@
+//
+//  ExpositionInfo.swift
+//  Expo1900
+//
+//  Created by 혜모리, Andrew on 2023/02/20.
+//
+
+import Foundation
+
+struct ExpositionInfo {
+    let title: String
+    let visitors: Int
+    let location: String
+    let duration: String
+    let description: String
+}
