558e12c3cab7cfa989998c117fa9bb663ef2f8e1
sehong <tjsgh0801@naver.com>
2023-02-20 15:06:29 +0900

    feat: MainView model 타입 구현

---
+++ Expo1900/Expo1900/Models/MainView.swift
@@ -0,0 +1,16 @@
+//
+//  MainView.swift
+//  Expo1900
+//
+//  Created by Seoyeon Hong on 2023/02/20.
+//
+
+import Foundation
+
+struct MainView: Codable {
+    var title: String
+    var visitors: Int
+    var location: String
+    var duration: String
+    var description: String
+}
