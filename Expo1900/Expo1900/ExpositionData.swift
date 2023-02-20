3b5dfbb210ee090246678ee9c3588c15dccbf31b
whdghks8654 <62684851+whdghks8654@users.noreply.github.com>
2023-02-20 16:04:58 +0900

    feat: ExpositionData 모델생성

---
+++ Expo1900/Expo1900/ExpositionData.swift
@@ -0,0 +1,24 @@
+//
+//  ExpositionData.swift
+//  Expo1900
+//
+//  Created by Goat,vetto on 2023/02/20.
+//
+
+import Foundation
+
+struct mainExpo: Decodable {
+    let title: String
+    let visitors: Int
+    let location: String
+    let duration: String
+    let description: String
+    
+    private enum CodingKeys: String, CodingKey {
+        case title
+        case visitors
+        case location
+        case duration
+        case description
+    }
+}
