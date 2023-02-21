0033ff900105f7ec137de7ec19345e428c129f4c
kaki <ycg02109@naver.com>
2023-02-21 10:12:10 +0900

    feat: ParisExpositionUniverselleViewController 화면 구성

---
+++ Expo1900/Expo1900/Controllers/ParisExpositionUniverselleViewController.swift
@@ -0,0 +1,27 @@
+//
+//  Expo1900 - ParisExpositionUniverselleViewController.swift
+//  Created by kaki, brody.
+//  Copyright © yagom academy. All rights reserved.
+// 
+
+import UIKit
+
+final class ParisExpositionUniverselleViewController: UIViewController {
+    
+    @IBOutlet weak var expositionTitle: UILabel!
+    @IBOutlet weak var expositionPoster: UIImageView!
+    @IBOutlet weak var expositionVisitors: UILabel!
+    @IBOutlet weak var expositionLocation: UILabel!
+    @IBOutlet weak var expositionDuration: UILabel!
+    @IBOutlet weak var expositionDescription: UILabel!
+    @IBOutlet weak var flagImage1: UIImageView!
+    @IBOutlet weak var flagImage2: UIImageView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
