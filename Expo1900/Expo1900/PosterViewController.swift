15be157e567b460b461c9bc27730d470560cb9ed
kkuldak1@naver.com <kkuldak1@naver.com>
2023-02-21 14:14:15 +0900

    feat: setTitleLabel() 구현 및 Canvas 활용을 위한 코드 구현

--- Expo1900/Expo1900/PosterViewController.swift
+++ Expo1900/Expo1900/PosterViewController.swift
@@ -13,10 +13,52 @@ final class PosterViewController: UIViewController {
         return decoder.decodeExposition()
     }()
     
+    private var stackView = UIStackView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        view.addSubview(stackView)
+        setTitleLabel()
     }
-
+    
+    func setTitleLabel() {
+        guard var title = exposition?.title else { return }
+        
+        if let index = title.range(of: "(")?.lowerBound {
+            title.insert(contentsOf: "\n", at: index)
+        }
+        
+        let titleLabel = UILabel()
+        titleLabel.text = title
+        titleLabel.numberOfLines = 0
+                
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        stackView.addSubview(titleLabel)
+        
+        titleLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 20).isActive = true
+        titleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        titleLabel.textAlignment = .center
+        titleLabel.font = .systemFont(ofSize: 26)
+    }
+    
 }
 
+// MARK: Canvas
+import SwiftUI
+struct ViewControllerRepresentable: UIViewControllerRepresentable {
+    typealias UIViewControllerType = PosterViewController
+    
+    func makeUIViewController(context: Context) -> PosterViewController {
+            return PosterViewController()
+        }
+
+    func updateUIViewController(_ uiViewController: PosterViewController, context: Context) {
+    }
+}
+@available(iOS 13.0.0, *)
+struct ViewPreview: PreviewProvider {
+    static var previews: some View {
+        ViewControllerRepresentable()
+    }
+}
