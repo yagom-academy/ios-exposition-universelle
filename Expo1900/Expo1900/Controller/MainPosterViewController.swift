188b4c5bbdc3b236dc49dff739987fd97d63e210
HarryHyeon <hharry0728@gmail.com>
2023-02-21 11:46:44 +0900

    feat: 메인 화면의 스크롤뷰, 타이틀 레이블 정의

--- Expo1900/Expo1900/Controller/MainPosterViewController.swift
+++ Expo1900/Expo1900/Controller/MainPosterViewController.swift
@@ -8,11 +8,39 @@ import UIKit
 
 class MainPosterViewController: UIViewController {
 
+    private let stackView = UIStackView()
+    private let scrollView = UIScrollView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = .red
+        self.view.backgroundColor = .white
+        configureTitleLabel()
     }
-
+    
+    func configureScrollView() {
+        self.view.addSubview(scrollView)
+        self.scrollView.addSubview(stackView)
+        
+        NSLayoutConstraint.activate([
+            scrollView.topAnchor.constraint(equalTo: self.view.topAnchor),
+            scrollView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
+            scrollView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor),
+            scrollView.topAnchor.constraint(equalTo: self.view.bottomAnchor)
+        ])
+    }
+    
+    func configureTitleLabel() {
+        let label = UILabel()
+        label.text = """
+        파리 만국박람회 1900
+        (L'Exposition de Paris 1900)
+        """
+        label.font = .preferredFont(forTextStyle: .largeTitle)
+        label.textAlignment = .center
+        label.numberOfLines = 2
+        label.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(label)
+    }
 
 }
 
