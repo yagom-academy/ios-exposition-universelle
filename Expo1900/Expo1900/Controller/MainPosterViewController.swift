3a2d690962fa79f6b048a754d2192fab08103ece
HarryHyeon <hharry0728@gmail.com>
2023-02-21 15:34:52 +0900

    feat: configureTextView 메서드 정의

--- Expo1900/Expo1900/Controller/MainPosterViewController.swift
+++ Expo1900/Expo1900/Controller/MainPosterViewController.swift
@@ -1,6 +1,6 @@
 //
 //  Expo1900 - ViewController.swift
-//  Created by yagom. 
+//  Created by Harry, 레옹아범.
 //  Copyright © yagom academy. All rights reserved.
 // 
 
@@ -39,6 +39,7 @@ final class MainPosterViewController: UIViewController {
         configureTitleLabel()
         configureMainPosterImage()
         configureContentLabel()
+        configureTextView()
     }
     
     private func configureScrollView() {
@@ -54,21 +55,22 @@ final class MainPosterViewController: UIViewController {
     
     private func configureTitleLabel() {
         let label = UILabel()
+        
         label.text = """
         파리 만국박람회 1900
         (L'Exposition de Paris 1900)
         """
         label.font = .preferredFont(forTextStyle: .title1)
-        label.textColor = .black
         label.textAlignment = .center
         label.numberOfLines = 2
-        label.translatesAutoresizingMaskIntoConstraints = false
+
         self.stackView.addArrangedSubview(label)
     }
     
     private func configureMainPosterImage() {
         guard let image = UIImage(named: "poster") else { return }
         let imageView = UIImageView()
+        
         imageView.image = image
         
         self.stackView.addArrangedSubview(imageView)
@@ -77,9 +79,8 @@ final class MainPosterViewController: UIViewController {
     private func configureContentLabel() {
         let label = UILabel()
         let text = "방문객 : 48,130,300 명"
-        label.textColor = .black
+        
         label.numberOfLines = 1
-        label.translatesAutoresizingMaskIntoConstraints = false
         
         let fontSize = UIFont.preferredFont(forTextStyle: .title3)
         let attributedText = NSMutableAttributedString(string: text)
@@ -92,6 +93,18 @@ final class MainPosterViewController: UIViewController {
         label.attributedText = attributedText
         
         self.stackView.addArrangedSubview(label)
+    }
+    
+    private func configureTextView() {
+        let textView = UITextView()
+        
+        textView.font = .preferredFont(forTextStyle: .body)
+        textView.text = "dsfadsf"
+        textView.isScrollEnabled = false
+        textView.isEditable = false
+        textView.isSelectable = false
+        
+        self.stackView.addArrangedSubview(textView)
     }
     
     private func configureStackView() {
