de32545fdf26d0020bbdd0ece84aa36d1d7fcbc7
ROWAN <kimkj93@naver.com>
2023-02-21 17:54:35 +0900

    feat: ExpoViewController 메서드 기능 분리
    1. 메인 화면 구성을 위한 메서드 정의 및 기능 분리
    2. 접근제어자 및 final 키워드 사용
    3. Storyboard scroll 작동 확인을 위한 auto layout 적용

--- Expo1900/Expo1900/ExpoViewController.swift
+++ Expo1900/Expo1900/ExpoViewController.swift
@@ -6,40 +6,81 @@
 
 import UIKit
 
-class ExpoViewController: UIViewController {
-    
+final class ExpoViewController: UIViewController {
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var visitorLabel: UILabel!
     @IBOutlet weak var locationLabel: UILabel!
     @IBOutlet weak var durationLabel: UILabel!
     @IBOutlet weak var descriptionLabel: UILabel!
     @IBOutlet weak var posterImage: UIImageView!
-    var expoUniverselle: ExpoUniverselle?
+    @IBOutlet var flags: [UIImageView]!
+    @IBOutlet weak var goButton: UIButton!
+    private var expoUniverselle: ExpoUniverselle?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-       
-        let jsonDecoder: JSONDecoder = JSONDecoder()
-        guard let dataAsset: NSDataAsset = NSDataAsset(name: "exposition_universelle_1900") else {
-            return
-        }
+        decodeExpoData()
+        setMainScene()
+    }
+    
+    private func decodeExpoData() {
+        let jsonDecoder = JSONDecoder()
+        guard let dataAsset = NSDataAsset(name: "exposition_universelle_1900") else { return }
         
         do {
             self.expoUniverselle = try jsonDecoder.decode(ExpoUniverselle.self, from: dataAsset.data)
         } catch {
             print(error.localizedDescription)
         }
-        
-        if let expoData = expoUniverselle {
-            self.titleLabel.text = expoData.title
-            self.visitorLabel.text = "방문객 : \(expoData.visitors) 명"
-            self.locationLabel.text = "개최지 : \(expoData.location)"
-            self.durationLabel.text = "개최 기간: \(expoData.duration)"
-            self.descriptionLabel.text = expoData.description
-        }
-        
     }
-
-
+    
+    private func setMainScene() {
+        guard let expoData = self.expoUniverselle else { return }
+        
+        setImages()
+        setLabels(from: expoData)
+    }
+    
+    private func setImages() {
+        guard let poster = UIImage(named: "poster"),
+              let flag = UIImage(named: "flag") else { return }
+        
+        self.posterImage.image = poster
+        self.flags.forEach { $0.image = flag }
+    }
+    
+    private func setLabels(from expoData: ExpoUniverselle) {
+        guard let decimalVisitors = convertToDecimal(from: expoData.visitors) else { return }
+        
+        visitorLabel.text = "방문객 : \(decimalVisitors) 명"
+        locationLabel.text = "개최지 : \(expoData.location)"
+        durationLabel.text = "개최 기간 : \(expoData.duration)"
+        
+        convertTextSize(of: visitorLabel, range: NSMakeRange(0, 3))
+        convertTextSize(of: locationLabel, range: NSMakeRange(0, 3))
+        convertTextSize(of: durationLabel, range: NSMakeRange(0, 5))
+        
+        self.titleLabel.text = expoData.title
+        self.descriptionLabel.text = expoData.description
+    }
+    
+    private func convertToDecimal(from visitors: Int) -> String? {
+        let numberformatter = NumberFormatter()
+        numberformatter.numberStyle = .decimal
+        
+        guard let decimalVisitors = numberformatter.string(for: visitors) else { return nil }
+        
+        return decimalVisitors
+    }
+    
+    private func convertTextSize(of label: UILabel, range: NSRange) {
+        guard let target = label.text else { return }
+        
+        let fontSize = UIFont.systemFont(ofSize: 20)
+        let attributedString = NSMutableAttributedString(string: target)
+        
+        attributedString.addAttribute(.font, value: fontSize, range: range)
+        label.attributedText = attributedString
+    }
 }
 
