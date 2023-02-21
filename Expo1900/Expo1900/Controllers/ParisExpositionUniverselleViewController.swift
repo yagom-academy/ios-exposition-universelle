ec738d4a6b23580a9039d3615832422f4cc86c5a
seunghyunCheon <csh7215@naver.com>
2023-02-21 10:36:02 +0900

    feat: loadExpositionData, setupUI 함수 구현

--- Expo1900/Expo1900/Controllers/ParisExpositionUniverselleViewController.swift
+++ Expo1900/Expo1900/Controllers/ParisExpositionUniverselleViewController.swift
@@ -8,20 +8,47 @@ import UIKit
 
 final class ParisExpositionUniverselleViewController: UIViewController {
     
-    @IBOutlet weak var expositionTitle: UILabel!
-    @IBOutlet weak var expositionPoster: UIImageView!
-    @IBOutlet weak var expositionVisitors: UILabel!
-    @IBOutlet weak var expositionLocation: UILabel!
-    @IBOutlet weak var expositionDuration: UILabel!
-    @IBOutlet weak var expositionDescription: UILabel!
-    @IBOutlet weak var flagImage1: UIImageView!
-    @IBOutlet weak var flagImage2: UIImageView!
+    @IBOutlet private weak var expositionTitle: UILabel!
+    @IBOutlet private weak var expositionPoster: UIImageView!
+    @IBOutlet private weak var expositionVisitors: UILabel!
+    @IBOutlet private weak var expositionLocation: UILabel!
+    @IBOutlet private weak var expositionDuration: UILabel!
+    @IBOutlet private weak var expositionDescription: UILabel!
+    @IBOutlet private weak var flagImage1: UIImageView!
+    @IBOutlet private weak var flagImage2: UIImageView!
+    
+    private var expositionData: ExpositionUniverselle?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        loadExpositionData()
+        setupUI()
     }
-
-
+    
+    private func loadExpositionData() {
+        let jsonDecoder = JSONDecoder()
+        guard let dataAsset = NSDataAsset(name: "exposition_universelle_1900") else {
+            return
+        }
+        
+        do {
+            expositionData = try jsonDecoder.decode(ExpositionUniverselle.self, from: dataAsset.data)
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
+    
+    private func setupUI() {
+        guard let expositionData = expositionData else { return }
+        
+        expositionTitle.text = expositionData.title
+        expositionPoster.image = UIImage(named: "poster")
+        expositionVisitors.text = "\(expositionData.visitors)"
+        expositionLocation.text = expositionData.location
+        expositionDuration.text = expositionData.duration
+        expositionDescription.text = expositionData.description
+        flagImage1.image = UIImage(named: "flag")
+        flagImage2.image = UIImage(named: "flag")
+    }
 }
 
