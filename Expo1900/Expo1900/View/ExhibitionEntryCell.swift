90290e70f561f520411ba66b0b087e86f70d30ca
minsu3007 <minsu3007@gmail.com>
2023-02-22 14:52:01 +0900

    feat: ExhibitionEntryCell 스택뷰 생성 및 프로퍼티 생성

---
+++ Expo1900/Expo1900/View/ExhibitionEntryCell.swift
@@ -0,0 +1,62 @@
+//
+//  ExhibitionEntryCell.swift
+//  Expo1900
+//
+//  Created by 강민수 on 2023/02/22.
+//
+
+import UIKit
+
+class ExhibitionEntryCell: UITableViewCell {
+    
+    var exhibitionImage = {
+        let imageView = UIImageView()
+        
+        return imageView
+    }()
+    
+    var titleLabel = {
+        let label = UILabel()
+        label.font = .preferredFont(forTextStyle: .title2)
+        
+        return label
+    }()
+    
+    var contentLabel = {
+        let label = UILabel()
+        label.font = .preferredFont(forTextStyle: .title3)
+        
+        return label
+    }()
+    
+    private let stackView = {
+        let stack = UIStackView()
+        stack.translatesAutoresizingMaskIntoConstraints = false
+        stack.alignment = .fill
+        stack.distribution = .equalSpacing
+        stack.spacing = 5
+        
+        return stack
+    }()
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        configureStackView()
+    }
+    
+    func configureStackView() {
+        self.contentView.addSubview(stackView)
+        
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalTo: self.contentView.topAnchor),
+            stackView.bottomAnchor.constraint(equalTo: self.contentView.bottomAnchor),
+            stackView.leadingAnchor.constraint(equalTo: self.contentView.leadingAnchor),
+            stackView.trailingAnchor.constraint(equalTo: self.contentView.trailingAnchor),
+        ])
+    }
+}
