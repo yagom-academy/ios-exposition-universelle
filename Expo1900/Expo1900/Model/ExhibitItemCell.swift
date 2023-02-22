ac622780b492bb3f8e2c729fb787ade203669c8c
Jun <59365211+juun97@users.noreply.github.com>
2023-02-22 17:40:24 +0900

    refactor : Custom Cell 생성자 수정

--- Expo1900/Expo1900/Model/ExhibitItemCell.swift
+++ Expo1900/Expo1900/Model/ExhibitItemCell.swift
@@ -15,20 +15,32 @@ class ExhibitItemCell: UITableViewCell {
     private let titleLabel = UILabel()
     private let subTitleLabel = UILabel()
     
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        settingTest()
+    }
 
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
     
-    
-    init(coder aDecoder: NSCoder) {
-        super.init(coder: aDecoder)!
-   
+    func settingTest() {
         self.addSubview(stackView)
         
         stackView.translatesAutoresizingMaskIntoConstraints = false
-
+        subStackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        stackView.spacing = 10
+        
         NSLayoutConstraint.activate([
             stackView.leadingAnchor.constraint(equalTo: self.leadingAnchor),
             stackView.trailingAnchor.constraint(equalTo: self.trailingAnchor),
@@ -40,26 +52,23 @@ class ExhibitItemCell: UITableViewCell {
         
         itemImageView.center = self.center
         itemImageView.contentMode = .scaleAspectFit
-        itemImageView.widthAnchor.constraint(equalToConstant: 40).isActive = true
+        itemImageView.widthAnchor.constraint(equalToConstant: 85).isActive = true
+        itemImageView.heightAnchor.constraint(equalToConstant: 85).isActive = true
         
         subStackView.axis = .vertical
         subStackView.distribution = .fill
-        subStackView.alignment = .fill
+        subStackView.alignment = .leading
         
         titleLabel.font = .systemFont(ofSize: 18)
         subTitleLabel.font = .systemFont(ofSize: 13)
+        subTitleLabel.numberOfLines = 0
+        subTitleLabel.textAlignment = .left
         
         subStackView.addArrangedSubview(titleLabel)
         subStackView.addArrangedSubview(subTitleLabel)
-        
+
         stackView.addArrangedSubview(itemImageView)
         stackView.addArrangedSubview(subStackView)
-     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
     }
     
     func setProperty(imageName: String?, title: String?, subTitle: String?) {
