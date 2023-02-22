5ee86f33f1fddc8cb19e7566261f655968517a35
HarryHyeon <hharry0728@gmail.com>
2023-02-22 12:13:49 +0900

    feat: decodeJson() 메서드 정의

--- Expo1900/Expo1900/Controller/ExhibitionEntryTableViewController.swift
+++ Expo1900/Expo1900/Controller/ExhibitionEntryTableViewController.swift
@@ -9,19 +9,28 @@ import UIKit
 
 class ExhibitionEntryTableViewController: UITableViewController {
 
+    var exhibitionItem: [ExhibitionItem] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         configureNavigationBar()
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        decodeJson()
     }
     
     private func configureNavigationBar() {
         self.navigationController?.navigationBar.isHidden = false
         self.navigationItem.title = "한국의 출품작"
+    }
+    
+    private func decodeJson() {
+        let jsonDecoder = JSONDecoder()
+        guard let data = NSDataAsset(name: "items") else { return }
+        
+        do {
+            exhibitionItem = try jsonDecoder.decode([ExhibitionItem].self, from: data.data)
+        } catch {
+            print(error.localizedDescription)
+        }
     }
 
     // MARK: - Table view data source
@@ -36,59 +45,5 @@ class ExhibitionEntryTableViewController: UITableViewController {
         return 0
     }
 
-    /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
-        return cell
-    }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
