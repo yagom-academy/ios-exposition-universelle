3e2165eef2cefe5887097b6d7495edf849aa6a67
kkuldak1@naver.com <kkuldak1@naver.com>
2023-02-22 15:08:20 +0900

    feat: ExhibitItemViewController 내 디코딩된 JSON 프로퍼티 구현

--- Expo1900/Expo1900/ExhibitItemViewController.swift
+++ Expo1900/Expo1900/ExhibitItemViewController.swift
@@ -9,17 +9,35 @@ import UIKit
 
 final class ExhibitItemViewController: UIViewController {
     
+    private var exhibitItems: [ExhibitItem]? = {
+        let decoder = Decoder()
+        return decoder.decodeExhibitItem()
+    }()
+    
     override func viewDidLoad() {
+        super.viewDidLoad()
+        
         view.backgroundColor = .white
         self.title = "한국의 출품작"
-  
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
+        
         self.navigationController?.isNavigationBarHidden = false
     }
     
 }
 
+extension ExhibitItemViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+    
+}
 
--- Expo1900/Expo1900/Model/Decoder.swift
+++ Expo1900/Expo1900/Model/Decoder.swift
@@ -12,13 +12,25 @@ final class Decoder {
     
     private let decoder = JSONDecoder()
     private let expositionFileName = "exposition_universelle_1900"
-    
+    private let exhibitItemFileName = "items"
+
     func decodeExposition() -> Exposition? {
         guard let dataAsset: NSDataAsset = NSDataAsset(name: expositionFileName) else { return nil }
         
         do {
             let exposition: Exposition = try decoder.decode(Exposition.self, from: dataAsset.data)
             return exposition
+        } catch {
+            return nil
+        }
+    }
+    
+    func decodeExhibitItem() -> [ExhibitItem]? {
+        guard let dataAsset: NSDataAsset = NSDataAsset(name: exhibitItemFileName) else { return nil }
+        
+        do {
+            let exhibitItems: [ExhibitItem] = try decoder.decode([ExhibitItem].self, from: dataAsset.data)
+            return exhibitItems
         } catch {
             return nil
         }
