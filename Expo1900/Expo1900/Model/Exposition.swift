bb88f7423fce59bbada320d6a34169c1963f6a34
kkuldak1@naver.com <kkuldak1@naver.com>
2023-02-22 13:08:56 +0900

    refactor: 네이밍 수정

--- Expo1900/Expo1900/Model/Exposition.swift
+++ Expo1900/Expo1900/Model/Exposition.swift
@@ -7,7 +7,7 @@
 
 struct Exposition: Decodable {
     let title: String
-    let visitors: Int
+    let accumulatedVisitor: Int
     let location: String
     let duration: String
     let description: String
