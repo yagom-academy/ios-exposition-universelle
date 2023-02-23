74903056cd7f20d717189499421ab136c24b93e0
Hanseul Lee <61936306+christy-hs-lee@users.noreply.github.com>
2023-02-23 12:44:52 +0900

    feat: mainView 구현 및 json decoding

--- Expo1900/Expo1900/Sources/Controller/ViewController.swift
+++ Expo1900/Expo1900/Sources/Controller/ViewController.swift
@@ -8,7 +8,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var visitorNumberLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var durationLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        let decoder = JSONDecoder()
+        guard let expositionUniverselle = NSDataAsset(name: "exposition_universelle_1900") else { return }
+        
+        guard let decodedExpositionUniverselle = try? decoder.decode(ExpositionUniverselle.self, from: expositionUniverselle.data) else { return }
+        
+        titleLabel.text = decodedExpositionUniverselle.title
+        visitorNumberLabel.text = "방문객 : \(decodedExpositionUniverselle.visitorNumber) 명"
+        locationLabel.text = "개최지 : \(decodedExpositionUniverselle.location)"
+        durationLabel.text = "개최 기간 : \(decodedExpositionUniverselle.duration)"
+        descriptionLabel.text = decodedExpositionUniverselle.description
     }
 }
