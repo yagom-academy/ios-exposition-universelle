945391d28220bdec3443c62e7504baea97d0e59e
Hanseul Lee <61936306+christy-hs-lee@users.noreply.github.com>
2023-02-23 15:41:00 +0900

    feat: ItemViewController에서 tableView 구현

--- Expo1900/Expo1900/Sources/Controller/ItemViewController.swift
+++ Expo1900/Expo1900/Sources/Controller/ItemViewController.swift
@@ -2,14 +2,33 @@
 //  ItemViewController.swift
 //  Expo1900
 //
-//  Created by Jinah Park on 2023/02/23.
+//  Created by Rhode, Christy on 2023/02/23.
 //
 
 import UIKit
 
-class ItemViewController: UIViewController {
+class ItemViewController: UIViewController, UITableViewDataSource {
+    
+    @IBOutlet var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.dataSource = self
+    }
+    
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return 10
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        return cell
     }
 }
