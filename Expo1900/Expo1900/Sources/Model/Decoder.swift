e488ce87ed0c04ea28a7bf75318b710a79db1e22
Hanseul Lee <61936306+christy-hs-lee@users.noreply.github.com>
2023-03-03 00:59:51 +0900

    refactor: Decoder 구조체 수정

--- Expo1900/Expo1900/Sources/Model/Decoder.swift
+++ Expo1900/Expo1900/Sources/Model/Decoder.swift
@@ -8,25 +8,30 @@
 import UIKit
 
 struct Decoder {
+    let decoder: JSONDecoder = JSONDecoder()
+    
     func decodeExpositionJson() -> ExpositionUniverselle {
-        let decoder: JSONDecoder = JSONDecoder()
-        let emptyData: ExpositionUniverselle = .init(title: NameSpace.emptyString, visitorCount: Int.zero, location: NameSpace.emptyString, duration: NameSpace.emptyString, description: NameSpace.emptyString)
+        let emptyData: ExpositionUniverselle =
+        ExpositionUniverselle(title: NameSpace.emptyString,
+                            visitorCount: Int.zero,
+                            location: NameSpace.emptyString,
+                            duration: NameSpace.emptyString,
+                            description: NameSpace.emptyString)
         
-        guard let expositionUniverselle: NSDataAsset  = NSDataAsset(name: "exposition_universelle_1900") else { return emptyData }
-        
-        guard let decodedExpositionUniverselle: ExpositionUniverselle = try? decoder.decode(ExpositionUniverselle.self, from: expositionUniverselle.data) else { return emptyData }
-        
+        guard let expositionUniverselle: NSDataAsset  = NSDataAsset(name: "exposition_universelle_1900"),
+              let decodedExpositionUniverselle: ExpositionUniverselle = try? decoder.decode(ExpositionUniverselle.self, from: expositionUniverselle.data)
+        else { return emptyData }
+
         return decodedExpositionUniverselle
     }
     
     func decodeItemsJson() -> [ExhibitItem] {
         let emptyArray: [ExhibitItem] = []
-        let decoder: JSONDecoder = JSONDecoder()
         
-        guard let items: NSDataAsset = NSDataAsset(name: "items") else { return emptyArray }
+        guard let items: NSDataAsset = NSDataAsset(name: "items"),
+              let decodedItems: [ExhibitItem] = try? decoder.decode([ExhibitItem].self, from: items.data)
+        else { return emptyArray }
 
-        guard let decodedItems: [ExhibitItem] = try? decoder.decode([ExhibitItem].self, from: items.data) else { return emptyArray }
-        
         return decodedItems
     }
 }
