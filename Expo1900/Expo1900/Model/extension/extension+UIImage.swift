55f0efe92bf122bad0a121c7eb2c9dfe0b4d660e
kkuldak1@naver.com <kkuldak1@naver.com>
2023-03-05 23:22:29 +0900

    refactor: 사용하지 않는 코드 및 불필요한 주석 삭제 (UIImage, SceneDelegate, AppDelegate)

--- Expo1900/Expo1900/Model/extension/extension+UIImage.swift
+++ Expo1900/Expo1900/Model/extension/extension+UIImage.swift
@@ -9,36 +9,6 @@ import UIKit
 
 extension UIImage {
     
-    func squareImage(length: CGFloat = 65) -> UIImage? {
-        let originWidth: CGFloat = self.size.width
-        let originHeight: CGFloat = self.size.height
-        var resizedWidth: CGFloat = length
-        var resizedHeight: CGFloat = length
-        
-        UIGraphicsBeginImageContext(CGSize(width: length, height: length))
-        UIColor.clear.set()
-        UIRectFill(CGRect(x: 0.0, y: 0.0, width: length, height: length))
-        
-        let sizeRatio = length / max(originWidth, originHeight)
-        if originWidth > originHeight {
-            resizedWidth = length
-            resizedHeight = originHeight * sizeRatio
-        } else {
-            resizedWidth = originWidth * sizeRatio
-            resizedHeight = length
-        }
-        
-        self.draw(in: CGRect(x: length / 2 - resizedWidth / 2,
-                             y: length / 2 - resizedHeight / 2,
-                             width: resizedWidth,
-                             height: resizedHeight))
-        
-        let resizedImage = UIGraphicsGetImageFromCurrentImageContext()
-        UIGraphicsEndImageContext()
-        
-        return resizedImage
-    }
-    
     func resizeHeightByWidth(width: CGFloat = 65) -> CGFloat {
         let originWidth: CGFloat = self.size.width
         let originHeight: CGFloat = self.size.height
