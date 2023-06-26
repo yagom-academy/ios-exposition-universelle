583d5522b50e07f4087cd6fd20cdc911ad3a7f7d
Hoon94 <dleognsdl1@naver.com>
2023-06-26 19:02:15 +0900

    feat: Item 타입 및 Assets 추가

---
+++ Expo1900/Expo1900/Model/Item.swift
@@ -0,0 +1,20 @@
+//
+//  Item.swift
+//  Expo1900
+//
+//  Created by idinaloq, hoon on 2023/06/26.
+//
+
+struct Item: Decodable {
+    var name: String
+    var imageName: String
+    var shortDescription: String
+    var description: String
+    
+    enum CodingKeys: String, CodingKey {
+        case name
+        case imageName = "image_name"
+        case shortDescription = "short_desc"
+        case description = "desc"
+    }
+}
