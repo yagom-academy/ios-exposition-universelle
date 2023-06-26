f64a064112370033115b41987f83b44e1ee2226f
bubblecocoa <bubblecocoa@gmail.com>
2023-06-26 10:44:17 +0900

    feat: json 데이터와 매칭할 모델 타입 구현

---
+++ Expo1900/Expo1900/Model/ExpositionUniverselle.swift
@@ -0,0 +1,14 @@
+//
+//  ExpositionUniverselle.swift
+//  Expo1900
+//
+//  Created by Erick, 비모 on 2023/06/26.
+//
+
+struct ExpositionUniverselle: Codable {
+    var title: String
+    var visitors: Int
+    var location: String
+    var duration: String
+    var description: String
+}
---
+++ Expo1900/Expo1900/Model/Items.swift
@@ -0,0 +1,13 @@
+//
+//  Items.swift
+//  Expo1900
+//
+//  Created by Erick, 비모 on 2023/06/26.
+//
+
+struct Items: Codable {
+    var name: String
+    var imageName: String
+    var shortDesc: String
+    var desc: String
+}
