e5ead0bde1ef511820bc4f28f58f479efbf2f61f
iOS-Yetti <yechan123456@gmail.net>
2023-06-26 21:51:09 +0900

    feat: JSON타입의 decoding을 위한 ExpoGuide타입 구현 및 Entry타입 네이밍 수정

--- Expo1900/Expo1900/Model/Entry.swift
+++ Expo1900/Expo1900/Model/Entry.swift
@@ -5,19 +5,17 @@
 //  Created by mint, Yetti on 2023/06/26.
 //
 
-import Foundation
-
 struct Entry: Codable {
     let name: String
     let imageName: String
-    let shortDesc: String
-    let desc: String
+    let shortDescription: String
+    let description: String
     
     enum CodingKeys: String, CodingKey {
         case imageName = "image_name"
-        case shortDesc = "short_desc"
+        case shortDescription = "short_desc"
         
         case name
-        case desc
+        case description
     }
 }
---
+++ Expo1900/Expo1900/Model/ExpoGuide.swift
@@ -0,0 +1,14 @@
+//
+//  ExpoGuide.swift
+//  Expo1900
+//
+//  Created by mint, Yetti on 2023/06/26.
+//
+
+struct ExpoGuide: Codable {
+    let title: String
+    let visitors: Int
+    let location: String
+    let duration: String
+    let description: String
+}
