bda580cc7531889d899bef2691dd7984d3655af8
DasanKim <tempzipp@gmail.com>
2023-06-26 21:58:08 +0900

    ✨feat: JSON 포멧의 데이터와 매칭되는 Entry 타입 구현

--- Expo1900/Expo1900/Model/Entry.swift
+++ Expo1900/Expo1900/Model/Entry.swift
@@ -2,7 +2,19 @@
 //  Entry.swift
 //  Expo1900
 //
-//  Created by Yena on 2023/06/26.
+//  Created by Dasan & Moon on 2023/06/26.
 //
 
-import Foundation
+struct Entry: Codable {
+    let name: String
+    let image: String
+    let shortDescription: String
+    let description: String
+    
+    enum CodingKeys: String, CodingKey {
+        case name
+        case image = "image_name"
+        case shortDescription = "short_desc"
+        case description = "desc"
+    }
+}
