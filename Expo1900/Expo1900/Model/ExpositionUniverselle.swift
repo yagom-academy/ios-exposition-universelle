5ee32cccfab99df90422b231266fa50738fe64d7
idinaloq <idinaloq@icloud.com>
2023-06-26 18:39:30 +0900

    feat: ExpositionUniverselle 타입 추가

---
+++ Expo1900/Expo1900/Model/ExpositionUniverselle.swift
@@ -0,0 +1,14 @@
+//
+//  ExpositionUniverselle.swift
+//  Expo1900
+//
+//  Created by idinaloq, hoon on 2023/06/26.
+//
+
+struct ExpositionUniverselle: Decodable {
+    var title: String
+    var visitors: Int
+    var location: String
+    var duration: String
+    var description: String
+}
