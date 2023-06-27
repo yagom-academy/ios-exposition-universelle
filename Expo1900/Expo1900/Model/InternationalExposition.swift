29cfd278f78e3a269e8d4b705ff480666b03f059
karen <karenrodanthe@icloud.com>
2023-06-27 16:34:58 +0900

    🆕feat: JSON타입의 Decoding을 위한 Items 구조체 구현

---
+++ Expo1900/Expo1900/Model/InternationalExposition.swift
@@ -0,0 +1,7 @@
+//
+//  InternationalExposition.swift
+//  Expo1900
+//
+//  Created by karen on 2023/06/27.
+//
+
---
+++ Expo1900/Expo1900/Model/Items.swift
@@ -0,0 +1,13 @@
+//
+//  Items.swift
+//  Expo1900
+//
+//  Created by karen on 2023/06/27.
+//
+
+struct Items: Decodable {
+    let name: String
+    let imageName: String
+    let shortDescription: String
+    let description: String
+}
