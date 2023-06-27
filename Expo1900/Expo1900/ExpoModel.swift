297f2238430ec08aa7c24ebccb860645326bc514
MaryJo-github <jotjdal0215@gmail.com>
2023-06-27 09:10:18 +0900

    :sparkles: Feat: ExpoModel 생성 및 구현

---
+++ Expo1900/Expo1900/ExpoModel.swift
@@ -0,0 +1,14 @@
+//
+//  ExpoModel.swift
+//  Expo1900
+//
+//  Created by MARY, KOBE on 2023/06/27.
+//
+
+struct ExpoModel {
+    var title: String
+    var visitors: Int
+    var location: String
+    var duration: String
+    var description: String
+}
