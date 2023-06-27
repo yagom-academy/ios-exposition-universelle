62d8d5072bdf96463d0bd8261c47ea00c57f36b5
idinaloq <idinaloq@icloud.com>
2023-06-27 14:12:36 +0900

    refactor: item의 description 네이밍 변경

--- Expo1900/Expo1900/Model/Item.swift
+++ Expo1900/Expo1900/Model/Item.swift
@@ -9,12 +9,13 @@ struct Item: Decodable {
     let name: String
     let imageName: String
     let shortDescription: String
-    let description: String
+    let totalDescription: String
     
     enum CodingKeys: String, CodingKey {
         case name
         case imageName = "image_name"
         case shortDescription = "short_desc"
-        case description = "desc"
+        case totalDescription = "desc"
+        
     }
 }
