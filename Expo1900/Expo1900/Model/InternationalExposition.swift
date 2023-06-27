cd0e9bf4c2a62eb68c4882f990272bdd171c999b
karen <karenrodanthe@icloud.com>
2023-06-27 16:38:52 +0900

    🆕feat: JSON타입의 Decoding을 위한 InternationalExposition구조체 구현

--- Expo1900/Expo1900/Model/InternationalExposition.swift
+++ Expo1900/Expo1900/Model/InternationalExposition.swift
@@ -5,3 +5,11 @@
 //  Created by karen on 2023/06/27.
 //
 
+struct InternationalExposition: Decodable {
+    let title: String
+    let visitors: UInt
+    let location: String
+    let duration: String
+    let description: String
+    
+}
