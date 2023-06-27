e63d462bca3eee6ff089997eb17ef449d5ae7b62
Kobe <mskang@xingxingmobility.com>
2023-06-27 16:33:56 +0900

    :white_check_mark: Test: ItemsModelTests 생성 및 구현.

--- Expo1900/ItemsModelTests/ItemsModelTests.swift
+++ Expo1900/ItemsModelTests/ItemsModelTests.swift
@@ -6,30 +6,27 @@
 //
 
 import XCTest
-
+@testable import Expo1900
 final class ItemsModelTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
+	func test_출품작목록json데이터를_parsing했을때_정보를추출한다() {
+		// given
+		guard let dataAsset = NSDataAsset(name: "items") else {
+			return
+		}
+		let decoder = JSONDecoder()
+		let nameExpectation = "불경"
+		let imageNameExpectation = "buddhism"
+		let shortDescriptionExpectation = "석가모니와 그 제자들의 가르침을 모아놓은, 불교의 경전"
+		let descriptionExpectation = "불경(佛經, 영어: Buddhist texts)은 석가모니와 그 제자들의 가르침을 모아놓은, 불교의 경전이다.\n\n중복되는 예도 많고 인도, 중국, 티베트, 한국을 거치면서 같은 경전이 여러가지 이름으로 불리기도 한다. 불교의 경전은 1차 결집은 석가모니의 말을 제자들이 다시 암송하는 것이었다. 즉 현재처럼 책으로 만들어진 것이 아니라, 같이 암송하여 석가모니의 말임을 확인하는 것이 결집이었다. 그 후 3차 결집에서는 패엽에 기록하였고 이후 경, 논, 율 의 3장 삼장으로 분류하여 각국에서 대장경을 결집하였다.\n\n현재 일본의 신수대장경이 가장 체계적인 정리이므로 불교 연구에서 대장경의 표준이다. 대한민국 해인사의 팔만대장경도 2004년 현재 전산화가 완료되었다.\n\n불교 경전은 상좌부 불교 계통인 아함경 등 상좌부 경전 / 대승불교의 대승경전 / 티베트 불교, 즉 밀교의 금강승 계통의 경전으로 나눌 수 있다. 밀교는 일반적으로 대승에 속한다."
+		
+		// when
+		let products = try! decoder.decode([ItemsModel].self, from: dataAsset.data)
+		
+		// then
+		XCTAssertEqual(products[1].name, nameExpectation)
+		XCTAssertEqual(products[1].imageName, imageNameExpectation)
+		XCTAssertEqual(products[1].shortDescription, shortDescriptionExpectation)
+		XCTAssertEqual(products[1].desc, descriptionExpectation)
+	}
 
 }
