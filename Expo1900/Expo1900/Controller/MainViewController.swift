8853029512350da36871ddba55b3f4fb931b42d7
iOS-Yetti <yechan123456@gmail.net>
2023-06-27 18:16:53 +0900

    feat: 스토리보드에 scrollView, stackView 추가 및 DecodeError, Decodable의 extension 구현

--- Expo1900/Expo1900/Controller/MainViewController.swift
+++ Expo1900/Expo1900/Controller/MainViewController.swift
@@ -6,11 +6,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class MainViewController: UIViewController {
+    let expoGuide: [ExpoGuide] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
 
---
+++ Expo1900/Expo1900/Error/DecodeError.swift
@@ -0,0 +1,11 @@
+//
+//  DecodeError.swift
+//  Expo1900
+//
+//  Created by mint, Yetti on 2023/06/27.
+//
+
+enum DecodeError: Error {
+    case noFile
+    case decodeFailed
+}
