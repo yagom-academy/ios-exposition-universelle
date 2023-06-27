b6912cb64def96c5b1c8856a013d1fafef89e0a4
Whales <Whales.w.Jin@gmail.com>
2023-06-27 11:41:30 +0900

    feat: ExpositionUniverselle 구조체에 formattedVisitors 프로퍼티 생성
    - 방문자 수에 천단위 구분기호를 넣기 위해서 NumberFormatter 사용

--- Expo1900/Expo1900/Model/ExpositionUniverselle.swift
+++ Expo1900/Expo1900/Model/ExpositionUniverselle.swift
@@ -5,10 +5,22 @@
 //  Created by Max, Whales on 2023/06/27.
 //
 
+import Foundation
+
 struct ExpositionUniverselle: Decodable {
     let title: String
     let visitors: UInt
     let location: String
     let duration: String
     let description: String
+    
+    var formattedVisitors: String {
+        let numberFormatter = NumberFormatter()
+        
+        guard let result = numberFormatter.string(for: visitors) else {
+            return String(visitors)
+        }
+        
+        return result
+    }
 }
