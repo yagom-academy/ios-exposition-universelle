4f864ca5e86ae02513c175165f8a157556b68c17
redmango1446@gmail.com <redmango1446>
2023-06-27 11:14:03 +0900

    refactor: item 타입 프로퍼티명 수정

--- Expo1900/Model/Item.swift
+++ Expo1900/Model/Item.swift
@@ -8,13 +8,13 @@
 struct Item: Decodable {
     let name: String
     let imageName: String
-    let shortDesc: String
-    let desc: String
+    let shortDescription: String
+    let description: String
     
     enum CodingKeys: String, CodingKey {
         case name
         case imageName = "image_name"
-        case shortDesc = "short_desc"
-        case desc
+        case shortDescription = "short_desc"
+        case description
     }
 }
