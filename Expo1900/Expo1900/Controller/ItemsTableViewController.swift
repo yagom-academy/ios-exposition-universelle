66d1a7b7cfbeb02d505188a789dd3d8a06b19822
bubblecocoa <bubblecocoa@gmail.com>
2023-06-27 19:15:29 +0900

    feat: ItemsTableViewController에서 필요한 데이터 디코딩 메서드 추가

--- Expo1900/Expo1900/Controller/ItemsTableViewController.swift
+++ Expo1900/Expo1900/Controller/ItemsTableViewController.swift
@@ -9,18 +9,33 @@ import UIKit
 
 class ItemsTableViewController: UITableViewController {
     static let id = "itemsTableViewControllerID"
+    
+    var items: [Item] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        decodeJSONToItems()
     }
 
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 0
+    }
+    
+    private func decodeJSONToItems() {
+        guard let json = NSDataAsset(name: "items") else { return }
+        let decoder = JSONDecoder()
+        
+        do {
+            items = try decoder.decode([Item].self, from: json.data)
+        } catch {
+            print(error.localizedDescription)
+        }
     }
 }
