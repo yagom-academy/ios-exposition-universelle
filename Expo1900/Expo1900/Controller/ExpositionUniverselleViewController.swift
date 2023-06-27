b3abbe5edf79d5c5b15507386517d805bc943bf0
h-suo <phstoo1021@naver.com>
2023-06-27 17:08:51 +0900

    feat: Asset의 Json파일을 디코딩하여 Label 텍스트에 설정

--- Expo1900/Expo1900/Controller/ExpositionUniverselleViewController.swift
+++ Expo1900/Expo1900/Controller/ExpositionUniverselleViewController.swift
@@ -7,7 +7,7 @@
 import UIKit
 
 final class ExpositionUniverselleViewController: UIViewController {
-
+    
     @IBOutlet private weak var titleLabel: UILabel!
     @IBOutlet private weak var visitorsLabel: UILabel!
     @IBOutlet private weak var locationLabel: UILabel!
@@ -16,9 +16,22 @@ final class ExpositionUniverselleViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        guard let json = NSDataAsset(name: "exposition_universelle_1900") else { return }
+        let decoder = JSONDecoder()
+        do {
+            let expositionUniverselle = try decoder.decode(ExpositionUniverselle.self, from: json.data)
+            titleLabel.text = expositionUniverselle.title
+            visitorsLabel.text = "방문객 : " + String(expositionUniverselle.visitors) + "명"
+            locationLabel.text = "개최지 : " + expositionUniverselle.location
+            durationLabel.text = "개최 기간 : " + expositionUniverselle.duration
+            descriptionLabel.text = expositionUniverselle.description
+        } catch {
+            print(error.localizedDescription)
+        }
     }
     
     @IBAction private func tapShowItemsButton(_ sender: UIButton) {
+        
     }
 }
