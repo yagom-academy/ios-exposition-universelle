9b30e5f5d6fa077c7c3138a218f6c8c6b526bd47
bubblecocoa <bubblecocoa@gmail.com>
2023-06-27 13:16:41 +0900

    refactor: Item구조체의 변수명 약자에서 풀네임으로 변경

--- Expo1900/Expo1900/Model/Item.swift
+++ Expo1900/Expo1900/Model/Item.swift
@@ -8,12 +8,13 @@
 struct Item: Codable {
     var name: String
     var imageName: String
-    var shortDesc: String
-    var desc: String
+    var shortDescription: String
+    var description: String
     
     enum CodingKeys: String, CodingKey {
-        case name, desc
+        case name
         case imageName = "image_name"
-        case shortDesc = "short_desc"
+        case shortDescription = "short_desc"
+        case description = "desc"
     }
 }
