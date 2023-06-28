1297acf9de8051b550d74bd494dd9958f13745c3
iOS-Yetti <yechan123456@gmail.net>
2023-06-28 14:48:59 +0900

    feat: 테이블뷰 셀 내부에 스택뷰로 구조 구현

--- Expo1900/Expo1900/Controller/EntryListViewController.swift
+++ Expo1900/Expo1900/Controller/EntryListViewController.swift
@@ -8,7 +8,25 @@
 import UIKit
 
 class EntryListViewController: UIViewController {
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
     }
+}
+
+extension EntryListViewController: UITableViewDataSource, UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+    
 }
