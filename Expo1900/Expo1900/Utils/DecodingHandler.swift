4e9e1125aead6ea7c078a781256f972eaa9be6f3
JusBug <pksgwo123@gmail.com>
2023-06-28 20:18:28 +0900

    feat: JSON 데이터를 제네릭 타입으로 변환하는 DecodingHandler 구현

---
+++ Expo1900/Expo1900/Utils/DecodingHandler.swift
@@ -0,0 +1,40 @@
+//
+//  DecodingHandler.swift
+//  Expo1900
+//
+//  Created by 박종화 on 2023/06/28.
+//
+
+import Foundation
+
+enum DecodingHandler {
+    enum FileName: String {
+        case exposition = "exposition"
+        case entry = "entry"
+        
+        var name: String {
+            return self.rawValue
+        }
+    }
+    
+    static func decode<T: Codable>(fileName file: FileName) -> T? {
+        let decoder = JSONDecoder()
+        
+        guard let path = Bundle.main.path(forResource: file.name, ofType: "json") else {
+            print("Bundle에서 \(file.name).json 파일을 찾을 수 없습니다.")
+            return nil
+        }
+        
+        guard let content = try? String(contentsOfFile: path).data(using: .utf8) else {
+            print("\(file.name).json 파일의 내용을 읽을 수 없습니다.")
+            return nil
+        }
+            
+        guard let result = try? decoder.decode(T.self, from: content) else {
+            print("\(file.name).json 파일을 \(T.self)으로 변환하는데 실패하였습니다.")
+            return nil
+        }
+        
+        return result
+    }
+}
