6d10467c2820e932883fb0421b70cad09b57d9ef
agilestarskim <agilestars@kakao.com>
2023-06-28 20:27:53 +0900

    feat: storyboard에서 UI 연동

--- Expo1900/Expo1900/Controller/ExpositionIntroViewController.swift
+++ Expo1900/Expo1900/Controller/ExpositionIntroViewController.swift
@@ -6,4 +6,16 @@
 
 import UIKit
 
-class ExpositionIntroViewController: UIViewController { }
+class ExpositionIntroViewController: UIViewController {
+    @IBOutlet weak private var expositionTitleLabel: UILabel!
+    @IBOutlet weak private var visitorsLabel: UILabel!
+    @IBOutlet weak private var locationLabel: UILabel!
+    @IBOutlet weak private var durationLabel: UILabel!
+    @IBOutlet weak private var descriptionLabel: UILabel!
+    
+    override func viewDidLoad() {
+       super.viewDidLoad()
+    }
+    
+    @IBAction private func tabKoreanEntryButton(_ sender: UIButton) { }
+}
