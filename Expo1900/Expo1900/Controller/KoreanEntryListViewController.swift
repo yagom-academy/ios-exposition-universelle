9c796c7f78ccb6102d78388fad78cd4da46f569f
JusBug <pksgwo123@gmail.com>
2023-06-28 20:45:09 +0900

    feat: KoreanEnreyListVC UI 연동, TableView delegate, datasource 채택

--- Expo1900/Expo1900/Controller/KoreanEntryListViewController.swift
+++ Expo1900/Expo1900/Controller/KoreanEntryListViewController.swift
@@ -8,7 +8,27 @@
 import UIKit
 
 class KoreanEntryListViewController: UIViewController {
+    @IBOutlet weak private var entryTableView: UITableView!
+    private var entryList: [Entry] = DecodingHandler.decode(fileName: .entry) ?? []
+    static let identifier = "koreanEntryListViewController"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.entryTableView.delegate = self
+        self.entryTableView.dataSource = self
+    }
+}
+
+extension KoreanEntryListViewController: UITableViewDelegate {
+
+}
+
+extension KoreanEntryListViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
     }
 }
