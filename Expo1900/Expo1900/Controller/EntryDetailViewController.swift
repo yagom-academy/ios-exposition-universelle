1b39068a1c9f6aa04abc2627cc33fdbf04fc7d1e
agilestarskim <agilestars@kakao.com>
2023-06-28 20:40:48 +0900

    feat: KoreanEntryListVC, EntryDetailVC 생성

---
+++ Expo1900/Expo1900/Controller/EntryDetailViewController.swift
@@ -0,0 +1,14 @@
+//
+//  EntryDetailViewController.swift
+//  Expo1900
+//
+//  Created by 김민성 on 2023/06/28.
+//
+
+import UIKit
+
+class EntryDetailViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ Expo1900/Expo1900/Controller/KoreanEntryListViewController.swift
@@ -0,0 +1,14 @@
+//
+//  KoreanEntryListViewController.swift
+//  Expo1900
+//
+//  Created by Minsup, Jusbug on 2023/06/28.
+//
+
+import UIKit
+
+class KoreanEntryListViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
