d6d7059b455a610242ea0b9a2d7550202783b824
Whales <Whales.w.Jin@gmail.com>
2023-06-28 16:17:16 +0900

    feat: StackView에 descriptionLabel 추가
    - descriptionLabel 추가에 따른 constraints 사항 추가

--- Expo1900/Expo1900/ViewController.swift
+++ Expo1900/Expo1900/ViewController.swift
@@ -15,12 +15,15 @@ class ViewController: UIViewController {
         stackView.axis = .vertical
         stackView.translatesAutoresizingMaskIntoConstraints = false
         stackView.alignment = .center
+        stackView.spacing = 10
+        
         return stackView
     }()
     
     private let mainScrollView = {
         let scrollView = UIScrollView()
         scrollView.translatesAutoresizingMaskIntoConstraints = false
+        
         return scrollView
     }()
     
@@ -34,14 +37,14 @@ class ViewController: UIViewController {
     }
 
     private func setUpEntity() {
-        guard let expositionUniverselleEntity = NSDataAsset.init(name: "exposition_universelle_1900") else {
+        guard let entity = NSDataAsset(name: "exposition_universelle_1900") else {
             return
         }
         
         let decorder = JSONDecoder()
         
         do {
-            expositionEntity = try decorder.decode(ExpositionEntity.self, from: expositionUniverselleEntity.data)
+            expositionEntity = try decorder.decode(ExpositionEntity.self, from: entity.data)
             isSetUpEntity = true
         } catch {
             isSetUpEntity = false
@@ -52,39 +55,63 @@ class ViewController: UIViewController {
         view.addSubview(mainScrollView)
         mainScrollView.addSubview(verticalStackView)
         
+        let safeArea = view.safeAreaLayoutGuide
+        
         NSLayoutConstraint.activate([
-            mainScrollView.topAnchor.constraint(equalTo: view.topAnchor),
-            mainScrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
-            mainScrollView.leftAnchor.constraint(equalTo: view.leftAnchor),
-            mainScrollView.rightAnchor.constraint(equalTo: view.rightAnchor)
+            mainScrollView.topAnchor.constraint(equalTo: safeArea.topAnchor),
+            mainScrollView.bottomAnchor.constraint(equalTo: safeArea.bottomAnchor),
+            mainScrollView.leftAnchor.constraint(equalTo: safeArea.leftAnchor, constant: 10),
+            mainScrollView.rightAnchor.constraint(equalTo: safeArea.rightAnchor, constant: -10),
+            
+            verticalStackView.topAnchor.constraint(equalTo: mainScrollView.topAnchor),
+            verticalStackView.bottomAnchor.constraint(equalTo: mainScrollView.bottomAnchor),
+            verticalStackView.leftAnchor.constraint(equalTo: mainScrollView.leftAnchor),
+            verticalStackView.widthAnchor.constraint(equalTo: mainScrollView.widthAnchor)
         ])
     }
     
     private func configureStackView() {
-            if isSetUpEntity {
-                let titleLabel = {
-                    let label = UILabel()
-                    label.text = expositionEntity?.title
-                    return label
-                }()
+        if isSetUpEntity {
+            let titleLabel = {
+                let label = UILabel()
+                label.text = expositionEntity?.title
+                label.textAlignment = .center
+                
+                return label
+            }()
 
-                let posterImageView = {
-                    let image = UIImageView()
-                    image.image = UIImage(named: "poster")
-                    return image
-                }()
+            let posterImageView = {
+                let image = UIImageView()
+                image.image = UIImage(named: "poster")
+                
+                return image
+            }()
+            
+            let descriptionLabel = {
+                let textLabel = UILabel()
+                textLabel.lineBreakMode = NSLineBreakMode.byWordWrapping
+                textLabel.numberOfLines = 0
+                textLabel.text = expositionEntity?.description
+                textLabel.translatesAutoresizingMaskIntoConstraints = false
+                
+                return textLabel
+            }()
+            
+            let safeArea = view.safeAreaLayoutGuide
 
-                verticalStackView.addArrangedSubview(titleLabel)
-                verticalStackView.addArrangedSubview(posterImageView)
-            } else {
-                let errorLabel = {
-                    let label = UILabel()
-                    label.text = "데이터를 불러오지 못했습니다."
-                    return label
-                }()
+            verticalStackView.addArrangedSubview(titleLabel)
+            verticalStackView.addArrangedSubview(posterImageView)
+            verticalStackView.addArrangedSubview(descriptionLabel)
+        } else {
+            let errorLabel = {
+                let label = UILabel()
+                label.text = "데이터를 불러오지 못했습니다."
+                
+                return label
+            }()
 
-                verticalStackView.addArrangedSubview(errorLabel)
-            }
+            verticalStackView.addArrangedSubview(errorLabel)
         }
+    }
 }
 
