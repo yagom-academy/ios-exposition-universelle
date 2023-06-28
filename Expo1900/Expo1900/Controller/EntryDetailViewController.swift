8aad85aa795fef6bba56da160a49982e93155562
agilestarskim <agilestars@kakao.com>
2023-06-28 20:57:45 +0900

    feat: EntryDetailVC UI 연동 및 데이터 바인딩

--- Expo1900/Expo1900/Controller/EntryDetailViewController.swift
+++ Expo1900/Expo1900/Controller/EntryDetailViewController.swift
@@ -7,8 +7,21 @@
 
 import UIKit
 
-class EntryDetailViewController: UIViewController {
+final class EntryDetailViewController: UIViewController {
+    var entry: Entry?
+    @IBOutlet weak private var entryDescriptionLabel: UILabel!
+    @IBOutlet weak private var entryImageView: UIImageView!
+    static let identifier = "entryDetailViewController"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.configureComponents()
+    }
+    
+    private func configureComponents() {
+        if let entry = self.entry {
+            self.entryImageView.image = UIImage(named: entry.imageName)
+            self.entryDescriptionLabel.text = entry.description
+        }
     }
 }
