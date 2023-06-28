1d0d3548aa39f23feed6d98db3eb31525dcbe8bc
Zion <lion2212@naver.com>
2023-06-28 19:09:10 +0900

    feat: CodeBase UI 사전작업

--- Expo1900/Expo1900/SceneDelegate.swift
+++ Expo1900/Expo1900/SceneDelegate.swift
@@ -11,7 +11,12 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        let navigationController = UINavigationController(rootViewController: MainViewController())
+        
+        window = UIWindow(windowScene: windowScene)
+        window?.rootViewController = navigationController
+        window?.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) { }
