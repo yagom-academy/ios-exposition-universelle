c665d46b49de6003c2cb98e1d95cb68a3b44b586
bubblecocoa <bubblecocoa@gmail.com>
2023-06-29 10:23:23 +0900

    style: IBOutlet 프로퍼티 위치 변경

--- Expo1900/Expo1900/Controller/ItemDetailViewController.swift
+++ Expo1900/Expo1900/Controller/ItemDetailViewController.swift
@@ -12,6 +12,9 @@ final class ItemDetailViewController: UIViewController {
     
     private let item: Item
     
+    @IBOutlet weak var itemDetailImageView: UIImageView!
+    @IBOutlet weak var itemDescriptionLabel: UILabel!
+    
     init?(item: Item, coder: NSCoder) {
         self.item = item
         super.init(coder: coder)
@@ -20,9 +23,6 @@ final class ItemDetailViewController: UIViewController {
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-
-    @IBOutlet weak var itemDetailImageView: UIImageView!
-    @IBOutlet weak var itemDescriptionLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
