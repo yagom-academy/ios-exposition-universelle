d55e9e404eddb2ef8f2ef00cd94e1ced5ae8c079
mint3382 <mskim0926@naver.com>
2023-06-29 22:42:47 +0900

    feat: EntryDetailViewController 생성

---
+++ Expo1900/Expo1900/Controller/EntryDetailViewController.swift
@@ -0,0 +1,14 @@
+//
+//  EntryDetailViewController.swift
+//  Expo1900
+//
+//  Created by mint, Yetti on 2023/06/29.
+//
+
+import UIKit
+
+class EntryDetailViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
