c8aa224266d0077b9b662974a8b925e74363d0fa
Serena <sha0720@naver.com>
2023-06-29 12:42:36 +0900

    feat: setUpTableViewConstraints 함수 생성

---
+++ Expo1900/Expo1900/Model/KoreaEntryDataSource.swift
@@ -0,0 +1,8 @@
+//
+//  KoreaEntryDataSource.swift
+//  Expo1900
+//
+//  Created by Hyun A Song on 2023/06/29.
+//
+
+import Foundation
--- Expo1900/Expo1900/ViewController/KoreaEntryViewController.swift
+++ Expo1900/Expo1900/ViewController/KoreaEntryViewController.swift
@@ -7,13 +7,32 @@
 
 import UIKit
 
-class KoreaEntryViewController: UIViewController {
+class KoreaEntryViewController: UIViewController, UITableViewDelegate {
+    lazy var tableView: UITableView = {
+       let tableView = UITableView()
+        tableView.delegate = self
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        return tableView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        view.addSubview(tableView)
+        
         view.backgroundColor = .brown
         
         navigationItem.backButtonTitle = "한국의 출품작"
         navigationItem.title = "한국의 출품작"
+        
+        setUpTableViewConstraints()
+    }
+    
+    private func setUpTableViewConstraints() {
+        NSLayoutConstraint.activate([
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            tableView.topAnchor.constraint(equalTo: view.topAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
     }
 }
