d9baabc7c2e136a6663d0a5fe3479b50fe9f8986
Kobe <mskang@xingxingmobility.com>
2023-06-30 19:26:02 +0900

    :recycle: Refactor: cell 악세서리 타입 추가.

--- Expo1900/Expo1900/View/KoreaEntryTableViewCell.swift
+++ Expo1900/Expo1900/View/KoreaEntryTableViewCell.swift
@@ -14,6 +14,7 @@ class KoreaEntryTableViewCell: UITableViewCell {
 
     override func awakeFromNib() {
         super.awakeFromNib()
+		self.accessoryType = .disclosureIndicator
         setUpUI()
     }
 
