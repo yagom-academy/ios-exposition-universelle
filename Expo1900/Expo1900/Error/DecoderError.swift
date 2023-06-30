4c5d2ea811fb88207ef993d8446e6a83f7e32190
DasanKim <tempzipp@gmail.com>
2023-06-30 19:23:46 +0900

    ♻️refactor: DecoderError 타입 내 description 프로퍼티 추가

--- Expo1900/Expo1900/Error/DecoderError.swift
+++ Expo1900/Expo1900/Error/DecoderError.swift
@@ -8,4 +8,16 @@
 enum DecoderError: Error {
     case notFoundAsset
     case decodeFailed
+    case unexpectedError
+    
+    var description: String {
+        switch self {
+        case .notFoundAsset:
+            return "에셋을 찾을 수 없습니다."
+        case .decodeFailed:
+            return "디코딩에 실패했습니다."
+        case .unexpectedError:
+            return "알 수 없는 에러"
+        }
+    }
 }
