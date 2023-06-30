491385229d5d61da23dd2280f18de7045bcc43ff
DasanKim <tempzipp@gmail.com>
2023-06-30 19:22:58 +0900

    ✨feat: DataAsset 및 Storyboard에 관한 Namespace 추가

---
+++ Expo1900/Expo1900/Utility/Namespace/DataAssetNamespace.swift
@@ -0,0 +1,11 @@
+//
+//  DataAssetNamespace.swift
+//  Expo1900
+//
+//  Created by Dasan & Moon on 2023/06/30.
+//
+
+enum DataAssetNamespace {
+    static let introduction = "exposition_universelle_1900"
+    static let entryList = "items"
+}
---
+++ Expo1900/Expo1900/Utility/Namespace/StoryboardNamespace.swift
@@ -0,0 +1,18 @@
+//
+//  StoryboardNamespace.swift
+//  Expo1900
+//
+//  Created by Dasan & Moon on 2023/06/30.
+//
+
+enum StoryboardNamespace {
+    enum Name {
+        static let main = "Main"
+    }
+
+    enum ID {
+        static let entryCell = "entryCell"
+        static let entryListViewController = "EntryListViewController"
+        static let entryDetailViewController = "EntryDetailViewController"
+    }
+}
