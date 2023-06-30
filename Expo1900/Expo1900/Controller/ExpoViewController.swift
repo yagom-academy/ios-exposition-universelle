1c7c6442c1df1fde342ac8e28986133dbff1e658
Kobe <mskang@xingxingmobility.com>
2023-06-30 09:32:14 +0900

    :sparkles: Feat: ExpoViewController 구현.
    ExpoViewController 내부 구조를 구현하고 JSON 데이터를 파싱하여 데이터를 올바른 위치에 넣어주었습니다.

--- Expo1900/Expo1900/Controller/ExpoViewController.swift
+++ Expo1900/Expo1900/Controller/ExpoViewController.swift
@@ -6,13 +6,37 @@
 
 import UIKit
 
-class ExpoViewController: UIViewController {
-
+final class ExpoViewController: UIViewController {
+	private let expoModel: ExpoModel? = {
+		guard let dataAsset = NSDataAsset(name: "exposition_universelle_1900") else {
+			return nil
+		}
+		let decoder = JSONDecoder()
+		let expo = try? decoder.decode(ExpoModel.self, from: dataAsset.data)
+		return expo
+	}()
+	
+	@IBOutlet weak var titleLabel: UILabel!
+	@IBOutlet weak var visitorsLabel: UILabel!
+	@IBOutlet weak var locationLabel: UILabel!
+	@IBOutlet weak var durationLabel: UILabel!
+	@IBOutlet weak var descriptionLabel: UILabel!
+	
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setUpLabels()
     }
 
-
+	private func setUpLabels() {
+		guard let expoModel = expoModel,
+			  let visitors = expoModel.visitors.changeToString() else {
+			return
+		}
+		titleLabel.text = expoModel.title.replacingOccurrences(of: "(", with: "\n(")
+		visitorsLabel.text = "방문객 : \(visitors) 명"
+		locationLabel.text = "개최지 : \(expoModel.location)"
+		durationLabel.text = "개최 기간 : \(expoModel.duration)"
+		descriptionLabel.text = expoModel.description
+	}
 }
 
