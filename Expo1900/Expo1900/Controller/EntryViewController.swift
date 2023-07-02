41646f068ce359508d0239250cfbfb96575a383c
yyss99 <yyssjj9999@gmail.com>
2023-07-02 17:19:06 +0900

    🚚build: EntryViewController 파일 추가

---
+++ Expo1900/Expo1900/Controller/EntryViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ExpoEntryViewController.swift
+//  Expo1900
+//
+//  Created by yyss99, kyungmin on 2023/07/02.
+//
+
+import UIKit
+
+class EntryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
