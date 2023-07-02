0f451e637606b0cf51a896c64abee9b68ce11bbe
YaRkyungmin <kkgm94@naver.com>
2023-07-02 19:14:11 +0900

    🚚build: EntryDetailViewController 파일 추가

---
+++ Expo1900/Expo1900/Controller/EntryDetailViewController.swift
@@ -0,0 +1,17 @@
+//
+//  EntryDetailViewController.swift
+//  Expo1900
+//
+//  Created by kangkyungmin on 2023/07/02.
+//
+
+import UIKit
+
+class EntryDetailViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
