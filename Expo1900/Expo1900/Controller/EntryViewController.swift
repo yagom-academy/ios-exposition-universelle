279be9abca89165263d28b82434b5e33bc1e7ac3
YaRkyungmin <kkgm94@naver.com>
2023-07-02 19:13:12 +0900

    ✨feat: EntryViewController  기능 구현

--- Expo1900/Expo1900/Controller/EntryViewController.swift
+++ Expo1900/Expo1900/Controller/EntryViewController.swift
@@ -9,7 +9,47 @@ import UIKit
 
 class EntryViewController: UIViewController {
 
+    var expoEntrys: [ExpoEntry] = [ExpoEntry]()
+    @IBOutlet weak var entryTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        entryTableView.dataSource = self
+        entryTableView.delegate = self
+        
+        let jsonDecoder: JSONDecoder = JSONDecoder()
+        
+        guard let dataAsset = NSDataAsset(name: "items") else { return }
+        
+        do {
+            expoEntrys = try jsonDecoder.decode([ExpoEntry].self, from: dataAsset.data)
+        } catch {
+            print("오류")
+        }
+        
+    }
+}
+
+extension EntryViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return expoEntrys.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let expoEntry = expoEntrys[indexPath.row]
+        
+        guard let entryCell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as? EntryTableViewCell else {
+            return UITableViewCell()
+        }
+        entryCell.configureView(expoEntry: expoEntry)
+        
+        return entryCell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        guard let entryDetailViewController = storyboard?.instantiateViewController(withIdentifier: "entryDetailViewController") as? EntryDetailViewController else { return }
+        
+        navigationController?.pushViewController(entryDetailViewController, animated: true)
     }
 }
