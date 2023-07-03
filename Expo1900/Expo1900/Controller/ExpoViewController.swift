39b7aeb3abc19611aaf3e8b9ebe0c8cfe5f6185c
Kobe <mskang@xingxingmobility.com>
2023-07-03 20:58:11 +0900

    :recycle: Refactor: final, JSONDecoder, backBarButtonItem, NameSpace 수정.
    클래스의 final 키워드를 추가, JSONDecode를 추가, backBarButtonItem을 추가, 매직 리터럴 네임 스페이스를 추가하였습니다.

--- Expo1900/Expo1900/Controller/ExpoViewController.swift
+++ Expo1900/Expo1900/Controller/ExpoViewController.swift
@@ -7,14 +7,7 @@
 import UIKit
 
 final class ExpoViewController: UIViewController {
-	private let expoModel: ExpoModel? = {
-		guard let dataAsset = NSDataAsset(name: "exposition_universelle_1900") else {
-			return nil
-		}
-		let decoder = JSONDecoder()
-		let expo = try? decoder.decode(ExpoModel.self, from: dataAsset.data)
-		return expo
-	}()
+	private let expoModel: ExpoModel? = try? Decoder.decodeJSON(dataAssetName: JSONDataNameSpace.expoData)
 	
 	@IBOutlet weak var titleLabel: UILabel!
 	@IBOutlet weak var visitorsLabel: UILabel!
@@ -29,7 +22,7 @@ final class ExpoViewController: UIViewController {
 	
 	override func viewWillAppear(_ animated: Bool) {
 		super.viewWillAppear(animated)
-		let backBarButtonItem = UIBarButtonItem(title: "메인", style: .plain, target: self, action: nil)
+		let backBarButtonItem = UIBarButtonItem(title: ViewControllerTitleNameSpace.expo, style: .plain, target: self, action: nil)
 		navigationItem.backBarButtonItem = backBarButtonItem
 		navigationController?.setNavigationBarHidden(true, animated: false)
 	}
@@ -40,10 +33,18 @@ final class ExpoViewController: UIViewController {
 			return
 		}
 		titleLabel.text = expoModel.title.replacingOccurrences(of: "(", with: "\n(")
-		visitorsLabel.text = "방문객 : \(visitors) 명"
-		locationLabel.text = "개최지 : \(expoModel.location)"
-		durationLabel.text = "개최 기간 : \(expoModel.duration)"
+		visitorsLabel.text = ExpoNameSpace.visitors + visitors + ExpoNameSpace.visitiorsUint
+		locationLabel.text = ExpoNameSpace.location + expoModel.location
+		durationLabel.text = ExpoNameSpace.duration + expoModel.duration
 		descriptionLabel.text = expoModel.description
 	}
 }
 
+extension ExpoViewController {
+	private enum ExpoNameSpace {
+		static let visitors: String = "방문객 : "
+		static let visitiorsUint: String = " 명"
+		static let location: String = "개최지 : "
+		static let duration: String = "개최 기간 : "
+	}
+}
