7009747c11874a7376c3d6f3c13b5b3f80b57a4f
yyss99 <yyssjj9999@gmail.com>
2023-07-03 10:23:14 +0900

    ✨feat: 한국의 출품작 셀 선택 시 기능 구현

--- Expo1900/Expo1900/Controller/EntryViewController.swift
+++ Expo1900/Expo1900/Controller/EntryViewController.swift
@@ -9,8 +9,13 @@ import UIKit
 
 class EntryViewController: UIViewController {
 
-    var expoEntrys: [ExpoEntry] = [ExpoEntry]()
+    var expoEntries: [ExpoEntry] = [ExpoEntry]()
     @IBOutlet weak var entryTableView: UITableView!
+    
+    override func viewWillAppear(_ animated: Bool) {
+        navigationController?.navigationBar.isHidden = false
+        navigationItem.title = "한국의 출품작"
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,7 +28,7 @@ class EntryViewController: UIViewController {
         guard let dataAsset = NSDataAsset(name: "items") else { return }
         
         do {
-            expoEntrys = try jsonDecoder.decode([ExpoEntry].self, from: dataAsset.data)
+            expoEntries = try jsonDecoder.decode([ExpoEntry].self, from: dataAsset.data)
         } catch {
             print("오류")
         }
@@ -33,11 +38,11 @@ class EntryViewController: UIViewController {
 
 extension EntryViewController: UITableViewDataSource, UITableViewDelegate {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return expoEntrys.count
+        return expoEntries.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let expoEntry = expoEntrys[indexPath.row]
+        let expoEntry = expoEntries[indexPath.row]
         
         guard let entryCell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as? EntryTableViewCell else {
             return UITableViewCell()
@@ -48,7 +53,13 @@ extension EntryViewController: UITableViewDataSource, UITableViewDelegate {
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let expoEntry = self.expoEntries[indexPath.row]
+    
         guard let entryDetailViewController = storyboard?.instantiateViewController(withIdentifier: "entryDetailViewController") as? EntryDetailViewController else { return }
+        
+        entryDetailViewController.entryImage = UIImage(named: expoEntry.imageName)
+        entryDetailViewController.entryDetailDescription = expoEntry.entryDescription
+        entryDetailViewController.entryName = expoEntry.name
         
         navigationController?.pushViewController(entryDetailViewController, animated: true)
     }
