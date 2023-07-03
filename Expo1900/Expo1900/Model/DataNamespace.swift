ad1b54f34aedbb3a3df530bbaf9fbac9fde2cf14
karen <karenrodanthe@icloud.com>
2023-07-04 06:20:37 +0900

    🆕feat: 매직리터럴방지를 위한 네임스페이스 구현

---
+++ Expo1900/Expo1900/Model/DataNamespace.swift
@@ -0,0 +1,22 @@
+//
+//  DataNamespace.swift
+//  Expo1900
+//
+//  Created by karen on 2023/07/04.
+//
+
+enum DataNamespace {
+    static let main = "메인"
+    static let poster = "poster"
+    static let koreanItem = "한국의 출품작"
+    static let visitors = "방문객"
+    static let location = "개최지"
+    static let duration = "개최 기간"
+    static let buttonTitle = "한국의 출품작 보러가기"
+    static let flag = "flag"
+    static let expositionUniverselle = "exposition_universelle_1900"
+    static let items = "items"
+    static let cellIdentifier = "ExhibitCell"
+    static let empty = ""
+}
+
