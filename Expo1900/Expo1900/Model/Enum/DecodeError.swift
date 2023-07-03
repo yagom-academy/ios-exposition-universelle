604015201712f5a1092f92dfdf5d83e274850c0c
Kobe <mskang@xingxingmobility.com>
2023-07-03 20:42:57 +0900

    :sparkles: Feat: DecodeError 타입 생성.

---
+++ Expo1900/Expo1900/Model/Enum/DecodeError.swift
@@ -0,0 +1,21 @@
+//
+//  DecodeError.swift
+//  Expo1900
+//
+//  Created by Minseong Kang on 2023/07/03.
+//
+import Foundation
+
+enum DecodeError: LocalizedError {
+	case NotFoundFile
+	case FailedDecode
+	
+	var errorDescription: String? {
+		switch self {
+		case .NotFoundFile:
+			return "파일을 찾을 수 없습니다."
+		case .FailedDecode:
+			return "디코딩에 실패했습니다."
+		}
+	}
+}
