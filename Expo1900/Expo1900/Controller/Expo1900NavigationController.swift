893b7dbf0c13b106a51a478958fe1a6f6aecd851
DasanKim <tempzipp@gmail.com>
2023-07-04 10:21:51 +0900

    🩹chore: 주석 수정 및 접근 제어 설정

--- Expo1900/Expo1900/Controller/Expo1900NavigationController.swift
+++ Expo1900/Expo1900/Controller/Expo1900NavigationController.swift
@@ -2,12 +2,12 @@
 //  Expo1900NavigationController.swift
 //  Expo1900
 //
-//  Created by 조호준 on 2023/07/04.
+//  Created by Dasan & Moo on 2023/07/04.
 //
 
 import UIKit
 
-class Expo1900NavigationController: UINavigationController {
+final class Expo1900NavigationController: UINavigationController {
     override var shouldAutorotate: Bool {
         return (self.topViewController?.shouldAutorotate)!
     }
