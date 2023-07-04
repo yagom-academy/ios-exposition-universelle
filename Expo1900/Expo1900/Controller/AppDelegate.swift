e0db46564d46eb4ac9d5786b4e3a607c7703f6ca
iOS-Yetti <yechan123456@gmail.net>
2023-07-04 21:45:32 +0900

    refactor: AppDelegate 내에 사용하지 않는 메서드 삭제 및 스토리보드 수정

--- Expo1900/Expo1900/Controller/AppDelegate.swift
+++ Expo1900/Expo1900/Controller/AppDelegate.swift
@@ -17,15 +17,4 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             return .all
         }
     }
-
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        return true
-    }
-
-    // MARK: UISceneSession Lifecycle
-
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
-    }
 }
