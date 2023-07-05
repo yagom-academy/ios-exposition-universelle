b94f68a2faff89d2deee76ae5856dabdc0c16e21
Kobe <mskang@xingxingmobility.com>
2023-07-05 10:39:24 +0900

    :recycle: Refactor: init 메서드 생성으로 인한 의존성 주입 구현.

--- Expo1900/Expo1900/Controller/EntryDetailViewController.swift
+++ Expo1900/Expo1900/Controller/EntryDetailViewController.swift
@@ -11,28 +11,28 @@ final class EntryDetailViewController: UIViewController {
 	@IBOutlet weak var koreaEntryImageView: UIImageView!
 	@IBOutlet weak var entryDescLabel: UILabel!
     
-    var koreanEntryImage: UIImage?
-    var entryDescription: String?
-    var entryTitle: String?
+	private let itemsModel: ItemsModel
+	
+	init?(itemsModel: ItemsModel, coder: NSCoder) {
+		self.itemsModel = itemsModel
+		super.init(coder: coder)
+	}
+	
+	required init?(coder: NSCoder) {
+		fatalError("init(coder:) has not been implemented")
+	}
 	
     override func viewDidLoad() {
         super.viewDidLoad()
 		
-		guard let koreanEntryImage = koreanEntryImage,
-              let entryDescription = entryDescription else {
-            return
-        }
-		
-		koreaEntryImageView.image = koreanEntryImage
-		entryDescLabel.text = entryDescription
+		koreaEntryImageView.image = UIImage(named: itemsModel.imageName)
+		entryDescLabel.text = itemsModel.desc
     }
 	
 	override func viewWillAppear(_ animated: Bool) {
 		super.viewWillAppear(animated)
         
-		guard let entryTitle = entryTitle else { return }
-        
-		self.title = entryTitle
+		self.title = itemsModel.name
 		navigationController?.setNavigationBarHidden(false, animated: false)
 	}
 }
