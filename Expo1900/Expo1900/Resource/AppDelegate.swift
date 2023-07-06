7fcf0be074be84c2c6e27f36e1f6c2c816008c0f
Kobe <mskang@xingxingmobility.com>
2023-07-06 17:42:55 +0900

    :sparkles: Feat: application(_:supportedInterfaceOrientationsFor:) 메서드 생성, restrictRotation 프로퍼티 생성.
    AppDelegate 내부에 첫 화면을 세로 모드로만 유지하기 위하여 application(_:supportedInterfaceOrientationsFor:) 메서드를 생성하고 restrictRotation 프로퍼티를 생성하였습니다.

--- Expo1900/Expo1900/Resource/AppDelegate.swift
+++ Expo1900/Expo1900/Resource/AppDelegate.swift
@@ -8,8 +8,7 @@ import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
+	var restrictRotation: UIInterfaceOrientationMask = .portrait
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
@@ -29,7 +28,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-
-
+	
+	func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
+		return self.restrictRotation
+	}
 }
 
