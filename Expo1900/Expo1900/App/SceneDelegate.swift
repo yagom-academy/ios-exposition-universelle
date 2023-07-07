71cbafdb76f2323eb4bfa5cd1464309108397654
Serena <sha0720@naver.com>
2023-07-07 10:17:37 +0900

    style: 컨벤션 수정

--- Expo1900/Expo1900/App/SceneDelegate.swift
+++ Expo1900/Expo1900/App/SceneDelegate.swift
@@ -13,8 +13,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         let navigationController = CommonNavigationController()
-        appCoordinator = AppCoordinator(navigationController)
         
+        appCoordinator = AppCoordinator(navigationController)
         setNavigationAppearance()
         window = UIWindow(windowScene: windowScene)
         window?.rootViewController = navigationController
