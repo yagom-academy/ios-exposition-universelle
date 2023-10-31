cb64667c18f8879e72231b8df51fc705e7914ee0
ue-mu <ym14170@gmail.com>
2023-10-31 10:29:25 +0900

    refactor: Item구조체명 변경 및 private 키워드 추가

--- Expo1900/Expo1900/Model/EntryItem.swift
+++ Expo1900/Expo1900/Model/EntryItem.swift
@@ -5,13 +5,13 @@
 //  Created by hyunMac on 10/30/23.
 //
 
-struct Items: Decodable {
+struct EntryItem: Decodable {
     let name: String
     let imageName: String
     let shortDesc: String
     let desc: String
     
-    enum CodingKeys: String, CodingKey {
+    private enum CodingKeys: String, CodingKey {
         case name
         case imageName = "image_name"
         case shortDesc = "short_desc"
