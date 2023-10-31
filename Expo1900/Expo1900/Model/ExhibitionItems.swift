db74ae96b056aab62dcb64c3cf9803651e3b62cb
kiseok <carti1108@naver.com>
2023-10-31 10:41:07 +0900

    docs: ExhibitionItems 모델 파일 추가, 파일 위치 변경

---
+++ Expo1900/Expo1900/Model/ExhibitionItems.swift
@@ -0,0 +1,20 @@
+//
+//  ExhibitionItems.swift
+//  Expo1900
+//
+//  Created by Kiseok, Toy on 2023/10/31.
+//
+
+
+struct ExhibitionItems: Codable {
+    let name: String
+    let imageName: String
+    let shortDesc: String
+    let desc: String
+    
+    enum CodingKeys: String, CodingKey {
+        case name, desc
+        case imageName = "image_name"
+        case shortDesc = "short_desc"
+    }
+}
