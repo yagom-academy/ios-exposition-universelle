ce2d655e48775bf895347834caf2af526475db47
woohyeon3382 <woohyeon3382@naver.com>
2023-10-31 10:34:51 +0900

    docs: ExpoInfo 모델타입 추가

---
+++ Expo1900/Expo1900/ExpoInfo.swift
@@ -0,0 +1,14 @@
+//
+//  ExpoInfo.swift
+//  Expo1900
+//
+//  Created by kiseok, Toy on 10/31/23.
+//
+
+struct ExpoInfo: Codable {
+    let title: String
+    let visitors: Int
+    let location: String
+    let duration: String
+    let description: String
+}
