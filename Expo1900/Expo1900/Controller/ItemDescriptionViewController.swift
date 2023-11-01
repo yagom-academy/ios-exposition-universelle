711aef10d0f9151868772a391909bf9739732bac
woohyeon3382 <woohyeon3382@naver.com>
2023-11-01 18:08:27 +0900

    feat: 세부사항 화면을 관리하는 ViewController생성 및 파일 생성

---
+++ Expo1900/Expo1900/Controller/ItemDescriptionViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ItemDescriptionViewController.swift
+//  Expo1900
+//
+//  Created by 김우현 on 11/1/23.
+//
+
+import UIKit
+
+class ItemDescriptionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
