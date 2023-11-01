75b14dfd951437c3af1ebaf764f59a38f41e508b
kiseok <carti1108@naver.com>
2023-11-01 16:51:18 +0900

    refactor: 불필요한 주석 제거

--- Expo1900/Expo1900/Controller/ExpoItemListViewController.swift
+++ Expo1900/Expo1900/Controller/ExpoItemListViewController.swift
@@ -12,22 +12,11 @@ class ExpoItemListViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         navigationController?.isNavigationBarHidden = false
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
