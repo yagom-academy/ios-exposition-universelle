5dbba1750b32d94dafa877c052a43780a7487a27
kiseok <carti1108@naver.com>
2023-11-01 16:14:47 +0900

    docs: ExpoItemViewController 파일 생성 및 스토리보드에 ExpoItemViewController 추가

---
+++ Expo1900/Expo1900/Controller/ExpoItemListViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ExpoItemListViewController.swift
+//  Expo1900
+//
+//  Created by Kiseok on 2023/11/01.
+//
+
+import UIKit
+
+class ExpoItemListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
