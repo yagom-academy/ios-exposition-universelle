986544f2e561042eba245db3b21d1e0c51b28048
Morgan <devjoon@naver.com>
2023-11-01 19:50:38 +0900

    feat: json파일을 디코딩하는 코드 구현

--- Expo1900/Expo1900/Controller/ViewController.swift
+++ Expo1900/Expo1900/Controller/ViewController.swift
@@ -8,11 +8,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var expoTitle: UILabel!
+    @IBOutlet weak var expoItem: UIImageView!
+    @IBOutlet weak var expoVisitors: UILabel!
+    @IBOutlet weak var expoLocation: UILabel!
+    @IBOutlet weak var expoDuration: UILabel!
+    @IBOutlet weak var expoDescription: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        guard let asset = NSDataAsset.init(name: "exposition_universelle_1900") else {
+            return
+        }
+        
+        let decoder = JSONDecoder()
+        
+        var product: Exposition.ExpositionInfo?
+        
+        do {
+            product = try decoder.decode(Exposition.ExpositionInfo.self, from: asset.data)
+        } catch {
+            print(error.localizedDescription)
+        }
+        
+        print(product!.title)
     }
-
-
 }
 
