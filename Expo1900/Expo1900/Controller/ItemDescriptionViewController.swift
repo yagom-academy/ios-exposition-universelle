33883ec532b8030bcde6822bf74c9f177ac636d2
woohyeon3382 <woohyeon3382@naver.com>
2023-11-01 18:11:28 +0900

    feat: UI요소를 코드와 연결

--- Expo1900/Expo1900/Controller/ItemDescriptionViewController.swift
+++ Expo1900/Expo1900/Controller/ItemDescriptionViewController.swift
@@ -9,21 +9,16 @@ import UIKit
 
 class ItemDescriptionViewController: UIViewController {
 
+    @IBOutlet weak var itemImageView: UIImageView!
+    @IBOutlet weak var itemDescription: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+    
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    
 
 }
