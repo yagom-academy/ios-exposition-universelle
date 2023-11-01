e5cbb78b222f14f3bb1cb469bbfbc7afce94982b
kiseok <carti1108@naver.com>
2023-11-01 16:52:30 +0900

    ExpoItemListViewController에 tableView IBOulet 연결

--- Expo1900/Expo1900/Controller/ExpoItemListViewController.swift
+++ Expo1900/Expo1900/Controller/ExpoItemListViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class ExpoItemListViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
