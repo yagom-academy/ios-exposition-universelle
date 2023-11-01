749d1b4046cc0a99ecebf466a86034226792ead2
Hisop <hisop@Hisopui-MacBookPro.local>
2023-11-02 00:08:18 +0900

    refactor: CulturalAsset 테스트 메서드 수정

--- Expo1900/DecodingTests/DecodingTests.swift
+++ Expo1900/DecodingTests/DecodingTests.swift
@@ -10,7 +10,7 @@ import XCTest
 
 final class DecodingTests: XCTestCase {
     var decoder = JSONDecoder()
-
+    
     func test_dataAsset이_exposition_universelle_1900_일_때_Decoding이_정상적으로_이루어지는지() {
         let title: String = "파리 만국박람회"
         let visitors: Int = 48130300
@@ -34,25 +34,50 @@ final class DecodingTests: XCTestCase {
             duration: duration,
             explanation: explanation
         )
-
+        
         XCTAssertEqual(exposition, try! decoder.decode(Exposition.self, from: json))
     }
     
     func test_dataAsset이_items_일_때_Decoding이_정상적으로_이루어지는지() {
-        guard let dataAsset = NSDataAsset(name: "items") else {
-            return
-        }
-
-        XCTAssertNoThrow(try decoder.decode([CulturalAsset].self, from: dataAsset.data))
+        let name: String = "직지심체요절"
+        let imageName: String = "jikji"
+        let shortDescription: String = "백운화상 경한(景閑)이 1372년에 초록한 불교 서적"
+        let detailDescription: String = "《백운화상초록불조직지심체요절》(白雲和尙抄錄佛祖直指心體要節)"
+        let json: Data = """
+[{
+    "name":"\(name)",
+    "image_name":"\(imageName)",
+    "short_desc":"\(shortDescription)",
+    "desc":"\(detailDescription)"
+}]
+""".data(using: .utf8)!
+        
+        let culturalAsset = [CulturalAsset(
+            name: name,
+            imageName: imageName,
+            shortDescription: shortDescription,
+            detailDescription: detailDescription
+        )]
+        
+        XCTAssertEqual(culturalAsset, try! decoder.decode([CulturalAsset].self, from: json))
     }
 }
 
 extension Exposition: Equatable {
     public static func == (lhs: Exposition, rhs: Exposition) -> Bool {
-        return lhs.title == rhs.title && 
+        return lhs.title == rhs.title &&
         lhs.visitors == rhs.visitors &&
         lhs.location == rhs.location &&
         lhs.duration == rhs.duration &&
         lhs.explanation == rhs.explanation
+    }
+}
+
+extension CulturalAsset: Equatable {
+    public static func == (lhs: CulturalAsset, rhs: CulturalAsset) -> Bool {
+        return lhs.name == rhs.name &&
+        lhs.imageName == rhs.imageName &&
+        lhs.shortDescription == rhs.shortDescription &&
+        lhs.detailDescription == rhs.detailDescription
     }
 }
