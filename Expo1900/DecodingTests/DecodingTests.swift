3904a1367f4e96fa65d06b0f2cd3f90e5fe64e5e
charles <kch1759@naver.com>
2023-11-01 23:45:46 +0900

    refactor: Exposition의 테스트 함수 수정

--- Expo1900/DecodingTests/DecodingTests.swift
+++ Expo1900/DecodingTests/DecodingTests.swift
@@ -12,11 +12,30 @@ final class DecodingTests: XCTestCase {
     var decoder = JSONDecoder()
 
     func test_dataAsset이_exposition_universelle_1900_일_때_Decoding이_정상적으로_이루어지는지() {
-        guard let dataAsset = NSDataAsset(name: "exposition_universelle_1900") else {
-            return
-        }
+        let title: String = "파리 만국박람회"
+        let visitors: Int = 48130300
+        let location: String = "프랑스 파리"
+        let duration: String = "1900. 04. 14"
+        let explanation: String = "1900년 파리 만국박람회(지금의 세계 박람회[World’s Fair/Exposition/Expo])"
+        let json: Data = """
+{
+    "title":"\(title)",
+    "visitors":\(visitors),
+    "location":"\(location)",
+    "duration":"\(duration)",
+    "description":"\(explanation)"
+}
+""".data(using: .utf8)!
+        
+        let exposition = Exposition(
+            title: title,
+            visitors: visitors,
+            location: location,
+            duration: duration,
+            explanation: explanation
+        )
 
-        XCTAssertNoThrow(try decoder.decode(Exposition.self, from: dataAsset.data))
+        XCTAssertEqual(exposition, try! decoder.decode(Exposition.self, from: json))
     }
     
     func test_dataAsset이_items_일_때_Decoding이_정상적으로_이루어지는지() {
@@ -24,6 +43,16 @@ final class DecodingTests: XCTestCase {
             return
         }
 
-        XCTAssertNoThrow(try decoder.decode([CulturalAssets].self, from: dataAsset.data))
+        XCTAssertNoThrow(try decoder.decode([CulturalAsset].self, from: dataAsset.data))
+    }
+}
+
+extension Exposition: Equatable {
+    public static func == (lhs: Exposition, rhs: Exposition) -> Bool {
+        return lhs.title == rhs.title && 
+        lhs.visitors == rhs.visitors &&
+        lhs.location == rhs.location &&
+        lhs.duration == rhs.duration &&
+        lhs.explanation == rhs.explanation
     }
 }
