bdf3ec896f5928758d8a087afb5f628c53e0410e
kiseok <carti1108@naver.com>
2023-11-01 10:28:49 +0900

    refactor: ExhibitionItems모델의 desc, shortDesc 네이밍 description, shortDescription으로 변경

--- Expo1900/Expo1900/Model/ExhibitionItems.swift
+++ Expo1900/Expo1900/Model/ExhibitionItems.swift
@@ -9,12 +9,12 @@
 struct ExhibitionItems: Decodable {
     let name: String
     let imageName: String
-    let shortDesc: String
-    let desc: String
+    let shortDescription: String
+    let description: String
     
     enum CodingKeys: String, CodingKey {
-        case name, desc
+        case name, description
         case imageName = "image_name"
-        case shortDesc = "short_desc"
+        case shortDescription = "short_desc"
     }
 }
