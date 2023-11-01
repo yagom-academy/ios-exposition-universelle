cf8a8892b9599636f6f1521eabdb4703b3ea7b61
woohyeon3382 <woohyeon3382@naver.com>
2023-11-01 10:25:09 +0900

    refactor: ExpoInfo타입의 네이밍 수정

--- Expo1900/Expo1900/Model/ExpoInformation.swift
+++ Expo1900/Expo1900/Model/ExpoInformation.swift
@@ -1,11 +1,11 @@
 //
-//  ExpoInfo.swift
+//  ExpoInformation.swift
 //  Expo1900
 //
 //  Created by kiseok, Toy on 10/31/23.
 //
 
-struct ExpoInfo: Decodable {
+struct ExpoInformation: Decodable {
     let title: String
     let visitors: Int
     let location: String
