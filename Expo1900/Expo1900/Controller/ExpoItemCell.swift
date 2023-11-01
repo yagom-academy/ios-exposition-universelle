63d5f494c6b59ee66a517a09bf1d19c50555bb76
kiseok <carti1108@naver.com>
2023-11-01 18:00:30 +0900

    docs: ExpoItemCell파일 추가

---
+++ Expo1900/Expo1900/Controller/ExpoItemCell.swift
@@ -0,0 +1,23 @@
+//
+//  ExpoItemCell.swift
+//  Expo1900
+//
+//  Created by Kiseok on 2023/11/01.
+//
+
+import UIKit
+
+class ExpoItemCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
