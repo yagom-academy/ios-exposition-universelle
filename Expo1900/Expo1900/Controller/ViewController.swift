5dd01f9473bdc096700925393f605721a75f44a0
woohyeon3382 <woohyeon3382@naver.com>
2023-11-01 14:37:33 +0900

    feat: 스토리보드 객체를 ViewController프로퍼티로 연결

--- Expo1900/Expo1900/Controller/ViewController.swift
+++ Expo1900/Expo1900/Controller/ViewController.swift
@@ -7,10 +7,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var mainTitleLabel: UILabel!
+    @IBOutlet weak var mainImageView: UIImageView!
+    @IBOutlet weak var visitorNumberLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var durationLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var leftFlagLabel: UIImageView!
+    @IBOutlet weak var rightFlagLabel: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
     }
 
 
