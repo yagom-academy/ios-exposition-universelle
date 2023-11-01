73891d8cf0ee98df05defe55b3fa02108bb3916e
Hisop <hisop@Hisopui-MacBookPro.local>
2023-11-01 16:26:05 +0900

    feat: Exposition에 대한 유닛테스트 추가

---
+++ Expo1900/DecodingTests/DecodingTests.swift
@@ -0,0 +1,21 @@
+//
+//  DecodingTests.swift
+//  DecodingTests
+//
+//  Created by Hisop on 2023/11/01.
+//
+
+import XCTest
+@testable import Expo1900
+
+final class DecodingTests: XCTestCase {
+    var decoder = JSONDecoder()
+
+    func test_dataAsset이_exposition_universelle_1900_일_때_Decoding이_정상적으로_이루어지는지() {
+        guard let dataAsset = NSDataAsset(name: "exposition_universelle_1900") else {
+            return
+        }
+
+        XCTAssertNoThrow(try decoder.decode(Exposition.self, from: dataAsset.data))
+    }
+}
