74323c4ee99fb33c4a9ce5949b807daaf95863cf
jyubong <nane030@naver.com>
2023-11-02 14:29:48 +0900

    feat: ExpositionViewcontroller storyboard UI 생성, outlet 연결
    storyboard로 image 삽입, UI요소들 stackView, scrollView로 연결
    label을 outlet으로 연결

---
+++ Expo1900/Expo1900/Controller/ExpositionViewController.swift
@@ -0,0 +1,22 @@
+//
+//  Expo1900 - ViewController.swift
+//  Created by jyubong, mireu
+//  Copyright © yagom academy. All rights reserved.
+// 
+
+import UIKit
+
+class ExpositionViewController: UIViewController {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var visitorsLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var durationLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
