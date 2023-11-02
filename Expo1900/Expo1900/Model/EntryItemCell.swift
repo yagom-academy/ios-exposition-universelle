6a22e1159a2b30f5c922567ae8c47447498482dd
Morgan <devjoon@naver.com>
2023-11-02 23:14:41 +0900

    feat: EntryItemCell 구현

---
+++ Expo1900/Expo1900/Model/EntryItemCell.swift
@@ -0,0 +1,23 @@
+//
+//  EntryItemCell.swift
+//  Expo1900
+//
+//  Created by hyunMac on 11/2/23.
+//
+
+import UIKit
+
+class EntryItemCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
