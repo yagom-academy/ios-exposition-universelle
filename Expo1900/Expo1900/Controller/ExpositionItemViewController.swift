757e9752ef33c77f367835fa280d23d4b9f3b3df
jyubong <nane030@naver.com>
2023-11-02 16:05:47 +0900

    feat: tableview cell 구현, contentConfiguration으로 cell UI 설정

--- Expo1900/Expo1900/Controller/ExpositionItemViewController.swift
+++ Expo1900/Expo1900/Controller/ExpositionItemViewController.swift
@@ -15,7 +15,9 @@ class ExpositionItemViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        tableView.dataSource = self
+        
         decodeData()
     }
     
@@ -39,6 +41,19 @@ extension ExpositionItemViewController: UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "itemCell", for: indexPath)
+        let item = expositionItems[indexPath.row]
+        
+        var content = cell.defaultContentConfiguration()
+        
+        content.text = item.name
+        content.secondaryText = item.shortDescription
+        content.image = UIImage(named: item.imageName)
+        
+        let imageSize = CGSize(width: 60, height: 60)
+        content.imageProperties.maximumSize = imageSize
+        content.imageProperties.reservedLayoutSize = imageSize
+        
+        cell.contentConfiguration = content
         
         return cell
     }
