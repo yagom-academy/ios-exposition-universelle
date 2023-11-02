1992b2265dc1423c83ed91ba3a8931bc8e70d055
kiseok <carti1108@naver.com>
2023-11-02 14:59:30 +0900

    refactor: ExpoItemCell의 불필요한 메서드 삭제

--- Expo1900/Expo1900/Controller/ExpoItemCell.swift
+++ Expo1900/Expo1900/Controller/ExpoItemCell.swift
@@ -12,15 +12,5 @@ final class ExpoItemCell: UITableViewCell {
     @IBOutlet weak var itemImageView: UIImageView!
     @IBOutlet weak var itemNameLabel: UILabel!
     @IBOutlet weak var itemDescriptionLabel: UILabel!
-    
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-    }
 
 }
