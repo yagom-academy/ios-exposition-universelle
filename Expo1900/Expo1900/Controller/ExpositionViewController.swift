f00a85858852878e984192285708e7106b1d4748
mireu79 <https://github.com/mireu79>
2023-11-02 14:39:52 +0900

    feat: JSON데이터를 디코딩하는 메서드 구현

--- Expo1900/Expo1900/Controller/ExpositionViewController.swift
+++ Expo1900/Expo1900/Controller/ExpositionViewController.swift
@@ -13,10 +13,24 @@ class ExpositionViewController: UIViewController {
     @IBOutlet weak var locationLabel: UILabel!
     @IBOutlet weak var durationLabel: UILabel!
     @IBOutlet weak var descriptionLabel: UILabel!
+    
+    var exposition: Exposition?
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        decodeData()
     }
 
+    func decodeData() {
+        let decoder = JSONDecoder()
+        
+        guard let dataAsset: NSDataAsset = NSDataAsset(name: "exposition_universelle_1900") else { return }
+        
+        do {
+            exposition = try decoder.decode(Exposition.self, from: dataAsset.data)
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
 }
