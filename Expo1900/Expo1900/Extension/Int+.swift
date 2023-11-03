d80dcd2a98eb22d713b40fdf865b3f269b0a58de
kiseok <carti1108@naver.com>
2023-11-03 11:12:29 +0900

    docs: Int extension파일 추가 및 convertToDecimalString메서드 구현

---
+++ Expo1900/Expo1900/Extension/Int+.swift
@@ -0,0 +1,14 @@
+//
+//  Int+.swift
+//  Expo1900
+//
+//  Created by Kiseok on 2023/11/03.
+//
+
+import Foundation
+
+extension Int {
+    func convertToDecimalString() -> String {
+        return String(self).convertToDecimal()
+    }
+}
