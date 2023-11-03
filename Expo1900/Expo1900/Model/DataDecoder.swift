fc776510c1a519659220cc1be43b89b5a3597d12
kiseok <carti1108@naver.com>
2023-11-03 15:33:16 +0900

    feat: DataDecoder구조체 추가 및 parse메서드 구현

--- Expo1900/Expo1900/Model/DataDecoder.swift
+++ Expo1900/Expo1900/Model/DataDecoder.swift
@@ -5,4 +5,15 @@
 //  Created by 김우현 on 11/3/23.
 //
 
-import Foundation
+import UIKit
+
+struct DataDecoder<Element: Decodable> {
+    
+    mutating func parse(assetName: String) throws -> Element {
+        let jsonDecoder = JSONDecoder()
+        guard let dataAsset = NSDataAsset(name: assetName) else { throw ParseError.parseError }
+        guard let decodedData = try? jsonDecoder.decode(Element.self, from: dataAsset.data) else { throw ParseError.parseError }
+        
+        return decodedData
+    }
+}
