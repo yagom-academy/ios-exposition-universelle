3ddfefedc2ad30ae0b36f0c2609a0ef99fdcf06e
kiseok <carti1108@naver.com>
2023-11-04 01:52:43 +0900

    feat: 코드 순서 변경 및 에러 변경

--- Expo1900/Expo1900/Model/DataDecoder.swift
+++ Expo1900/Expo1900/Model/DataDecoder.swift
@@ -10,8 +10,8 @@ import UIKit
 struct DataDecoder<Element: Decodable> {
     
     func parse(assetName: String) throws -> Element {
+        guard let dataAsset = NSDataAsset(name: assetName) else { throw ParseError.dataAssetError }
         let jsonDecoder = JSONDecoder()
-        guard let dataAsset = NSDataAsset(name: assetName) else { throw ParseError.parseFailure }
         guard let decodedData = try? jsonDecoder.decode(Element.self, from: dataAsset.data) else { throw ParseError.parseFailure }
         
         return decodedData
