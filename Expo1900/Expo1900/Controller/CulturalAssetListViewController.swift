998c8bddfc2fe853d9d76f958a2c39648ff0bd30
charles <kch1759@naver.com>
2023-11-05 17:12:50 +0900

    style: CulturalAssetListViewController의 cultualAsset의 공백 추가

--- Expo1900/Expo1900/Controller/CulturalAssetListViewController.swift
+++ Expo1900/Expo1900/Controller/CulturalAssetListViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 final class CulturalAssetListViewController: UITableViewController {
-    private var culturalAssets:[CulturalAsset] = []
+    private var culturalAssets: [CulturalAsset] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
