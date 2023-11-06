e5b5ec6a24b2b46b4126b39ad7616b3b96cbcc06
woohyeon3382 <woohyeon3382@naver.com>
2023-11-06 15:17:21 +0900

    refactor: showAppTerminatingAlert메서드 접근제어 키워드 추가

--- Expo1900/Expo1900/Controller/ExpoItemListViewController.swift
+++ Expo1900/Expo1900/Controller/ExpoItemListViewController.swift
@@ -26,10 +26,10 @@ final class ExpoItemListViewController: UIViewController {
         navigationController?.isNavigationBarHidden = false
     }
     
-    func showAppTerminatingAlert() {
+    private func showAppTerminatingAlert() {
         let title = "시스템 오류가 발생했습니다."
         let message = "Data를 불러오는데 실패했습니다. 앱이 종료됩니다."
-        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert) 
         let terminateAction = UIAlertAction(title: "지금 종료", style: .destructive) { _ in
             exit(1)
         }
