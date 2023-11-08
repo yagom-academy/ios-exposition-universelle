cadd74134b7bad508352e20126f2fc64761f88f7
woohyeon3382 <woohyeon3382@naver.com>
2023-11-08 14:51:15 +0900

    feat: HomeViewController 화면 전환 방지 기능 구현

--- Expo1900/Expo1900/Controller/AppDelegate.swift
+++ Expo1900/Expo1900/Controller/AppDelegate.swift
@@ -8,8 +8,15 @@ import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
+    var shouldSupportAllOrientaion = false
 
-
+    func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
+        if shouldSupportAllOrientaion {
+            return [.all]
+        } else {
+            return [.portrait]
+        }
+    }
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
