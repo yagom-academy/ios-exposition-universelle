0c7a1c2c735b26d898c5e299133d791d7610b3d6
KKOMGI <kkomgi90@gmail.com>
2024-03-04 21:05:43 +0900

    feat: 모델 타입 생성

---
+++ Expo1900/Entry.swift
@@ -0,0 +1,13 @@
+//
+//  Entry.swift
+//  Expo1900
+//
+//  Created by gama, hamzzi on 3/4/24.
+//
+
+struct Entry: Codable {
+    let name: String
+    let summary: String
+    let Description: String
+    let photoUrl: String
+}
