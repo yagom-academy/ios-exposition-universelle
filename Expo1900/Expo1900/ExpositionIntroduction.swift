b67349261a90d35431286f2c7abb466abbc3e672
danny <minzami1215@gmail.com>
2024-03-04 23:12:14 +0900

    feat: expo_assets 파일 추가, ExpositionIntroduction 모델 타입 추가

---
+++ Expo1900/Expo1900/ExpositionIntroduction.swift
@@ -0,0 +1,14 @@
+//
+//  ExpositionIntroduction.swift
+//  Expo1900
+//
+//  Created by Danny on 3/4/24.
+//
+
+struct ExpositionIntroduction {
+    var title: String
+    var visitors: Int
+    var location: String
+    var duration: String
+    var description: String
+}
