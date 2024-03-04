29a9af3769c3a3bd1b78aa3d96bd07e2e058b7cc
danny <minzami1215@gmail.com>
2024-03-04 23:26:08 +0900

    출품작 상세 데이터를 받아오기 위한 KoreanHeritage struct 타입 생성

---
+++ Expo1900/Expo1900/KoreanHeritage.swift
@@ -0,0 +1,12 @@
+//
+//  KoreanHeritage.swift
+//  Expo1900
+//
+//  Created by Danny, Gray on 3/4/24.
+//
+
+struct KoreanHeritage {
+    var name: String
+    var imageName: String
+    var description: String
+}
