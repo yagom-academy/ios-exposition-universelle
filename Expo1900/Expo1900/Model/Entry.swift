2971dfe23b602839d654857eaee2efabf099c209
PrismSpirit <yeolkeut@gmail.com>
2024-03-05 17:59:34 +0900

    feat: Entry Model 추가

---
+++ Expo1900/Expo1900/Model/Entry.swift
@@ -0,0 +1,18 @@
+//
+//  Entry.swift
+//  Expo1900
+//
+//  Created by Jaehun Lee on 3/5/24.
+//
+
+struct Entry: Decodable {
+    let imageName: String
+    let name: String
+    let shortDescription: String
+    
+    private enum CodingKeys: String, CodingKey {
+        case imageName = "image_name"
+        case name = "name"
+        case shortDescription = "short_desc"
+    }
+}
