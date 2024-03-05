896c0b5704e88b70ae54d3f9df87f918647b5154
Diana-yjh <projin1234@gmail.com>
2024-03-05 17:51:54 +0900

    feat: Exposition Model 추가

---
+++ Expo1900/Expo1900/Model/Exposition.swift
@@ -0,0 +1,14 @@
+//
+//  Exposition.swift
+//  Expo1900
+//
+//  Created by Yejin Hong on 3/5/24.
+//
+
+struct Exposition: Decodable {
+    let title: String
+    let visitors: Int
+    let location: String
+    let duration: String
+    let description: String
+}
