5cfcf2681800a6eecde66414289ffad7106fa414
yawoong2 <adamzoong@naver.com>
2024-03-06 23:18:16 +0900

    feat: ExpositionIntroduction타입에서 json 디코드를 위해 Codable 프로토콜 준수 및 CodingKeys 생성

--- Expo1900/Expo1900/Model/ExpositionIntroduction.swift
+++ Expo1900/Expo1900/Model/ExpositionIntroduction.swift
@@ -5,10 +5,18 @@
 //  Created by Danny, Gray on 3/4/24.
 //
 
-struct ExpositionIntroduction: Equatable {
+struct ExpositionIntroduction: Equatable, Codable {
     let title: String
     let visitors: Int
     let location: String
     let duration: String
     let description: String
+    
+    enum CodingKeys: String, CodingKey {
+        case title
+        case visitors
+        case location
+        case duration
+        case description
+    }
 }
