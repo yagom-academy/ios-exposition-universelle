a0d1c0502c8141b4f18d156863732a986a1d2d03
yawoong2 <adamzoong@naver.com>
2024-03-06 23:09:12 +0900

    feat: KoreanHeritage타입에서 json 디코드를 위해 Codable 프로토콜 준수 및 CodingKeys 생성

--- Expo1900/Expo1900/Model/KoreanHeritage.swift
+++ Expo1900/Expo1900/Model/KoreanHeritage.swift
@@ -5,8 +5,14 @@
 //  Created by Danny, Gray on 2024/03/04.
 //
 
-struct KoreanHeritage: Equatable {
+struct KoreanHeritage: Equatable, Codable {
     let name: String
     let imageName: String
     let shortDescription: String
+    
+    enum CodingKeys: String, CodingKey {
+        case name
+        case imageName = "image_name"
+        case shortDescription = "short_desc"
+    }
 }
