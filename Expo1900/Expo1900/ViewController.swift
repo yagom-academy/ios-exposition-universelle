b78c7e1f6112d5ab2ce33f66fad9151773585278
danny <minzami1215@gmail.com>
2024-03-07 15:43:44 +0900

    feat: json 정보를 디코드와하여 초기화면에 타이틀 출력

--- Expo1900/Expo1900/ViewController.swift
+++ Expo1900/Expo1900/ViewController.swift
@@ -7,12 +7,27 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var testLabel: UILabel!
+    var expositionIntroduction: ExpositionIntroduction!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let jsonDecoder: JSONDecoder = JSONDecoder()
+        guard let dataAsset: NSDataAsset = NSDataAsset(name: "exposition_universelle_1900") else {
+            return
+        }
+        
+        do {
+            self.expositionIntroduction = try jsonDecoder.decode(ExpositionIntroduction.self, from: dataAsset.data)
+        } catch {
+            print(error.localizedDescription)
+        }
+        
+        testLabel.text = expositionIntroduction.title
     }
-
 
 }
 
