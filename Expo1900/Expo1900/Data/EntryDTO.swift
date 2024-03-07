d7dc64df17d7ad134f8374c74403774a85ac2b6c
Diana-yjh <projin1234@gmail.com>
2024-03-07 17:12:21 +0900

    feat: decoding을 위한 EntryDTO 생성 및 Formatting 함수 구현

---
+++ Expo1900/Expo1900/Data/EntryDTO.swift
@@ -0,0 +1,34 @@
+//
+//  EntryDTO.swift
+//  Expo1900
+//
+//  Created by Yejin Hong on 3/7/24.
+//
+
+struct EntryDTO: Decodable {
+    let name: String
+    let imageName: String
+    let description: String
+    let shortDescription: String
+    
+    private enum CodingKeys: String, CodingKey {
+        case name
+        case imageName = "image_name"
+        case description = "desc"
+        case shortDescription = "short_desc"
+    }
+}
+
+extension EntryDTO {
+    func toModel() -> Entry {
+        return .init(name: name,
+                     imageName: imageName,
+                     shortDescription: shortDescription)
+    }
+    
+    func toModel() -> EntryDetail {
+        return .init(name: name,
+                     imageName: imageName,
+                     description: description)
+    }
+}
