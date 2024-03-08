158da015d6749b9380c24d2ee1ecad6ba3882489
yawoong2 <adamzoong@naver.com>
2024-03-08 16:41:40 +0900

    feat: 커스텀 테이블 뷰를 위한 KoreanHeritageTableViewCell 추가

---
+++ Expo1900/Expo1900/Model/KoreanHeritageTableViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  KoreanHeritageViewCell.swift
+//  Expo1900
+//
+//  Created by Danny, Gray on 2024/03/08.
+//
+
+import UIKit
+
+class KoreanHeritageTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var titleLabel : UILabel!
+    @IBOutlet weak var shortDescriptionTextView : UITextView!
+}
