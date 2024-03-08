81ec642bfffb7bc6518e26480cd074596d4b00a3
PrismSpirit <yeolkeut@gmail.com>
2024-03-08 11:12:01 +0900

    feat: storyboard와 ViewController 연결

--- Expo1900/Expo1900/Controller/ViewController.swift
+++ Expo1900/Expo1900/Controller/ViewController.swift
@@ -7,6 +7,15 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var posterImageView: UIImageView!
+    @IBOutlet var visitorsLabel: UILabel!
+    @IBOutlet var locationLabel: UILabel!
+    @IBOutlet var durationLabel: UILabel!
+    @IBOutlet var descriptionLabel: UILabel!
+    @IBOutlet var leftFlagImageView: UIImageView!
+    @IBOutlet var rightFlagImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
