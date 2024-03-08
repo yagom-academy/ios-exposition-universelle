f22336a78df66a0d125ba1177cb9e1752b56f724
PrismSpirit <yeolkeut@gmail.com>
2024-03-08 11:36:09 +0900

    feat: storyboard와 EntryDetailViewController 연결

---
+++ Expo1900/Expo1900/Controller/EntryDetailViewController.swift
@@ -0,0 +1,14 @@
+//
+//  EntryDetailViewController.swift
+//  Expo1900
+//
+//  Created by Jaehun Lee on 3/8/24.
+//
+
+import UIKit
+
+class EntryDetailViewController: UIViewController {
+    @IBOutlet var entryImageView: UIImageView!
+    @IBOutlet var entryDescriptionLabel: UILabel!
+    
+}
