4cdad55acd51e8e7d2b3e9d7a831c63b1ad74ca9
Diana-yjh <projin1234@gmail.com>
2024-03-08 11:28:01 +0900

    feat: storyboard와EntryTableViewCell 연결

---
+++ Expo1900/Expo1900/Controller/EntryTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  EntryTableViewCell.swift
+//  Expo1900
+//
+//  Created by Yejin Hong on 3/8/24.
+//
+
+import UIKit
+
+class EntryTableViewCell: UITableViewCell {
+    @IBOutlet var entryImageView: UIImageView!
+    @IBOutlet var entryNameLabel: UILabel!
+    @IBOutlet var entryShortDescriptionLabel: UILabel!
+    
+}
---
+++ Expo1900/Expo1900/Controller/EntryTableViewController.swift
@@ -0,0 +1,12 @@
+//
+//  EntryTableViewController.swift
+//  Expo1900
+//
+//  Created by Yejin Hong on 3/8/24.
+//
+
+import UIKit
+
+class EntryTableViewController: UITableViewController {
+    
+}
