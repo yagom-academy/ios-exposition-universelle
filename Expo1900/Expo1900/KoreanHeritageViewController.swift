6a53e53ba69fb744ff0fb8a2efa162833d72e210
yawoong2 <adamzoong@naver.com>
2024-03-08 16:38:52 +0900

    feat: 한국 출품작 목록 화면에 TableView 구현

--- Expo1900/Expo1900/KoreanHeritageViewController.swift
+++ Expo1900/Expo1900/KoreanHeritageViewController.swift
@@ -7,10 +7,41 @@
 
 import UIKit
 
-class KoreanHeritageViewController: UIViewController {
+class KoreanHeritageViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var koreanHeritageTableView: UITableView!
+    let cellIdentifier: String = "koreanHeritageCell"
+    var koreanHeritage: [KoreanHeritage]!
+    
+    // 섹션에 해당하는 row 개수
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return koreanHeritage.count
+    }
+    
+    // row마다 해당하는 cell 반환하기
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: KoreanHeritageTableViewCell = tableView.dequeueReusableCell(withIdentifier: self.cellIdentifier, for: indexPath) as! KoreanHeritageTableViewCell
+        
+//        cell.titleLabel?.text = koreanHeritage[indexPath.row]
+//        print(koreanHeritage)
+        return cell
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.koreanHeritageTableView.delegate = self
+        self.koreanHeritageTableView.dataSource = self
+        
+        let jsonDecoder: JSONDecoder = JSONDecoder()
+        guard let dataAsset: NSDataAsset = NSDataAsset(name: "items") else {
+            return
+        }
+        
+        do {
+            self.koreanHeritage = try jsonDecoder.decode([KoreanHeritage].self,  from: dataAsset.data)
+        } catch {
+            print(error.localizedDescription)
+        }
     }
     
 }
