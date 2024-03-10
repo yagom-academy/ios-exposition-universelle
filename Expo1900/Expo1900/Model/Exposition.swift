684d84362cf20e535fa864e847fe085f312bbd65
KKOMGI <kkomgi90@gmail.com>
2024-03-10 21:57:06 +0900

    add: Json 인코딩, 디코딩 을 위한 타입 생성

---
+++ Expo1900/Expo1900/Model/Exposition.swift
@@ -0,0 +1,14 @@
+//
+//  Exposition.swift
+//  Expo1900
+//
+//  Created by gama, hamzzi on 3/10/24.
+//
+
+struct Exposition: Codable {
+    let title: String
+    let visitors: String
+    let location: String
+    let duration: String
+    let description: String
+}
---
+++ Expo1900/Expo1900/Model/Items.swift
@@ -0,0 +1,20 @@
+//
+//  Entry.swift
+//  Expo1900
+//
+//  Created by gama, hamzzi on 3/4/24.
+//
+
+struct Items: Codable {
+    let name: String
+    let summary: String
+    let description: String
+    let imageUrl: String
+    
+    enum CodingKeys: String, CodingKey {
+        case name
+        case summary = "short_desc"
+        case description = "desc"
+        case imageUrl = "image_name"
+    }
+}
