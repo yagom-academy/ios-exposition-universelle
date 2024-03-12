efcd9aa2509a416f1773763dcfe080b072efd502
forseaest <u3z@naver.com>
2024-03-12 22:26:32 +0900

    fix: Exposition의 visitors 프로퍼티 데이터 타입 수정

--- Expo1900/Expo1900/Model/Exposition.swift
+++ Expo1900/Expo1900/Model/Exposition.swift
@@ -7,7 +7,7 @@
 
 struct Exposition: Codable {
     let title: String
-    let visitors: String
+    let visitors: UInt
     let location: String
     let duration: String
     let description: String
