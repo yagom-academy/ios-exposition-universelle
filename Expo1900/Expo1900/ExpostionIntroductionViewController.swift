4efdc391ee072544abe325628b4dd79bab90c1af
danny <minzami1215@gmail.com>
2024-03-13 22:23:21 +0900

    refactor, design: ViewController 네이밍 변경, 메인 화면 뷰의 방문객, 개최지, 개최기간 요구사항 디자인에 맞게 변경

--- Expo1900/Expo1900/ExpostionIntroductionViewController.swift
+++ Expo1900/Expo1900/ExpostionIntroductionViewController.swift
@@ -6,7 +6,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ExpostionIntroductionViewController: UIViewController {
     
     @IBOutlet weak var mainTitleLabel: UILabel!
     @IBOutlet weak var subTitleLabel: UILabel!
@@ -42,9 +42,24 @@ class ViewController: UIViewController {
         mainTitleLabel.text = String(expositionIntroduction.title.prefix(13))
         subTitleLabel.text = String(expositionIntroduction.title.suffix(28))
         posterImageView.image = UIImage(named: "poster.png")
-        visitorLabel.text = "방문객: \(visitorString) 명"
-        locationLabel.text = "개최지 : \(expositionIntroduction.location)"
-        durationLabel.text = "개최 기간 : \(expositionIntroduction.duration)"
+        
+        let visitorText = "방문객 : \(visitorString) 명"
+        let attributedvisitorString = NSMutableAttributedString(string: visitorText)
+        let visitorAttributes: [NSAttributedString.Key: Any] = [.font: UIFont.systemFont(ofSize: 20)]
+        attributedvisitorString.addAttributes(visitorAttributes, range: NSRange(location: 0, length: 3))
+        visitorLabel.attributedText = attributedvisitorString
+        
+        let locationText = "개최지 : \(expositionIntroduction.location)"
+        let locationAttributedString = NSMutableAttributedString(string: locationText)
+        let locationAttributes: [NSAttributedString.Key: Any] = [.font: UIFont.systemFont(ofSize: 20)]
+        locationAttributedString.addAttributes(locationAttributes, range: NSRange(location: 0, length: 3))
+        locationLabel.attributedText = locationAttributedString
+        
+        let durationText = "개최 기간 : \(expositionIntroduction.duration)"
+        let durationAttributedString = NSMutableAttributedString(string: durationText)
+        let durationAttributes: [NSAttributedString.Key: Any] = [.font: UIFont.systemFont(ofSize: 20)]
+        durationAttributedString.addAttributes(durationAttributes, range: NSRange(location: 0, length: 6))
+        durationLabel.attributedText = durationAttributedString
         descriptionLabel.numberOfLines = 0
         descriptionLabel.text = expositionIntroduction.description
     }
