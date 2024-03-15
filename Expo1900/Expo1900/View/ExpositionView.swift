a473fe59bed7053c9752a853559495507f3370c4
Diana-yjh <projin1234@gmail.com>
2024-03-15 17:34:15 +0900

    refactor: ExpostionView에서 사용하지 않는 Outlet 삭제

--- Expo1900/Expo1900/View/ExpositionView.swift
+++ Expo1900/Expo1900/View/ExpositionView.swift
@@ -13,18 +13,11 @@ protocol ExpositionViewDelegate: AnyObject {
 
 class ExpositionView: UIView {
     @IBOutlet var titleLabel: UILabel!
-    @IBOutlet var posterImageView: UIImageView!
     @IBOutlet var visitorsLabel: UILabel!
-    @IBOutlet var visitorsNumberLabel: UILabel!
     @IBOutlet var locationLabel: UILabel!
-    @IBOutlet var locationStringLabel: UILabel!
     @IBOutlet var durationLabel: UILabel!
-    @IBOutlet var durationStringLabel: UILabel!
     @IBOutlet var descriptionLabel: UILabel!
-    @IBOutlet var leftFlagImageView: UIImageView!
-    @IBOutlet var showKoreanEntryButton: UIButton!
-    @IBOutlet var rightFlagImageView: UIImageView!
-    
+
     weak var delegate: ExpositionViewDelegate?
     
     @IBAction func moveToKoreanEntryButtonTouchedUp(_ sender: UIButton) {
@@ -33,9 +26,9 @@ class ExpositionView: UIView {
     
     func updateComponents(with exposition: Exposition) {
         self.titleLabel.text = exposition.formattedTitle
-        self.visitorsNumberLabel.text = NumberFormatter().string(with: exposition.visitors)
-        self.locationStringLabel.text = exposition.location
-        self.durationStringLabel.text = exposition.duration
+        self.visitorsLabel.text = NumberFormatter().string(with: exposition.visitors)
+        self.locationLabel.text = exposition.location
+        self.durationLabel.text = exposition.duration
         self.descriptionLabel.text = exposition.description
     }
 }
